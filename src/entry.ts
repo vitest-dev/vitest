54a0406e6c770c513a4092784153a13e2d8d84f8
Anthony Fu <anthonyfu117@hotmail.com>
2021-12-06 06:26:40 +0800

    docs: error message

--- src/entry.ts
+++ src/entry.ts
@@ -1,7 +1,7 @@
 import { run } from './run'
 
 if (!process.__vite_node__ || !process.__vitest__)
-  throw new Error('Vite can only run in Vite environment, please use the CLI to start the process')
+  throw new Error('Vitest can only run in vite-node environment, please use the CLI to start the process')
 
 const inlineOptions = process.__vite_node__.server.config.test || {}
 const cliOptions = process.__vitest__.options || {}
