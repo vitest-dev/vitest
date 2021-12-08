3dd50f86f17e53a7b5fe81dc3b9df5b02cba7d98
aslemammad <zorofight94@gmail.com>
2021-12-09 03:05:05 +0330

    tsconfig sourceMap

--- bin/vitest.mjs
+++ bin/vitest.mjs
@@ -1,16 +1,18 @@
 #!/usr/bin/env node
 
-import { fileURLToPath } from 'url'
-import { resolve } from 'path'
+import { fileURLToPath } from "url";
+import { resolve } from "path";
+import { install } from "source-map-support";
 
-const argv = process.argv.slice(2)
-const filename = fileURLToPath(import.meta.url)
-const entry = resolve(filename, '../../dist/node/cli.js')
+install({ environment: "node", hookRequire: true });
 
-if (argv.includes('--coverage')) {
-  process.argv.splice(2, 0, process.argv[0], entry)
-  await import('c8/bin/c8.js')
-}
-else {
-  await import('../dist/node/cli.js')
+const argv = process.argv.slice(2);
+const filename = fileURLToPath(import.meta.url);
+const entry = resolve(filename, "../../dist/node/cli.js");
+
+if (argv.includes("--coverage")) {
+  process.argv.splice(2, 0, process.argv[0], entry);
+  await import("c8/bin/c8.js");
+} else {
+  await import("../dist/node/cli.js");
 }
