437d41ecb02a9ca8cd68fb3a7febb9371abfdfb0
Anthony Fu <anthonyfu117@hotmail.com>
2021-12-09 22:19:29 +0800

    chore: update

--- src/reporters/render.ts
+++ src/reporters/render.ts
@@ -6,118 +6,101 @@ import cliTruncate from 'cli-truncate'
 import stripAnsi from 'strip-ansi'
 import elegantSpinner from 'elegant-spinner'
 import logSymbols from 'log-symbols'
+import { Task } from '../types'
 
 const pointer = chalk.yellow(figures.pointer)
 const skipped = chalk.yellow(figures.arrowDown)
 
-interface Task {
-  title: string
-  isEnabled(): boolean
-  isSkipped(): boolean
-  isPending(): boolean
-  isFailed(): boolean
-  isCompleted(): boolean
-  hasFailed(): boolean
-  subtasks: Task[]
-  output: string
-  spinner: () => string
-}
+const spinnerMap = new WeakMap<Task, () => string>()
+const outputMap = new WeakMap<Task, string>()
 
-interface Options {
-  showSubtasks: boolean
-  collapse: boolean
-  clearOutput: boolean
-}
+const getSymbol = (task: Task) => {
+  if (task.mode === 'skip' || task.mode === 'todo')
+    return skipped
 
-const getSymbol = (task: Task, options: Options) => {
-  if (!task.spinner)
-    task.spinner = elegantSpinner()
+  // pending
+  if (!task.result || task.result.state === 'run') {
+    if (task.type === 'suite')
+      return pointer
+    let spinner = spinnerMap.get(task)
+    if (!spinner) {
+      spinner = elegantSpinner()
+      spinnerMap.set(task, spinner)
+    }
+    return chalk.yellow(spinner())
+  }
 
-  if (task.isPending())
-    return options.showSubtasks !== false && task.subtasks.length > 0 ? pointer : chalk.yellow(task.spinner())
-
-  if (task.isCompleted())
+  if (task.result.state === 'pass')
     return logSymbols.success
 
-  if (task.hasFailed())
-    return task.subtasks.length > 0 ? pointer : logSymbols.error
-
-  if (task.isSkipped())
-    return skipped
+  if (task.result.state === 'fail') {
+    return task.type === 'suite'
+      ? pointer
+      : logSymbols.error
+  }
 
   return ' '
 }
 
-const renderHelper = (tasks: Task[], options: Options, level = 0) => {
+const renderHelper = (tasks: Task[], level = 0) => {
   let output: string[] = []
 
   for (const task of tasks) {
-    if (task.isEnabled()) {
-      const skipped = task.isSkipped() ? ` ${chalk.dim('[skipped]')}` : ''
+    const skipped = task.mode === 'skip' || task.mode === 'todo' ? ` ${chalk.dim('[skipped]')}` : ''
 
-      output.push(indentString(` ${getSymbol(task, options)} ${task.title}${skipped}`, level, { indent: '  ' }))
+    output.push(indentString(` ${getSymbol(task)} ${task.name}${skipped}`, level, { indent: '  ' }))
 
-      if ((task.isPending() || task.isSkipped() || task.hasFailed()) && task.output != null) {
-        let data: string | undefined = task.output
+    if ((task.result?.state !== 'pass') && outputMap.get(task) != null) {
+      let data: string | undefined = outputMap.get(task)
 
-        if (typeof data === 'string') {
-          data = stripAnsi(data.trim().split('\n').filter(Boolean).pop()!)
-
-          if (data === '')
-            data = undefined
-        }
-
-        if (data != null) {
-          const out = indentString(`${figures.arrowRight} ${data}`, level, { indent: '  ' })
-          output.push(`   ${chalk.gray(cliTruncate(out, process.stdout.columns - 3))}`)
-        }
+      if (typeof data === 'string') {
+        data = stripAnsi(data.trim().split('\n').filter(Boolean).pop()!)
+        if (data === '')
+          data = undefined
       }
 
-      if ((task.isPending() || task.hasFailed() || options.collapse === false) && (task.hasFailed() || options.showSubtasks !== false) && task.subtasks.length > 0)
-        output = output.concat(renderHelper(task.subtasks, options, level + 1))
+      if (data != null) {
+        const out = indentString(`${figures.arrowRight} ${data}`, level, { indent: '  ' })
+        output.push(`   ${chalk.gray(cliTruncate(out, process.stdout.columns - 3))}`)
+      }
     }
+
+    if ((task.result?.state === 'fail' || !task.result || task.result.state === 'run') && task.type === 'suite' && task.tasks.length > 0)
+      output = output.concat(renderHelper(task.tasks, level + 1))
   }
 
   return output.join('\n')
 }
 
-const render = (tasks: Task[], options: Options) => {
-  logUpdate(renderHelper(tasks, options))
-}
+export const createRenderer = (_tasks: Task[]) => {
+  let tasks = _tasks
+  let timer: any
 
-export class UpdateRenderer {
-  id: any
-
-  constructor(public tasks: Task[], public options: Options) {
-    Object.assign(options, {
-      showSubtasks: true,
-      collapse: true,
-      clearOutput: false,
-    }, options)
+  function run() {
+    const content = renderHelper(tasks)
+    logUpdate(content)
   }
 
-  render() {
-    if (this.id) {
-      // Do not render if we are already rendering
-      return
-    }
-
-    this.id = setInterval(() => {
-      render(this.tasks, this.options)
-    }, 100)
-  }
-
-  end(err: unknown) {
-    if (this.id) {
-      clearInterval(this.id)
-      this.id = undefined
-    }
-
-    render(this.tasks, this.options)
-
-    if (this.options.clearOutput && err === undefined)
-      logUpdate.clear()
-    else
+  return {
+    start() {
+      if (timer)
+        return this
+      timer = setInterval(run, 200)
+      return this
+    },
+    update(_tasks: Task[]) {
+      tasks = _tasks
+      run()
+      return this
+    },
+    stop() {
+      if (timer) {
+        clearInterval(timer)
+        timer = undefined
+      }
+      run()
       logUpdate.done()
+      return this
+    },
   }
 }
