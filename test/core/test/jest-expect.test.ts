23c764ca25bed7017d5e3609868b711cdfba7d6e
aslemammad <zorofight94@gmail.com>
2021-12-10 15:56:11 +0330

    lint

--- test/core/test/jest-expect.test.ts
+++ test/core/test/jest-expect.test.ts
@@ -1,35 +1,35 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect } from 'vitest'
 
-describe("jest-expect", () => {
-  it("basic", () => {
-    expect(1).toBe(1);
-    expect(null).toBeNull();
-    expect(null).toBeDefined();
-    expect(null).not.toBeUndefined();
-    expect([]).toBeTruthy();
-    expect(0).toBeFalsy();
-    expect("Hello").toMatch(/llo/);
-    expect("Hello").toMatch("llo");
-    expect("Hello").toContain("llo");
-    expect(["Hello"]).toContain("Hello");
-    expect([{ text: "Hello" }]).toContainEqual({ text: "Hello" });
-    expect([{ text: "Bye" }]).not.toContainEqual({ text: "Hello" });
-  });
+describe('jest-expect', () => {
+  it('basic', () => {
+    expect(1).toBe(1)
+    expect(null).toBeNull()
+    expect(null).toBeDefined()
+    expect(null).not.toBeUndefined()
+    expect([]).toBeTruthy()
+    expect(0).toBeFalsy()
+    expect('Hello').toMatch(/llo/)
+    expect('Hello').toMatch('llo')
+    expect('Hello').toContain('llo')
+    expect(['Hello']).toContain('Hello')
+    expect([{ text: 'Hello' }]).toContainEqual({ text: 'Hello' })
+    expect([{ text: 'Bye' }]).not.toContainEqual({ text: 'Hello' })
+  })
 
-  it("object", () => {
-    expect({}).toEqual({});
-    expect({}).not.toBe({});
-    expect({}).not.toStrictEqual({});
+  it('object', () => {
+    expect({}).toEqual({})
+    expect({}).not.toBe({})
+    expect({}).not.toStrictEqual({})
 
-    const foo = {};
-    const complex = { foo: 1, bar: { foo: "foo", bar: 100 } };
+    const foo = {}
+    const complex = { foo: 1, bar: { foo: 'foo', bar: 100 } }
 
-    expect(foo).toBe(foo);
-    expect(foo).toStrictEqual(foo);
-    expect(complex).toMatchObject({});
-    expect(complex).toMatchObject({ foo: 1 });
-    expect([complex]).toMatchObject([{ foo: 1 }]);
-    expect(complex).not.toMatchObject({ foo: 2 });
-    expect(complex).toMatchObject({ bar: { bar: 100 } });
-  });
-});
+    expect(foo).toBe(foo)
+    expect(foo).toStrictEqual(foo)
+    expect(complex).toMatchObject({})
+    expect(complex).toMatchObject({ foo: 1 })
+    expect([complex]).toMatchObject([{ foo: 1 }])
+    expect(complex).not.toMatchObject({ foo: 2 })
+    expect(complex).toMatchObject({ bar: { bar: 100 } })
+  })
+})
