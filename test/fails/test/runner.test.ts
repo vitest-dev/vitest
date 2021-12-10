cc63cce2d25292a0e7888ff2f5eab765dd703237
Anthony Fu <anthonyfu117@hotmail.com>
2021-12-11 01:30:33 +0800

    chore: use `exec` directly

--- test/fails/test/runner.test.ts
+++ test/fails/test/runner.test.ts
@@ -1,27 +1,29 @@
 import { resolve } from 'path'
+import { exec } from 'child_process'
 import fg from 'fast-glob'
-import { execa } from 'execa'
 import which from 'which'
 import { describe, it, expect } from 'vitest'
 
 describe('should fails', async() => {
   const root = resolve(__dirname, '../fixtures')
   const cli = resolve(__dirname, '../../../bin/vitest.mjs')
-  const node = await which('node')
   const files = await fg('*.test.ts', { cwd: root })
 
   for (const file of files) {
     it(file, async() => {
       let error: any
-      try {
-        await execa(node, [cli, file], {
+      await new Promise<void>((resolve) => {
+        exec(`node '${cli}' ${file}`, {
+          env: {
+            ...process.env,
+            NO_COLOR: 'true',
+          },
           cwd: root,
-          env: { NO_COLOR: 'true' },
+        }, (e) => {
+          error = e
+          resolve()
         })
-      }
-      catch (e) {
-        error = e
-      }
+      })
 
       expect(error).toBeTruthy()
       const msg = String(error)
