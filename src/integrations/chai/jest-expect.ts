7e7944827799c3016bf93b814af16963443d4377
aslemammad <zorofight94@gmail.com>
2021-12-10 16:21:51 +0330

    re-format

--- src/integrations/chai/jest-expect.ts
+++ src/integrations/chai/jest-expect.ts
@@ -5,54 +5,29 @@ import { ChaiPlugin } from './types'
 export function JestChaiExpect(): ChaiPlugin {
   return (chai, utils) => {
     const proto = chai.Assertion.prototype
-    utils.addMethod(
-      proto,
-      'toEqual',
-      function(this: Chai.Assertion, expected: any) {
-        return this.eql(expected)
-      },
-    )
-    utils.addMethod(
-      proto,
-      'toStrictEqual',
-      function(this: Chai.Assertion, expected: any) {
-        return this.equal(expected)
-      },
-    )
-    utils.addMethod(
-      proto,
-      'toBe',
-      function(this: Chai.Assertion, expected: any) {
-        return this.equal(expected)
-      },
-    )
-    utils.addMethod(
-      proto,
-      'toMatchObject',
-      function(this: Chai.Assertion, expected: any) {
-        return this.containSubset(expected)
-      },
-    )
-    utils.addMethod(
-      proto,
-      'toMatch',
-      function(this: Chai.Assertion, expected: string | RegExp) {
-        if (typeof expected === 'string') return this.include(expected)
-        else return this.match(expected)
-      },
-    )
-    utils.addMethod(
-      proto,
-      'toContain',
-      function(this: Chai.Assertion, item: any) {
-        return this.contain(item)
-      },
-    )
+    utils.addMethod(proto, 'toEqual', function(this: Chai.Assertion, expected: any) {
+      return this.eql(expected)
+    })
+    utils.addMethod(proto, 'toStrictEqual', function(this: Chai.Assertion, expected: any) {
+      return this.equal(expected)
+    })
+    utils.addMethod(proto, 'toBe', function(this: Chai.Assertion, expected: any) {
+      return this.equal(expected)
+    })
+    utils.addMethod(proto, 'toMatchObject', function(this: Chai.Assertion, expected: any) {
+      return this.containSubset(expected)
+    })
+    utils.addMethod(proto, 'toMatch', function(this: Chai.Assertion, expected: string | RegExp) {
+      if (typeof expected === 'string')
+        return this.include(expected)
+      else
+        return this.match(expected)
+    })
+    utils.addMethod(proto, 'toContain', function(this: Chai.Assertion, item: any) {
+      return this.contain(item)
+    })
 
-    utils.addMethod(
-      proto,
-      'toContainEqual',
-      function(this: Chai.AssertionStatic, expected: any) {
+    utils.addMethod(proto, 'toContainEqual', function(this: Chai.AssertionStatic, expected: any) {
         const obj = utils.flag(this, 'object')
         const index = Array.from(obj).findIndex((item) => {
           try {
@@ -102,38 +77,22 @@ export function JestChaiExpect(): ChaiPlugin {
     utils.addMethod(proto, 'toBeDefined', function(this: Chai.Assertion) {
       return this.not.be.undefined
     })
-    utils.addMethod(
-      proto,
-      'toBeInstanceOf',
-      function(this: Chai.Assertion, obj: any) {
-        return this.instanceOf(obj)
-      },
-    )
+    utils.addMethod(proto, 'toBeInstanceOf', function(this: Chai.Assertion, obj: any) {
+      return this.instanceOf(obj)
+    })
 
     // mock
-    utils.addMethod(
-      proto,
-      'toHaveBeenCalledTimes',
-      function(this: Chai.Assertion, number: number) {
-        return this.callCount(number)
-      },
-    )
-    utils.addMethod(
-      proto,
-      'toHaveBeenCalledOnce',
-      function(this: Chai.Assertion) {
-        return this.callCount(1)
-      },
-    )
+    utils.addMethod(proto, 'toHaveBeenCalledTimes', function(this: Chai.Assertion, number: number) {
+      return this.callCount(number)
+    })
+    utils.addMethod(proto, 'toHaveBeenCalledOnce', function(this: Chai.Assertion) {
+      return this.callCount(1)
+    })
     utils.addMethod(proto, 'toHaveBeenCalled', function(this: Chai.Assertion) {
       return this.called
     })
-    utils.addMethod(
-      proto,
-      'toHaveBeenCalledWith',
-      function(this: Chai.Assertion, ...args: any[]) {
-        return this.calledWith(...args)
-      },
-    )
+    utils.addMethod(proto, 'toHaveBeenCalledWith', function(this: Chai.Assertion, ...args: any[]) {
+      return this.calledWith(...args)
+    })
   }
 }
