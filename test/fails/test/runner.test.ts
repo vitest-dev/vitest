2625dfa22a2dda2ce5378b6dc3568b45bdb72b1f
Anthony Fu <anthonyfu117@hotmail.com>
2021-12-11 00:57:46 +0800

    chore: update

--- test/fails/test/runner.test.ts
+++ test/fails/test/runner.test.ts
@@ -1,17 +1,23 @@
 import { resolve } from 'path'
 import fg from 'fast-glob'
 import { execa } from 'execa'
+import which from 'which'
 import { describe, it, expect } from 'vitest'
 
 describe('should fails', async() => {
   const root = resolve(__dirname, '../fixtures')
+  const cli = resolve(__dirname, '../../../bin/vitest.mjs')
+  const node = await which('node')
   const files = await fg('*.test.ts', { cwd: root })
 
   for (const file of files) {
     it(file, async() => {
       let error: any
       try {
-        await execa('npx', ['vitest', file], { cwd: root, env: { NO_COLOR: 'true' } })
+        await execa(node, [cli, file], {
+          cwd: root,
+          env: { NO_COLOR: 'true' },
+        })
       }
       catch (e) {
         error = e
