f1fcb536ebee690320274d8046c5450a30abfde6
Vladimir Sheremet <sheremet.va@icloud.com>
2021-12-11 10:56:18 +0300

    refactor: renaming expect -> assertion inside jest.extend

--- src/integrations/chai/jest-extend.ts
+++ src/integrations/chai/jest-extend.ts
@@ -38,12 +38,12 @@ const getMatcherState = (assertion: Chai.AssertionStatic & Chai.Assertion) => {
 
 function JestExtendPlugin(expects: MatchersObject): ChaiPlugin {
   return (c, utils) => {
-    Object.entries(expects).forEach(([expectName, expectFn]) => {
+    Object.entries(expects).forEach(([expectAssertionName, expectAssertion]) => {
       function expectSyncWrapper(this: Chai.AssertionStatic & Chai.Assertion, ...args: any[]) {
         const { state, isNot, actual } = getMatcherState(this)
 
         // @ts-expect-error
-        const { pass, message } = expectFn.call(state, actual, ...args) as SyncExpectationResult
+        const { pass, message } = expectAssertion.call(state, actual, ...args) as SyncExpectationResult
 
         if ((pass && isNot) || (!pass && !isNot))
           c.expect.fail(message())
@@ -53,15 +53,15 @@ function JestExtendPlugin(expects: MatchersObject): ChaiPlugin {
         const { state, isNot, actual } = getMatcherState(this)
 
         // @ts-expect-error
-        const { pass, message } = await expectFn.call(state, actual, ...args) as SyncExpectationResult
+        const { pass, message } = await expectAssertion.call(state, actual, ...args) as SyncExpectationResult
 
         if ((pass && isNot) || (!pass && !isNot))
           c.expect.fail(message())
       }
 
-      const expectWrapper = isAsyncFunction(expectFn) ? expectAsyncWrapper : expectSyncWrapper
+      const expectAssertionWrapper = isAsyncFunction(expectAssertion) ? expectAsyncWrapper : expectSyncWrapper
 
-      utils.addMethod(chai.Assertion.prototype, expectName, expectWrapper)
+      utils.addMethod(chai.Assertion.prototype, expectAssertionName, expectAssertionWrapper)
     })
   }
 }
