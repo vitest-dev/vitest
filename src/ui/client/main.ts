0fd6a099498979c522137ba763ccd604cf95ec68
wheatjs <jacobrclevenger@gmail.com>
2021-12-11 14:59:33 -0500

    fix(ui): fix crash when opening ui

--- src/ui/client/main.ts
+++ src/ui/client/main.ts
@@ -1,6 +1,5 @@
-// import { createApp } from 'vue'
-// import App from './App.vue'
+import { createApp } from 'vue'
+import App from './App.vue'
 
-console.log('Testing')
-// createApp(App)
-//   .mount('#app')
+createApp(App)
+  .mount('#app')
