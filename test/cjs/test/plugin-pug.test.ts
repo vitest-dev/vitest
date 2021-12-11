a5f0a1965cedc1ab01598965e5c9b104d2b7ecba
Shinigami92 <chrissi92@hotmail.de>
2021-12-11 21:00:45 +0100

    test: add a second test-case

--- test/cjs/test/plugin-pug.test.ts
+++ test/cjs/test/plugin-pug.test.ts
@@ -7,11 +7,11 @@ import { plugin } from '@prettier/plugin-pug'
 describe('plugin-pug', () => {
   test('should handle class attributes', () => {
     const expected: string = readFileSync(
-      resolve(__dirname, 'formatted.pug'),
+      resolve(__dirname, 'class-attributes-formatted.pug'),
       'utf8',
     )
     const code: string = readFileSync(
-      resolve(__dirname, 'unformatted.pug'),
+      resolve(__dirname, 'class-attributes-unformatted.pug'),
       'utf8',
     )
     const actual: string = format(code, {
@@ -19,6 +19,20 @@ describe('plugin-pug', () => {
       plugins: [plugin],
       semi: false,
     })
+
+    expect(actual).toBe(expected)
+  })
+
+  test('should handle slash token', () => {
+    const expected: string = readFileSync(
+      resolve(__dirname, 'slash-formatted.pug'),
+      'utf8',
+    )
+    const code: string = readFileSync(
+      resolve(__dirname, 'slash-unformatted.pug'),
+      'utf8',
+    )
+    const actual: string = format(code, { parser: 'pug', plugins: [plugin] })
 
     expect(actual).toBe(expected)
   })
