062c13763c8cfdf7cfca8b1fb3da9c4ac6b8c546
Alex Lohr <alex.lohr@logmein.com>
2021-12-14 11:14:46 +0100

    tests: without cjs imports

--- test/solid/vitest.config.ts
+++ test/solid/vitest.config.ts
@@ -6,10 +6,12 @@ import solidPlugin from 'vite-plugin-solid'
 export default defineConfig({
   test: {
     environment: 'jsdom',
+    include: ['test/Hello.test.tsx'],
   },
   plugins: [solidPlugin()],
   build: {
     target: 'esnext',
     polyfillDynamicImport: false,
+    ssr: false,
   },
 })
