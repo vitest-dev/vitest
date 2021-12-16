7759dfea592faa51a851fad32209279bd1bb175b
Anthony Fu <anthonyfu117@hotmail.com>
2021-12-17 01:40:02 +0800

    fix: version query removal

--- packages/vitest/src/node/execute.ts
+++ packages/vitest/src/node/execute.ts
@@ -185,7 +185,8 @@ export function normalizeId(id: string): string {
     .replace(/^\/@id\//, '')
     .replace(/^__vite-browser-external:/, '')
     .replace(/^node:/, '')
-    .replace(/\?.*$/, '') // remove query
+    .replace(/[?&]v=\w+/, '?') // remove ?v= query
+    .replace(/\?$/, '?') // remove end query mark
 }
 
 export async function shouldExternalize(id: string, config: Pick<ExecuteOptions, 'inline' | 'external'>) {
