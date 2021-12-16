48162d479a04f5a285b77ea304eab43a94e2a6e1
Vladimir Sheremet <sheremet.va@icloud.com>
2021-12-16 16:21:00 +0300

    test: added test for several mocks

--- test/core/test/mocked-no-mocks.test.ts
+++ test/core/test/mocked-no-mocks.test.ts
@@ -1,7 +1,9 @@
 import { test, vi, expect } from 'vitest'
 import { mockedA } from '../src/mockedA'
+import { mockedB } from '../src/mockedB'
 
 vi.mock('../src/mockedA.ts')
+vi.mock('../src/mockedB.ts')
 
 // it may seem the tests are identical, but they test
 // that mockedA wasnt calld twice since it is called inside different suites
@@ -9,4 +11,11 @@ test('testing mocking module without __mocks__', () => {
   mockedA()
 
   expect(mockedA).toHaveBeenCalledTimes(1)
+})
+
+test('mocking several modules work', () => {
+  mockedB()
+
+  expect(mockedA).toHaveBeenCalledTimes(2)
+  expect(mockedB).toHaveBeenCalledTimes(1)
 })
