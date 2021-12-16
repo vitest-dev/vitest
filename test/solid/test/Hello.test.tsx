608b8767fa4058c58ab0edf23bdacd8411d0b2c1
Alex Lohr <alex.lohr@logmein.com>
2021-12-16 13:46:08 +0100

    test: fix solid imports

--- test/solid/test/Hello.test.tsx
+++ test/solid/test/Hello.test.tsx
@@ -1,19 +1,33 @@
-import { describe, expect, test } from 'vitest'
+import { afterAll, describe, expect, test } from 'vitest'
 import { render } from 'solid-js/web'
 import { Hello } from '../components/Hello'
 
 describe('Solid.js Hello.tsx', () => {
+  const unmount: (() => void)[] = []
+  afterAll(() => {
+    unmount.forEach(u => u())
+    document.body.innerHTML = ''
+  })
+
   test('mounts and unmounts', () => {
     const container = document.createElement('main')
     document.body.appendChild(container)
-    const unmount = render(() => <Hello count={4} />, container)
+    unmount.push(render(() => <Hello count={4} />, container))
     expect(container).toBeTruthy()
     expect(container.innerHTML).toContain('4 x 2 = 8')
-    expect(container.innerHTML).toMatchSnapshot()
-    unmount()
-    expect(container.innerHTML).toBe('')
-    document.body.removeChild(container)
   })
 
-  test.todo('updates on button click')
+  test('updates on button click', async() => {
+    const container = document.createElement('div')
+    document.body.appendChild(container)
+    unmount.push(render(() => <Hello count={4} />, container))
+    const button = container.querySelector('button')
+    expect(button).toBeTruthy()
+    expect(button).toBeInstanceOf(HTMLButtonElement)
+    button?.dispatchEvent(
+      new MouseEvent('click', { bubbles: true, button: 0, composed: true }),
+    )
+    await new Promise<void>(resolve => setTimeout(() => resolve(), 17))
+    expect(container.innerHTML).toContain('4 x 3 = 12')
+  })
 })
--- test/solid/vitest.config.ts
+++ test/solid/vitest.config.ts
@@ -1,17 +1,27 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite'
+import path from 'path'
 import solidPlugin from 'vite-plugin-solid'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   test: {
     environment: 'jsdom',
-    include: ['test/Hello.test.tsx'],
+    threads: false,
   },
   plugins: [solidPlugin()],
   build: {
     target: 'esnext',
     polyfillDynamicImport: false,
     ssr: false,
+  },
+  resolve: {
+    // unfortunately, this is still required, since conditions settings do not yet work
+    // in the internal vite server
+    alias: {
+      'solid-js/web': path.resolve(__dirname, './node_modules/solid-js/web/dist/dev.js'),
+      'solid-js/store': path.resolve(__dirname, './node_modules/solid-js/store/dist/dev.js'),
+      'solid-js': path.resolve(__dirname, './node_modules/solid-js/dist/dev.js'),
+    },
   },
 })
