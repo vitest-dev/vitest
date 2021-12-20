1e989066f996fbdffcf86e442fecf4d789a8e81e
Anthony Fu <anthonyfu117@hotmail.com>
2021-12-21 01:09:55 +0800

    Update scripts/get-contributors.ts

--- scripts/get-contributors.ts
+++ scripts/get-contributors.ts
@@ -5,24 +5,6 @@ const { GITHUB_TOKEN: token } = process.env
 
 type Contributor = {
   login: string;
-  id: number;
-  node_id: string;
-  avatar_url: string;
-  gravatar_id: string;
-  url: string;
-  html_url: string;
-  followers_url: string;
-  following_url: string;
-  gists_url: string;
-  starred_url: string;
-  subscriptions_url: string;
-  organizations_url: string;
-  repos_url: string;
-  events_url: string;
-  received_events_url: string;
-  type: string;
-  site_admin: boolean;
-  contributions: number;
 };
 
 async function fetchContributors() {
