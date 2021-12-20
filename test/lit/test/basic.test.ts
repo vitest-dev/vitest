6e931c7b66f486665b92c3bcad5146ac9a6f84b4
Joaquín Sánchez <joaquin.sanchez@fi2net.es>
2021-12-20 18:07:16 +0100

    test: fix `lit` (#234)

--- test/lit/test/basic.test.ts
+++ test/lit/test/basic.test.ts
@@ -9,6 +9,15 @@ declare global {
 
 describe('Button with increment', async() => {
   beforeEach(async() => {
+    // todo@to-be-removed: https://github.com/vitest-dev/vitest/pull/234
+    Object.defineProperty(Element.prototype, 'localName', {
+      get() {
+        return this.tagName?.toLowerCase() ?? 'unknown'
+      },
+      enumerable: false,
+      configurable: true,
+    })
+
     document.body.innerHTML = ''
 
     await window.happyDOM.whenAsyncComplete()
