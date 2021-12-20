7d842c9682656cb8c649e17c4a11c563ecffd6f5
Anthony Fu <anthonyfu117@hotmail.com>
2021-12-20 12:10:50 +0800

    chore: exit the process after test finished

--- packages/vitest/src/node/cli.ts
+++ packages/vitest/src/node/cli.ts
@@ -1,6 +1,5 @@
 import readline from 'readline'
 import cac from 'cac'
-import c from 'picocolors'
 import type { UserConfig } from '../types'
 import { version } from '../../package.json'
 import { ensurePackageInstalled } from '../utils'
@@ -45,8 +44,6 @@ cli
 
 cli.parse()
 
-const PROCESS_EXIT_TIMEOUT = 5_000
-
 async function dev(cliFilters: string[], argv: UserConfig) {
   if (argv.watch == null)
     argv.watch = !process.env.CI && !process.env.NODE_V8_COVERAGE && !argv.silent && !argv.run
@@ -85,13 +82,8 @@ async function run(cliFilters: string[], options: UserConfig) {
       await ctx.close()
   }
 
-  if (!ctx.config.watch) {
-    const timer = setTimeout(() => {
-      console.error(c.red('Process hanging for 5 seconds after all tests are done. Exiting...'))
-      process.exit(1)
-    }, PROCESS_EXIT_TIMEOUT)
-    timer.unref()
-  }
+  if (!ctx.config.watch)
+    process.exit()
 }
 
 function registerConsoleShortcuts(ctx: Vitest) {
