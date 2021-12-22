b52c529565a812b42358898a3fee64994c8d67ff
MitchelvanBever <mitchel@storyofams.com>
2021-12-22 09:22:23 +0100

    refactor: no need for async

--- test/testing-lib-react/src/App.test.tsx
+++ test/testing-lib-react/src/App.test.tsx
@@ -4,12 +4,12 @@ import { fireEvent, render, screen } from './utils/test-utils'
 import App from './App'
 
 describe('Simple working test', () => {
-  it('the title is visible', async() => {
+  it('the title is visible', () => {
     render(<App />)
     expect(screen.getByText(/Hello Vite \+ React!/i)).toBeDefined()
   })
 
-  it('should increment count on click', async() => {
+  it('should increment count on click', () => {
     render(<App />)
     fireEvent.click(screen.getByRole('button'))
     expect(screen.findByText(/count is: 1/i)).toBeDefined()
