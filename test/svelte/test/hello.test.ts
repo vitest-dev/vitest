7150866eec9929cf4a3c90df3e4867f2a404953a
tigerabrodi <tigerabrodi@gmail.com>
2021-12-25 19:07:00 +0100

    fix: remove user event, ain't working with svelte

--- test/svelte/test/hello.test.ts
+++ test/svelte/test/hello.test.ts
@@ -1,4 +1,4 @@
-import { cleanup, render, screen, userEvent } from '../utils/test-utils'
+import { cleanup, fireEvent, render, screen } from '../utils/test-utils'
 import Hello from '../components/Hello.svelte'
 
 describe('Hello.svelte', () => {
@@ -12,14 +12,13 @@ describe('Hello.svelte', () => {
     expect(container.innerHTML).toMatchSnapshot()
   })
 
-  /* TODO: From some reason this test breaks if not using async/await, even though it shouldn't be used with userEvent considering it doesn't return a promise. */
   it.only('updates on button click', async() => {
     render(Hello, { count: 4 })
     const btn = screen.getByRole('button')
     const div = screen.getByText('4 x 2 = 8')
-    await userEvent.click(btn)
+    await fireEvent.click(btn)
     expect(div.innerHTML).toBe('4 x 3 = 12')
-    await userEvent.click(btn)
+    await fireEvent.click(btn)
     expect(div.innerHTML).toBe('4 x 4 = 16')
   })
 })
--- test/svelte/utils/test-utils.ts
+++ test/svelte/utils/test-utils.ts
@@ -1,3 +1,2 @@
 /* eslint-disable import/export */
 export * from '@testing-library/svelte'
-export { default as userEvent } from '@testing-library/user-event'
