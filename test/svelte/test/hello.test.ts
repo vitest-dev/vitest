0710c7063820c0545efdd98c8274f65b4f1c3f53
Tiger Abrodi <49603590+tigerabrodi@users.noreply.github.com>
2021-12-25 20:13:52 +0100

    fix: use good practices from testing lib svelte (#311)

--- test/svelte/test/hello.test.ts
+++ test/svelte/test/hello.test.ts
@@ -1,13 +1,11 @@
-import { cleanup, fireEvent, render } from '@testing-library/svelte'
+import { cleanup, fireEvent, render, screen } from '@testing-library/svelte'
 import Hello from '../components/Hello.svelte'
-
-const { click } = fireEvent
 
 describe('Hello.svelte', () => {
   // TODO: @testing-library/svelte claims to add this automatically but it doesn't work without explicit afterEach
   afterEach(() => cleanup())
 
-  it('mounts', async() => {
+  it('mounts', () => {
     const { container } = render(Hello, { count: 4 })
     expect(container).toBeTruthy()
     expect(container.innerHTML).toContain('4 x 2 = 8')
@@ -15,12 +13,12 @@ describe('Hello.svelte', () => {
   })
 
   it('updates on button click', async() => {
-    const { getByText, getByRole } = render(Hello, { count: 4 })
-    const btn = getByRole('button')
-    const div = getByText('4 x 2 = 8')
-    await click(btn)
+    render(Hello, { count: 4 })
+    const btn = screen.getByRole('button')
+    const div = screen.getByText('4 x 2 = 8')
+    await fireEvent.click(btn)
     expect(div.innerHTML).toBe('4 x 3 = 12')
-    await click(btn)
+    await fireEvent.click(btn)
     expect(div.innerHTML).toBe('4 x 4 = 16')
   })
 })
