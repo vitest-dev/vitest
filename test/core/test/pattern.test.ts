0e04c45016e95c551fe26db9bc0e2ee31c4d3aac
Anthony Fu <anthonyfu117@hotmail.com>
2021-12-27 02:55:14 +0800

    feat: indent to inline snap

---
+++ test/core/test/pattern.test.ts
@@ -0,0 +1,30 @@
+import { expect, test } from 'vitest'
+
+test('pattern', () => {
+  const size = 10
+  const g = '🟩'
+  const r = '🟥'
+
+  function pattern() {
+    return Array.from({ length: size })
+      .map((_, y) => Array.from({ length: size })
+        .map((__, x) => {
+          return (x * y) % 3 ? g : r
+        }))
+      .map(i => i.join(' '))
+      .join('\n')
+  }
+
+  expect(`\n${pattern()}`).toMatchInlineSnapshot(`
+    "
+    🟥 🟥 🟥 🟥 🟥 🟥 🟥 🟥 🟥 🟥
+    🟥 🟩 🟩 🟥 🟩 🟩 🟥 🟩 🟩 🟥
+    🟥 🟩 🟩 🟥 🟩 🟩 🟥 🟩 🟩 🟥
+    🟥 🟥 🟥 🟥 🟥 🟥 🟥 🟥 🟥 🟥
+    🟥 🟩 🟩 🟥 🟩 🟩 🟥 🟩 🟩 🟥
+    🟥 🟩 🟩 🟥 🟩 🟩 🟥 🟩 🟩 🟥
+    🟥 🟥 🟥 🟥 🟥 🟥 🟥 🟥 🟥 🟥
+    🟥 🟩 🟩 🟥 🟩 🟩 🟥 🟩 🟩 🟥
+    🟥 🟩 🟩 🟥 🟩 🟩 🟥 🟩 🟩 🟥
+    🟥 🟥 🟥 🟥 🟥 🟥 🟥 🟥 🟥 🟥"`)
+})
