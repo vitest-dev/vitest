f27c19300a02f3f54e37d0b4900e6b5b099a7a03
Ivan Demchuk <ivan.demchuk@gmail.com>
2021-12-29 15:15:14 +0200

    feat: add spy name to assertion messages

--- packages/vitest/src/integrations/jest-mock.ts
+++ packages/vitest/src/integrations/jest-mock.ts
@@ -94,6 +94,8 @@ export type MaybeMocked<T> = T extends MockableFunction
     ? MockedObject<T>
     : T
 
+export type EnhancedSpy<TArgs extends any[] = any[], TReturns = any> = JestMockCompat<TArgs, TReturns> & SpyImpl<TArgs, TReturns>
+
 export interface MockWithArgs<T extends MockableFunction>
   extends JestMockCompatFn<ArgumentsOf<T>, ReturnType<T>> {
   new (...args: ConstructorArgumentsOf<T>): T
@@ -124,7 +126,7 @@ type Awaited<T> = T extends Promise<infer R> ? R : never
 function enhanceSpy<TArgs extends any[], TReturns>(
   spy: SpyImpl<TArgs, TReturns>,
 ): JestMockCompat<TArgs, TReturns> {
-  const stub = spy as unknown as JestMockCompat<TArgs, TReturns> & SpyImpl<TArgs, TReturns>
+  const stub = spy as unknown as EnhancedSpy<TArgs, TReturns>
 
   let implementation: ((...args: TArgs) => TReturns) | undefined
 
