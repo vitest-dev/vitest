8f49af0e56a9296dd9b52be3897217defe813d50
JoostK <joost.koehoorn@gmail.com>
2022-01-02 00:29:58 +0100

    fix: prevent tests from stalling if an error occurs (fix #374)
    The serialization logic of error objects would mutate the error object in-place,
    retaining its original constructor and leaving non-enumerable properties intact.
    When an object of type `Error` is sent over a `MessagePort` an issue may occur
    which resulted in the tests stalling. The exact reason as to why the tests get
    stuck before this fix are unclear, but serializing the `Error` instance into
    a plain object avoids the issue.
    
    This commit changes the serialization logic to create a new object without
    its original constructor and then copies over all properties to the clone, which
    prevents the tests from stalling. An integration fixture is added in the `fails`
    workspace to prevent regressions.
    
    These changes make the custom `nameStr` and `stackStr` properties redundant,
    as `name` and `stack` are now copied over when traversing the prototype chain of
    an `Error` object. These properties are not removed as that would be cleanup work
    outside the scope of this fix.
    
    Fixes #374

--- packages/vitest/src/runtime/error.ts
+++ packages/vitest/src/runtime/error.ts
@@ -1,8 +1,10 @@
 import { format } from 'util'
 import { stringify } from '../integrations/chai/jest-matcher-utils'
 
+const OBJECT_PROTO = Object.getPrototypeOf({})
+
 // https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Structured_clone_algorithm
-export function serializeError(val: any, seen = new WeakSet()): any {
+export function serializeError(val: any, seen = new WeakMap()): any {
   if (!val || typeof val === 'string')
     return val
   if (typeof val === 'function')
@@ -17,20 +19,32 @@ export function serializeError(val: any, seen = new WeakSet()): any {
     return `${val.toString()} ${format(val.sample)}`
 
   if (seen.has(val))
-    return val
-  seen.add(val)
+    return seen.get(val)
 
   if (Array.isArray(val)) {
-    val = val.map((e) => {
-      return serializeError(e, seen)
+    const clone: any[] = new Array(val.length)
+    seen.set(val, clone)
+    val.forEach((e, i) => {
+      clone[i] = serializeError(e, seen)
     })
+    return clone
   }
   else {
-    Object.keys(val).forEach((key) => {
-      val[key] = serializeError(val[key], seen)
-    })
+    // Objects with `Error` constructors appear to cause problems during worker communication
+    // using `MessagePort`, so the serialized error object is being recreated as plain object.
+    const clone = Object.create(null)
+    seen.set(val, clone)
+
+    let obj = val
+    while (obj && obj !== OBJECT_PROTO) {
+      Object.getOwnPropertyNames(obj).forEach((key) => {
+        if (!(key in clone))
+          clone[key] = serializeError(obj[key], seen)
+      })
+      obj = Object.getPrototypeOf(obj)
+    }
+    return clone
   }
-  return val
 }
 
 export function processError(err: any) {
