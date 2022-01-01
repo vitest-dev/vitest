d63c94ca7b6da5fa62dd7e35022d2559e1d4773f
dominikg <dominik.goepel@gmx.de>
2022-01-01 18:19:06 +0100

    test: remove throwing errors in teardown examples

--- test/global-setup/setupFiles/named-exports.js
+++ test/global-setup/setupFiles/named-exports.js
@@ -1,12 +1,12 @@
 const sleep = async n => new Promise(resolve => setTimeout(resolve, n))
 
-let start
+// let start
 
 export async function setup() {
   // setup something eg start a server, db or whatever
   // const server = await start()
   // console.log('globalSetup named-exports.js')
-  start = Date.now()
+  // start = Date.now()
   await sleep(25)
 }
 
@@ -14,8 +14,8 @@ export async function teardown() {
   // tear it down here
   // await server.close()
   await sleep(25)
-  const duration = Date.now() - start
+  // const duration = Date.now() - start
   // console.log(`globalTeardown named-exports.js, took ${(duration)}ms`)
-  if (duration > 4000)
-    throw new Error('error from teardown in globalSetup named-exports.js')
+  // if (duration > 4000)
+  //  throw new Error('error from teardown in globalSetup named-exports.js')
 }
