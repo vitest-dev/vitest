d37af08d316ed8ad0d61bdc00084f4097e44b58a
Anthony Fu <anthonyfu117@hotmail.com>
2022-01-03 23:38:53 +0800

    test: add vue suspense test (#290)

---
+++ test/vue/test/async.test.ts
@@ -0,0 +1,29 @@
+import { nextTick } from 'vue'
+import { flushPromises, mount } from '@vue/test-utils'
+import AsyncWrapper from '../components/AsyncWrapper.vue'
+
+test('async component with suspense', async() => {
+  expect(AsyncWrapper).toBeTruthy()
+
+  let resolve: Function
+  // eslint-disable-next-line promise/param-names
+  const promise = new Promise(_resolve => resolve = _resolve)
+  const wrapper = mount(AsyncWrapper, {
+    props: {
+      promise,
+    },
+  })
+
+  await nextTick()
+
+  expect(wrapper.text()).toContain('fallback')
+
+  resolve()
+
+  await flushPromises()
+  await nextTick()
+  await nextTick()
+
+  const text = wrapper.text()
+  expect(text).toContain('resolved')
+})
