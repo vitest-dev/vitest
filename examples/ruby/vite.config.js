050666222c9adc5fa03590bd4cc4a9952b2a3af6
Anthony Fu <anthonyfu117@hotmail.com>
2022-01-05 05:51:06 +0800

    chore: update

--- examples/ruby/vite.config.js
+++ examples/ruby/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import ViteRuby from 'vite-plugin-ruby'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue(), ViteRuby.default()],
+  plugins: [Vue(), ViteRuby.default()],
   test: {
     environment: 'jsdom',
   },
