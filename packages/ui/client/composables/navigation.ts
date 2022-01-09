ca0a1a840a0b9b1bdb5d9c0e2f985f566ef4e570
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2022-01-09 16:31:26 +0100

    fix: initial file on F5

--- packages/ui/client/composables/navigation.ts
+++ packages/ui/client/composables/navigation.ts
@@ -1,11 +1,34 @@
 import type { Ref } from 'vue'
+import { client, findById } from './client'
+import { activeFileId } from './params'
 import type { File } from '#types'
 
 const currentModuleSymbol = Symbol('current-module')
 const showSummarySymbol = Symbol('show-summary')
 
 export function provideCurrentModule() {
+  const route = useRoute()
   const currentModule = ref<File | undefined>(undefined)
+  if (route.hash) {
+    const params = new URLSearchParams(route.hash.slice(1))
+    if (params.has('file')) {
+      const file = params.get('file')!
+      const current = findById(file)
+      if (current) {
+        activeFileId.value = file
+        currentModule.value = current
+      }
+      else {
+        watchOnce(
+          () => client.state.getFiles(),
+          () => {
+            activeFileId.value = file
+            currentModule.value = findById(file)
+          },
+        )
+      }
+    }
+  }
   provide(currentModuleSymbol, currentModule)
   return currentModule
 }
