d9f3677777891a335b1d3cae68d171d0ff01fd5d
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2022-01-11 13:33:02 +0100

    chore: fix split panes layout

--- packages/ui/client/composables/navigation.ts
+++ packages/ui/client/composables/navigation.ts
@@ -8,37 +8,26 @@ const showSummarySymbol = Symbol('show-summary')
 const summaryVisibleSymbol = Symbol('summary-visible')
 
 export function initializeNavigation() {
-  const route = useRoute()
   const currentModule = ref<File | undefined>(undefined)
   const summaryVisible = ref(true)
-  if (route.hash) {
-    const params = new URLSearchParams(route.hash.slice(1))
-    if (params.has('file')) {
-      const file = params.get('file')!
-      if (file && file.length > 0) {
-        const current = findById(file)
-        if (current) {
-          currentModule.value = current
-          if (current?.id)
-            activeFileId.value = current.id
-
+  const file = activeFileId.value
+  if (file && file.length > 0) {
+    const current = findById(file)
+    if (current) {
+      currentModule.value = current
+      summaryVisible.value = false
+    }
+    else {
+      watchOnce(
+        () => client.state.getFiles(),
+        () => {
+          currentModule.value = findById(file)
           summaryVisible.value = false
-        }
-        else {
-          watchOnce(
-            () => client.state.getFiles(),
-            () => {
-              currentModule.value = findById(file)
-              if (currentModule.value?.id)
-                activeFileId.value = currentModule.value.id
-
-              summaryVisible.value = false
-            },
-          )
-        }
-      }
+        },
+      )
     }
   }
+
   function showSummary(show: boolean) {
     summaryVisible.value = show
     if (show) {
--- packages/ui/client/pages/index.vue
+++ packages/ui/client/pages/index.vue
@@ -1,51 +1,54 @@
 <script setup lang="ts">
 // @ts-expect-error missing types
 import { Pane, Splitpanes } from 'splitpanes'
+import type { VueInstance } from '@vueuse/core'
 import { initializeNavigation } from '../composables/navigation'
 
 const summaryVisible = initializeNavigation()
-const sizes = reactive([33, 33, 34])
-const summarySize = ref(sizes[1] + sizes[2])
+const split = ref<VueInstance | null>(null)
+const mainSizes = reactive([33, 67])
+const detailSizes = reactive([33, 67])
 
-watch(() => sizes[0], (s) => {
-  summarySize.value = 100 - s
-}, { immediate: true })
-
-const onResized = useDebounceFn((event: { size: number }[]) => {
-  if (event.length === 2) {
-    sizes[0] = event[0].size
-  }
-  else {
-    event.forEach((e, i) => {
-      sizes[i] = e.size
-    })
-  }
+const onMainResized = useDebounceFn((event: { size: number }[]) => {
+  event.forEach((e, i) => {
+    mainSizes[i] = e.size
+  })
+}, 0)
+const onModuleResized = useDebounceFn((event: { size: number }[]) => {
+  event.forEach((e, i) => {
+    detailSizes[i] = e.size
+  })
 }, 0)
 
-onMounted(() => {
+function resizeMain() {
   const width = window.innerWidth
   const panelWidth = Math.min(width / 3, 300)
-  const panelPercent = panelWidth / width * 100
-  sizes[0] = panelPercent
-  sizes[1] = panelPercent
-  sizes[2] = 100 - panelPercent * 2
-})
+  const w = (100 * panelWidth) / width
+  mainSizes[0] = w
+  mainSizes[1] = 100 - w
+  detailSizes[0] = w
+  detailSizes[1] = 100 - w
+}
 </script>
 
 <template>
   <div h-screen w-screen overflow="hidden">
-    <Splitpanes @resized="onResized">
-      <Pane :size="sizes[0]">
+    <Splitpanes ref="split" @resized="onMainResized" @ready="resizeMain">
+      <Pane :size="mainSizes[0]">
         <Navigation />
       </Pane>
-      <Pane v-if="summaryVisible" :size="summarySize">
-        <ReportSummary />
-      </Pane>
-      <Pane v-if="!summaryVisible" key="suites" :size="sizes[1]">
-        <Suites />
-      </Pane>
-      <Pane v-if="!summaryVisible" key="file-details" :size="sizes[2]">
-        <FileDetails />
+      <Pane :size="mainSizes[1]">
+        <transition>
+          <ReportSummary v-if="summaryVisible" key="summary" />
+          <Splitpanes v-else key="detail" @resized="onModuleResized">
+            <Pane :size="detailSizes[0]">
+              <Suites />
+            </Pane>
+            <Pane :size="detailSizes[1]">
+              <FileDetails />
+            </Pane>
+          </Splitpanes>
+        </transition>
       </Pane>
     </Splitpanes>
   </div>
