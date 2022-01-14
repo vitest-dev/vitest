6a5b70f06926d584a168a6bf23dbaeef39389a0b
Horacio Herrera <hi@horacioh.com>
2022-01-14 13:56:33 +0100

    fix(example): update RTL example
    - `toBeInTheDocument` makes more sense than `toBeDefined`
    - add a test setup file to include jest-dom assertions

--- examples/react-testing-lib/src/App.test.tsx
+++ examples/react-testing-lib/src/App.test.tsx
@@ -1,16 +1,16 @@
 import { describe, expect, it } from 'vitest'
-import { render, screen, userEvent } from './utils/test-utils'
 import App from './App'
+import { render, screen, userEvent } from './utils/test-utils'
 
 describe('Simple working test', () => {
   it('the title is visible', () => {
     render(<App />)
-    expect(screen.getByText(/Hello Vite \+ React!/i)).toBeDefined()
+    expect(screen.getByText(/Hello Vite \+ React!/i)).toBeInTheDocument()
   })
 
-  it('should increment count on click', () => {
+  it('should increment count on click', async() => {
     render(<App />)
     userEvent.click(screen.getByRole('button'))
-    expect(screen.findByText(/count is: 1/i)).toBeDefined()
+    expect(await screen.findByText(/count is: 1/i)).toBeInTheDocument()
   })
 })
--- examples/react-testing-lib/vite.config.ts
+++ examples/react-testing-lib/vite.config.ts
@@ -10,5 +10,6 @@ export default defineConfig({
   test: {
     global: true,
     environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
   },
 })
