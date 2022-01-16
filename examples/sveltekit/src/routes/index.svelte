44fe2db582e193dadcff52b190939ca027f778b4
Puru Vijay <awesomepuruvj@gmail.com>
2022-01-16 17:16:36 +0530

    Beautify Hello.svelte

--- examples/sveltekit/src/routes/index.svelte
+++ examples/sveltekit/src/routes/index.svelte
@@ -1,2 +1,8 @@
+<script>
+import Hello from "$lib/Hello.svelte";
+
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Hello count={10} />
