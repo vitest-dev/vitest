af24a2e2c03b3464f6b0e4474f55ab5ab34709e2
Puru Vijay <awesomepuruvj@gmail.com>
2022-01-16 18:03:33 +0530

    Format index.svelte

--- examples/sveltekit/src/routes/index.svelte
+++ examples/sveltekit/src/routes/index.svelte
@@ -1,7 +1,7 @@
 <script>
-import Hello from "$lib/Hello.svelte";
-
+  import Hello from '$lib/Hello.svelte';
 </script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
