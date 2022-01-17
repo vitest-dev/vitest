2ab30334c3ae377fc82f63f6ba954d4b54efc8bb
spigbbbbb@gmil.com <pionxzh>
2022-01-18 00:16:08 +0800

    fix(test): use built-in spyOn to mock process.cwd()

--- test/core/test/file-path.test.ts
+++ test/core/test/file-path.test.ts
@@ -1,87 +1,80 @@
-import { afterEach, describe, expect, it } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import { isWindows, slash, toFilePath } from '../../../packages/vite-node/src/utils'
 
-const originalCwd = process.cwd()
-
-function mockCwd(cwd: string) {
-  Object.defineProperty(process, 'cwd', {
-    value: () => cwd,
-  })
-}
-
-function restoreCwd() {
-  Object.defineProperty(process, 'cwd', {
-    value: originalCwd,
-  })
-}
-
 describe('toFilePath', () => {
-  afterEach(() => {
-    restoreCwd()
-  })
-
+  // the following tests will work incorrectly on unix systems
   if (isWindows) {
-    // these test will work incorrectly on unix systems
     it('windows', () => {
       const root = 'C:/path/to/project'
-      mockCwd(root)
-
       const id = '/node_modules/pkg/file.js'
       const expected = 'C:/path/to/project/node_modules/pkg/file.js'
+
+      const processSpy = vi.spyOn(process, 'cwd').mockReturnValue(root)
       const filePath = toFilePath(id, root)
+      processSpy.mockRestore()
+
       expect(slash(filePath)).toEqual(expected)
     })
 
     it('windows with /@fs/', () => {
       const root = 'C:/path/to/project'
-      mockCwd(root)
-
       const id = '/@fs/C:/path/to/project/node_modules/pkg/file.js'
       const expected = 'C:/path/to/project/node_modules/pkg/file.js'
+
+      const processSpy = vi.spyOn(process, 'cwd').mockReturnValue(root)
       const filePath = toFilePath(id, root)
+      processSpy.mockRestore()
+
       expect(slash(filePath)).toEqual(expected)
     })
   }
 
+  // the following tests will work incorrectly on windows systems
   if (!isWindows) {
-    // these test will work incorrectly on windows systems
     it('unix', () => {
       const root = '/path/to/project'
-      mockCwd(root)
-
       const id = '/node_modules/pkg/file.js'
       const expected = '/path/to/project/node_modules/pkg/file.js'
+
+      const processSpy = vi.spyOn(process, 'cwd').mockReturnValue(root)
       const filePath = toFilePath(id, root)
+      processSpy.mockRestore()
+
       expect(slash(filePath)).toEqual(expected)
     })
 
     it('unix with /@fs/', () => {
       const root = '/path/to/project'
-      mockCwd(root)
-
       const id = '/@fs//path/to/project/node_modules/pkg/file.js'
       const expected = '/path/to/project/node_modules/pkg/file.js'
+
+      const processSpy = vi.spyOn(process, 'cwd').mockReturnValue(root)
       const filePath = toFilePath(id, root)
+      processSpy.mockRestore()
       expect(slash(filePath)).toEqual(expected)
     })
 
     it('unix in first level catalog', () => {
       const root = '/root'
-      mockCwd(root)
-
       const id = '/node_modules/pkg/file.js'
       const expected = '/root/node_modules/pkg/file.js'
+
+      const processSpy = vi.spyOn(process, 'cwd').mockReturnValue(root)
       const filePath = toFilePath(id, root)
+      processSpy.mockRestore()
+
       expect(slash(filePath)).toEqual(expected)
     })
 
     it('unix with /@fs/ in first level catalog', () => {
       const root = '/root'
-      mockCwd(root)
-
       const id = '/@fs//root/node_modules/pkg/file.js'
       const expected = '/root/node_modules/pkg/file.js'
+
+      const processSpy = vi.spyOn(process, 'cwd').mockReturnValue(root)
       const filePath = toFilePath(id, root)
+      processSpy.mockRestore()
+
       expect(slash(filePath)).toEqual(expected)
     })
   }
