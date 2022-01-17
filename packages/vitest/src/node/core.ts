673e13abff0c7927e7008a08af939e8ccd08cd27
Shogo Iwano <shiwano@gmail.com>
2022-01-17 17:25:45 +0900

    fix(vitest): respect process.exitCode when exit method is called (#549)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- packages/vitest/src/node/core.ts
+++ packages/vitest/src/node/core.ts
@@ -360,7 +360,7 @@ export class Vitest {
     Promise.race([closePromise, timeoutPromise]).then(
       () => {
         clearTimeout(timeout)
-        process.exit(0)
+        process.exit()
       },
       (err) => {
         clearTimeout(timeout)
