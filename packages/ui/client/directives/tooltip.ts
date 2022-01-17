5f5c8a3e296a79476c2bcbad0ca71a4138b64225
Michel EDIGHOFFER <edimitchel@gmail.com>
2022-01-17 14:15:25 +0100

    fix: clean code

--- packages/ui/client/directives/tooltip.ts
+++ packages/ui/client/directives/tooltip.ts
@@ -1,28 +1,26 @@
 import tippy from 'tippy.js'
-import type { Directive, FunctionDirective } from 'vue'
-import type { Instance, Placement } from 'tippy.js'
+import type { Directive } from 'vue'
+import type { Instance, Placement, Props } from 'tippy.js'
 
-const getTippyInstance = (el: Element, instanceCallback: (instance: Instance) => void): void => {
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-  const instance = '_tippy' in el ? el._tippy as Instance : undefined
-  if (instance) instanceCallback(instance)
+const getTippyInstance = (el: Element, instanceCallback: (instance?: Instance) => void): void => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  instanceCallback('_tippy' in el ? el._tippy as Instance : undefined)
 }
 
-const tippyMount: FunctionDirective = (el: Element, { value, modifiers }) => {
-  tippy(el, {
+const tooltip: Directive = (el: Element, { value, oldValue, modifiers }) => {
+  const config: Partial<Props> = {
     content: value,
-    delay: 200,
-    hideOnClick: false,
     placement: Object.keys(modifiers).at(0) as unknown as Placement,
+  }
+  getTippyInstance(el, (instance) => {
+    instance === undefined // mount if instance not yet created, else update content
+      ? tippy(el, {
+        delay: 200,
+        hideOnClick: false,
+        ...config,
+      })
+      : value !== oldValue && instance.setProps(config)
   })
 }
-
-const tooltip: Directive = {
-  mounted: tippyMount,
-  updated(el: Element, { value, oldValue }) {
-    getTippyInstance(el, instance => value !== oldValue && instance.setContent(value))
-  },
-}
-
 export default tooltip
