d86554214c5180f1b5a1ecc09ea0218df9b27d18
Michel EDIGHOFFER <edimitchel@gmail.com>
2022-01-17 14:00:48 +0100

    fix: avoid tooltip sur rendering

--- packages/ui/client/directives/tooltip.ts
+++ packages/ui/client/directives/tooltip.ts
@@ -1,12 +1,28 @@
 import tippy from 'tippy.js'
-import type { Directive } from 'vue'
-import type { Placement } from 'tippy.js'
+import type { Directive, FunctionDirective } from 'vue'
+import type { Instance, Placement } from 'tippy.js'
 
-const tooltip: Directive = (el, { value, modifiers }) => {
+const getTippyInstance = (el: Element, instanceCallback: (instance: Instance) => void): void => {
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+  const instance = '_tippy' in el ? el._tippy as Instance : undefined
+  if (instance) instanceCallback(instance)
+}
+
+const tippyMount: FunctionDirective = (el: Element, { value, modifiers }) => {
   tippy(el, {
     content: value,
+    delay: 200,
+    hideOnClick: false,
     placement: Object.keys(modifiers).at(0) as unknown as Placement,
   })
+}
+
+const tooltip: Directive = {
+  mounted: tippyMount,
+  updated(el: Element, { value, oldValue }) {
+    getTippyInstance(el, instance => value !== oldValue && instance.setContent(value))
+  },
 }
 
 export default tooltip
