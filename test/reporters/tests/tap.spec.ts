d350c2d8a81689ae82db73243f092694d693a3c0
Ivan Demchuk <ivan.demchuk@gmail.com>
2022-01-19 11:37:49 +0200

    test: add test for tap-flat reporter

--- test/reporters/tests/tap.spec.ts
+++ test/reporters/tests/tap.spec.ts
@@ -1,10 +1,24 @@
 import { TapReporter } from '../../../packages/vitest/src/node/reporters/tap'
+import { TapFlatReporter } from '../../../packages/vitest/src/node/reporters/tap-flat'
 import { getContext } from '../src/context'
 import { files } from '../src/data'
 
 test('tap reporter', async () => {
     // Arrange
     const reporter = new TapReporter()
+    const context = getContext()
+
+    // Act
+    reporter.onInit(context.vitest)
+    await reporter.onFinished(files)
+
+    // Assert
+    expect(context.output).toMatchSnapshot()
+})
+
+test('tap-flat reporter', async () => {
+    // Arrange
+    const reporter = new TapFlatReporter()
     const context = getContext()
 
     // Act
