9eaa5ff946d62faac0faa3a1e432415a262ee7f8
Anthony Fu <anthonyfu117@hotmail.com>
2022-01-19 13:27:33 +0800

    feat: improve inline snapshot indent

--- test/core/test/inline-snap.test.ts
+++ test/core/test/inline-snap.test.ts
@@ -14,11 +14,35 @@ expect('foo').toMatchInlineSnapshot(\`{
     replaceInlineSnap(code, s, 3, '"bar"')
     replaceInlineSnap(code, s, 40, '"bar\nfoo"')
     expect(s.toString()).toMatchInlineSnapshot(`
-"
-expect('foo').toMatchInlineSnapshot('\\"bar\\"')
-expect('foo').toMatchInlineSnapshot(\`\\"bar
-foo\\"
-\`)
-"`)
+      "
+      expect('foo').toMatchInlineSnapshot('\\"bar\\"')
+      expect('foo').toMatchInlineSnapshot(\`
+        \\"bar
+        foo\\"
+      \`)
+      "
+    `)
+  })
+
+  it('replaceInlineSnap with indenetation', async() => {
+    const indent = '  '
+    const code = `
+${indent}expect('foo').toMatchInlineSnapshot('"foo"')
+${indent}expect('foo').toMatchInlineSnapshot(\`{
+${indent}  "foo": \\\`\\\`,
+${indent}}\`)
+`
+    const s = new MagicString(code)
+    replaceInlineSnap(code, s, 3, '"bar"')
+    replaceInlineSnap(code, s, 60, '"bar\nfoo"')
+    expect(s.toString()).toMatchInlineSnapshot(`
+      "
+        expect('foo').toMatchInlineSnapshot('\\"bar\\"')
+        expect('foo').toMatchInlineSnapshot(\`
+          \\"bar
+          foo\\"
+        \`)
+      "
+    `)
   })
 })
