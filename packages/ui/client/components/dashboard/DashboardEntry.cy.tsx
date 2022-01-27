8c1269fbd913e29590854abb21e6b9c36b57be58
Jessica Sachs <jess@jessicasachs.io>
2022-01-26 19:10:37 -0500

    bug: reproduction of failing vite + cypress setup

--- packages/ui/client/components/dashboard/DashboardEntry.cy.tsx
+++ packages/ui/client/components/dashboard/DashboardEntry.cy.tsx
@@ -1,35 +1,38 @@
-import faker from '@faker-js/faker'
-import DashboardEntry from './DashboardEntry.vue'
+// import faker from '@faker-js/faker'
+// import DashboardEntry from './DashboardEntry.vue'
 
-const body = () => (<div data-testid="body-content">{ faker.lorem.words(2) }</div>)
-const header = () => (<div data-testid="header-content">{ faker.hacker.phrase() }</div>)
-const bodySelector = '[data-testid=body-content]'
-const headerSelector = '[data-testid=header-content]'
-const tailSelector = '[data-testid=tail]'
-
-// Used as a workaround until unocss HMR is compatible w Cy.
-it('initial', () => {
-  cy.mount(<DashboardEntry v-slots={{ body, header }}/>)
+it('runs', () => {
+  expect(true).to.be.true;
 })
+// const body = () => (<div data-testid="body-content">{ faker.lorem.words(2) }</div>)
+// const header = () => (<div data-testid="header-content">{ faker.hacker.phrase() }</div>)
+// const bodySelector = '[data-testid=body-content]'
+// const headerSelector = '[data-testid=header-content]'
+// const tailSelector = '[data-testid=tail]'
 
-describe('DashboardEntry', () => {
-  it('tail is rendered by default', () => {
-    cy.mount(<DashboardEntry v-slots={{ body, header }}/>)
-      .get(tailSelector)
-      .should('exist')
-  })
+// // Used as a workaround until unocss HMR is compatible w Cy.
+// it('initial', () => {
+//   cy.mount(<DashboardEntry v-slots={{ body, header }}/>)
+// })
 
-  it('tail is not shown when true', () => {
-    cy.mount(<DashboardEntry tail v-slots={{ body, header }}/>)
-      .get(tailSelector)
-      .should('not.exist')
-  })
+// describe('DashboardEntry', () => {
+//   it('tail is rendered by default', () => {
+//     cy.mount(<DashboardEntry v-slots={{ body, header }}/>)
+//       .get(tailSelector)
+//       .should('exist')
+//   })
 
-  it('renders the body and header slots', () => {
-    cy.mount(<DashboardEntry v-slots={{ body, header }}/>)
-      .get(bodySelector)
-      .should('be.visible')
-      .get(headerSelector)
-      .should('be.visible')
-  })
-})
+//   it('tail is not shown when true', () => {
+//     cy.mount(<DashboardEntry tail v-slots={{ body, header }}/>)
+//       .get(tailSelector)
+//       .should('not.exist')
+//   })
+
+//   it('renders the body and header slots', () => {
+//     cy.mount(<DashboardEntry v-slots={{ body, header }}/>)
+//       .get(bodySelector)
+//       .should('be.visible')
+//       .get(headerSelector)
+//       .should('be.visible')
+//   })
+// })
