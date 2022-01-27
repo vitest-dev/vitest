489ad6012c8db214b245995db6b20bbe4d092468
dominikg <dominik.goepel@gmx.de>
2022-01-27 19:12:18 +0100

    fix: lint

--- test/global-setup/test/setup-files.test.ts
+++ test/global-setup/test/setup-files.test.ts
@@ -1,6 +1,6 @@
 
 test('something has been added to global by setupFiles entry', async() => {
-  // @ts-expect-error
+  // @ts-expect-error type
   const result = something
   expect(result).toBe('something')
 })
