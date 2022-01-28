3b2d23f66bef6b63890bfc65e27612358436b017
Ivan Demchuk <ivan.demchuk@gmail.com>
2022-01-28 12:32:05 +0200

    test: add test for checking vue coverage

---
+++ test/coverage-test/test/vue.test.ts
@@ -0,0 +1,29 @@
+
+/**
+ * @vitest-environment happy-dom
+ */
+
+import { expect, test } from 'vitest'
+import { mount } from '@vue/test-utils'
+import Hello from '../src/Hello.vue'
+
+test('vue 3 coverage', async() => {
+  expect(Hello).toBeTruthy()
+
+  const wrapper = mount(Hello, {
+    props: {
+      count: 4,
+    },
+  })
+
+  expect(wrapper.text()).toContain('4 x 2 = 8')
+  expect(wrapper.html()).toMatchSnapshot()
+
+  await wrapper.get('button').trigger('click')
+
+  expect(wrapper.text()).toContain('4 x 3 = 12')
+
+  await wrapper.get('button').trigger('click')
+
+  expect(wrapper.text()).toContain('4 x 4 = 16')
+})
