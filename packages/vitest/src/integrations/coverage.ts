ce68e2b4d4151eb18c47493f7a6dddd129223867
Cédric Exbrayat <cexbrayat@users.noreply.github.com>
2022-02-03 03:14:20 +0100

    feat: ignore cypress files in coverage by default (#651)

--- packages/vitest/src/integrations/coverage.ts
+++ packages/vitest/src/integrations/coverage.ts
@@ -13,6 +13,7 @@ const defaultExcludes = [
   'coverage/**',
   'packages/*/test{,s}/**',
   '**/*.d.ts',
+  'cypress/**',
   'test{,s}/**',
   'test{,-*}.{js,cjs,mjs,ts,tsx,jsx}',
   '**/*{.,-}test.{js,cjs,mjs,ts,tsx,jsx}',
