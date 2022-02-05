b6ef51cf2a70d0a8b64a7ec3eff4a70532fc73ab
Jan Müller <janmueller3698@gmail.com>
2022-02-05 13:35:55 +0100

    feat(ui): indent report for nested failed tasks

--- packages/ui/client/components/views/ViewReport.vue
+++ packages/ui/client/components/views/ViewReport.vue
@@ -6,16 +6,20 @@ const props = defineProps<{
   file?: File
 }>()
 
-function collectFailed(task: Task): Task[] {
+type LeveledTask = Task & {
+  level: number
+}
+
+function collectFailed(task: Task, level: number): LeveledTask[] {
   if (task.result?.state !== 'fail') return []
 
   if (task.type === 'test')
-    return [task]
+    return [{ ...task, level }]
   else
-    return [task, ...task.tasks.flatMap(t => collectFailed(t))]
+    return [{ ...task, level }, ...task.tasks.flatMap(t => collectFailed(t, level + 1))]
 }
 
-const failed = computed(() => props.file?.tasks.flatMap(t => collectFailed(t)) || [])
+const failed = computed(() => props.file?.tasks.flatMap(t => collectFailed(t, 0)) || [])
 
 function relative(p: string) {
   if (p.startsWith(config.value.root))
@@ -28,9 +32,8 @@ function relative(p: string) {
   <div h-full class="scrolls">
     <template v-if="failed.length">
       <div v-for="task of failed" :key="task.id">
-        <div bg="red-500/10" text="red-500 sm" p="x3 y2" m-2 rounded>
+        <div bg="red-500/10" text="red-500 sm" p="x3 y2" m-2 rounded :style="{ 'margin-left': `${2 * task.level + 0.5}rem`}">
           {{ task.name }}
-
           <div v-if="task.result?.error">
             <pre><b>{{ task.result.error.name || task.result.error.nameStr }}</b>: {{ task.result.error.message }}</pre>
             <pre v-for="stack, i of task.result.error.stacks || []" :key="i" op80> - {{ relative(stack.file) }}:{{ stack.line }}:{{ stack.column }}</pre>
