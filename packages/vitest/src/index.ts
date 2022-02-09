4aa09c80937ea6573b7caae7ca80fcbc3cb30559
Vladimir <sheremet.va@icloud.com>
2022-02-09 16:43:45 +0300

    fix: fixed typescript error when calling assertion (#709)

--- packages/vitest/src/index.ts
+++ packages/vitest/src/index.ts
@@ -122,15 +122,15 @@ declare global {
 
     // eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error
     // @ts-ignore build namspace conflict
-    type VitestAssertion<A> = {
+    type VitestAssertion<A, T> = {
       [K in keyof A]: A[K] extends Chai.Assertion
-        ? Assertion<any>
+        ? Assertion<T>
         : A[K] extends (...args: any[]) => any
           ? A[K] // not converting function since they may contain overload
-          : VitestAssertion<A[K]>
-    }
+          : VitestAssertion<A[K], T>
+    } & ((type: string, message?: string) => Assertion)
 
-    interface Assertion<T = any> extends VitestAssertion<Chai.Assertion>, JestAssertion<T> {
+    interface Assertion<T = any> extends VitestAssertion<Chai.Assertion, T>, JestAssertion<T> {
       resolves: Promisify<Assertion<T>>
       rejects: Promisify<Assertion<T>>
     }
