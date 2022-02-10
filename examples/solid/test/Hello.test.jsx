fb80e7909d6f0fc87bd82cbd103b2da76602a58d
Alex Lohr <alex.lohr@logmein.com>
2022-02-10 11:26:36 +0100

    improve example

--- examples/solid/test/Hello.test.jsx
+++ examples/solid/test/Hello.test.jsx
@@ -16,10 +16,18 @@ describe('<Hello />', () => {
     const container = document.createElement('div')
     document.body.appendChild(container)
     const unmount = render(() => <Hello count={4} />, container)
-    container.querySelector('button').dispatchEvent(
+    const button = container.querySelector('button')
+    const buttonClicked = new Promise((resolve) => {
+      const handler = (ev) => {
+        button.removeEventListener('click', handler)
+        resolve(ev)
+      }
+      button.addEventListener('click', handler)
+    })
+    button.dispatchEvent(
       new MouseEvent('click', { bubbles: true, cancelable: true, composed: true }),
     )
-    await new Promise(resolve => setTimeout(resolve, 100))
+    await buttonClicked
     expect(container.innerHTML).toMatchSnapshot()
     unmount()
     document.body.removeChild(container)
--- examples/solid/vite.config.mjs
+++ examples/solid/vite.config.mjs
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import solid from 'solid-start'
 
@@ -12,5 +15,9 @@ export default defineConfig({
   plugins: [solid({ ssr: false, dev: true })],
   resolve: {
     conditions: ['development', 'browser'],
+    alias: {
+      'solid-js/dist/server.js': 'solid-js/dist/dev.js',
+      'solid-js/web/dist/server.js': 'solid-js/web/dist/dev.js',
+    },
   },
 })
