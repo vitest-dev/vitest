b04dd8ffeffb528346d4d65bf1758666b41945d7
Anthony Fu <anthonyfu117@hotmail.com>
2022-02-18 21:44:23 +0800

    fix(vite-node): build spliting chunks

--- packages/vite-node/rollup.config.js
+++ packages/vite-node/rollup.config.js
@@ -6,13 +6,14 @@ import json from '@rollup/plugin-json'
 import alias from '@rollup/plugin-alias'
 import pkg from './package.json'
 
-const entries = [
-  'src/index.ts',
-  'src/server.ts',
-  'src/client.ts',
-  'src/utils.ts',
-  'src/cli.ts',
-]
+const entries = {
+  index: 'src/index.ts',
+  server: 'src/server.ts',
+  types: 'src/types.ts',
+  client: 'src/client.ts',
+  utils: 'src/utils.ts',
+  cli: 'src/cli.ts',
+}
 
 const external = [
   ...Object.keys(pkg.dependencies || {}),
@@ -38,30 +39,38 @@ const plugins = [
 ]
 
 export default () => [
-  ...entries.map(input => ({
-    input,
-    output: [
-      {
-        file: input.replace('src/', 'dist/').replace('.ts', '.js'),
-        format: 'esm',
-      },
-      {
-        file: input.replace('src/', 'dist/').replace('.ts', '.cjs'),
-        format: 'cjs',
-      },
-    ],
+  {
+    input: entries,
+    output: {
+      dir: 'dist',
+      format: 'esm',
+      sourcemap: 'inline',
+      entryFileNames: '[name].js',
+    },
     external,
     plugins,
-  })),
-  ...entries.map(input => ({
-    input,
+  },
+  {
+    input: entries,
     output: {
-      file: input.replace('src/', '').replace('.ts', '.d.ts'),
+      dir: 'dist',
+      format: 'cjs',
+      sourcemap: 'inline',
+      entryFileNames: '[name].cjs',
+    },
+    external,
+    plugins,
+  },
+  {
+    input: entries,
+    output: {
+      dir: process.cwd(),
+      entryFileNames: '[name].d.ts',
       format: 'esm',
     },
     external,
     plugins: [
       dts({ respectExternal: true }),
     ],
-  })),
+  },
 ]
