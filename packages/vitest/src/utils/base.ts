51b26ad8dc990dd7fb7b95210717feecfff851d1
Joaquín Sánchez <userquin@gmail.com>
2022-02-19 15:51:42 +0100

    cleanup: remove trailing spaces

--- packages/vitest/src/utils/base.ts
+++ packages/vitest/src/utils/base.ts
@@ -53,11 +53,12 @@ export function clone<T>(val: T): T {
  */
 
 export function toArray<T>(array?: Nullable<Arrayable<T>>): Array<T> {
-  if (array === null || array === undefined) 
+  if (array === null || array === undefined)
     array = []
   
   if (Array.isArray(array))
     return array
+
   return [array]
 }
 
