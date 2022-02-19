cded36b77ec837e14076e2f9482186ebafc14eee
Joaquín Sánchez <userquin@gmail.com>
2022-02-19 16:00:40 +0100

    fix lint

--- packages/vitest/src/utils/base.ts
+++ packages/vitest/src/utils/base.ts
@@ -55,7 +55,7 @@ export function clone<T>(val: T): T {
 export function toArray<T>(array?: Nullable<Arrayable<T>>): Array<T> {
   if (array === null || array === undefined)
     array = []
-  
+
   if (Array.isArray(array))
     return array
 
