ae7164737c87f6095d8063ed9db0d1eab09d0112
TrickyPi <530257315@qq.com>
2022-02-21 22:33:33 +0800

    fix: add config.d.ts && change defineConfig function type

--- packages/vitest/src/config.ts
+++ packages/vitest/src/config.ts
@@ -1,6 +1,11 @@
 import type { UserConfig } from 'vite'
+import type { InlineConfig } from './types'
+
+interface UserConfig$1 extends UserConfig {
+  test?: InlineConfig
+}
 
 export { configDefaults } from './defaults'
-export function defineConfig(config: UserConfig) {
+export function defineConfig(config: UserConfig$1) {
   return config
 }
