f5189fd3e3b3e361948ea30c6e2d5732fe6ff6a0
Joaquín Sánchez <joaquin.sanchez@fi2net.es>
2022-02-21 09:16:13 +0100

    feat(docs): add pwa with auto update strategy (#573)

--- docs/src/components/Avatar.vue
+++ docs/src/components/Avatar.vue
@@ -1,33 +1,39 @@
 <script setup lang="ts">
+import type { CoreTeam } from '../contributors'
+import { lazyLoad } from '../images'
+
 defineProps<{
-  avatar: string
-  name: string
-  github: string
-  twitter?: string
-  sponsors?: boolean
+  avatar: CoreTeam['avatar']
+  name: CoreTeam['name']
+  github: CoreTeam['github']
+  twitter: CoreTeam['twitter']
+  sponsors: CoreTeam['sponsors']
+  description: CoreTeam['description']
 }>()
 </script>
 
 <template>
   <div text-center>
-    <img :src="avatar" rounded-full h-25 w-25>
+    <img :loading="lazyLoad" :src="avatar" rounded-full min-w-25 min-h-25 h-25 w-25 :alt="`${name}'s avatar`">
     <div text-xl mt-1 mb-1>
       {{ name }}
     </div>
-    <div op60>
-      <slot />
-    </div>
+    <div op60 v-html="description" />
     <div flex="~ inline gap-2" py-4 text-2xl>
       <a
         class="i-carbon-logo-github inline-block text-current op30 hover:op100 mya transition duration-200"
         :href="`https://github.com/${github}`"
         target="_blank"
+        rel="noopener noreferrer"
+        :aria-label="`${name} on GitHub`"
       />
       <a
         v-if="twitter"
         class="i-carbon-logo-twitter inline-block text-1.3em mya text-current op30 hover:op100 transition duration-200"
         :href="`https://twitter.com/${twitter}`"
         target="_blank"
+        rel="noopener noreferrer"
+        :aria-label="`${name} on Twitter`"
       />
     </div>
     <br>
@@ -35,6 +41,7 @@ defineProps<{
       v-if="sponsors"
       class="btn mt-1 bg-pink-500 hover:bg-pink-600"
       target="_blank"
+      rel="noopener noreferrer"
       :href="`https://github.com/sponsors/${github}`"
     >
       <span class="i-carbon-favorite-filled inline-block" />
