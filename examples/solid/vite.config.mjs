bb313d45441ebe27c2e142d918958d0eab3e8d90
Alex Lohr <alex.lohr@logmein.com>
2022-02-22 09:39:39 +0100

    fix config

--- examples/solid/vite.config.mjs
+++ examples/solid/vite.config.mjs
@@ -8,11 +8,13 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     transformMode: {
-      web: ['.[jt]sx'],
+      web: [/.[jt]sx/],
     },
     deps: {
       inline: [/solid-js/],
     },
+    threads: false,
+    isolate: false,
   },
   plugins: [solid({ ssr: false, dev: true })],
   resolve: {
