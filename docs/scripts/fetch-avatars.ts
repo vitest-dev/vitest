e5bf4b04a4f3bc344b2abc6cf11c053463c063a9
Anthony Fu <anthonyfu117@hotmail.com>
2022-03-08 03:54:35 +0800

    chore: avatar size

--- docs/scripts/fetch-avatars.ts
+++ docs/scripts/fetch-avatars.ts
@@ -1,11 +1,13 @@
 import fs from 'fs-extra'
 import { $fetch } from 'ohmyfetch'
+import { coreTeamMembers } from '../src/contributors'
 
 const pathContributors = '../docs/contributors.json'
 const dirAvatars = '../docs/public/user-avatars/'
 const dirSponsors = '../docs/public/sponsors/'
 
 let contributors: string[] = []
+const team = coreTeamMembers.map(i => i.github)
 
 async function download(url: string, fileName: string) {
   const image = await $fetch(url, { responseType: 'arrayBuffer' })
@@ -16,7 +18,7 @@ async function fetchAvatars() {
   await fs.ensureDir(dirAvatars)
   contributors = JSON.parse(await fs.readFile(pathContributors, { encoding: 'utf-8' }))
 
-  await Promise.all(contributors.map(async name => download(`https://github.com/${name}.png`, `${dirAvatars}${name}.png`)))
+  await Promise.all(contributors.map(async name => download(`https://github.com/${name}.png?size${team.includes(name) ? 100 : 40}`, `${dirAvatars}${name}.png`)))
 }
 
 async function fetchSponsors() {
