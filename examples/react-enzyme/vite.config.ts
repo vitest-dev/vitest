5b37d99ce0353bc80ffea91e1b79ec42c1bd42d9
wael-Fadlallah <wael.fudlallah@gmail.com>
2022-03-10 15:12:13 +0400

    update defineConfig to be imported from vitest
    When I import the defineConfig from Vite it doesn't except test as an argument  
    ```
    Argument of type '{ plugins: any[]; test: {}; }' is not assignable to parameter of type 'UserConfigExport'.
      Object literal may only specify known properties, and 'test' does not exist in type 'UserConfigExport'
    ```

--- examples/react-enzyme/vite.config.ts
+++ examples/react-enzyme/vite.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest" />
 
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [react()],
