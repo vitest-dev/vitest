6606a2e4fdfef658d2eab5db332311d0ff091580
aslemammad <zorofight94@gmail.com>
2022-03-13 10:43:32 +0330

    update

--- packages/vitest/src/node/plugins/index.ts
+++ packages/vitest/src/node/plugins/index.ts
@@ -21,6 +21,7 @@ export async function VitestPlugin(
 
   async function WebPlugin() {
     await ensurePackageInstalled('@vitest/web')
+    // @ts-expected-error @vitest/web depends on vitest and vice versa
     return (await import('@vitest/web')).default('/')
   }
 
