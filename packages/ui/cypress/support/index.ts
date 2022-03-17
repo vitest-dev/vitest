09cf89e74bd7df3a32cf4fed635364e8438e22b4
Jessica Sachs <jess@jessicasachs.io>
2022-03-17 03:51:13 -0400

    adding faker seed back in

--- packages/ui/cypress/support/index.ts
+++ packages/ui/cypress/support/index.ts
@@ -1,10 +1,10 @@
-// import faker from '@faker-js/faker'
+import faker from '@faker-js/faker'
 import '../../client/global-setup'
 
 import { registerMount } from './mount'
 
-// before(() => {
-//   faker.seed(0)
-// })
+before(() => {
+  faker.seed(0)
+})
 
 registerMount()
