559919233e60b9356a08bc9bc7f0261f9ab18af9
Anthony Fu <anthonyfu117@hotmail.com>
2022-03-17 23:50:39 +0800

    feat: new `setupOnce` utils

---
+++ packages/vitest/src/runtime/setup-once.ts
@@ -0,0 +1,25 @@
+import type { Awaitable } from '../types'
+import { getWorkerState } from '../utils'
+
+const filesCount = new Map<string, number>()
+const cache = new Map<string, any>()
+
+/**
+ * This utils allows computational intensive tasks to only be ran once
+ * across test reruns to improve the watch mode performance.
+ *
+ * @expiremental
+ */
+export async function setupOnce<T>(fn: (() => Awaitable<T>), key?: string): Promise<T> {
+  if (!key) {
+    const filepath = getWorkerState().filepath || '__unknown_files__'
+    filesCount.set(filepath, (filesCount.get(filepath) || 0) + 1)
+    const count = filesCount.get(filepath)!
+    key = `${filepath}:${count}`
+  }
+
+  if (!cache.has(key))
+    cache.set(key, fn())
+
+  return await cache.get(key)
+}
