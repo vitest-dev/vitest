2dfcdf93c790fbc05076c87b557a0f8949b685c1
Ari Perkkiö <ari.perkkio@gmail.com>
2022-03-19 08:51:31 +0100

    test(reporters): add outputFile tests (#978)

--- test/reporters/tests/reporters.spec.ts
+++ test/reporters/tests/reporters.spec.ts
@@ -1,4 +1,6 @@
+import { existsSync, readFileSync, rmSync } from 'fs'
 import { afterEach, expect, test, vi } from 'vitest'
+import { normalize, resolve } from 'pathe'
 import { JsonReporter } from '../../../packages/vitest/src/node/reporters/json'
 import { JUnitReporter } from '../../../packages/vitest/src/node/reporters/junit'
 import { TapReporter } from '../../../packages/vitest/src/node/reporters/tap'
@@ -48,11 +50,38 @@ test('JUnit reporter', async() => {
   vi.setSystemTime(1642587001759)
 
   // Act
-  reporter.onInit(context.vitest)
+  await reporter.onInit(context.vitest)
   await reporter.onFinished(files)
 
   // Assert
   expect(context.output).toMatchSnapshot()
+})
+
+test('JUnit reporter with outputFile', async() => {
+  // Arrange
+  const reporter = new JUnitReporter()
+  const outputFile = resolve('report.xml')
+  const context = getContext()
+  context.vitest.config.outputFile = outputFile
+
+  vi.mock('os', () => ({
+    hostname: () => 'hostname',
+  }))
+
+  vi.setSystemTime(1642587001759)
+
+  // Act
+  await reporter.onInit(context.vitest)
+  await reporter.onFinished(files)
+
+  // Assert
+  const output = context.output.replace(normalize(process.cwd()), '<process-cwd>')
+  expect(output).toMatchSnapshot()
+  expect(existsSync(outputFile)).toBe(true)
+  expect(readFileSync(outputFile, 'utf8')).toMatchSnapshot()
+
+  // Cleanup
+  rmSync(outputFile)
 })
 
 test('json reporter', async() => {
@@ -68,4 +97,27 @@ test('json reporter', async() => {
 
   // Assert
   expect(JSON.parse(context.output)).toMatchSnapshot()
+})
+
+test('json reporter with outputFile', async() => {
+  // Arrange
+  const reporter = new JsonReporter()
+  const outputFile = resolve('report.json')
+  const context = getContext()
+  context.vitest.config.outputFile = outputFile
+
+  vi.setSystemTime(1642587001759)
+
+  // Act
+  reporter.onInit(context.vitest)
+  await reporter.onFinished(files)
+
+  // Assert
+  const output = context.output.replace(normalize(process.cwd()), '<process-cwd>')
+  expect(output).toMatchSnapshot()
+  expect(existsSync(outputFile)).toBe(true)
+  expect(readFileSync(outputFile, 'utf8')).toMatchSnapshot()
+
+  // Cleanup
+  rmSync(outputFile)
 })
