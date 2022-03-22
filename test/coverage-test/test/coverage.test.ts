5d4e7f6e7a85ae334dae89899b59b43ede49b8a2
yoho <panyoho@gmail.com>
2022-03-22 13:28:51 +0800

    test: test

--- test/coverage-test/test/coverage.test.ts
+++ test/coverage-test/test/coverage.test.ts
@@ -1,6 +1,13 @@
+import fs from 'fs'
+import { resolve } from 'pathe'
 import { expect, test } from 'vitest'
 import { pythagoras } from '../src'
 
 test('Math.sqrt()', async() => {
   expect(pythagoras(3, 4)).toBe(5)
+})
+
+test('coverage', async() => {
+  const stat = fs.statSync(resolve('./coverage/'))
+  expect(stat.isDirectory()).toBe(true)
 })
