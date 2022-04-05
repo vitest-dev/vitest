55805a2e5760e3ea6aac73a5d6a7e8030eede942
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2022-04-05 22:48:13 +0200

    feat(ui): add `ansi-to-html` on report and console

--- packages/ui/client/components/views/ViewConsoleOutput.vue
+++ packages/ui/client/components/views/ViewConsoleOutput.vue
@@ -1,15 +1,26 @@
 <script setup lang="ts">
 import { getNames } from '@vitest/ws-client'
 import { client, currentLogs as logs } from '~/composables/client'
+import { isDark } from '~/composables/dark'
+import { createAnsiToHtmlFilter } from '~/composables/error'
 
 function formatTime(t: number) {
   return (new Date(t)).toLocaleTimeString()
 }
 
-function formatConsoleLog(log: string) {
-  // TODO: support ASNI colors
-  return log.trim()
-}
+const formattedLogs = computed(() => {
+  const data = logs.value
+  if (data) {
+    const filter = createAnsiToHtmlFilter(isDark.value)
+    return data.map(({ taskId, type, time, content }) => {
+      const trimmed = content.trim()
+      const value = filter.toHtml(trimmed)
+      return value !== trimmed
+        ? { taskId, type, time, html: true, content: value }
+        : { taskId, type, time, html: false, content }
+    })
+  }
+})
 
 function getTaskName(id?: string) {
   const task = id && client.state.idMap.get(id)
@@ -18,16 +29,17 @@ function getTaskName(id?: string) {
 </script>
 
 <template>
-  <div v-if="logs?.length" h-full class="scrolls" flex flex-col data-testid="logs">
-    <div v-for="log of logs" :key="log.taskId" font-mono>
+  <div v-if="formattedLogs?.length" h-full class="scrolls" flex flex-col data-testid="logs">
+    <div v-for="{ taskId, type, time, html, content } of formattedLogs" :key="taskId" font-mono>
       <div border="b base" p-4>
         <div
           text-xs mb-1
-          :class="log.type === 'stderr' ? 'text-red-600 dark:text-red-300': 'op30'"
+          :class="type === 'stderr' ? 'text-red-600 dark:text-red-300': 'op30'"
         >
-          {{ formatTime(log.time) }} | {{ getTaskName(log.taskId) }} | {{ log.type }}
+          {{ formatTime(time) }} | {{ getTaskName(taskId) }} | {{ type }}
         </div>
-        <pre v-text="formatConsoleLog(log.content)" />
+        <pre v-if="html" v-html="content" />
+        <pre v-else v-text="content" />
       </div>
     </div>
   </div>
--- packages/ui/client/composables/error.ts
+++ packages/ui/client/composables/error.ts
@@ -1,3 +1,5 @@
+import Filter from 'ansi-to-html'
+
 export function shouldOpenInEditor(name: string, fileName?: string) {
   return fileName && name.endsWith(fileName)
 }
@@ -5,4 +7,11 @@ export function shouldOpenInEditor(name: string, fileName?: string) {
 export async function openInEditor(name: string, line: number, column: number) {
   const url = encodeURI(`${name}:${line}:${column}`)
   await fetch(`/__open-in-editor?file=${url}`)
+}
+
+export function createAnsiToHtmlFilter(dark: boolean) {
+  return new Filter({
+    fg: dark ? '#000' : '#FFF',
+    bg: dark ? '#FFF' : '#000',
+  })
 }
