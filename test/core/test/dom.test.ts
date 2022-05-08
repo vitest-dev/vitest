07eeb28c6ea10d4c61a31ca9c1d3e611a054d4a1
Cédric Exbrayat <cexbrayat@users.noreply.github.com>
2022-05-08 08:37:14 +0200

    chore: repro --no-threads issues (#1060)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>
    Co-authored-by: Vladimir Sheremet <sheremet.va@icloud.com>

--- test/core/test/dom.test.ts
+++ test/core/test/dom.test.ts
@@ -1,7 +1,15 @@
 /**
  * @vitest-environment jsdom
  */
+
+/* eslint-disable vars-on-top */
+
 import { expect, it, vi } from 'vitest'
+
+declare global {
+  // eslint-disable-next-line no-var
+  var __property_jsdom: unknown
+}
 
 it('jsdom', () => {
   expect(window).toBeDefined()
@@ -29,84 +37,83 @@ it('defined on self/window are defined on global', () => {
   expect(self).toBeDefined()
   expect(window).toBeDefined()
 
-  expect(self.__property).not.toBeDefined()
-  expect(window.__property).not.toBeDefined()
-  expect(globalThis.__property).not.toBeDefined()
+  expect(self.__property_jsdom).not.toBeDefined()
+  expect(window.__property_jsdom).not.toBeDefined()
+  expect(globalThis.__property_jsdom).not.toBeDefined()
 
-  globalThis.__property = 'defined_value'
+  globalThis.__property_jsdom = 'defined_value'
 
-  expect(__property).toBe('defined_value')
-  expect(self.__property).toBe('defined_value')
-  expect(window.__property).toBe('defined_value')
-  expect(globalThis.__property).toBe('defined_value')
+  expect(__property_jsdom).toBe('defined_value')
+  expect(self.__property_jsdom).toBe('defined_value')
+  expect(window.__property_jsdom).toBe('defined_value')
+  expect(globalThis.__property_jsdom).toBe('defined_value')
 
-  self.__property = 'test_value'
+  self.__property_jsdom = 'test_value'
 
-  expect(__property).toBe('test_value')
-  expect(self.__property).toBe('test_value')
-  expect(window.__property).toBe('test_value')
-  expect(globalThis.__property).toBe('test_value')
+  expect(__property_jsdom).toBe('test_value')
+  expect(self.__property_jsdom).toBe('test_value')
+  expect(window.__property_jsdom).toBe('test_value')
+  expect(globalThis.__property_jsdom).toBe('test_value')
 
-  window.__property = 'new_value'
+  window.__property_jsdom = 'new_value'
 
-  expect(__property).toBe('new_value')
-  expect(self.__property).toBe('new_value')
-  expect(window.__property).toBe('new_value')
-  expect(globalThis.__property).toBe('new_value')
+  expect(__property_jsdom).toBe('new_value')
+  expect(self.__property_jsdom).toBe('new_value')
+  expect(window.__property_jsdom).toBe('new_value')
+  expect(globalThis.__property_jsdom).toBe('new_value')
 
-  globalThis.__property = 'global_value'
+  globalThis.__property_jsdom = 'global_value'
 
-  expect(__property).toBe('global_value')
-  expect(self.__property).toBe('global_value')
-  expect(window.__property).toBe('global_value')
-  expect(globalThis.__property).toBe('global_value')
+  expect(__property_jsdom).toBe('global_value')
+  expect(self.__property_jsdom).toBe('global_value')
+  expect(window.__property_jsdom).toBe('global_value')
+  expect(globalThis.__property_jsdom).toBe('global_value')
 
   const obj = {}
 
-  self.__property = obj
+  self.__property_jsdom = obj
 
-  expect(self.__property).toBe(obj)
-  expect(window.__property).toBe(obj)
-  expect(globalThis.__property).toBe(obj)
+  expect(self.__property_jsdom).toBe(obj)
+  expect(window.__property_jsdom).toBe(obj)
+  expect(globalThis.__property_jsdom).toBe(obj)
 })
 
 it('usage with defineProperty', () => {
-  Object.defineProperty(self, '__property', {
+  Object.defineProperty(self, '__property_jsdom', {
     get: () => 'self_property',
     configurable: true,
   })
 
-  expect(__property).toBe('self_property')
-  expect(self.__property).toBe('self_property')
-  expect(globalThis.__property).toBe('self_property')
-  expect(window.__property).toBe('self_property')
+  expect(__property_jsdom).toBe('self_property')
+  expect(self.__property_jsdom).toBe('self_property')
+  expect(globalThis.__property_jsdom).toBe('self_property')
+  expect(window.__property_jsdom).toBe('self_property')
 
-  Object.defineProperty(window, '__property', {
+  Object.defineProperty(window, '__property_jsdom', {
     get: () => 'window_property',
     configurable: true,
   })
 
-  expect(__property).toBe('window_property')
-  expect(self.__property).toBe('window_property')
-  expect(globalThis.__property).toBe('window_property')
-  expect(window.__property).toBe('window_property')
+  expect(__property_jsdom).toBe('window_property')
+  expect(self.__property_jsdom).toBe('window_property')
+  expect(globalThis.__property_jsdom).toBe('window_property')
+  expect(window.__property_jsdom).toBe('window_property')
 
-  Object.defineProperty(globalThis, '__property', {
+  Object.defineProperty(globalThis, '__property_jsdom', {
     get: () => 'global_property',
     configurable: true,
   })
 
-  expect(__property).toBe('global_property')
-  expect(self.__property).toBe('global_property')
-  expect(globalThis.__property).toBe('global_property')
-  expect(window.__property).toBe('global_property')
+  expect(__property_jsdom).toBe('global_property')
+  expect(self.__property_jsdom).toBe('global_property')
+  expect(globalThis.__property_jsdom).toBe('global_property')
+  expect(window.__property_jsdom).toBe('global_property')
 })
 
 it('can call global functions without window works as expected', async () => {
   const noop = vi.fn()
 
   expect(() => addEventListener('abort', noop)).not.toThrow()
-  expect(() => scrollTo()).not.toThrow()
   expect(() => requestAnimationFrame(noop)).not.toThrow()
   expect(() => window.requestAnimationFrame(noop)).not.toThrow()
   expect(() => self.requestAnimationFrame(noop)).not.toThrow()
