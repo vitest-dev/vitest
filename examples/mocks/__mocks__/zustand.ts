a61e316abdeaf3ec038489b3c8fa9939af0646f9
Vladimir Sheremet <sheremet.va@icloud.com>
2022-05-09 10:46:59 +0300

    test: fix test for zustand

--- examples/mocks/__mocks__/zustand.ts
+++ examples/mocks/__mocks__/zustand.ts
@@ -1,13 +1,8 @@
 import actualCreate from 'zustand'
 
-// a variable to hold reset functions for all stores declared in the app
-const storeResetFns = new Set()
-
 // when creating a store, we get its initial state, create a reset function and add it in the set
 const create = vi.fn((createState) => {
   const store = actualCreate(createState)
-  const initialState = store.getState()
-  storeResetFns.add(() => store.setState(initialState, true))
   return store
 })
 
