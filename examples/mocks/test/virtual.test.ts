c491fb448ad1e8020fa6c6936fb3c98ad12966d2
Vladimir <sheremet.va@icloud.com>
2022-05-13 16:47:30 +0300

    fix: can mock non-existent paths and virtual modules via __mocks__ (#1298)

---
+++ examples/mocks/test/virtual.test.ts
@@ -0,0 +1,24 @@
+// @ts-expect-error vscode is not installed
+import * as vscodeMocks from 'vscode-mocks'
+// @ts-expect-error vscode is not installed
+import * as vscodeFactory from 'vscode-factory'
+// @ts-expect-error virtual module
+import * as virtual from 'virtual-module'
+
+vi.mock('vscode-mocks')
+vi.mock('vscode-factory', () => {
+  return { factory: true }
+})
+vi.mock('virtual-module')
+
+it('mocks not installed in mocks folder', () => {
+  expect(vscodeMocks.folder).toBe(true)
+})
+
+it('mocks not installed in mocks factory', () => {
+  expect(vscodeFactory.factory).toBe(true)
+})
+
+it('mocks virtual modules in mocks folder', () => {
+  expect(virtual.value).toBe('folder')
+})
