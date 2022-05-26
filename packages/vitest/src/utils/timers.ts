8878347c3f79ff7ce9f096a479d39e891500a992
Vladimir Sheremet <sheremet.va@icloud.com>
2022-05-26 19:56:19 +0300

    fix: birpc using safe timers

--- packages/vitest/src/utils/timers.ts
+++ packages/vitest/src/utils/timers.ts
@@ -1,4 +1,37 @@
-export const setTimeout = globalThis.setTimeout
-export const setInterval = globalThis.setInterval
-export const clearInterval = globalThis.clearInterval
-export const clearTimeout = globalThis.clearTimeout
+const {
+  setTimeout: safeSetTimeout,
+  setInterval: safeSetInterval,
+  clearInterval: safeClearInterval,
+  clearTimeout: safeClearTimeout,
+} = globalThis
+
+export {
+  safeSetTimeout as setTimeout,
+  safeSetInterval as setInterval,
+  safeClearInterval as clearInterval,
+  safeClearTimeout as clearTimeout,
+}
+
+// can only work with sync code to not potentially mess with other code
+export function withSafeTimers(fn: () => void) {
+  const currentSetTimeout = globalThis.setTimeout
+  const currentSetInterval = globalThis.setInterval
+  const currentClearInterval = globalThis.clearInterval
+  const currentClearTimeout = globalThis.clearTimeout
+
+  try {
+    globalThis.setTimeout = safeSetTimeout
+    globalThis.setInterval = safeSetInterval
+    globalThis.clearInterval = safeClearInterval
+    globalThis.clearTimeout = safeClearTimeout
+
+    const result = fn()
+    return result
+  }
+  finally {
+    globalThis.setTimeout = currentSetTimeout
+    globalThis.setInterval = currentSetInterval
+    globalThis.clearInterval = currentClearInterval
+    globalThis.clearTimeout = currentClearTimeout
+  }
+}
