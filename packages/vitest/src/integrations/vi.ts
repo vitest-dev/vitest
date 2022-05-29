30e844aecac9721514d893dd435dce92fbb8811c
Anthony Fu <anthonyfu117@hotmail.com>
2022-05-29 14:49:27 +0800

    chore: try fix dynamicImportSettled (#1392)

--- packages/vitest/src/integrations/vi.ts
+++ packages/vitest/src/integrations/vi.ts
@@ -244,13 +244,13 @@ class VitestUtils {
     const state = getWorkerState()
     const promises: Promise<unknown>[] = []
     for (const mod of state.moduleCache.values()) {
-      if (!mod.exports && mod.promise)
+      if (mod.promise)
         promises.push(mod.promise)
     }
-    await Promise.all(promises)
+    await Promise.allSettled(promises)
     // wait until the end of the loop, so `.then` on modules called,
     // like in import('./example').then(...)
-    await new Promise(resolve => setTimeout(resolve)).then(() => Promise.resolve())
+    await new Promise(resolve => setTimeout(resolve, 1)).then(() => Promise.resolve())
   }
 }
 
