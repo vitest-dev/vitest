a52d421187e4a6f278abe6bf7764b15df0b785a6
Anthony Fu <anthonyfu117@hotmail.com>
2022-06-05 23:18:44 +0800

    chore: update

--- packages/vitest/src/types/tasks.ts
+++ packages/vitest/src/types/tasks.ts
@@ -50,7 +50,7 @@ export interface Test<ExtraContext = {}> extends TaskBase {
 export type Task = Test | Suite | File
 
 export type DoneCallback = (error?: any) => void
-export type TestFunction<ExtraContext = {}> = (context: TestContext & ExtraContext) => Awaitable<any>
+export type TestFunction<ExtraContext = {}> = (context: TestContext & ExtraContext) => Awaitable<any> | void
 
 // jest's ExtractEachCallbackArgs
 type ExtractEachCallbackArgs<T extends ReadonlyArray<any>> = {
