b3b081dab32c8a4ca61f053f5b643650ca77d5d1
yoho <panyoho@gmail.com>
2022-06-20 23:12:39 +0800

    chore: update

--- test/vite-node/src/deps.ts
+++ test/vite-node/src/deps.ts
@@ -3,7 +3,9 @@ console.log('deps')
 
 export {}
 
-import.meta.hot && import.meta.hot.accept(() => {
-  // eslint-disable-next-line no-console
-  console.log('[deps.ts] hot reload!')
-})
+if (import.meta.hot) {
+  import.meta.hot.accept(() => {
+    // eslint-disable-next-line no-console
+    console.log('[deps.ts] hot reload!')
+  })
+}
