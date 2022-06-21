73a722ffeeb8f1fbfbec3657048c9285b241e020
Vladimir Sheremet <sheremet.va@icloud.com>
2022-06-21 11:48:53 +0300

    chore: generate inline snapshot test instead of using one

--- test/snapshots/test-update/validate-inline-js.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'fs/promises'
-
-export async function validateNoSnapshots(filepath: string) {
-  const content = await fs.readFile(filepath, 'utf8')
-
-  if (content.includes('toMatchInlineSnapshot(`'))
-    expect.fail('snapshots should not be generated before running test')
-}
