0e6a9a54516b89d927ad89e39a3e98a8e2baadcc
Elliot Westlake <17042376+elliotwestlake@users.noreply.github.com>
2022-07-04 22:28:24 +0100

    fix lock and add timeout to tests

--- test/related/tests/force-rerun.test.ts
+++ test/related/tests/force-rerun.test.ts
@@ -15,10 +15,10 @@ describe('forceRerunTrigger', () => {
     writeFile(fileName, '', error => console.error(error))
     const { stdout } = await run()
     expect(stdout).toContain('1 passed')
-  })
+  }, 60_000)
 
   it('should run no tests if file does not exist', async () => {
     const { stdout } = await run()
     expect(stdout).toContain('No test files found, exiting with code 0')
-  })
+  }, 60_000)
 })
