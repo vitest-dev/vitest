5caf10923ca934dfdaf6a0a78e29af1a7870c8f8
Puru Vijay <devpuruvj@gmail.com>
2022-07-08 23:29:32 +0530

    Update example with latest sveltekit

--- examples/sveltekit/vite.config.js
+++ examples/sveltekit/vite.config.js
@@ -1,5 +1,5 @@
 /// <reference types="vitest" />
-import { sveltekit } from '@sveltejs/kit/experimental/vite'
+import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
