4aeeee092e49dcf56a1c2d6f9408256ef4498257
Simon Abbott <simon.abbott@airfordable.com>
2022-07-16 10:08:10 -0500

    fix: mock properties of classes/functions (fix #1523) (#1648)

--- test/core/test/mocked.test.ts
+++ test/core/test/mocked.test.ts
@@ -1,21 +1,22 @@
-import { assert, expect, test, vi, vitest } from 'vitest'
+import { assert, describe, expect, test, vi, vitest } from 'vitest'
 // @ts-expect-error not typed module
 import { value as virtualValue } from 'virtual-module'
 import { two } from '../src/submodule'
 import * as mocked from '../src/mockedA'
 import { mockedB } from '../src/mockedB'
+import { MockedC, asyncFunc } from '../src/mockedC'
 import * as globalMock from '../src/global-mock'
 
 vitest.mock('../src/submodule')
-vitest.mock('virtual-module', () => {
-  return { value: 'mock' }
-})
+vitest.mock('virtual-module', () => ({ value: 'mock' }))
+vitest.mock('../src/mockedC')
 
 test('submodule is mocked to return "two" as 3', () => {
   assert.equal(3, two)
 })
 
 test('globally mocked files are mocked', () => {
+  // Mocked in setup.ts
   expect(globalMock.mocked).toBe(true)
 })
 
@@ -30,4 +31,35 @@ test('can mock esm', () => {
 
 test('mocked exports should override original exports', () => {
   expect(virtualValue).toBe('mock')
+})
+
+describe('mocked classes', () => {
+  test('should not delete the prototype', () => {
+    expect(MockedC).toBeTypeOf('function')
+    expect(MockedC.prototype.doSomething).toBeTypeOf('function')
+  })
+
+  test('should mock the constructor', () => {
+    const instance = new MockedC()
+
+    expect(instance.value).not.toBe(42)
+    expect(MockedC).toHaveBeenCalledOnce()
+  })
+
+  test('should mock functions in the prototype', () => {
+    const instance = new MockedC()
+
+    expect(instance.doSomething).toBeTypeOf('function')
+    expect(instance.doSomething()).not.toBe('A')
+
+    expect(MockedC.prototype.doSomething).toHaveBeenCalledOnce()
+    expect(MockedC.prototype.doSomething).not.toHaveReturnedWith('A')
+  })
+})
+
+test('async functions should be mocked', () => {
+  expect(asyncFunc()).toBeUndefined()
+  expect(vi.mocked(asyncFunc).mockResolvedValue).toBeDefined()
+  vi.mocked(asyncFunc).mockResolvedValue('foo')
+  expect(asyncFunc()).resolves.toBe('foo')
 })
