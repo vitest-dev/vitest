8aafe5624dc3e204a2208e715d5d3c8f51ca7d9b
Adam <adam@haglund.dev>
2022-07-27 17:33:17 +0200

    dont allow buildCommand to be overridden

--- packages/vitest/src/integrations/env/miniflare.ts
+++ packages/vitest/src/integrations/env/miniflare.ts
@@ -1,10 +1,10 @@
-import type { Environment } from '../../types'
+import type { Environment, EnvironmentOptions } from '../../types'
 import { populateGlobal } from './utils'
 
 // https://miniflare.dev/testing/ava#isolated-tests
 export default <Environment>({
   name: 'miniflare',
-  async setup(global, { miniflare: options }) {
+  async setup(global, { miniflare: options }: { miniflare: EnvironmentOptions['miniflare'] }) {
     const { Miniflare } = await import('miniflare')
 
     const miniflare = new Miniflare({
@@ -12,20 +12,22 @@ export default <Environment>({
       envPath: true,
       packagePath: true,
       wranglerConfigPath: true,
-      // We don't want to rebuild our worker for each test, we're already doing
-      // it once before we run all tests in package.json, so disable it here.
-      // This will override the option in wrangler.toml.
-      buildCommand: undefined,
 
       sourceMap: true,
       scriptRequired: false,
 
       // Disable checks that we're running inside a request handler
+      // https://github.com/cloudflare/miniflare/issues/292#issuecomment-1165450037
       globalTimers: true,
       globalAsyncIO: true,
       globalRandom: true,
 
       ...options,
+
+      // We don't want to rebuild our worker for each test, we're already doing
+      // it once before we run all tests in package.json, so disable it here.
+      // This will override the option in wrangler.toml.
+      buildCommand: undefined,
     })
 
     populateGlobal(global, { miniflare })
