1d98205478f857ba227de6fa154580fbbb466336
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2022-08-05 18:34:19 +0300

    chore: disable Node Loader tests

--- test/esm/test/executes.spec.ts
+++ test/esm/test/executes.spec.ts
@@ -2,7 +2,8 @@ import { __assign } from 'tslib'
 import { parse } from 'css-what'
 import { expect, test } from 'vitest'
 
-test('imported libs have incorrect ESM, but still work', () => {
+// TODO check on Linux Node 14
+test.skip('imported libs have incorrect ESM, but still work', () => {
   expect(__assign({}, { a: 1 })).toEqual({ a: 1 })
   expect(parse('a')).toBeDefined()
 })
