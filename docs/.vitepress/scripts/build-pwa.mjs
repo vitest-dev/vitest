3deb25bd52ef66bd08a13e87bb0e6982beeceb10
Anthony Fu <anthonyfu117@hotmail.com>
2022-08-15 17:31:28 +0800

    chore: upgrade esno

--- docs/.vitepress/scripts/build-pwa.mjs
+++ docs/.vitepress/scripts/build-pwa.mjs
@@ -1,13 +1,14 @@
+import { fileURLToPath } from 'url'
 import { resolveConfig } from 'vite'
-import type { VitePluginPWAAPI } from 'vite-plugin-pwa'
-import { optimizePages } from './assets'
+import jiti from 'jiti'
 
 const rebuildPwa = async () => {
   const config = await resolveConfig({}, 'build', 'production')
   // when `vite-plugin-pwa` is presented, use it to regenerate SW after rendering
-  const pwaPlugin: VitePluginPWAAPI = config.plugins.find(i => i.name === 'vite-plugin-pwa')?.api
+  const pwaPlugin = config.plugins.find(i => i.name === 'vite-plugin-pwa')?.api
   const pwa = pwaPlugin && !pwaPlugin.disabled
-  await optimizePages(pwa)
+  const assets = jiti(fileURLToPath(import.meta.url))('./assets.ts')
+  await assets.optimizePages(pwa)
   if (pwa)
     await pwaPlugin.generateSW()
 }
