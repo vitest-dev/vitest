4ff81ae86fce7c27e49d678d4a8f70407952742f
Anthony Fu <anthonyfu117@hotmail.com>
2022-08-16 00:51:27 +0800

    chore: update

--- packages/coverage-c8/rollup.config.js
+++ packages/coverage-c8/rollup.config.js
@@ -16,6 +16,8 @@ const external = [
   ...builtinModules,
   ...Object.keys(pkg.dependencies || {}),
   ...Object.keys(pkg.peerDependencies || {}),
+  'c8/lib/report.js',
+  'c8/lib/commands/check-coverage.js',
   'vitest',
   'vitest/node',
   'vitest/config',
