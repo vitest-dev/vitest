ab7f008367ef5ba326d07ea0e15e2e0071f20d7e
Anthony Fu <anthonyfu117@hotmail.com>
2022-08-29 17:18:38 +0800

    chore: fix test config

--- test/reporters/vitest.config.ts
+++ test/reporters/vitest.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    exclude: [
-      'fixtures/**',
-    ],
+    exclude: ['node_modules', 'fixtures', 'dist'],
+    testTimeout: 100000,
   },
 })
