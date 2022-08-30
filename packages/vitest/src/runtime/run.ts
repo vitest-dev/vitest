1acde93d7bd75a9236f5fcd78203074ccbcac91c
yoho <panyoho@gmail.com>
2022-08-30 13:13:58 +0800

    chore: update sort rules

--- packages/vitest/src/runtime/run.ts
+++ packages/vitest/src/runtime/run.ts
@@ -351,7 +351,7 @@ async function runBenchmarkSuit(suite: Suite) {
       suite.result!.state = 'pass'
 
       benchmarkInstance.tasks
-        .sort((a, b) => b.result!.hz - a.result!.hz)
+        .sort((a, b) => b.result!.mean - a.result!.mean)
         .forEach((cycle, idx) => {
           const benchmark = benchmarkMap[cycle.name || '']
           if (benchmark) {
