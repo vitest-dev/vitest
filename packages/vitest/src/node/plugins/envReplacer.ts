51824896ba5c6dacab8610a2c8aa8c4a8e7e39fc
Tony Trinh <tony19@gmail.com>
2022-09-01 05:32:37 -0500

    fix(env-replacer): don't modify string literals (#1943)
    * fix(env-replacer): don't modify string literals
    
    fix #1941
    
    * test(env-replacer): verify string literals are ignored
    
    * chore: fix typo envRelacer.ts -> envReplacer.ts
    
    * chore: fix typo envRelacer.ts -> envReplacer.ts
    
    * chore: move strip-literal to correct package

--- packages/vitest/src/node/plugins/envReplacer.ts
+++ packages/vitest/src/node/plugins/envReplacer.ts
@@ -1,5 +1,6 @@
 import MagicString from 'magic-string'
 import type { Plugin } from 'vite'
+import { stripLiteral } from 'strip-literal'
 
 // so people can reassign envs at runtime
 // import.meta.env.VITE_NAME = 'app' -> process.env.VITE_NAME = 'app'
@@ -8,9 +9,11 @@ export const EnvReplacerPlugin = (): Plugin => {
     name: 'vitest:env-replacer',
     enforce: 'pre',
     transform(code) {
-      let s: MagicString | null = null
+      if (!/\bimport\.meta\.env\b/g.test(code))
+        return null
 
-      const envs = code.matchAll(/\bimport\.meta\.env\b/g)
+      let s: MagicString | null = null
+      const envs = stripLiteral(code).matchAll(/\bimport\.meta\.env\b/g)
 
       for (const env of envs) {
         s ||= new MagicString(code)
