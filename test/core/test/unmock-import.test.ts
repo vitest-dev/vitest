03f7450957de6d18826040c7c5a1ea98712b251c
cyly <786156072@qq.com>
2022-09-02 22:08:38 +0800

    fix: delete mock in moduleCache when unmock (#1947)

---
+++ test/core/test/unmock-import.test.ts
@@ -0,0 +1,26 @@
+import { afterEach, beforeEach, expect, test, vi } from 'vitest'
+
+// https://github.com/vitest-dev/vitest/issues/1940
+beforeEach(() => {
+  vi.doMock('/data', () => ({
+    data: {
+      state: 'STARTED',
+    },
+  }))
+})
+afterEach(() => {
+  vi.doUnmock('/data')
+})
+
+test('first import', async () => {
+  // @ts-expect-error I know this
+  const { data } = await import('/data')
+  data.state = 'STOPPED'
+  expect(data.state).toBe('STOPPED')
+})
+
+test('second import should had been re-mock', async () => {
+  // @ts-expect-error I know this
+  const { data } = await import('/data')
+  expect(data.state).toBe('STARTED')
+})
