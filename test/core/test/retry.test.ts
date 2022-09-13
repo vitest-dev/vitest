6ed7c2418b272594d29a72bf7486d16e3de60442
Bartosz Golebiowski <bartosz.golebiowski24@gmail.com>
2022-09-13 11:37:03 +0200

    fix: propagate `describe` options (#2025)
    close: #1998
    
    Co-authored-by: golebiowskib <bartosz.golebiowski@@ttpsc.pl>

--- test/core/test/retry.test.ts
+++ test/core/test/retry.test.ts
@@ -1,4 +1,4 @@
-import { expect, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
 
 let count1 = 0
 it('retry test', () => {
@@ -23,3 +23,44 @@ it('result', () => {
   expect(count2).toEqual(2)
   expect(count3).toEqual(3)
 })
+
+describe('description retry', () => {
+  let count4 = 0
+  let count5 = 0
+  it('test should inherit options from the description block if missing', () => {
+    count4 += 1
+    expect(count4).toBe(2)
+  })
+
+  it('test should not inherit options from the description block if exists', () => {
+    count5 += 1
+    expect(count5).toBe(5)
+  }, { retry: 5 })
+}, { retry: 2 })
+
+describe.each([
+  { a: 1, b: 1, expected: 2 },
+  { a: 1, b: 2, expected: 3 },
+  { a: 2, b: 1, expected: 3 },
+])('describe object add($a, $b)', ({ a, b, expected }) => {
+  let flag1 = false
+  let flag2 = false
+  let flag3 = false
+  it(`returns ${expected}`, () => {
+    flag1 = !flag1
+    expect(a + b).toBe(expected)
+    expect(flag1).toBe(false)
+  })
+
+  it(`returned value not be greater than ${expected}`, () => {
+    flag2 = !flag2
+    expect(a + b).not.toBeGreaterThan(expected)
+    expect(flag2).toBe(false)
+  })
+
+  it(`returned value not be less than ${expected}`, () => {
+    flag3 = !flag3
+    expect(a + b).not.toBeLessThan(expected)
+    expect(flag3).toBe(false)
+  })
+}, { retry: 2 })
