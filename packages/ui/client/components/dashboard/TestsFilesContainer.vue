4a3f76e474904e35efb3c52bbbee75937813008b
webfansplz <308241863@qq.com>
2022-10-11 15:46:33 +0800

    feat(ui): add tests summary in the dashboard

--- packages/ui/client/components/dashboard/TestsFilesContainer.vue
+++ packages/ui/client/components/dashboard/TestsFilesContainer.vue
@@ -1,9 +1,9 @@
 <template>
   <div gap-0 flex="~ col gap-4" h-full justify-center items-center>
     <div bg-header rounded-lg p="y4 x2">
-      <!-- <section aria-labelledby="tests" m="y-4 x-2">
+      <section aria-labelledby="tests" m="y-4 x-2">
         <TestsEntry />
-      </section> -->
+      </section>
       <TestFilesEntry />
     </div>
   </div>
