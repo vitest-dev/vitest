7808f5ea4e0ab060ce1f5033edd2084c972e40a8
Joaquín Sánchez <joaquin.sanchez@fi2net.es>
2022-10-13 12:03:32 +0200

    chore: clenup comments

--- packages/vitest/src/utils/index.ts
+++ packages/vitest/src/utils/index.ts
@@ -14,11 +14,7 @@ export * from './tasks'
 export * from './base'
 export * from './global'
 export * from './timers'
-
-export { isNode, isBrowser } from './env'
-// export const isNode = typeof process < 'u' && typeof process.stdout < 'u' && !process.versions?.deno && !globalThis.window
-// export const isNode = typeof process !== 'undefined' && typeof process.platform !== 'undefined'
-// export const isBrowser = typeof window !== 'undefined'
+export * from './env'
 
 export const isWindows = isNode && process.platform === 'win32'
 export const getRunMode = () => getWorkerState().config.mode
