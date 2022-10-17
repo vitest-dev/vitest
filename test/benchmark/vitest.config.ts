e7a99a616946a8887d0119404cbf568cbb4d65b2
kainstar <461345042@qq.com>
2022-10-17 17:23:53 +0800

    fix: ignore benchmark.reporters in getSerializableConfig and add test case

--- test/benchmark/vitest.config.ts
+++ test/benchmark/vitest.config.ts
@@ -1,12 +1,25 @@
 import { defineConfig } from 'vitest/config'
 
+const noop = () => {}
+
 export default defineConfig({
   test: {
     update: false,
     allowOnly: true,
     benchmark: {
       outputFile: './bench.json',
-      reporters: ['json'],
+      reporters: ['json', {
+        onInit: noop,
+        onPathsCollected: noop,
+        onCollected: noop,
+        onFinished: noop,
+        onTaskUpdate: noop,
+        onTestRemoved: noop,
+        onWatcherStart: noop,
+        onWatcherRerun: noop,
+        onServerRestart: noop,
+        onUserConsoleLog: noop,
+      }],
     },
   },
 })
