f41d6ef7e43cc956a5bb7e5ee2d89e35ab0d410a
yoho <panyoho@gmail.com>
2022-10-21 12:09:35 +0800

    fix: window

--- packages/vitest/src/node/reporters/utils.ts
+++ packages/vitest/src/node/reporters/utils.ts
@@ -1,4 +1,5 @@
 import type { ViteNodeRunner } from 'vite-node/client'
+import { normalize } from 'pathe'
 import type { Reporter } from '../../types'
 import { BenchmarkReportsMap, ReportersMap } from './index'
 import type { BenchmarkBuiltinReporters, BuiltinReporters } from './index'
@@ -6,7 +7,7 @@ import type { BenchmarkBuiltinReporters, BuiltinReporters } from './index'
 async function loadCustomReporterModule<C extends Reporter>(path: string, runner: ViteNodeRunner): Promise<new () => C> {
   let customReporterModule: { default: new () => C }
   try {
-    customReporterModule = await runner.executeId(`/@fs/${path}`)
+    customReporterModule = await runner.executeId(normalize(`/@fs/${path}`))
   }
   catch (customReporterModuleError) {
     throw new Error(`Failed to load custom Reporter from ${path}`, { cause: customReporterModuleError as Error })
