0a33ef9e8005a455b8d20675fb27ab13ed84ab0b
yoho <907415276@qq.com>
2022-10-22 17:47:56 +0800

    chore: update
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- packages/vitest/src/node/reporters/utils.ts
+++ packages/vitest/src/node/reporters/utils.ts
@@ -7,7 +7,7 @@ import type { BenchmarkBuiltinReporters, BuiltinReporters } from './index'
 async function loadCustomReporterModule<C extends Reporter>(path: string, runner: ViteNodeRunner): Promise<new () => C> {
   let customReporterModule: { default: new () => C }
   try {
-    customReporterModule = await runner.executeId(normalize(`/@fs/${path}`))
+    customReporterModule = await runner.executeFile(path)
   }
   catch (customReporterModuleError) {
     throw new Error(`Failed to load custom Reporter from ${path}`, { cause: customReporterModuleError as Error })
