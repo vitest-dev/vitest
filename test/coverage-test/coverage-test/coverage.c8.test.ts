ac2a34e2f4a5e96506a475f7c386ba70d09a94a1
OrestHk <hrynchuk.orest@gmail.com>
2022-10-25 19:38:24 +0200

    test: Add tests on c8 coverage report

--- test/coverage-test/coverage-test/coverage.c8.test.ts
+++ test/coverage-test/coverage-test/coverage.c8.test.ts
@@ -1,6 +1,23 @@
 import fs from 'fs'
+import { execa } from 'execa'
 import { resolve } from 'pathe'
 import { expect, test } from 'vitest'
+
+async function run(...runOptions: string[]): Promise<string> {
+  const root = resolve(__dirname, '..')
+
+  const { stdout } = await execa('npx', ['vitest', 'run', ...runOptions], {
+    cwd: root,
+    env: {
+      ...process.env,
+      CI: 'true',
+      NO_COLOR: 'true',
+    },
+    windowsHide: false,
+  })
+
+  return stdout
+}
 
 test('coverage c8', async () => {
   const coveragePath = resolve('./coverage/tmp/')
@@ -8,4 +25,18 @@ test('coverage c8', async () => {
   expect(stat.isDirectory()).toBe(true)
   const files = fs.readdirSync(coveragePath)
   expect(files.length > 0).toBe(true)
+})
+
+test('Should show coverage', async () => {
+  const stdout = await run('--config', 'vitest.config-c8-coverage.ts', '--coverage')
+
+  // For Vue SFC and vue + ts files
+  expect(stdout).contain('not-SFC.ts')
+  expect(stdout).not.contain('not-SFC.ts?vue')
+  expect(stdout).contain('not-SFC.vue')
+  expect(stdout).contain('SFC.vue')
+
+  // For ts and js files
+  expect(stdout).contain('math.ts')
+  expect(stdout).contain('utils.js')
 })
--- test/coverage-test/vitest.config-c8-coverage.ts
+++ test/coverage-test/vitest.config-c8-coverage.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  plugins: [vue()],
   test: {
     include: [
       './coverage-test/*.c8.test.ts',
+      './coverage-test/c8/**/*test.ts',
     ],
+    coverage: {
+      include: ['src/**'],
+      extension: ['.ts', '.vue', '.js'],
+    },
   },
 })
