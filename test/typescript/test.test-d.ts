bdff5e479c0e865fcdf008d02568cebdfcf453db
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2022-10-27 09:13:41 +0200

    chore: fix types

--- test/typescript/test.test-d.ts
+++ test/typescript/test.test-d.ts
@@ -2,15 +2,12 @@ import { describe, expectTypeOf, test } from 'vitest'
 
 describe('test', () => {
   test('some-test', () => {
-    expectTypeOf(45).toBe(45)
-    // expectTypeOf(45).toBe<Thor['name.first']>(80)
-    // expectTypeOf(45).toBe<Thor['name.first.second.third']>(80)
-    // expectTypeOf(45).toBe('22')
+    expectTypeOf(45).toEqualTypeOf(45)
   })
 
   describe('test2', () => {
     test('some-test 2', () => {
-      expectTypeOf(45).toBe(45)
+      expectTypeOf(45).toEqualTypeOf(45)
     })
   })
 })
