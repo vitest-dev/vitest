798246445f55c75e5b394347d3e350ce77bff61b
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2022-11-21 17:00:05 +0100

    chore: test "broken" ESM packages

--- test/esm/test/executes.spec.ts
+++ test/esm/test/executes.spec.ts
@@ -3,7 +3,7 @@ import { parse } from 'css-what'
 import { expect, test } from 'vitest'
 
 // TODO check on Linux Node 14
-test.skip('imported libs have incorrect ESM, but still work', () => {
+test('imported libs have incorrect ESM, but still work', () => {
   expect(__assign({}, { a: 1 })).toEqual({ a: 1 })
   expect(parse('a')).toBeDefined()
 })
