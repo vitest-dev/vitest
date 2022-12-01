623293f594743bc885ca1a03ea7f90546d4546f7
yoho <panyoho@gmail.com>
2022-12-01 15:33:09 +0800

    docs: format title

--- test/core/test/each.test.ts
+++ test/core/test/each.test.ts
@@ -9,6 +9,24 @@ test.each([
 })
 
 test.each([
+  [1, 1, 2],
+  [1, 2, 3],
+  [2, 1, 3],
+])(
+  (a, b, expected) => `${a} + ${b} = ${expected}`,
+  (a, b, expected) => expect(a + b).toBe(expected),
+)
+
+test.each([
+  { a: 1, b: 1, expected: 2 },
+  { a: 1, b: 2, expected: 3 },
+  { a: 2, b: 1, expected: 3 },
+])(
+  ({ a, b, expected }) => `${a} + ${b} = ${expected}`,
+  ({ a, b, expected }) => expect(a + b).toBe(expected),
+)
+
+test.each([
   ['string', true],
   ['string', false],
 ])('can be parsed', (a, b) => {
