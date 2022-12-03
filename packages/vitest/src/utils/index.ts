45c5c45ab0b36a0077e318bff1b638829158538f
yoho <panyoho@gmail.com>
2022-12-03 17:32:58 +0800

    feat: format test tablename (#2405)
    * feat: custom format test.each named
    
    * docs: format title
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: upadte
    
    * docs: update
    
    * chore: revert
    
    * chore: update
    
    * feat: object attr getter
    
    * docs: object getter

--- packages/vitest/src/utils/index.ts
+++ packages/vitest/src/utils/index.ts
@@ -180,3 +180,15 @@ export function createDefer<T>(): DeferPromise<T> {
   p.reject = reject!
   return p
 }
+
+export function objectAttr(source: any, path: string, defaultValue = undefined) {
+  // a[3].b -> a.3.b
+  const paths = path.replace(/\[(\d+)\]/g, '.$1').split('.')
+  let result = source
+  for (const p of paths) {
+    result = Object(result)[p]
+    if (result === undefined)
+      return defaultValue
+  }
+  return result
+}
