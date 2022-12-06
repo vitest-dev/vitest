d85c92c5aef3c18c64bd166ac09b8f5284ad3c02
yoho <panyoho@gmail.com>
2022-12-06 13:56:17 +0800

    chore: copy ui to vitest dist

---
+++ packages/vitest/scripts/copy-ui-to-vitest.ts
@@ -0,0 +1,19 @@
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import { resolve } from 'pathe'
+import fg from 'fast-glob'
+
+const root = resolve(fileURLToPath(import.meta.url), '../../../../packages')
+
+const ui = resolve(root, 'ui/dist/client')
+const vitest = resolve(root, 'vitest/dist/html-report/')
+
+const files = fg.sync('**/*', { cwd: ui })
+
+fs.mkdirSync(vitest)
+fs.mkdirSync(resolve(vitest, 'assets'))
+
+files.forEach((f) => {
+  fs.copyFileSync(resolve(ui, f), resolve(vitest, f))
+})
+
