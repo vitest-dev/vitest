9208d4cdfc53e0154724f8d2a5ae86c50bf2432a
yoho <panyoho@gmail.com>
2022-12-06 14:44:44 +0800

    fix: copy file

--- packages/vitest/scripts/copy-ui-to-vitest.ts
+++ packages/vitest/scripts/copy-ui-to-vitest.ts
@@ -9,9 +9,14 @@ const ui = resolve(root, 'ui/dist/report')
 const vitest = resolve(root, 'vitest/dist/html-report/')
 
 const files = fg.sync('**/*', { cwd: ui })
+const originFiles = fg.sync('**/*', { cwd: vitest })
 
-fs.mkdirSync(vitest)
-fs.mkdirSync(resolve(vitest, 'assets'))
+originFiles.forEach((f) => {
+  fs.unlinkSync(resolve(vitest, f))
+})
+
+if (!fs.existsSync(resolve(vitest, 'assets')))
+  fs.mkdirSync(resolve(vitest, 'assets'), { recursive: true })
 
 files.forEach((f) => {
   fs.copyFileSync(resolve(ui, f), resolve(vitest, f))
