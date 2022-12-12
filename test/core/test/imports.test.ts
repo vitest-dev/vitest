404c5daf4b38f0772929141cf9121c4756ffea54
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2022-12-12 12:49:20 +0100

    fix: don't throw unhandled error, if import was caught

--- test/core/test/imports.test.ts
+++ test/core/test/imports.test.ts
@@ -53,3 +53,9 @@ test('dynamic import has null prototype', async () => {
 
   expect(Object.getPrototypeOf(stringTimeoutMod)).toBe(null)
 })
+
+test('dynamic imports throws an error', async () => {
+  const path = './some-unknown-path'
+  const imported = import(path)
+  await expect(imported).rejects.toThrowError(/Failed to load/)
+})
