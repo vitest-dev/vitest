63d51362f4e28a08205aaf4751b65a852222b4dc
GurkiranSingh <gurkiransinghk@gmail.com>
2022-12-11 20:42:51 -0500

    chore: deleted example
    Signed-off-by: GurkiranSingh <gurkiransinghk@gmail.com>

--- examples/react-istanbul-coverage/src/Component.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import { expect, it } from "vitest";
-import { Component } from "./Component";
-
-it('should not render content', () => {
-    render(<Component>Content</Component>)
-    expect(screen.getByRole('button', {name: /Expand/}))
-    expect(screen.queryByText(/Content/)).toBeNull()
-})
