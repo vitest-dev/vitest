03feb9b053ef8627710f9ed57d4a79a3b98818ce
Trim21 <trim21.me@gmail.com>
2023-01-05 05:03:13 +0800

    extend basic reporter

--- packages/vitest/src/node/reporters/basic.ts
+++ packages/vitest/src/node/reporters/basic.ts
@@ -1,5 +1,27 @@
 import { DefaultReporter } from './default'
+import type { UserConsoleLog } from '#types'
 
 export class BasicReporter extends DefaultReporter {
   isTTY = false as const
+
+  async onTestRemoved() {
+  }
+
+  onCollected() {
+  }
+
+  async onFinished(files = this.ctx.state.getFiles(), errors = this.ctx.state.getUnhandledErrors()) {
+    await super.onFinished(files, errors)
+  }
+
+  async stopListRender() {
+    this.renderer = undefined
+  }
+
+  onUserConsoleLog(log: UserConsoleLog) {
+    if (!this.shouldLog(log))
+      return
+
+    super.onUserConsoleLog(log)
+  }
 }
