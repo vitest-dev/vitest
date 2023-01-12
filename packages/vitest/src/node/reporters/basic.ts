032a0328fb8b41025ee0c8bdf308d4f1f4aeb9e4
Trim21 <trim21.me@gmail.com>
2023-01-12 08:40:34 +0800

    fix lint

--- packages/vitest/src/node/reporters/basic.ts
+++ packages/vitest/src/node/reporters/basic.ts
@@ -10,14 +10,9 @@ export class BasicReporter extends BaseReporter {
   onCollected() {
   }
 
-  async onFinished(files = this.ctx.state.getFiles(), errors = this.ctx.state.getUnhandledErrors()) {
-    await super.onFinished(files, errors)
-  }
-
   onUserConsoleLog(log: UserConsoleLog) {
-    if (!this.shouldLog(log)) {
+    if (!this.shouldLog(log))
       return
-    }
 
     super.onUserConsoleLog(log)
   }
