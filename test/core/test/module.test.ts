c93a2e5573c86650170e9e512bc150d8ff343937
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2023-01-16 13:17:52 +0100

    revert: don't rely on environment for interopDefault

--- test/core/test/module.test.ts
+++ test/core/test/module.test.ts
@@ -30,17 +30,21 @@ describe('validating nested defaults in isolation', () => {
   it.each([
     nestedDefaultCjs,
     nestedDefaultExternalCjs,
-  ])('nested default should stay, because environment is node', (mod) => {
+  ])('nested default should be resolved, because environment is not node', (mod) => {
     expect(mod).toHaveProperty('default')
-    expect(mod.default).toHaveProperty('default')
-    expect(mod.default.default.a).toBe('a')
-    expect(mod.default.default.b).toBe('b')
+    expect(mod.default).not.toHaveProperty('default')
+    expect(mod.default.a).toBe('a')
+    expect(mod.default.b).toBe('b')
+    expect(mod.a).toBe('a')
+    expect(mod.b).toBe('b')
   })
 
-  it('don\'t interop external module.exports, because environment is node', () => {
+  it('externalized "module.exports" CJS module interops default', () => {
     expect(moduleDefaultCjs).toHaveProperty('default')
     expect(moduleDefaultCjs.default).toHaveProperty('a')
-    expect(moduleDefaultCjs).not.toHaveProperty('a')
+    expect(moduleDefaultCjs.default.a).toBe('a')
+    expect(moduleDefaultCjs).toHaveProperty('a')
+    expect(moduleDefaultCjs.a).toBe('a')
   })
 })
 
