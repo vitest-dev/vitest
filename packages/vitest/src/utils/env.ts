b5d1390824b572c03507b262473b4c3e3bf8b8a6
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2023-01-20 20:40:17 +0100

    refactor: use std-env instead of ci-info

--- packages/vitest/src/utils/env.ts
+++ packages/vitest/src/utils/env.ts
@@ -1,3 +1,3 @@
 export const isNode: boolean = typeof process < 'u' && typeof process.stdout < 'u' && !process.versions?.deno && !globalThis.window
 export const isBrowser: boolean = typeof window !== 'undefined'
-export { isCI } from 'ci-info'
+export { isCI } from 'std-env'
