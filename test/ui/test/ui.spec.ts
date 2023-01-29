df9cfc51d92967b6e4a2e99d89b6f1ecd08b8efc
yoho <panyoho@gmail.com>
2023-01-29 10:57:07 +0800

    fix: test timeout

--- test/ui/test/ui.spec.ts
+++ test/ui/test/ui.spec.ts
@@ -1,70 +1,53 @@
 import { resolve } from 'node:path'
-import { describe, expect, it } from 'vitest'
+import { expect, it } from 'vitest'
 import { execa } from 'execa'
 import { browserErrors, page, withRetry } from '../setup'
 
-describe('ui', () => {
-  const root = resolve(__dirname, '../fixtures')
+const root = resolve(__dirname, '../fixtures')
+const uiPort = '9527'
+const reportPort = '9528'
 
-  it('should load ui', async () => {
-    const port = '9527'
-    const ui = execa('npx', ['vitest', '--ui', '--api.port', port, '--open', 'false'], {
-      cwd: root,
-      env: {
-        ...process.env,
-        CI: 'true',
-        NO_COLOR: 'true',
-      },
-      stdio: 'inherit',
-    })
-
-    ui.catch(e => e)
-
-    await withRetry(async () => {
-      await page.goto(`http://localhost:${port}/__vitest__/`)
-    })
-    expect(await (await page.$('#app'))?.innerHTML()).not.toBe('')
-    expect(browserErrors.length).toEqual(0)
-
-    ui.cancel()
+it('should load ui', async () => {
+  const ui = execa('npx', ['vitest', '--ui', '--api.port', uiPort, '--open', 'false', '--reporter=html', '--outputFile=html/index.html'], {
+    cwd: root,
+    env: {
+      ...process.env,
+      CI: 'true',
+      NO_COLOR: 'true',
+    },
+    stdio: 'inherit',
   })
+
+  ui.catch(e => e)
+
+  await withRetry(async () => {
+    await page.goto(`http://localhost:${uiPort}/__vitest__/`)
+  })
+  expect(await (await page.$('#app'))?.innerHTML()).not.toBe('')
+  expect(browserErrors.length).toEqual(0)
+
+  ui.cancel()
 })
 
-describe('ui report', () => {
-  const root = resolve(__dirname, '../fixtures')
-  const port = '9528'
-
-  it('should load ui', async () => {
-    // build reporter
-    await execa('npx', ['vitest', 'run', '--reporter=html', '--outputFile=html/index.html'], {
-      cwd: root,
-      env: {
-        ...process.env,
-        CI: 'true',
-        NO_COLOR: 'true',
-      },
-      stdio: 'inherit',
-    }).catch(e => e)
-
-    // preview report
-    const html = execa('npx', ['vite', 'preview', '--outDir', 'html', '--port', port], {
-      cwd: root,
-      env: {
-        ...process.env,
-        CI: 'true',
-        NO_COLOR: 'true',
-      },
-      stdio: 'inherit',
-    })
-
-    html.catch(e => e)
-
-    await withRetry(async () => {
-      await page.goto(`http://localhost:${port}/__vitest__/`)
-    })
-    expect(await (await page.$('#app'))?.innerHTML()).not.toBe('')
-    expect(browserErrors.length).toEqual(0)
-
-    html.cancel()
+it('should load report', async () => {
+  // preview report
+  const html = execa('npx', ['vite', 'preview', '--outDir', 'html', '--port', reportPort], {
+    cwd: root,
+    env: {
+      ...process.env,
+      CI: 'true',
+      NO_COLOR: 'true',
+    },
+    stdio: 'inherit',
   })
+
+  html.catch(e => e)
+
+  await withRetry(async () => {
+    await page.goto(`http://localhost:${reportPort}/__vitest__/`)
+  })
+  expect(await (await page.$('#app'))?.innerHTML()).not.toBe('')
+  expect(browserErrors.length).toEqual(0)
+
+  html.cancel()
 })
