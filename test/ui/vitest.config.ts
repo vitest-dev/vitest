534aeaa5a15035df027a753c65fcda9ad58e3f28
yoho <panyoho@gmail.com>
2023-01-29 18:21:42 +0800

    chore: update config

--- test/ui/vitest.config.ts
+++ test/ui/vitest.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vitest/config'
 
-const timeout = process.env.CI ? 50000 : 30000
-
 export default defineConfig({
   test: {
     setupFiles: ['./setup.ts'],
     globalSetup: ['./globalSetup.ts'],
     exclude: ['node_modules', 'fixtures', 'dist'],
-    testTimeout: timeout,
-    hookTimeout: timeout,
   },
 })
