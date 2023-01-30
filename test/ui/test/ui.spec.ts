0867b346067a08cb5f3c7eba01a66f05076585fa
yoho <panyoho@gmail.com>
2023-01-30 11:20:08 +0800

    chore: update

--- test/ui/test/ui.spec.ts
+++ test/ui/test/ui.spec.ts
@@ -1,6 +1,5 @@
 import { resolve } from 'node:path'
 import { expect, it } from 'vitest'
-import kill from 'kill-port'
 import { execaCommand } from 'execa'
 import { browserErrors, killProcess, page, withLoadUrl } from '../setup'
 
@@ -8,8 +7,7 @@ const root = resolve(__dirname, '../fixtures')
 const uiPort = 9000
 const reportPort = 9001
 
-async function run(command: string, url: string, port: number) {
-  await kill(port)
+async function run(command: string, url: string) {
   let error: any
   const subProcess = execaCommand(command, {
     cwd: root,
@@ -46,7 +44,6 @@ it('should load ui', async () => {
   const kill = await run(
     `npx vitest --ui --open false --api.port ${uiPort} --reporter=html --outputFile=html/index.html`,
     `http://localhost:${uiPort}/__vitest__/`,
-    uiPort,
   )
   expect((await (await page.$('#app'))?.innerHTML() || '').length).not.toBe(0)
   expect(browserErrors.length).toEqual(0)
@@ -57,7 +54,6 @@ it('should load report', async () => {
   const kill = await run(
     `npx vite preview --outDir html --strict-port --base __vitest__ --port ${reportPort}`,
      `http://localhost:${reportPort}/__vitest__/`,
-     reportPort,
   )
   try {
     expect((await (await page.$('#app'))?.innerHTML() || '').length).not.toBe(0)
