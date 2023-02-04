efe65b2fa4a3766720b17d9085dd9d4cfa31d2af
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-04 11:20:37 -0800

    test: remove ts-expect-error from toHaveProperty test for array index number

--- test/core/test/jest-expect.test.ts
+++ test/core/test/jest-expect.test.ts
@@ -235,9 +235,7 @@ describe('jest-expect', () => {
     expect(complex).toHaveProperty('bar.arr.0')
     expect(complex).toHaveProperty(['bar', 'arr', '0'])
     expect(complex).toHaveProperty(['bar', 'arr', '0'], 'first')
-    // @ts-expect-error - passes number inside the array
     expect(complex).toHaveProperty(['bar', 'arr', 0])
-    // @ts-expect-error - passes number inside the array
     expect(complex).toHaveProperty(['bar', 'arr', 0], 'first')
     expect(complex).toHaveProperty('bar.arr.1.zoo', 'monkey')
     expect(complex).toHaveProperty(['bar', 'arr', '1', 'zoo'], 'monkey')
