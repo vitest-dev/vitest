a6c192f57672b8dba598a30c48edd55d66497bfb
yoho <panyoho@gmail.com>
2023-02-07 00:44:21 +0800

    test: format named

--- test/ui/test/ui.spec.ts
+++ test/ui/test/ui.spec.ts
@@ -1,5 +1,5 @@
 import { resolve } from 'node:path'
-import { beforeAll, expect, it } from 'vitest'
+import { beforeAll, describe, expect, it } from 'vitest'
 import { browserErrors, page, ports, startServerCommand, untilUpdated } from '../setup'
 
 const root = resolve(__dirname, '../fixtures')
@@ -23,18 +23,26 @@ it('dashboard', async () => {
   await untilUpdated(() => page.textContent('[aria-labelledby]'), '1 Pass 0 Fail 1 Total ')
 })
 
-it('file detail', async () => {
-  await page.click('.details-panel span')
+describe('file detail', async () => {
+  beforeAll(async () => {
+    await page.click('.details-panel span')
+  })
 
-  await page.click('[data-testid=btn-report]')
-  await untilUpdated(() => page.textContent('[data-testid=report]'), 'All tests passed in this file')
-  await untilUpdated(() => page.textContent('[data-testid=filenames]'), 'sample.test.ts')
+  it('report', async () => {
+    await page.click('[data-testid=btn-report]')
+    await untilUpdated(() => page.textContent('[data-testid=report]'), 'All tests passed in this file')
+    await untilUpdated(() => page.textContent('[data-testid=filenames]'), 'sample.test.ts')
+  })
 
-  await page.click('[data-testid=btn-graph]')
-  await untilUpdated(() => page.textContent('[data-testid=graph] text'), 'sample.test.ts')
+  it('graph', async () => {
+    await page.click('[data-testid=btn-graph]')
+    await untilUpdated(() => page.textContent('[data-testid=graph] text'), 'sample.test.ts')
+  })
 
-  await page.click('[data-testid=btn-console]')
-  await untilUpdated(() => page.textContent('[data-testid=console] pre'), 'log test')
+  it('console', async () => {
+    await page.click('[data-testid=btn-console]')
+    await untilUpdated(() => page.textContent('[data-testid=console] pre'), 'log test')
+  })
 })
 
 it('no error happen', () => {
