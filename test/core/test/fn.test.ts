62ffb3d30a48bc60603a40eab79427198227cdb8
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-10 17:57:05 -0800

    test: update toHaveNthReturnedWith test in fn.throws

--- test/core/test/fn.test.ts
+++ test/core/test/fn.test.ts
@@ -80,13 +80,13 @@ describe('mock', () => {
     fn()
 
     try {
-      expect(fn).toHaveNthReturnedWith(2, '2')
+      expect(fn).toHaveNthReturnedWith(2, '1')
       assert.fail('expect should throw, since 2nd call is thrown')
     }
     catch {}
 
     // not throws
-    expect(fn).not.toHaveNthReturnedWith(2, '2')
+    expect(fn).not.toHaveNthReturnedWith(2, '1')
 
     expect(fn).toHaveReturnedTimes(2)
     expect(fn).toHaveNthReturnedWith(3, '3')
