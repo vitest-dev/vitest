98d127c3e6081cb60c05b5a0d11278dd68d6e886
yoho <panyoho@gmail.com>
2023-02-12 22:48:42 +0800

    fix: types

--- packages/ui/client/shim.d.ts
+++ packages/ui/client/shim.d.ts
@@ -1,13 +1,6 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pages/client" />
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
-
 const __BASE_PATH__: string
 
 declare interface Window {
