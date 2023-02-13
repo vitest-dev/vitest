b5d1a4af902f6662d14d5e12271acfeb8e6a662a
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2023-02-13 13:21:34 +0100

    test: skip ui tests on windows for now

--- test/ui/test/html-report.spec.ts
+++ test/ui/test/html-report.spec.ts
@@ -1,58 +1,61 @@
 import { resolve } from 'node:path'
 import { beforeAll, describe, expect, it } from 'vitest'
 import { execaCommandSync } from 'execa'
-import { browserErrors, page, ports, startServerCommand, untilUpdated } from '../setup'
+import { browserErrors, isWindows, page, ports, startServerCommand, untilUpdated } from '../setup'
 
 const root = resolve(__dirname, '../fixtures')
 const port = ports.report
 
-beforeAll(async () => {
-  execaCommandSync('npx vitest run --reporter=html --outputFile=html/index.html', {
-    cwd: root,
-    env: {
-      ...process.env,
-      CI: 'true',
-      NO_COLOR: 'true',
-    },
-  })
+// TODO: fix flakyness on windows
+describe.skipIf(isWindows)('html report', () => {
+  beforeAll(async () => {
+    execaCommandSync('npx vitest run --reporter=html --outputFile=html/index.html', {
+      cwd: root,
+      env: {
+        ...process.env,
+        CI: 'true',
+        NO_COLOR: 'true',
+      },
+    })
 
-  const exit = await startServerCommand(
-    root,
+    const exit = await startServerCommand(
+      root,
     `npx vite preview --outDir html --strict-port --base /__vitest__/ --port ${port}`,
     `http://localhost:${port}/__vitest__/`,
-  )
+    )
 
-  return exit
-})
-
-it('dashboard', async () => {
-  await untilUpdated(() => page.textContent('[aria-labelledby]'), '1 Pass 0 Fail 1 Total ')
-})
-
-describe('file detail', async () => {
-  beforeAll(async () => {
-    await page.click('.details-panel span')
+    return exit
   })
 
-  it('report', async () => {
-    await page.click('[data-testid=btn-report]')
-    await untilUpdated(() => page.textContent('[data-testid=report]'), 'All tests passed in this file')
-    await untilUpdated(() => page.textContent('[data-testid=filenames]'), 'sample.test.ts')
+  it('dashboard', async () => {
+    await untilUpdated(() => page.textContent('[aria-labelledby]'), '1 Pass 0 Fail 1 Total ')
   })
 
-  it('graph', async () => {
-    await page.click('[data-testid=btn-graph]')
-    expect(page.url()).toMatch('graph')
-    await untilUpdated(() => page.textContent('[data-testid=graph] text'), 'sample.test.ts')
+  describe('file detail', async () => {
+    beforeAll(async () => {
+      await page.click('.details-panel span')
+    })
+
+    it('report', async () => {
+      await page.click('[data-testid=btn-report]')
+      await untilUpdated(() => page.textContent('[data-testid=report]'), 'All tests passed in this file')
+      await untilUpdated(() => page.textContent('[data-testid=filenames]'), 'sample.test.ts')
+    })
+
+    it('graph', async () => {
+      await page.click('[data-testid=btn-graph]')
+      expect(page.url()).toMatch('graph')
+      await untilUpdated(() => page.textContent('[data-testid=graph] text'), 'sample.test.ts')
+    })
+
+    it('console', async () => {
+      await page.click('[data-testid=btn-console]')
+      expect(page.url()).toMatch('console')
+      await untilUpdated(() => page.textContent('[data-testid=console] pre'), 'log test')
+    })
   })
 
-  it('console', async () => {
-    await page.click('[data-testid=btn-console]')
-    expect(page.url()).toMatch('console')
-    await untilUpdated(() => page.textContent('[data-testid=console] pre'), 'log test')
+  it('no error happen', () => {
+    expect(browserErrors.length).toEqual(0)
   })
-})
-
-it('no error happen', () => {
-  expect(browserErrors.length).toEqual(0)
 })
