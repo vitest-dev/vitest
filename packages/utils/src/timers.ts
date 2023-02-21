fd1f904d827cb8a2f50662addd4d42c17e01887f
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2023-02-21 21:48:00 +0100

    fix: use safe nextTick, reset timers in real next tick

--- packages/utils/src/timers.ts
+++ packages/utils/src/timers.ts
@@ -10,7 +10,12 @@ export function getSafeTimers() {
     clearImmediate: safeClearImmediate,
   } = (globalThis as any)[SAFE_TIMERS_SYMBOL] || globalThis
 
+  const {
+    nextTick: safeNextTick,
+  } = (globalThis as any)[SAFE_TIMERS_SYMBOL] || globalThis.process || { nextTick: (cb: () => void) => cb() }
+
   return {
+    nextTick: safeNextTick,
     setTimeout: safeSetTimeout,
     setInterval: safeSetInterval,
     clearInterval: safeClearInterval,
@@ -30,7 +35,12 @@ export function setSafeTimers() {
     clearImmediate: safeClearImmediate,
   } = globalThis
 
+  const {
+    nextTick: safeNextTick,
+  } = globalThis.process || { nextTick: cb => cb() }
+
   const timers = {
+    nextTick: safeNextTick,
     setTimeout: safeSetTimeout,
     setInterval: safeSetInterval,
     clearInterval: safeClearInterval,
--- packages/vitest/src/runtime/rpc.ts
+++ packages/vitest/src/runtime/rpc.ts
@@ -7,24 +7,35 @@ const { get } = Reflect
 const safeRandom = Math.random
 
 function withSafeTimers(fn: () => void) {
-  const { setTimeout, clearTimeout } = getSafeTimers()
+  const { setTimeout, clearTimeout, nextTick, setImmediate, clearImmediate } = getSafeTimers()
 
   const currentSetTimeout = globalThis.setTimeout
   const currentClearTimeout = globalThis.clearTimeout
   const currentRandom = globalThis.Math.random
+  const currentNextTick = globalThis.process.nextTick
+  const currentSetImmediate = globalThis.setImmediate
+  const currentClearImmediate = globalThis.clearImmediate
 
   try {
     globalThis.setTimeout = setTimeout
     globalThis.clearTimeout = clearTimeout
     globalThis.Math.random = safeRandom
+    globalThis.process.nextTick = nextTick
+    globalThis.setImmediate = setImmediate
+    globalThis.clearImmediate = clearImmediate
 
     const result = fn()
     return result
   }
   finally {
-    globalThis.setTimeout = currentSetTimeout
-    globalThis.clearTimeout = currentClearTimeout
-    globalThis.Math.random = currentRandom
+    nextTick(() => {
+      globalThis.setTimeout = currentSetTimeout
+      globalThis.clearTimeout = currentClearTimeout
+      globalThis.Math.random = currentRandom
+      globalThis.process.nextTick = currentNextTick
+      globalThis.setImmediate = currentSetImmediate
+      globalThis.clearImmediate = currentClearImmediate
+    })
   }
 }
 
