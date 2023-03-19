3cc66a8d6b67b47f43cf178fda8e0260c9277d73
Vladimir <sleuths.slews0s@icloud.com>
2023-03-19 12:38:25 +0100

    chore: cleanup

--- test/mixed-pools/vitest.config.ts
+++ test/mixed-pools/vitest.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   test: {
-    browser: {
-      headless: false,
-    },
     include: ['test/*.test.ts'],
     poolMatchGlobs: [
       ['**/test/*.child_process.test.ts', 'child_process'],
