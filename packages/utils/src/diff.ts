9a9959c660aaae6051b923f5f2cef20ba4768e50
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2023-04-06 15:36:36 +0200

    chore: no limit for diff output

--- packages/utils/src/diff.ts
+++ packages/utils/src/diff.ts
@@ -5,7 +5,6 @@ export * from './descriptors'
 
 export interface DiffOptions {
   showLegend?: boolean
-  outputDiffLines?: number
 }
 
 /**
@@ -20,7 +19,7 @@ export function unifiedDiff(actual: unknown, expected: unknown, options: DiffOpt
   const theme = getConcordanceTheme()
   const diff = diffDescriptors(actual, expected, { theme })
 
-  const { showLegend = true, outputDiffLines = 15 } = options
+  const { showLegend = true } = options
 
   const counts = {
     '+': 0,
@@ -31,44 +30,20 @@ export function unifiedDiff(actual: unknown, expected: unknown, options: DiffOpt
   const minus = `  ${c.green('+')}`
 
   const lines = diff.split(/\r?\n/g)
-  let firstErrorLine: number | null = null
-  lines.forEach((line, index) => {
-    if (line.startsWith(plus)) {
-      firstErrorLine ??= index
+  lines.forEach((line) => {
+    if (line.startsWith(plus))
       counts['+']++
-    }
-    else if (line.startsWith(minus)) {
-      firstErrorLine ??= index
+    else if (line.startsWith(minus))
       counts['-']++
-    }
   })
-  const isCompact = counts['+'] === 1 && counts['-'] === 1 && lines.length === 2
 
   let legend = ''
 
   if (showLegend) {
-    if (!isCompact) {
-      legend = `  ${c.green(`- Expected  - ${counts['-']}`)}
+    legend = `  ${c.green(`- Expected  - ${counts['-']}`)}
   ${c.red(`+ Received  + ${counts['+']}`)}
 
 `
-    }
-    else {
-      legend = '  Difference:\n\n'
-    }
-  }
-
-  if (firstErrorLine != null && outputDiffLines) {
-    const start = Math.max(0, firstErrorLine - 1)
-    const end = Math.min(lines.length, firstErrorLine + outputDiffLines)
-    const linesAfterCount = lines.length - end
-
-    const linesBefore = start ? `  ${c.gray(`... ${start} more line${start > 1 ? 's' : ''}\n`)}` : ''
-    const linesAfter = linesAfterCount ? `\n  ${c.gray(`... ${linesAfterCount} more line${linesAfterCount > 1 ? 's' : ''}\n`)}` : ''
-    const diffOutput = lines.slice(start, end).map(line => line.replace(/␊\s*$/, '')).join('\n')
-    const helperBunner = (linesAfter && (counts['+'] + counts['-'] > outputDiffLines)) ? `\n  Use ${c.gray('test.outputDiffLines')} to increase the number of lines shown.` : ''
-
-    return legend + linesBefore + diffOutput + linesAfter + helperBunner
   }
 
   return legend + diff
