6d5664c8aa9924745e86a0971412bdb784b209aa
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2023-04-06 15:40:04 +0200

    lint: fix

--- packages/vitest/src/integrations/chai/index.ts
+++ packages/vitest/src/integrations/chai/index.ts
@@ -82,7 +82,9 @@ Object.defineProperty(globalThis, GLOBAL_EXPECT, {
 
 export { assert, should } from 'chai'
 export { chai, globalExpect as expect }
-export const setupChaiConfig = (config: ChaiConfig) => {
+
+export function setupChaiConfig(config: ChaiConfig) {
   Object.assign(chai.config, config)
 }
+
 export type ChaiConfig = Omit<Partial<typeof chai.config>, 'useProxy' | 'proxyExcludedKeys'>
