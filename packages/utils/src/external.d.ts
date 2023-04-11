7af644440b4eb2dd1844b176b87328e52162b8df
Vladimir <sleuths.slews0s@icloud.com>
2023-04-11 10:11:16 +0200

    fix: publish utils and snapshot .d.ts files for typescript (#3172)

--- packages/utils/src/external.d.ts
+++ packages/utils/src/external.d.ts
@@ -1,9 +1,3 @@
 declare module 'concordance' {
-  interface DisplayOptions {
-    theme?: any
-    maxDepth?: number
-  }
-
-  export function diff(expected: unknown, actual: unknown, options?: DisplayOptions): string
-  export function formatDescriptor(descriptor: unknown, options?: DisplayOptions): string
+  export function diff(expected: unknown, actual: unknown, options?: any): string
 }
