30bb3268aeadc8224eaf71c4546d1877bd48bea5
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2023-04-27 14:32:29 +0200

    chore: remove misleading note

--- packages/vitest/src/integrations/vi.ts
+++ packages/vitest/src/integrations/vi.ts
@@ -172,7 +172,7 @@ function createVitest(): VitestUtils {
   if (!workerState) {
     const errorMsg = 'Vitest failed to access its internal state.'
       + '\n\nOne of the following is possible:'
-      + '\n- "vitest" is imported directly without running "vitest" command (to fix this, run "npx vitest")'
+      + '\n- "vitest" is imported directly without running "vitest" command'
       + '\n- "vitest" is imported inside "globalSetup" (to fix this, use "setupFiles" instead, because "globalSetup" runs in a different context)'
       + '\n- Otherwise, it might be a Vitest bug. Please report it to https://github.com/vitest-dev/vitest/issues\n'
     throw new Error(errorMsg)
