a3393b1525b73256c8df86fcf85d7ebd7b69c876
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2023-05-03 06:25:14 -0400

    fix(vitest): also check for vite relative to vitest package (#3274)
    Co-authored-by: Vladimir <sleuths.slews0s@icloud.com>

--- packages/vitest/src/node/pkg.ts
+++ packages/vitest/src/node/pkg.ts
@@ -1,13 +1,16 @@
+import url from 'node:url'
 import c from 'picocolors'
 import { isPackageExists } from 'local-pkg'
 import { EXIT_CODE_RESTART } from '../constants'
 import { isCI } from '../utils/env'
 
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
+
 export async function ensurePackageInstalled(
   dependency: string,
   root: string,
 ) {
-  if (isPackageExists(dependency, { paths: [root] }))
+  if (isPackageExists(dependency, { paths: [root, __dirname] }))
     return true
 
   const promptInstall = !isCI && process.stdout.isTTY
