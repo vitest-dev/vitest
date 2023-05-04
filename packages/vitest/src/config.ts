5382e8b636a5c1f1771916214fe9728485506fcd
Vladimir <sleuths.slews0s@icloud.com>
2023-05-04 15:25:49 +0200

    fix: remove duplicate type (#3303)

--- packages/vitest/src/config.ts
+++ packages/vitest/src/config.ts
@@ -1,21 +1,17 @@
 import type { ConfigEnv, UserConfig as ViteUserConfig } from 'vite'
 import type { ProjectConfig } from './types'
 
-export interface UserConfig extends ViteUserConfig {
-  test?: ViteUserConfig['test'] | undefined
-}
-
 export interface UserWorkspaceConfig extends ViteUserConfig {
-  test?: ProjectConfig | undefined
+  test?: ProjectConfig
 }
 
 // will import vitest declare test in module 'vite'
 export { configDefaults, defaultInclude, defaultExclude, coverageConfigDefaults } from './defaults'
 export { mergeConfig } from 'vite'
 
-export type { ConfigEnv }
-export type UserConfigFn = (env: ConfigEnv) => UserConfig | Promise<UserConfig>
-export type UserConfigExport = UserConfig | Promise<UserConfig> | UserConfigFn
+export type { ConfigEnv, ViteUserConfig as UserConfig }
+export type UserConfigFn = (env: ConfigEnv) => ViteUserConfig | Promise<ViteUserConfig>
+export type UserConfigExport = ViteUserConfig | Promise<ViteUserConfig> | UserConfigFn
 
 export type UserProjectConfigFn = (env: ConfigEnv) => UserWorkspaceConfig | Promise<UserWorkspaceConfig>
 export type UserProjectConfigExport = UserWorkspaceConfig | Promise<UserWorkspaceConfig> | UserProjectConfigFn
