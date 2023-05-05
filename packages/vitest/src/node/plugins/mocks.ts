71729c9c34f0ee09f945962084cd874d35020631
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2023-05-05 17:10:45 +0200

    feat: ensure mocking plugin is the last one

--- packages/vitest/src/node/plugins/mocks.ts
+++ packages/vitest/src/node/plugins/mocks.ts
@@ -2,11 +2,19 @@ import type { Plugin } from 'vite'
 import { hoistMocks } from '../hoistMocks'
 
 export function MocksPlugin(): Plugin {
-  return {
-    name: 'vite:mocks',
+  const plugin: Plugin = {
+    name: 'vitest:mocks',
     enforce: 'post',
     transform(code, id) {
       return hoistMocks(code, id, this.parse)
     },
+    configResolved(config: any) {
+      const pluginIndex = config.plugins.indexOf(plugin)
+      const [mocksPlugin] = config.plugins.splice(pluginIndex, 1)
+      // inject before import-analysis
+      config.plugins.splice(config.plugins.length - 1, 0, mocksPlugin)
+    },
   }
+
+  return plugin
 }
