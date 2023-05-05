ee41217b77e67ec86670efeeeef1f1fa817b48a4
Anthony Fu <anthonyfu117@hotmail.com>
2023-05-05 18:23:28 +0200

    fix: update

--- packages/vitest/src/node/plugins/mocks.ts
+++ packages/vitest/src/node/plugins/mocks.ts
@@ -2,19 +2,14 @@ import type { Plugin } from 'vite'
 import { hoistMocks } from '../hoistMocks'
 
 export function MocksPlugin(): Plugin {
-  const plugin: Plugin = {
+  return {
     name: 'vitest:mocks',
     enforce: 'post',
-    transform(code, id) {
-      return hoistMocks(code, id, this.parse)
-    },
-    configResolved(config: any) {
-      const pluginIndex = config.plugins.indexOf(plugin)
-      const [mocksPlugin] = config.plugins.splice(pluginIndex, 1)
-      // inject before import-analysis
-      config.plugins.splice(config.plugins.length - 1, 0, mocksPlugin)
+    transform: {
+      order: 'post', // ensure it always runs
+      handler(code, id) {
+        return hoistMocks(code, id, this.parse)
+      },
     },
   }
-
-  return plugin
 }
