07092c46ea528e295bded171e82a84b37272cb26
AriPerkkio <ari.perkkio@gmail.com>
2023-05-09 13:12:00 +0300

    test: add test case for #3131

--- test/config/test/utils.ts
+++ test/config/test/utils.ts
@@ -4,15 +4,17 @@ import stripAnsi from 'strip-ansi'
 export async function runVitest(mode: 'run' | 'watch', cliArguments: string[]) {
   const subprocess = execa('vitest', [mode, 'fixtures/test/', ...cliArguments])
   let error = ''
+  let output = ''
+
+  subprocess.stdout?.on('data', (data) => {
+    output += stripAnsi(data.toString())
+  })
 
   subprocess.stderr?.on('data', (data) => {
     error += stripAnsi(data.toString())
-
-    // Sometimes on Windows CI execa doesn't exit properly. Force exit when stderr is caught.
-    subprocess.kill()
   })
 
   await new Promise(resolve => subprocess.on('exit', resolve))
 
-  return { error }
+  return { output, error }
 }
