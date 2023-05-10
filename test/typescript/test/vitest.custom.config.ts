c53ae0791a95eb7b6c1a543230f6884183484385
Vladimir <sleuths.slews0s@icloud.com>
2023-05-10 15:38:19 +0200

    fix(typecheck): correctly resolve custom tsconfig path (#3342)

---
+++ test/typescript/test/vitest.custom.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    typecheck: {
+      allowJs: true,
+      include: ['**/*.test-d.*'],
+      tsconfig: '../tsconfig.custom.json',
+    },
+  },
+})
