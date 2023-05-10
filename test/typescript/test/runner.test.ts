06c66bbc8efe8fbd086d353cbaa7f4fdb3ce1dca
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2023-05-10 15:05:02 +0200

    fix(typecheck): correctly resolve custom tsconfig path

--- test/typescript/test/runner.test.ts
+++ test/typescript/test/runner.test.ts
@@ -48,4 +48,37 @@ describe('should fail', async () => {
       }
     })
   }, 30_000)
+
+  it('typecheks with custom tsconfig', async () => {
+    const { stderr } = await execa('npx', [
+      'vitest',
+      'typecheck',
+      '--run',
+      '--dir',
+      resolve(__dirname, '..', './failing'),
+      '--config',
+      resolve(__dirname, './vitest.custom.config.ts'),
+    ], {
+      cwd: root,
+      reject: false,
+      env: {
+        ...process.env,
+        CI: 'true',
+        NO_COLOR: 'true',
+      },
+    })
+
+    expect(stderr).toBeTruthy()
+    const lines = String(stderr).split(/\n/g)
+    const msg = lines
+      .filter(i => i.includes('TypeCheckError: '))
+      .reverse()
+      .join('\n')
+      .trim()
+      .replace(root, '<rootDir>')
+    expect(stderr).not.toMatch('files found, exiting with code')
+    // only one test file is failed, because only one is included in tsconfig
+    // + file with .only modifier
+    expect(msg).toMatchSnapshot()
+  }, 30_000)
 })
