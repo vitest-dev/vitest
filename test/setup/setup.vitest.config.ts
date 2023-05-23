db0afb1ae0fd873d6386936128a45001819a329a
AriPerkkio <ari.perkkio@gmail.com>
2023-05-23 17:37:21 +0300

    test(setup): remove execa usage

--- test/setup/setup.vitest.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vitest/config'
-
-export default defineConfig({
-  test: {
-    include: ['tests/empty-setup.test.ts'],
-    setupFiles: ['setupFiles/empty-setup.ts'],
-  },
-})
