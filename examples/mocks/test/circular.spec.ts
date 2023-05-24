632eef4037dad8712d3299650fb2f5d3ced6f671
Dunqing <dengqing0821@gmail.com>
2023-05-24 21:46:08 +0800

    fix(vite-node): circular import stuck (#3418)

--- examples/mocks/test/circular.spec.ts
+++ examples/mocks/test/circular.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test, vi } from 'vitest'
-import { main } from '../src/main.js'
+import { main, mainB } from '../src/main.js'
 import x from '../src/export-default-circle-index'
 
 vi.mock('../src/A', async () => ({
@@ -7,10 +7,19 @@ vi.mock('../src/A', async () => ({
   funcA: () => 'mockedA',
 }))
 
+vi.mock('../src/B', async () => ({
+  ...(await vi.importActual<any>('../src/B')),
+  funcB: () => 'mockedB',
+}))
+
 vi.mock('../src/export-default-circle-b')
 
 test('can import actual inside mock factory', () => {
   expect(main()).toBe('mockedA')
+})
+
+test('can import in top level and inside mock factory', () => {
+  expect(mainB()).toBe('mockedB')
 })
 
 test('can mock a circular dependency', () => {
