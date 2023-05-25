011797e939edc67565014118b97d4c7f0a9a9859
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2023-05-25 11:50:45 +0200

    chore: fix types

--- packages/web-worker/src/utils.ts
+++ packages/web-worker/src/utils.ts
@@ -73,7 +73,7 @@ export function getRunnerOptions(): any {
     moduleCache,
     mockMap,
     interopDefault: config.deps.interopDefault ?? true,
-    moduleDirectories: config.moduleDirectories,
+    moduleDirectories: config.deps.moduleDirectories,
     root: config.root,
     base: config.base,
   }
