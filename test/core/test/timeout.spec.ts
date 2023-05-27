5ed919edd65f38b204d3e65cf7531704e8f9f71d
btea <2356281422@qq.com>
2023-05-27 16:00:44 +0800

    test: add timeout test case

---
+++ test/core/test/timeout.spec.ts
@@ -0,0 +1,10 @@
+import { describe, expect, test } from 'vitest'
+
+describe('suite timeout', () => {
+  test('true is true after 5100ms', async () => {
+    await new Promise(resolve => setTimeout(resolve, 5100))
+    expect(true).toBe(true)
+  })
+}, {
+  timeout: 6000,
+})
