1252aea3b87335ba7d9ac4ff9f934f6ee7a9c8a0
Han Feng <fenghan770@gmail.com>
2023-06-03 11:01:52 +0800

    test: move to reporters

--- test/reporters/fixtures/function-as-name.test.ts
+++ test/reporters/fixtures/function-as-name.test.ts
@@ -5,24 +5,24 @@ class Bar {}
 
 describe(foo, () => {
   test(Bar, () => {
-    expect(0).toBe(1)
+    expect(0).toBe(0)
   })
 })
 
 describe(Bar, () => {
   test(foo, () => {
-    expect(0).toBe(1)
+    expect(0).toBe(0)
   })
 })
 
 describe.each([1])(foo, () => {
   test.each([1])(foo, () => {
-    expect(0).toBe(1)
+    expect(0).toBe(0)
   })
 })
 
 describe.each([1])(Bar, () => {
   test.each([1])(Bar, () => {
-    expect(0).toBe(1)
+    expect(0).toBe(0)
   })
 })
