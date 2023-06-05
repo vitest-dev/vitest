beaea428eb941c0b938ecc78ae3f5095ec6bdd05
Dunqing <dengqing0821@gmail.com>
2023-06-05 14:59:53 +0000

    test: should better

--- test/core/test/expect.test.ts
+++ test/core/test/expect.test.ts
@@ -1,72 +1,43 @@
-import { resolve } from 'node:path'
-import type { UserConfig } from 'vitest'
-import { describe, expect, expectTypeOf, test } from 'vitest'
 import { getCurrentTest } from '@vitest/runner'
-import { runVitest } from '../../test-utils'
+import { describe, expect, expectTypeOf, test } from 'vitest'
 
 describe('expect.soft', () => {
-  const run = (config?: UserConfig) => runVitest({ root: resolve(__dirname, './fixtures/expects'), exclude: [], setupFiles: [], testNamePattern: getCurrentTest()?.name, testTimeout: 4000, ...config }, ['soft'])
-
   test('types', () => {
-    expectTypeOf(expect).toEqualTypeOf(expect)
+    expectTypeOf(expect.soft).toEqualTypeOf(expect)
     expectTypeOf(expect.soft(7)).toEqualTypeOf(expect(7))
     expectTypeOf(expect.soft(5)).toHaveProperty('toBe')
     expectTypeOf(expect.soft(7)).not.toHaveProperty('toCustom')
   })
 
-  test('basic', async () => {
-    const { stderr } = await run()
-    expect(stderr).toContain('AssertionError: expected 1 to be 2')
-    expect(stderr).toContain('AssertionError: expected 2 to be 3')
+  test('return value', () => {
+    expect(expect.soft('test')).toHaveProperty('toBe')
+    expect(expect.soft('test')).toHaveProperty('toEqual')
   })
 
-  test('promise', async () => {
-    const { stderr } = await run()
-    expect(stderr).toContain('AssertionError: expected 2 to be 3')
-    expect(stderr).toContain('AssertionError: expected 1 to be 2')
-  })
-
-  test('with expect', async () => {
-    const { stderr } = await run()
-    expect(stderr).toContain('AssertionError: expected 1 to deeply equal 2')
-    expect(stderr).toContain('AssertionError: expected 10 to deeply equal 20')
-    expect(stderr).not.toContain('AssertionError: expected 2 to deeply equal 3')
-  })
-
-  test('with expect.extend', async () => {
-    const { stderr } = await run()
-    expect(stderr).toContain('AssertionError: expected 1 to deeply equal 2')
-    expect(stderr).toContain('Error: expected 3 to be divisible by 4')
-    expect(stderr).toContain('AssertionError: expected 5 to deeply equal 6')
-  })
-
-  test('passed', async () => {
-    const { stdout } = await run()
-    expect(stdout).toContain('✓ soft.test.ts > passed')
-  })
-
-  test('retry will passed', async () => {
-    const { stdout } = await run()
-    expect(stdout).toContain('✓ soft.test.ts > retry will passed')
-  })
-
-  test('retry will failed', async () => {
-    const { stderr } = await run()
-    expect(stderr).toContain('AssertionError: expected 1 to be 4')
-    expect(stderr).toContain('AssertionError: expected 2 to be 5')
-    expect(stderr).toContain('AssertionError: expected 3 to be 4')
-    expect(stderr).toContain('AssertionError: expected 4 to be 5')
-    expect(stderr).toContain('4/4')
-  })
-
-  test('using expect.soft for test', async () => {
-    const { stderr } = await run({
-      testNamePattern: 'retry will failed',
+  test('with extend', () => {
+    expect.extend({
+      toBeFoo(received) {
+        const { isNot } = this
+        return {
+          // do not alter your "pass" based on isNot. Vitest does it for you
+          pass: received === 'foo',
+          message: () => `${received} is${isNot ? ' not' : ''} foo`,
+        }
+      },
     })
-    expect.soft(stderr).toContain('AssertionError: expected 1 to be 4')
-    expect.soft(stderr).toContain('AssertionError: expected 2 to be 5')
-    expect.soft(stderr).toContain('AssertionError: expected 3 to be 4')
-    expect.soft(stderr).toContain('AssertionError: expected 4 to be 5')
-    expect.soft(stderr).toContain('4/4')
+    expect(expect.soft('test')).toHaveProperty('toBeFoo')
   })
-}, 4000)
+
+  test('should have multiple error', () => {
+    expect.soft(1).toBe(2)
+    expect.soft(2).toBe(3)
+    getCurrentTest()!.result!.state = 'run'
+    expect(getCurrentTest()?.result?.errors).toHaveLength(2)
+  })
+
+  test.fails('should be a failure', () => {
+    expect.soft('test1').toBe('test res')
+    expect.soft('test2').toBe('test res')
+    expect.soft('test3').toBe('test res')
+  })
+})
