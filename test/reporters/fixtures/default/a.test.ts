39b6f5082392e3948ab66068c24f383809906b4c
Dunqing <dengqing0821@gmail.com>
2023-06-08 02:55:07 +0000

    test: update test

--- test/reporters/fixtures/default/a.test.ts
+++ test/reporters/fixtures/default/a.test.ts
@@ -12,14 +12,14 @@ describe('a passed', () => {
 })
 
 describe('a failed', () => {
-  test.each([1, 2, 3])('a%d test', (d) => {
+  test.each([1, 2, 3])('a failed %d test', (d) => {
     expect(d).toBe(d)
   })
   test('a failed test', () => {
     expect(1).toBe(2)
   })
-  describe('nested a', () => {
-    test.each([1, 2, 3])('nested a%d test', (d) => {
+  describe('nested a failed', () => {
+    test.each([1, 2, 3])('nested a failed %d test', (d) => {
       expect(d).toBe(d)
     })
   })
--- test/reporters/tests/default.test.ts
+++ test/reporters/tests/default.test.ts
@@ -11,11 +11,53 @@ describe('default reporter', async () => {
 
   test('normal', async () => {
     const { stdout } = await run(['b1.test.ts', 'b2.test.ts'])
-    expect(stdout.split('\n').slice(2, -4).join('\n')).toMatchSnapshot()
+    expect(stdout).toMatchInlineSnapshot(`
+      "
+       RUN  v0.32.0 /root/p/vitest/test/reporters/fixtures
+
+       · default/b1.test.ts (13)
+       ❯ default/b2.test.ts (13)
+         ✓ b2 passed (6)
+         ❯ b2 failed (7)
+           ✓ b1 test
+           ✓ b2 test
+           ✓ b3 test
+           × b failed test
+           ✓ nested b (3)
+       ❯ default/b1.test.ts (13)
+         ✓ b1 passed (6)
+         ❯ b1 failed (7)
+           ✓ b1 test
+           ✓ b2 test
+           ✓ b3 test
+           × b failed test
+           ✓ nested b (3)
+       ❯ default/b2.test.ts (13)
+         ✓ b2 passed (6)
+         ❯ b2 failed (7)
+           ✓ b1 test
+           ✓ b2 test
+           ✓ b3 test
+           × b failed test
+           ✓ nested b (3)
+
+       Test Files  2 failed (2)
+            Tests  2 failed | 24 passed (26)
+         Start at  02:54:26
+         Duration  866ms (transform 33ms, setup 0ms, collect 54ms, tests 34ms, environment 0ms, prepare 174ms)
+
+      "
+    `)
+    expect(stdout).contain('✓ b2 test')
+    expect(stdout).not.contain('✓ nested b1 test')
+    expect(stdout).contain('× b failed test')
   }, 120000)
 
   test('show full test suite when only one file', async () => {
     const { stdout } = await run(['a.test.ts'])
-    expect(stdout.split('\n').slice(2, -4).join('\n')).toMatchSnapshot()
+    expect(stdout).contain('✓ a1 test')
+    expect(stdout).contain('✓ nested a3 test')
+    expect(stdout).contain('× a failed test')
+    expect(stdout).contain('nested a failed 1 test')
   }, 120000)
 })
