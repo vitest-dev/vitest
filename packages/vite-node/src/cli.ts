21bbc8147b6c6e9889995b8a443e36d50b33d91e
rxliuli <rxliuli@gmail.com>
2023-06-14 16:31:27 +0800

    feat: Make vite-node parsing behavior consistent with node/tsx/ts-node, ref: https://github.com/vitest-dev/vitest/issues/3571

--- packages/vite-node/src/cli.ts
+++ packages/vite-node/src/cli.ts
@@ -12,21 +12,31 @@ import { installSourcemapsSupport } from './source-map'
 const cli = cac('vite-node')
 
 cli
-  .version(version)
   .option('-r, --root <path>', 'Use specified root directory')
   .option('-c, --config <path>', 'Use specified config file')
   .option('-m, --mode <mode>', 'Set env mode')
   .option('-w, --watch', 'Restart on file changes, similar to "nodemon"')
   .option('--script', 'Use vite-node as a script runner')
   .option('--options <options>', 'Use specified Vite server options')
-  .help()
+  .option('-v, --version', 'Output the version number')
+  .option('-h, --help', 'Display help for command')
 
 cli
   .command('[...files]')
   .allowUnknownOptions()
   .action(run)
 
-cli.parse()
+cli.parse(process.argv, { run: false })
+
+if (cli.args.length === 0) {
+  cli.runMatchedCommand()
+}
+else {
+  const i = cli.rawArgs.indexOf(cli.args[0]) + 1
+  const scriptArgs = cli.rawArgs.slice(i).filter(it => it !== '--')
+  const executeArgs = [...cli.rawArgs.slice(0, i), '--', ...scriptArgs]
+  cli.parse(executeArgs)
+}
 
 export interface CliOptions {
   root?: string
@@ -35,10 +45,13 @@ export interface CliOptions {
   mode?: string
   watch?: boolean
   options?: ViteNodeServerOptionsCLI
+  version?: boolean
+  help?: boolean
   '--'?: string[]
 }
 
 async function run(files: string[], options: CliOptions = {}) {
+  // console.log('run', files, options)
   if (options.script) {
     files = [files[0]]
     options = {}
@@ -48,6 +61,15 @@ async function run(files: string[], options: CliOptions = {}) {
     process.argv = [...process.argv.slice(0, 2), ...(options['--'] || [])]
   }
 
+  if (options.version) {
+    cli.version(version)
+    cli.outputVersion()
+    process.exit(0)
+  }
+  if (options.help) {
+    cli.outputHelp()
+    process.exit(0)
+  }
   if (!files.length) {
     console.error(c.red('No files specified.'))
     cli.outputHelp()
---
+++ test/vite-node/test/cli.test.ts
@@ -0,0 +1,38 @@
+import { execa } from 'execa'
+import { resolve } from 'pathe'
+import { expect, it } from 'vitest'
+
+const cliPath = resolve(__dirname, '../../../packages/vite-node/src/cli.ts')
+const entryPath = resolve(__dirname, '../src/cli-parse-args.js')
+
+const parseResult = (s: string) => JSON.parse(s.replaceAll('\'', '"'))
+
+it('basic', async () => {
+  const result = await execa('npx', ['esno', cliPath, entryPath], { reject: true })
+  expect(result.stdout).include('/node')
+  expect(parseResult(result.stdout)).length(2)
+})
+
+it('--help', async () => {
+  const r1 = await execa('npx', ['esno', cliPath, '--help', entryPath], { reject: true })
+  expect(r1.stdout).include('help')
+  const r2 = await execa('npx', ['esno', cliPath, '-h', entryPath], { reject: true })
+  expect(r2.stdout).include('help')
+})
+
+it('--version', async () => {
+  const r1 = await execa('npx', ['esno', cliPath, '--version', entryPath], { reject: true })
+  expect(r1.stdout).include('vite-node/')
+  const r2 = await execa('npx', ['esno', cliPath, '-v', entryPath], { reject: true })
+  expect(r2.stdout).include('vite-node/')
+})
+
+it('script args', async () => {
+  const r1 = await execa('npx', ['esno', cliPath, entryPath, '--version', '--help'], { reject: true })
+  expect(parseResult(r1.stdout)).include('--version').include('--help')
+})
+
+it('script args in -- after', async () => {
+  const r1 = await execa('npx', ['esno', cliPath, entryPath, '--', '--version', '--help'], { reject: true })
+  expect(parseResult(r1.stdout)).include('--version').include('--help')
+})
