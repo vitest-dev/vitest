0cbb07b404d18b0330262d792afc8a779bcddffc
Vladimir <sleuths.slews0s@icloud.com>
2023-06-15 10:12:33 +0200

    fix(vite-node): correctly resolve virtual modules (#3544)

---
+++ examples/sveltekit/src/lib/add.ts
@@ -0,0 +1,8 @@
+import { dev } from '$app/environment'
+
+export function add(a: number, b: number) {
+  if (dev)
+    console.warn(`Adding ${a} and ${b}`)
+
+  return a + b
+}
---
+++ examples/sveltekit/vite.config.ts
@@ -0,0 +1,6 @@
+import { sveltekit } from '@sveltejs/kit/vite'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  plugins: [sveltekit()],
+})
