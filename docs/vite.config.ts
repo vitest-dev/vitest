2bef98afa917af8725af3e119088fba5ae55361b
userquin <userquin@gmail.com>
2023-06-16 17:44:23 +0200

    fix: fetch avatars chore: update vite config

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -10,10 +10,14 @@ export default defineConfig({
   optimizeDeps: {
     // vitepress is aliased with replacement `join(DIST_CLIENT_PATH, '/index')`
     // This needs to be excluded from optimization
-    exclude: ['vitepress'],
+    exclude: ['@vueuse/core', 'vitepress'],
+  },
+  server: {
+    hmr: {
+      overlay: false,
+    },
   },
   plugins: [
-    // TODO remove cast when moved to Vite 3
     Components({
       include: [/\.vue/, /\.md/],
       dirs: '.vitepress/components',
@@ -32,7 +36,7 @@ export default defineConfig({
           scale: 1.2,
         }),
       ],
-    }) as unknown as Plugin,
+    }),
     IncludesPlugin(),
   ],
 })
