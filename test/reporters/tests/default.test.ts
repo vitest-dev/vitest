c29fbaad40fc64731ac6cde2ed9bd4946027b90e
Dunqing <dengqing0821@gmail.com>
2023-06-17 21:30:28 +0800

    fix: update

--- test/reporters/tests/default.test.ts
+++ test/reporters/tests/default.test.ts
@@ -46,8 +46,6 @@ describe('default reporter', async () => {
     await vitest.waitForStdout('Input filename pattern')
     vitest.write('b\n')
     await vitest.waitForStdout('Filename pattern: b')
-    await vitest.waitForStdout('✓ b1.test.ts (13)')
-    await vitest.waitForStdout('✓ b2.test.ts (13)')
     await vitest.waitForStdout('PASS  Waiting for file changes...')
     expect(vitest.stdout).toContain('RERUN')
     expect(vitest.stdout).toContain('b1.test.ts')
