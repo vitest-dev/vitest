bec344b155396f9c3251ee79c098e8946c37508d
AriPerkkio <ari.perkkio@gmail.com>
2023-06-19 10:24:04 +0300

    feat!: disable `coverage.reportOnFailure` by default

--- packages/vitest/src/defaults.ts
+++ packages/vitest/src/defaults.ts
@@ -32,7 +32,7 @@ export const coverageConfigDefaults: ResolvedCoverageOptions = {
   cleanOnRerun: true,
   reportsDirectory: './coverage',
   exclude: defaultCoverageExcludes,
-  reportOnFailure: true,
+  reportOnFailure: false,
   reporter: [['text', {}], ['html', {}], ['clover', {}], ['json', {}]],
   // default extensions used by c8, plus '.vue' and '.svelte'
   // see https://github.com/istanbuljs/schema/blob/master/default-extension.js
