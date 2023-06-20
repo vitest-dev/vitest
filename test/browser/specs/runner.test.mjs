8129895bba7e0965ce67772b756698236ae93b5e
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2023-06-20 09:31:26 +0200

    chore: check no unhandled errors

--- test/browser/specs/runner.test.mjs
+++ test/browser/specs/runner.test.mjs
@@ -24,6 +24,8 @@ await test('tests are actually running', async () => {
   assert.ok(browserResultJson.testResults.length === 8, 'Not all the tests have been run')
   assert.ok(passedTests.length === 7, 'Some tests failed')
   assert.ok(failedTests.length === 1, 'Some tests have passed but should fail')
+
+  assert.doesNotMatch(stderr, /Unhandled Error/, 'doesn\'t have any unhandled errors')
 })
 
 await test('correctly prints error', () => {
