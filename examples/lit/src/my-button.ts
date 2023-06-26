6024e7ea6544d9db665f95fd800f8ab631176be8
userquin <userquin@gmail.com>
2023-06-26 15:13:59 +0200

    chore: include lit example

--- examples/lit/src/my-button.ts
+++ examples/lit/src/my-button.ts
@@ -1,13 +1,16 @@
 import { LitElement, css, html } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
 
+// once #3671 fixed, we can remove this hack
+const LitElementClass = LitElement
+
 /**
  * An example button.
  *
  * @slot - This button has a slot
  */
 @customElement('my-button')
-export class MyButton extends LitElement {
+export class MyButton extends LitElementClass {
   static styles = css`
     :host {
       display: block;
--- examples/lit/vite.config.ts
+++ examples/lit/vite.config.ts
@@ -1,11 +1,32 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from 'vite'
+import { isCI } from 'std-env'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
+export default defineConfig(() => {
+  if (process.env.IN_BROWSER_PROVIDER === 'true') {
+    return {
+      optimizeDeps: {
+        exclude: ['lit', 'lit/decorators.js'],
+      },
+      test: {
+        globals: true,
+        browser: {
+          enabled: true,
+          enableUI: !isCI,
+          headless: isCI,
+          name: 'chrome',
+          provider: process.env.BROWSER_PROVIDER || 'webdriverio',
+        },
+      },
+    }
+  }
+
+  return {
+    test: {
+      globals: true,
+      environment: 'jsdom',
+    },
+  }
 })
