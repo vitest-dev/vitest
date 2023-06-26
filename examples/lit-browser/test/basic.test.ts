07bff4cf58bb669548ca10225646907ecca5a137
userquin <userquin@gmail.com>
2023-06-26 16:03:51 +0200

    chore: update lit-browser example and fix package json browser examples

--- examples/lit-browser/test/basic.test.ts
+++ examples/lit-browser/test/basic.test.ts
@@ -3,38 +3,32 @@ import { beforeEach, describe, expect, it, vi } from 'vitest'
 import '../src/my-button'
 
 describe('Button with increment', async () => {
-  function getInsideButton(): HTMLElement | null | undefined {
-    return document.body.querySelector('my-button')?.shadowRoot?.querySelector('button')
+  function getInsideButton(idx: number): HTMLElement | null | undefined {
+    return document.body.querySelectorAll('my-button')?.[idx]?.shadowRoot?.querySelector('button')
   }
 
   beforeEach(async () => {
-    document.body.innerHTML = '<my-button name="World"></my-button>'
-    await new Promise<void>((resolve) => {
-      const interval = setInterval(() => {
-        if (getInsideButton()) {
-          clearInterval(interval)
-          resolve()
-        }
-      })
-    })
+    const element = document.createElement('my-button')
+    element.name = 'World'
+    document.body.appendChild(element)
   })
 
   it('should increment the count on each click', () => {
-    getInsideButton()?.click()
-    expect(getInsideButton()?.textContent).toContain('1')
+    getInsideButton(0)?.click()
+    expect(getInsideButton(0)?.textContent).toContain('1')
   })
 
   it('should show name props', () => {
-    getInsideButton()
-    expect(document.body.querySelector('my-button')?.shadowRoot?.innerHTML).toContain('World')
+    getInsideButton(1)
+    expect(document.body.querySelectorAll('my-button')?.[1]?.shadowRoot?.innerHTML).toContain('World')
   })
 
   it('should dispatch count event on button click', () => {
     const spyClick = vi.fn()
 
-    document.querySelector('my-button')!.addEventListener('count', spyClick)
+    document.querySelectorAll('my-button')[2].addEventListener('count', spyClick)
 
-    getInsideButton()?.click()
+    getInsideButton(2)?.click()
 
     expect(spyClick).toHaveBeenCalled()
   })
