8c188964d2859825428ece103b08a4ee034e7876
Han Feng <fenghan770@gmail.com>
2023-07-01 11:29:14 +0800

    refactor: resolve fn args

--- packages/runner/src/fixture.ts
+++ packages/runner/src/fixture.ts
@@ -34,29 +34,9 @@ export function mergeContextFixtures(fixtures: Record<string, any>, context: { f
   // Update dependencies of fixture functions
   fixtureArray.forEach((fixture) => {
     if (fixture.isFn) {
-      const { args, restParams } = parseFnArgs(fixture.value)
-
-      if (args.length <= 1 && !restParams) {
-        // async () => {}
-        // async (use) => {}
-        return
-      }
-
-      // exclude self
-      let deps = context.fixtures!.filter(item => item.index !== fixture.index)
-      if (args.length >= 2 && isObjectDestructuring(args[1])) {
-        const { props, restParams } = getDestructuredProps(args[1])
-        if (!restParams) {
-          // async (use, { a, b }) => {}
-          deps = deps.filter(item => props.includes(item.prop))
-        }
-      }
-
-      // async (...rest) => {}
-      // async (use, ...rest) => {}
-      // async (use, context) => {}
-      // async (use, { a, b, ...rest }) => {}
-      fixture.deps = deps
+      const usedProps = getUsedProps(fixture.value)
+      if (usedProps.length)
+        fixture.deps = context.fixtures!.filter(({ index, prop }) => index !== fixture.index && usedProps.includes(prop))
     }
   })
 
@@ -64,29 +44,12 @@ export function mergeContextFixtures(fixtures: Record<string, any>, context: { f
 }
 
 export function withFixtures(fn: Function, fixtures: FixtureItem[], context: TestContext & Record<string, any>) {
-  const { args, restParams } = parseFnArgs(fn)
-  if ((!args.length && !restParams)) {
-    // test('', () => {})
+  const usedProps = getUsedProps(fn)
+  if (!fixtures.length || !usedProps.length)
     return () => fn(context)
-  }
 
-  // test('', (context) => {})
-  // test('', ({ a, b, ...rest }) => {})
-  let filteredFixtures = fixtures
-
-  if (isObjectDestructuring(args[0])) {
-    const { props, restParams } = getDestructuredProps(args[0])
-    if (!props.length && !restParams) {
-      // test('', ({ }) => {})
-      return () => fn(context)
-    }
-    if (!restParams && props.length) {
-      // test('', ({ a, b }) => {})
-      filteredFixtures = fixtures.filter(item => props.includes(item.prop))
-    }
-  }
-
-  const pendingFixtures = resolveDeps(filteredFixtures)
+  const usedFixtures = fixtures.filter(({ prop }) => usedProps.includes(prop))
+  const pendingFixtures = resolveDeps(usedFixtures)
   let cursor = 0
 
   async function use(fixtureValue: any) {
@@ -99,7 +62,7 @@ export function withFixtures(fn: Function, fixtures: FixtureItem[], context: Tes
 
   async function next() {
     const { value } = pendingFixtures[cursor]
-    typeof value === 'function' ? await value(use, context) : await use(value)
+    typeof value === 'function' ? await value(context, use) : await use(value)
   }
 
   return () => next()
@@ -117,9 +80,7 @@ function resolveDeps(fixtures: FixtureItem[], depSet = new Set<FixtureItem>(), p
       throw new Error('circular fixture dependency')
 
     depSet.add(fixture)
-
     resolveDeps(fixture.deps, depSet, pendingFixtures)
-
     pendingFixtures.push(fixture)
     depSet.clear()
   })
@@ -127,113 +88,51 @@ function resolveDeps(fixtures: FixtureItem[], depSet = new Set<FixtureItem>(), p
   return pendingFixtures
 }
 
-/**
- * To smartly initialize fixtures based on usage, we need to know whether a fixture will be consumed in test function(or in another fixture function) or not, so this function was implemented to get the arguments of both the test function and the fixture function.
- *
- * e.g. `async (use, { a, b }, ...rest) => {}` => `{ args: ['use', '{a,b}'], restParams: true }`
- *
- */
-function parseFnArgs(fn: Function) {
-  let str = fn.toString()
-  str = str.slice(str.indexOf('(') + 1)
-  str = str.replace(/\s|\'.*\'|\".*\"|\`.*\`|\(.*\)/g, '')
-  const parentheses = ['(']
-  const curlyBrackets = []
-  const brackets = []
-  const args: string[] = []
-  let arg = ''
-  let i = 0
+function getUsedProps(fn: Function) {
+  const match = fn.toString().match(/[^(]*\(([^)]*)/)
+  if (!match)
+    return []
 
-  function addArg(a: string) {
-    args.push(a)
-    arg = ''
-  }
+  const args = splitByComma(match[1])
+  if (!args.length)
+    return []
 
-  while (i < str.length) {
-    const s = str[i++]
-    switch (s) {
-      case '(':
-        parentheses.push(s)
-        break
-      case ')':
-        parentheses.pop()
-        if (!parentheses.length) {
-          addArg(arg)
-          break
-        }
-        break
-      case '{':
-        curlyBrackets.push(s)
-        break
-      case '}':
-        curlyBrackets.pop()
-        break
-      case '[':
-        brackets.push(s)
-        break
-      case ']':
-        brackets.pop()
-        break
-      case ',':
-        if (!curlyBrackets.length && !brackets.length) {
-          addArg(arg)
-          continue
-        }
-        break
-    }
-    arg += s
-  }
-  const restParams = args.length > 0 && args.at(-1)?.startsWith('...')
-  const _args = restParams ? args.slice(0, -1) : args
-  return { args: _args.filter(Boolean), restParams }
+  const first = args[0]
+  if (!(first.startsWith('{') && first.endsWith('}')))
+    throw new Error('the first argument must use object destructuring pattern')
+
+  const _first = first.slice(1, -1).replace(/\s/g, '')
+  const props = splitByComma(_first).map((prop) => {
+    return prop.replace(/\:.*|\=.*/g, '')
+  })
+
+  const last = props.at(-1)
+  if (last && last.startsWith('...'))
+    throw new Error('Rest parameters are not supported')
+
+  return props
 }
 
-function isObjectDestructuring(str: string) {
-  return str.startsWith('{') && str.endsWith('}')
-}
-
-/**
- * `'{ a, b: { c }, ...rest }'` => `{ props: ['a', 'b'], restParams: true }`
- */
-function getDestructuredProps(str: string) {
-  str = str.replace(/\s/g, '')
-  const curlyBrackets = ['{']
-  const props: string[] = []
-  let prop = ''
-  let i = 1
-
-  function pushProp(p: string) {
-    p = p.trim()
-    p.length > 0 && props.push(p)
-    prop = ''
-  }
-
-  while (i < str.length) {
-    const s = str[i++]
-    if (s === '{')
-      curlyBrackets.push(s)
-    if (s === '}') {
-      if (curlyBrackets.length === 1) {
-        pushProp(prop)
-        break
-      }
-      else { curlyBrackets.pop() }
+function splitByComma(s: string) {
+  const result = []
+  const stack = []
+  let start = 0
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '{' || s[i] === '[') {
+      stack.push(s[i] === '{' ? '}' : ']')
     }
-    if (s === ',' && curlyBrackets.length === 1) {
-      pushProp(prop)
-      continue
+    else if (s[i] === stack[stack.length - 1]) {
+      stack.pop()
     }
-    prop += s
+    else if (!stack.length && s[i] === ',') {
+      const token = s.substring(start, i).trim()
+      if (token)
+        result.push(token)
+      start = i + 1
+    }
   }
-
-  const restParams = props.length > 0 && props.at(-1)?.startsWith('...')
-  const _props = restParams ? props.slice(0, -1) : props
-  return {
-    props: _props.map((p) => {
-      if (/\:|\=/.test(p))
-        return p.replace(/\:.*|\=.*/g, '')
-      return p
-    }),
-    restParams,
-  }
+  const lastToken = s.substring(start).trim()
+  if (lastToken)
+    result.push(lastToken)
+  return result
 }
