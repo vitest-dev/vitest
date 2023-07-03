936ab9828ce08cb545605f8f02bbbf8df90b3b96
Ray <i@mk1.io>
2023-07-03 21:09:00 +0800

    fix: revert typecheck part

--- packages/vitest/src/defaults.ts
+++ packages/vitest/src/defaults.ts
@@ -90,7 +90,7 @@ const config = {
   dangerouslyIgnoreUnhandledErrors: false,
   typecheck: {
     checker: 'tsc' as const,
-    include: ['**/?(*.){test,spec}-d.?(c|m)[jt]s?(x)'],
+    include: ['**/*.{test,spec}-d.?(c|m)[jt]s?(x)'],
     exclude: defaultExclude,
   },
   slowTestThreshold: 300,
