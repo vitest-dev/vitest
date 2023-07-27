b25f81705798d7ccf268617e7833060e8fdce79f
vinassefranche <vincent.francois@inato.com>
2023-07-27 11:10:05 +0200

    feat: backport types of defineConfig from vite

--- packages/vitest/src/config.ts
+++ packages/vitest/src/config.ts
@@ -10,12 +10,24 @@ export { configDefaults, defaultInclude, defaultExclude, coverageConfigDefaults 
 export { mergeConfig } from 'vite'
 
 export type { ConfigEnv, ViteUserConfig as UserConfig }
+
+export type UserConfigFnObject = (env: ConfigEnv) => ViteUserConfig
+export type UserConfigFnPromise = (env: ConfigEnv) => Promise<ViteUserConfig>
 export type UserConfigFn = (env: ConfigEnv) => ViteUserConfig | Promise<ViteUserConfig>
-export type UserConfigExport = ViteUserConfig | Promise<ViteUserConfig> | UserConfigFn
+export type UserConfigExport =
+  | ViteUserConfig
+  | Promise<ViteUserConfig>
+  | UserConfigFnObject
+  | UserConfigFnPromise
+  | UserConfigFn
 
 export type UserProjectConfigFn = (env: ConfigEnv) => UserWorkspaceConfig | Promise<UserWorkspaceConfig>
 export type UserProjectConfigExport = UserWorkspaceConfig | Promise<UserWorkspaceConfig> | UserProjectConfigFn
 
+export function defineConfig(config: ViteUserConfig): ViteUserConfig
+export function defineConfig(config: Promise<ViteUserConfig>): Promise<ViteUserConfig>
+export function defineConfig(config: UserConfigFnObject): UserConfigFnObject
+export function defineConfig(config: UserConfigExport): UserConfigExport
 export function defineConfig(config: UserConfigExport) {
   return config
 }
