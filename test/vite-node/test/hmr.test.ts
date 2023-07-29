711a6249f1092ddc95b0fb91ff1c080cdda7d62b
Vladimir <sleuths.slews0s@icloud.com>
2023-07-29 20:34:09 +0200

    fix(vite-node): correctly resolve hmr filepath (#3834)

---
+++ test/vite-node/test/hmr.test.ts
@@ -0,0 +1,17 @@
+import { test } from 'vitest'
+import { resolve } from 'pathe'
+import { editFile, runViteNodeCli } from '../../test-utils'
+
+test('hmr.accept works correctly', async () => {
+  const scriptFile = resolve(__dirname, '../src/script.js')
+
+  const viteNode = await runViteNodeCli('--watch', scriptFile)
+
+  await viteNode.waitForStderr('Hello!')
+
+  editFile(scriptFile, content => content.replace('Hello!', 'Hello world!'))
+
+  await viteNode.waitForStderr('Hello world!')
+  await viteNode.waitForStderr('Accept')
+  await viteNode.waitForStdout(`[vite-node] hot updated: ${scriptFile}`)
+})
