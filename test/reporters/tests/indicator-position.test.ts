d90f338ef1b21e3c77cdff78b8716cd627a8e7ee
Han Feng <fenghan770@gmail.com>
2023-08-01 22:20:59 +0800

    test: add test

---
+++ test/reporters/tests/indicator-position.test.ts
@@ -0,0 +1,37 @@
+import { readFileSync } from 'node:fs'
+import { expect, test } from 'vitest'
+import { resolve } from 'pathe'
+import { runVitest } from '../../test-utils'
+
+test('should print correct indicator position', async () => {
+  const filename = resolve('./fixtures/indicator-position.test.ts')
+  const { stderr } = await runVitest({ root: './fixtures' }, [filename])
+  const code = readFileSync(filename, 'utf-8')
+
+  expect(code).toMatch(/\r\n/)
+  expect(stderr).toBeTruthy()
+  expect(stderr).toMatchInlineSnapshot(`
+    "⎯⎯⎯⎯⎯⎯⎯ Failed Tests 1 ⎯⎯⎯⎯⎯⎯⎯
+
+     FAIL  indicator-position.test.ts > 
+    AssertionError: expected 2 to be 3 // Object.is equality
+
+    - Expected
+    + Received
+
+    - 3
+    + 2
+
+     ❯ indicator-position.test.ts:12:17
+         10| 
+         11| test('', async () => {
+         12|   expect(1 + 1).toBe(3)
+           |                 ^
+         13|   expect(1 + 1).toBe(2)
+         14|   expect(2 + 2).toBe(4)
+
+    ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯[1/1]⎯
+
+    "
+  `)
+})
