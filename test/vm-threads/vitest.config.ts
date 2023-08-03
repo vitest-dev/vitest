4fb3d97ad34ff76452e470e6f8b7afa38ec26622
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2023-08-03 16:15:55 +0200

    test: add tests for importing css/assets

--- test/vm-threads/vitest.config.ts
+++ test/vm-threads/vitest.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     experimentalVmThreads: true,
+    css: {
+      include: [/processed/],
+    },
     server: {
       deps: {
         external: [/src\/external/],
