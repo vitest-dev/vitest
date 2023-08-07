7ac694c29a7c01dc0ac1ec382a8e40f7d61c28f7
David Ortner <>
2023-08-07 21:12:33 +0200

    chore: [#3905] fixes linting

--- packages/vitest/src/integrations/env/happy-dom.ts
+++ packages/vitest/src/integrations/env/happy-dom.ts
@@ -1,51 +1,51 @@
-import { importModule } from "local-pkg";
-import type { Environment } from "../../types";
-import { populateGlobal } from "./utils";
+import { importModule } from 'local-pkg'
+import type { Environment } from '../../types'
+import { populateGlobal } from './utils'
 
 export default <Environment>{
-  name: "happy-dom",
-  transformMode: "web",
+  name: 'happy-dom',
+  transformMode: 'web',
   async setupVM() {
     const { Window } = (await importModule(
-      "happy-dom"
-    )) as typeof import("happy-dom");
-    const win = new Window() as any;
+      'happy-dom',
+    )) as typeof import('happy-dom')
+    const win = new Window() as any
 
     // TODO: browser doesn't expose Buffer, but a lot of dependencies use it
-    win.Buffer = Buffer;
-    win.Uint8Array = Uint8Array;
+    win.Buffer = Buffer
+    win.Uint8Array = Uint8Array
 
     // inject structuredClone if it exists
-    if (typeof structuredClone !== "undefined" && !win.structuredClone)
-      win.structuredClone = structuredClone;
+    if (typeof structuredClone !== 'undefined' && !win.structuredClone)
+      win.structuredClone = structuredClone
 
     return {
       getVmContext() {
-        return win;
+        return win
       },
       teardown() {
-        win.happyDOM.cancelAsync();
+        win.happyDOM.cancelAsync()
       },
-    };
+    }
   },
   async setup(global) {
     // happy-dom v3 introduced a breaking change to Window, but
     // provides GlobalWindow as a way to use previous behaviour
     const { Window, GlobalWindow } = (await importModule(
-      "happy-dom"
-    )) as typeof import("happy-dom");
-    const win = new (GlobalWindow || Window)({ url: "http://localhost:3000" });
+      'happy-dom',
+    )) as typeof import('happy-dom')
+    const win = new (GlobalWindow || Window)({ url: 'http://localhost:3000' })
 
     const { keys, originals } = populateGlobal(global, win, {
       bindFunctions: true,
-    });
+    })
 
     return {
       teardown(global) {
-        win.happyDOM.cancelAsync();
-        keys.forEach((key) => delete global[key]);
-        originals.forEach((v, k) => (global[k] = v));
+        win.happyDOM.cancelAsync()
+        keys.forEach(key => delete global[key])
+        originals.forEach((v, k) => (global[k] = v))
       },
-    };
+    }
   },
-};
+}
