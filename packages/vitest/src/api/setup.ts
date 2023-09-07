ca804ef990cd9cb68ab9c83cd02a60b6ffd25269
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2023-09-07 09:38:47 +0300

    fix(vitest): show error when calling API on files that user has no access to

--- packages/vitest/src/api/setup.ts
+++ packages/vitest/src/api/setup.ts
@@ -71,29 +71,28 @@ export function setup(vitestOrWorkspace: Vitest | WorkspaceProject, server?: Vit
         },
         async readSnapshotFile(snapshotPath) {
           if (!ctx.snapshot.resolvedPaths.has(snapshotPath) || !existsSync(snapshotPath))
-            return null
+            throw new Error(`Snapshot file "${snapshotPath}" does not exist.`)
           return fs.readFile(snapshotPath, 'utf-8')
         },
         async readTestFile(id) {
           if (!ctx.state.filesMap.has(id) || !existsSync(id))
-            return null
+            throw new Error(`Test file "${id}" was not registered, so it cannot be read using the API.`)
           return fs.readFile(id, 'utf-8')
         },
         async saveTestFile(id, content) {
-          // can save only already existing test file
           if (!ctx.state.filesMap.has(id) || !existsSync(id))
-            return
+            throw new Error(`Test file "${id}" was not registered, so it cannot be updated using the API.`)
           return fs.writeFile(id, content, 'utf-8')
         },
         async saveSnapshotFile(id, content) {
           if (!ctx.snapshot.resolvedPaths.has(id))
-            return
+            throw new Error(`Snapshot file "${id}" does not exist.`)
           await fs.mkdir(dirname(id), { recursive: true })
           return fs.writeFile(id, content, 'utf-8')
         },
         async removeSnapshotFile(id) {
           if (!ctx.snapshot.resolvedPaths.has(id) || !existsSync(id))
-            return
+            throw new Error(`Snapshot file "${id}" does not exist.`)
           return fs.unlink(id)
         },
         snapshotSaved(snapshot) {
