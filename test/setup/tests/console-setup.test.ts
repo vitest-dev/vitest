61a5e95f356e826253ba07143d1eb14043150a86
Dunqing <dengqing0821@gmail.com>
2023-09-19 13:34:25 +0800

    test: add test case

---
+++ test/setup/tests/console-setup.test.ts
@@ -0,0 +1,20 @@
+import path from 'node:path'
+import { describe, expect, test } from 'vitest'
+
+import { runVitest } from '../../test-utils'
+
+async function run() {
+  return await runVitest({
+    include: ['tests/fixtures/console.test.ts'],
+    setupFiles: ['setupFiles/console-setup.ts'],
+  })
+}
+
+describe('setup files console', () => {
+  test('print stdout and stderr correctly', async () => {
+    const { stdout, stderr } = await run()
+    const filepath = path.resolve(__dirname, '../setupFiles/console-setup.ts')
+    expect(stdout).toContain(`stdout | ${filepath}`)
+    expect(stderr).toContain(`stderr | ${filepath}`)
+  })
+})
