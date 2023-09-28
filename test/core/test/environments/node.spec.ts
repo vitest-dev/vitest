cbe133daf5899973d710797089dbc5f21a981398
Vladimir <sleuths.slews0s@icloud.com>
2023-09-28 09:16:35 +0200

    fix: overwrite global URL with environment's (#4164)

---
+++ test/core/test/environments/node.spec.ts
@@ -0,0 +1,11 @@
+// @vitest-environment node
+
+import { expect, test } from 'vitest'
+
+const nodeMajor = Number(process.version.slice(1).split('.')[0])
+
+test.runIf(nodeMajor > 16)('url correctly creates an object', () => {
+  expect(() => {
+    URL.createObjectURL(new Blob([]))
+  }).not.toThrow()
+})
