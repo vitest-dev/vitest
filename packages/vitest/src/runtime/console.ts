ec2e80406555d1eaceefc261d7a50675c2363882
Dunqing <dengqing0821@gmail.com>
2023-10-05 01:59:55 -0500

    fix(vitest): print file path instead of "unknown test" when logging (#4146)

--- packages/vitest/src/runtime/console.ts
+++ packages/vitest/src/runtime/console.ts
@@ -1,14 +1,36 @@
 import { Writable } from 'node:stream'
 import { Console } from 'node:console'
+import { relative } from 'node:path'
 import { getSafeTimers } from '@vitest/utils'
 import { RealDate } from '../integrations/mock/date'
 import type { WorkerGlobalState } from '../types'
+
+export const UNKNOWN_TEST_ID = '__vitest__unknown_test__'
+
+function getTaskIdByStack(root: string) {
+  const stack = new Error('STACK_TRACE_ERROR').stack?.split('\n')
+
+  if (!stack)
+    return UNKNOWN_TEST_ID
+
+  const index = stack.findIndex(line => line.includes('at Console.value (node:internal/console/'))
+  const line = index === -1 ? null : stack[index + 2]
+
+  if (!line)
+    return UNKNOWN_TEST_ID
+
+  const filepath = line.match(/at\s(.*)\s?/)?.[1]
+
+  if (filepath)
+    return relative(root, filepath)
+
+  return UNKNOWN_TEST_ID
+}
 
 export function createCustomConsole(state: WorkerGlobalState) {
   const stdoutBuffer = new Map<string, any[]>()
   const stderrBuffer = new Map<string, any[]>()
   const timers = new Map<string, { stdoutTime: number; stderrTime: number; timer: any }>()
-  const unknownTestId = '__vitest__unknown_test__'
 
   const { setTimeout, clearTimeout } = getSafeTimers()
 
@@ -63,7 +85,7 @@ export function createCustomConsole(state: WorkerGlobalState) {
 
   const stdout = new Writable({
     write(data, encoding, callback) {
-      const id = state?.current?.id ?? unknownTestId
+      const id = state?.current?.id ?? getTaskIdByStack(state.ctx.config.root)
       let timer = timers.get(id)
       if (timer) {
         timer.stdoutTime = timer.stdoutTime || RealDate.now()
@@ -84,7 +106,7 @@ export function createCustomConsole(state: WorkerGlobalState) {
   })
   const stderr = new Writable({
     write(data, encoding, callback) {
-      const id = state?.current?.id ?? unknownTestId
+      const id = state?.current?.id ?? getTaskIdByStack(state.ctx.config.root)
       let timer = timers.get(id)
       if (timer) {
         timer.stderrTime = timer.stderrTime || RealDate.now()
