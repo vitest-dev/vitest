17e7349cc5b5795cfb1291aa08c4b90399fec638
Dunqing <dengqing0821@gmail.com>
2023-10-06 10:34:34 -0500

    Update nested-test.test.ts

--- test/core/test/nested-test.test.ts
+++ test/core/test/nested-test.test.ts
@@ -3,13 +3,13 @@ import { expect, test } from 'vitest'
 test('nested test should throw error', () => {
   expect(() => {
     test('test inside test', () => {})
-  }).toThrowErrorMatchingInlineSnapshot(`"Nested tests are not supported"`)
+  }).toThrowErrorMatchingInlineSnapshot(`"Nested tests are not allowed"`)
 
   expect(() => {
     test.each([1, 2, 3])('test.each inside test %d', () => {})
-  }).toThrowErrorMatchingInlineSnapshot(`"Nested tests are not supported"`)
+  }).toThrowErrorMatchingInlineSnapshot(`"Nested tests are not allowed"`)
 
   expect(() => {
     test.skipIf(false)('test.skipIf inside test', () => {})
-  }).toThrowErrorMatchingInlineSnapshot(`"Nested tests are not supported"`)
+  }).toThrowErrorMatchingInlineSnapshot(`"Nested tests are not allowed"`)
 })
