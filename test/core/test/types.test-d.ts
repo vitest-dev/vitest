cd9a81eca931b29ca4992822bf1d462597ca1e2d
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2023-10-17 14:18:34 +0200

    chore: cleanup

--- test/core/test/types.test-d.ts
+++
@@ -1,6 +0,0 @@
-import { expectTypeOf, it } from 'vitest'
-
-it('typecheck works', () => {
-  expectTypeOf(4).toBeNumber()
-  expectTypeOf('').not.toBeNumber()
-})
--- test/core/vite.config.ts
+++ test/core/vite.config.ts
@@ -88,8 +88,5 @@ export default defineConfig({
         customResolver: () => resolve(__dirname, 'src', 'aliased-mod.ts'),
       },
     ],
-    typecheck: {
-      enabled: true,
-    },
   },
 })
