936d9884fb1c2c39f103f51fc0e2d81e3d113830
Cristopher Namchee <cristophernamchee12@gmail.com>
2023-11-02 21:46:22 +0700

    test: pass test

--- test/cli/test/exclude.test.ts
+++ test/cli/test/exclude.test.ts
@@ -2,18 +2,6 @@ import { expect, test } from 'vitest'
 
 import { runVitestCli } from '../../test-utils'
 
-test('should test nothing', async () => {
-  const { stderr } = await runVitestCli(
-    'run',
-    '--config',
-    'fixtures/exclude/vitest.exclude.config.ts',
-    '--exclude',
-    'fixtures/exclude/math.test.ts',
-  )
-
-  expect(stderr).toContain('No test files found, exiting with code 1')
-})
-
 test('should still test math.test.ts', async () => {
   const { stderr, stdout } = await runVitestCli(
     'run',
