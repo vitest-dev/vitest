c6d85fa27a34a8bc6075ff309e87b4b4fc8da792
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2023-11-15 15:56:58 +0100

    chore: fix entry resolution

--- packages/browser/src/node/index.ts
+++ packages/browser/src/node/index.ts
@@ -4,7 +4,6 @@ import { resolve } from 'node:path'
 import { builtinModules } from 'node:module'
 import sirv from 'sirv'
 import type { Plugin } from 'vite'
-import { defaultExclude, defaultInclude } from 'vitest/config'
 import type { WorkspaceProject } from 'vitest/node'
 import { injectVitestModule } from './esmInjector'
 
@@ -36,17 +35,16 @@ export default (project: WorkspaceProject, base = '/'): Plugin[] => {
     {
       name: 'vitest:browser:tests',
       enforce: 'pre',
-      async config(config) {
+      async config() {
         const {
-          include = defaultInclude,
-          exclude = defaultExclude,
+          include,
+          exclude,
           includeSource,
           dir,
           root,
-        } = config.test || {}
-        const projectRoot = dir || root || config.root
-        const resolvedRoot = projectRoot ? resolve(projectRoot) : process.cwd()
-        const entries = await project.globAllTestFiles(include, exclude, includeSource, resolvedRoot)
+        } = project.config
+        const projectRoot = dir || root
+        const entries = await project.globAllTestFiles(include, exclude, includeSource, projectRoot)
         return {
           optimizeDeps: {
             entries,
