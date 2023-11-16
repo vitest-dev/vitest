648bccb9409c78544f197f425395231472a9c4a9
Vladimir <sleuths.slews0s@icloud.com>
2023-11-16 09:31:26 +0100

    fix(browser): add vitest/ imports to entries (#4514)

--- packages/browser/src/node/index.ts
+++ packages/browser/src/node/index.ts
@@ -47,7 +47,12 @@ export default (project: WorkspaceProject, base = '/'): Plugin[] => {
         const entries = await project.globAllTestFiles(include, exclude, includeSource, projectRoot)
         return {
           optimizeDeps: {
-            entries,
+            entries: [
+              ...entries,
+              'vitest/utils',
+              'vitest/browser',
+              'vitest/runners',
+            ],
             exclude: [
               ...builtinModules,
               'vitest',
