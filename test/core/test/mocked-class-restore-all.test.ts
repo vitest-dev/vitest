24d5acc77a49c9e5aece3ae083a1b36861be21d3
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-11-29 10:00:27 +0900

    test: restoreAllMocks for mocked class methods (wip)

--- test/core/test/mocked-class-restore-all.test.ts
+++ test/core/test/mocked-class-restore-all.test.ts
@@ -4,10 +4,8 @@ import { MockedE } from '../src/mockedE'
 
 vi.mock('../src/mockedE')
 
-// this behavior looks odd but jest also doesn't seem to support this use case properly
-test(`mocked class method not restorable`, () => {
+test(`mocked class are not affected by restoreAllMocks`, () => {
   const instance1 = new MockedE()
-
   expect(instance1.testFn('a')).toMatchInlineSnapshot(`undefined`)
   expect(vi.mocked(instance1.testFn).mock.calls).toMatchInlineSnapshot(`
     [
@@ -24,30 +22,11 @@ test(`mocked class method not restorable`, () => {
     ]
   `)
 
-  // restoring instance method
-  vi.mocked(instance1.testFn).mockRestore()
-  expect(vi.mocked(instance1.testFn).mock.calls).toMatchInlineSnapshot(`
-    [
-      [
-        "a",
-      ],
-    ]
-  `)
-  expect(vi.mocked(MockedE.prototype.testFn).mock.calls).toMatchInlineSnapshot(`
-    [
-      [
-        "a",
-      ],
-    ]
-  `)
-
+  vi.restoreAllMocks()
   expect(instance1.testFn('b')).toMatchInlineSnapshot(`undefined`)
   expect(vi.mocked(instance1.testFn).mock.calls).toMatchInlineSnapshot(`
     [
       [
-        "a",
-      ],
-      [
         "b",
       ],
     ]
@@ -55,22 +34,28 @@ test(`mocked class method not restorable`, () => {
   expect(vi.mocked(MockedE.prototype.testFn).mock.calls).toMatchInlineSnapshot(`
     [
       [
-        "a",
-      ],
-      [
         "b",
       ],
     ]
   `)
 
-  // restoring prototype doesn't restore instance
-  vi.mocked(MockedE.prototype.testFn).mockRestore()
-  expect(vi.mocked(instance1.testFn).mock.calls).toMatchInlineSnapshot(`[]`)
-  expect(vi.mocked(MockedE.prototype.testFn).mock.calls).toMatchInlineSnapshot(`[]`)
-
-  expect(instance1.testFn('c')).toMatchInlineSnapshot(`undefined`)
+  const instance2 = new MockedE()
+  expect(instance2.testFn('c')).toMatchInlineSnapshot(`undefined`)
   expect(vi.mocked(instance1.testFn).mock.calls).toMatchInlineSnapshot(`
     [
+      [
+        "b",
+      ],
+      [
+        "c",
+      ],
+    ]
+  `)
+  expect(vi.mocked(instance2.testFn).mock.calls).toMatchInlineSnapshot(`
+    [
+      [
+        "b",
+      ],
       [
         "c",
       ],
@@ -78,6 +63,9 @@ test(`mocked class method not restorable`, () => {
   `)
   expect(vi.mocked(MockedE.prototype.testFn).mock.calls).toMatchInlineSnapshot(`
     [
+      [
+        "b",
+      ],
       [
         "c",
       ],
