beaac464dd2db5b13fab7eb06801231c88b0fe45
Dunqing <dengqing0821@gmail.com>
2023-12-04 17:04:05 +0800

    test: update snapshot

--- test/core/test/injector-mock.test.ts
+++ test/core/test/injector-mock.test.ts
@@ -1,46 +1,50 @@
-import { parseAst } from 'rollup/parseAst'
 import { expect, test } from 'vitest'
 import { hoistMocks } from '../../../packages/vitest/src/node/hoistMocks'
 
-function parse(code: string, options: any) {
-  return parseAst(code, options)
+async function hoistSimpleCode(code: string) {
+  return (await hoistMocks(code))?.code
 }
 
-function hoistSimpleCode(code: string) {
-  return hoistMocks(code, '/test.js', parse)?.code.trim()
-}
-
-test('hoists mock, unmock, hoisted', () => {
-  expect(hoistSimpleCode(`
+test('hoists mock, unmock, hoisted', async () => {
+  expect(await hoistSimpleCode(`
   vi.mock('path', () => {})
   vi.unmock('path')
   vi.hoisted(() => {})
   `)).toMatchInlineSnapshot(`
-    "if (typeof globalThis.vi === "undefined" && typeof globalThis.vitest === "undefined") { throw new Error("There are some problems in resolving the mocks API.\\nYou may encounter this issue when importing the mocks API from another module other than 'vitest'.\\nTo fix this issue you can either:\\n- import the mocks API directly from 'vitest'\\n- enable the 'globals' options") }
-    vi.mock('path', () => {})
+    "vi.mock('path', () => {})
     vi.unmock('path')
-    vi.hoisted(() => {})"
+    vi.hoisted(() => {})
+
+
+
+
+      "
   `)
 })
 
-test('always hoists import from vitest', () => {
-  expect(hoistSimpleCode(`
+test('always hoists import from vitest', async () => {
+  expect(await hoistSimpleCode(`
   import { vi } from 'vitest'
   vi.mock('path', () => {})
   vi.unmock('path')
   vi.hoisted(() => {})
   import { test } from 'vitest'
   `)).toMatchInlineSnapshot(`
-    "const { vi } = await import('vitest')
-    const { test } = await import('vitest')
-    vi.mock('path', () => {})
+    "vi.mock('path', () => {})
     vi.unmock('path')
-    vi.hoisted(() => {})"
+    vi.hoisted(() => {})
+
+      import { vi } from 'vitest'
+
+
+
+      import { test } from 'vitest'
+      "
   `)
 })
 
-test('always hoists all imports but they are under mocks', () => {
-  expect(hoistSimpleCode(`
+test('always hoists all imports but they are under mocks', async () => {
+  expect(await hoistSimpleCode(`
   import { vi } from 'vitest'
   import { someValue } from './path.js'
   import { someValue2 } from './path2.js'
@@ -49,25 +53,32 @@ test('always hoists all imports but they are under mocks', () => {
   vi.hoisted(() => {})
   import { test } from 'vitest'
   `)).toMatchInlineSnapshot(`
-    "const { vi } = await import('vitest')
-    const { test } = await import('vitest')
-    vi.mock('path', () => {})
+    "vi.mock('path', () => {})
     vi.unmock('path')
     vi.hoisted(() => {})
-    const { someValue } = await import('./path.js')
-    const { someValue2 } = await import('./path2.js')"
+
+      import { vi } from 'vitest'
+      import { someValue } from './path.js'
+      import { someValue2 } from './path2.js'
+
+
+
+      import { test } from 'vitest'
+      "
   `)
 })
 
-test('correctly mocks namespaced', () => {
-  expect(hoistSimpleCode(`
+test('correctly mocks namespaced', async () => {
+  expect(await hoistSimpleCode(`
   import { vi } from 'vitest'
   import add, * as AddModule from '../src/add'
   vi.mock('../src/add', () => {})
   `)).toMatchInlineSnapshot(`
-    "const { vi } = await import('vitest')
-    vi.mock('../src/add', () => {})
-    const AddModule = await import('../src/add')
-    const { default: add } = AddModule"
+    "vi.mock('../src/add', () => {})
+
+      import { vi } from 'vitest'
+      import add, * as AddModule from '../src/add'
+
+      "
   `)
 })
