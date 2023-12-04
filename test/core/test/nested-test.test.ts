9e60bb1a1b661afb427af2ab64068170cedac9e7
Dunqing <dengqing0821@gmail.com>
2023-12-04 22:35:25 +0800

    feat: update

--- test/core/test/nested-test.test.ts
+++ test/core/test/nested-test.test.ts
@@ -3,28 +3,28 @@ import { describe, expect, test } from 'vitest'
 test('nested test should throw error', () => {
   expect(() => {
     test('test inside test', () => {})
-  }).toThrowErrorMatchingInlineSnapshot(`[Error: Nested "test" or "it" are not allowed.]`)
+  }).toThrowErrorMatchingInlineSnapshot(`[Error: Calling the "test" or "it" function inside another "test" or "it" function is not allowed. Please, put it inside "describe" so it can be properly collected.]`)
 
   expect(() => {
     test.each([1, 2, 3])('test.each inside test %d', () => {})
-  }).toThrowErrorMatchingInlineSnapshot(`[Error: Nested "test" or "it" are not allowed.]`)
+  }).toThrowErrorMatchingInlineSnapshot(`[Error: Calling the "test" or "it" function inside another "test" or "it" function is not allowed. Please, put it inside "describe" so it can be properly collected.]`)
 
   expect(() => {
     test.skipIf(false)('test.skipIf inside test', () => {})
-  }).toThrowErrorMatchingInlineSnapshot(`[Error: Nested "test" or "it" are not allowed.]`)
+  }).toThrowErrorMatchingInlineSnapshot(`[Error: Calling the "test" or "it" function inside another "test" or "it" function is not allowed. Please, put it inside "describe" so it can be properly collected.]`)
 })
 
 describe('parallel tests', () => {
   test.concurrent('parallel test 1 with nested test', () => {
     expect(() => {
       test('test inside test', () => {})
-    }).toThrowErrorMatchingInlineSnapshot(`[Error: Nested "test" or "it" are not allowed.]`)
+    }).toThrowErrorMatchingInlineSnapshot(`[Error: Calling the "test" or "it" function inside another "test" or "it" function is not allowed. Please, put it inside "describe" so it can be properly collected.]`)
   })
   test.concurrent('parallel test 2 without nested test', () => {})
   test.concurrent('parallel test 3 without nested test', () => {})
   test.concurrent('parallel test 4 with nested test', () => {
     expect(() => {
       test('test inside test', () => {})
-    }).toThrowErrorMatchingInlineSnapshot(`[Error: Nested "test" or "it" are not allowed.]`)
+    }).toThrowErrorMatchingInlineSnapshot(`[Error: Calling the "test" or "it" function inside another "test" or "it" function is not allowed. Please, put it inside "describe" so it can be properly collected.]`)
   })
 })
