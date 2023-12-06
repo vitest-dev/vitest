1e4aa8e410f79b22d4df38a0fe7722bd76d7cfbe
Vladimir <sleuths.slews0s@icloud.com>
2023-12-06 10:19:10 +0100

    fix(runner): improve fixture error messages (#4673)

--- test/fails/fixtures/test-extend/fixture-error.test.ts
+++ test/fails/fixtures/test-extend/fixture-error.test.ts
@@ -1,4 +1,4 @@
-import { afterEach, beforeEach, describe, expectTypeOf, test } from 'vitest'
+import { afterEach, beforeEach, describe, expectTypeOf, test, expect } from 'vitest'
 
 describe('error thrown in beforeEach fixtures', () => {
   const myTest = test.extend<{ a: never }>({
@@ -37,4 +37,17 @@ describe('error thrown in test fixtures', () => {
 
   // eslint-disable-next-line unused-imports/no-unused-vars
   myTest('fixture errors', ({ a }) => {})
+})
+
+// TODO: enable when #4669 is fixed
+describe.skip('correctly fails when test times out', () => {
+  const myTest = test.extend<{ a: number }>({
+    a: async ({}, use) => {
+      await use(2)
+    },
+  })
+  myTest('test times out', async ({ a }) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+    expect(a).toBe(2)
+  }, 20)
 })
