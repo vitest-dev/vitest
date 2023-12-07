5cb1a0767b5591de33af6c5d62a372d840c065c9
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2023-12-07 13:02:09 +0100

    chore: add comment

--- test/changed/tests/workspaceRelated.test.ts
+++ test/changed/tests/workspaceRelated.test.ts
@@ -8,6 +8,7 @@ it('doesn\'t run any test in a workspace because there are no changes', async ()
   expect(stdout).toContain('No test files found, exiting with code 0')
 })
 
+// Fixes #4674
 it('related correctly runs only related tests inside a workspace', async () => {
   editFile(resolvePath(import.meta.url, '../fixtures/workspace/packages/packageA/index.js'), content => `${content}\n`)
   const { stdout, stderr } = await runVitestCli('--root', join(process.cwd(), './fixtures/workspace'), '--no-watch', '--changed')
