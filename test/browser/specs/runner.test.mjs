0c4ee5f3bea33bc2ba5988d7200274b00598f797
Mike Bland <mbland@acm.org>
2023-12-06 19:14:20 -0500

    chore(test/browser): extract runVitest helper
    This is in preparation for adding a new test file which will run the
    tests with config.base set to confirm the fix for #4686.

--- test/browser/specs/runner.test.mjs
+++ test/browser/specs/runner.test.mjs
@@ -1,27 +1,14 @@
 import assert from 'node:assert'
-import { readFile } from 'node:fs/promises'
 import test from 'node:test'
-import { execa } from 'execa'
+import runVitest from './run-vitest.mjs'
 
-const browser = process.env.BROWSER || (process.env.PROVIDER === 'playwright' ? 'chromium' : 'chrome')
-
-const { stderr, stdout } = await execa('npx', ['vitest', '--run', `--browser.name=${browser}`, '--browser.headless'], {
-  env: {
-    ...process.env,
-    CI: 'true',
-    NO_COLOR: 'true',
-  },
-  reject: false,
-})
-
-const browserResult = await readFile('./browser.json', 'utf-8')
-const browserResultJson = JSON.parse(browserResult)
-
-const getPassed = results => results.filter(result => result.status === 'passed')
-const getFailed = results => results.filter(result => result.status === 'failed')
-
-const passedTests = getPassed(browserResultJson.testResults)
-const failedTests = getFailed(browserResultJson.testResults)
+const {
+  stderr,
+  stdout,
+  browserResultJson,
+  passedTests,
+  failedTests,
+} = await runVitest()
 
 await test('tests are actually running', async () => {
   assert.ok(browserResultJson.testResults.length === 8, 'Not all the tests have been run')
