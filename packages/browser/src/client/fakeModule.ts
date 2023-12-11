75ce57686302ab12476fedff6214fa495a8216aa
Sam Thorogood <sam.thorogood@gmail.com>
2023-12-11 23:16:58 +1100

    simplify fakeModule

--- packages/browser/src/client/fakeModule.ts
+++ packages/browser/src/client/fakeModule.ts
@@ -3,85 +3,56 @@ function neverFunctionForProxy() {
   throw new Error(`should not get here`)
 }
 
-const originalSymbol = Symbol('orig')
-
 // allows replacing 'function' types on this module: passes through to the 'current value'
-function prepareFakeModuleForSpy(module: any) {
-  for (const key in module) {
-    let value: Function = module[key]
-    if (typeof value !== 'function')
-      continue
-
-    try {
-      delete module[key]
-    }
-    catch {
+function prepareFakeModuleForSpy(fakeModule: any, contents: any) {
+  for (const key in contents) {
+    if (typeof contents[key] !== 'function') {
+      fakeModule[key] = contents[key]
       continue
     }
 
     // uses Reflect to implement Proxy, but use 'current value' and not the original target
+    // this is a Proxy pointing _to_ Reflect, which means it "supports all operations" but on the dynamic target
     const currentValueReflect = new Proxy(Reflect, {
       get(_reflect, reflectKey) {
-        // TODO: does this support ctor mocking (new.target)
-        return (...args: any[]) => (Reflect as any)[reflectKey](value, ...args.slice(1))
+        return (...args: any[]) => (Reflect as any)[reflectKey](contents[key], ...args.slice(1))
       },
     })
-    const p = new Proxy(neverFunctionForProxy, currentValueReflect)
-
-    const get = () => p
-    get[originalSymbol] = true
-
-    const set = (v: any) => value = v
-    set[originalSymbol] = true
-
-    Object.defineProperty(module, key, {
-      get,
-      set,
-      enumerable: true,
-      configurable: true,
-    })
+    fakeModule[key] = new Proxy(neverFunctionForProxy, currentValueReflect)
   }
 }
 
 // this enables bound-like named imports with the import rewriting scheme
-export function buildFakeModule<T extends Record<string | symbol, any>>(contents: T): T {
-  const out: Record<string | symbol, any> = { [Symbol.toStringTag]: 'Module', ...contents }
+export function buildFakeModule<T extends Record<string | symbol, any>>(module: T): T {
+  const fakeModule: Record<string | symbol, any> = { [Symbol.toStringTag]: 'Module' }
+  const contents: Record<string | symbol, any> = { ...module }
 
-  prepareFakeModuleForSpy(out)
+  prepareFakeModuleForSpy(fakeModule, contents)
 
   // this intercepts tinyspy which uses Object.defineProperty to rewrite an object (even a "module")
   return new Proxy({}, {
     ownKeys(_target) {
-      return Reflect.ownKeys(out)
+      return Reflect.ownKeys(fakeModule)
     },
 
-    get(_target, p) {
-      return out[p]
+    set(_target, _property) {
+      return false
+    },
+
+    get(_target, property) {
+      return fakeModule[property]
     },
 
     getOwnPropertyDescriptor(_target, p) {
-      return Reflect.getOwnPropertyDescriptor(out, p)
+      return Reflect.getOwnPropertyDescriptor(contents, p)
     },
 
     defineProperty(_target, property, attributes) {
-      // detect original & restore, otherwise this causes recursive call to self
-      if ((attributes.get as any)?.[originalSymbol]) {
-        out[property] = contents[property]
-        return true
-      }
+      if (attributes.get || attributes.set)
+        throw new Error(`can't defineProperty with get/set on fake module`)
 
       if ('value' in attributes) {
-        out[property] = attributes.value
-        return true
-      }
-
-      if ('get' in attributes) {
-        if (!(attributes.set as any)?.[originalSymbol]) {
-          // tinyspy passes back the same setter from attributes
-          throw new Error(`setter must be original`)
-        }
-        // TODO: this predisposes a setter, which we could skip (different indirection)
-        out[property] = attributes.get!()
+        contents[property] = attributes.value
         return true
       }
 
