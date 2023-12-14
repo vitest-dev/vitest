6c9040f48f20ec24aaf4791c83fd0312e3922759
Sam Thorogood <sam.thorogood@gmail.com>
2023-12-14 14:19:03 +1100

    don't spy on vite

--- packages/browser/src/node/esmProxy.ts
+++ packages/browser/src/node/esmProxy.ts
@@ -4,6 +4,13 @@ import type { Expression } from 'estree'
 import type { Positioned } from './esmWalker'
 import { esmWalker } from './esmWalker'
 
+// don't allow mocking vitest itself
+// (this *also* changes the position of user code in the getImporter() helper)
+const skipImports = [
+  '^vitest$',
+  '^@vitest/',
+]
+
 export async function insertEsmProxy(
   code: string,
   id: string,
@@ -22,9 +29,18 @@ export async function insertEsmProxy(
 
   esmWalker(ast, {
     onDynamicImport(node) {
-      const replace = '__vitest_mocker__.wrap(() => import('
-      s.overwrite(node.start, (node.source as Positioned<Expression>).start, replace)
-      s.overwrite(node.end - 1, node.end, '))')
+      const expression = (node.source as Positioned<Expression>)
+      if (!(expression.type === 'Literal' && typeof expression.value === 'string'))
+        // this is a non-string import so vite won't change it anyway
+        return
+
+      const value = expression.value as string
+
+      if (skipImports.some(i => value.match(i)))
+        return
+
+      const replace = `__vitest_mocker__.wrap(() => import(${JSON.stringify(value)}))`
+      s.overwrite(node.start, node.end, replace)
     },
     onIdentifier() {},
     onImportMeta() {},
