f86d0e912f73e740c18d20cb8d0c979be1023c69
Jonathan MASSUCHETTI <jonathan.massuchetti@dappit.fr>
2023-12-14 11:30:05 +0100

    test(fixture): beforeEach concurrent tests issue reproduction

--- test/core/test/fixture-concurrent.test.ts
+++ test/core/test/fixture-concurrent.test.ts
@@ -1,9 +1,14 @@
-import { expect, test } from 'vitest'
+import { afterAll, beforeEach, expect, test } from 'vitest'
 
-export const myTest = test.extend<{
+let i = 0
+
+interface MyFixtures {
   a: string
   b: string
-}>({
+  counter: number
+}
+
+export const myTest = test.extend<MyFixtures>({
   a: async ({ task }: any, use) => {
     await new Promise<void>(resolve => setTimeout(resolve, 200))
     await use(task.id)
@@ -11,14 +16,54 @@ export const myTest = test.extend<{
   b: async ({ a }, use) => {
     await use(a)
   },
+  counter: async ({}, use) => {
+    await use(i++)
+  },
 })
 
-myTest.concurrent('fixture - concurrent test 1', ({ a, b, task }) => {
-  expect(a).toBe(task.id)
-  expect(b).toBe(task.id)
+/**
+ * The `beforeEach` hook causes the issue.
+ */
+beforeEach<MyFixtures>(({ counter }) => {
+  // this is a dummy expectation to make use of the fixture
+  expect(counter).toBeTypeOf('number')
 })
 
-myTest.concurrent('fixture - concurrent test 2', ({ a, b, task }) => {
+/**
+ * Let N be the number of concurrent tests in the suite and M the number of non-concurrent tests.
+ * The fixture should be invoked N+M times.
+ * The issue is that it is invoked (2*N - 1) + M times.
+ */
+afterAll(() => {
+  expect(i).toEqual(4)
+})
+
+myTest.concurrent('fixture - concurrent test 1', ({ a, b, counter, task }) => {
   expect(a).toBe(task.id)
   expect(b).toBe(task.id)
+  expect(counter).toBeTypeOf('number')
+})
+
+myTest.concurrent('fixture - concurrent test 2', ({ a, b, counter, task }) => {
+  expect(a).toBe(task.id)
+  expect(b).toBe(task.id)
+  expect(counter).toBeTypeOf('number')
+})
+
+myTest.concurrent('fixture - concurrent test 3', ({ a, b, counter, task }) => {
+  expect(a).toBe(task.id)
+  expect(b).toBe(task.id)
+  expect(counter).toBeTypeOf('number')
+})
+
+myTest.concurrent('fixture - concurrent test 4', ({ a, b, counter, task }) => {
+  expect(a).toBe(task.id)
+  expect(b).toBe(task.id)
+  expect(counter).toBeTypeOf('number')
+})
+
+myTest('fixture - non-concurrent test', ({ a, b, counter, task }) => {
+  expect(a).toBe(task.id)
+  expect(b).toBe(task.id)
+  expect(counter).toBeTypeOf('number')
 })
