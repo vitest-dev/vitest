e975b5729fbd519fe1dec2c8f9e40f75bdba2251
Eduardo San Martin Morote <posva@users.noreply.github.com>
2023-12-19 14:55:27 +0100

    types: use satisfies for defaults (#4738)

--- packages/vitest/src/defaults.ts
+++ packages/vitest/src/defaults.ts
@@ -101,6 +101,6 @@ const config = {
     exclude: defaultExclude,
   },
   slowTestThreshold: 300,
-}
+} satisfies UserConfig
 
-export const configDefaults: Required<Pick<UserConfig, keyof typeof config>> = Object.freeze(config)
+export const configDefaults = Object.freeze(config)
