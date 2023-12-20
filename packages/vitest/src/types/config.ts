669c6f252de28c3442faafb2a43b0e9d11c51c5f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-12-20 15:52:50 +0900

    chore: jsdoc

--- packages/vitest/src/types/config.ts
+++ packages/vitest/src/types/config.ts
@@ -674,9 +674,9 @@ export interface InlineConfig {
    * Intercept global console logging during tests to print each log labelled with test file and title.
    * This is also required for console log preview on Vitest UI.
    *
-   * Disabling this option might help to debug a code with normal synchronus terminal console logging.
+   * Disabling this option might help when you want to debug a code with normal synchronus terminal console logging.
    *
-   * This option has no effect on browser pool and vmThreads pool. (?)
+   * This option has no effect on browser pool since Vitest preserves original logging on browser devtools.
    *
    * @default true
    */
