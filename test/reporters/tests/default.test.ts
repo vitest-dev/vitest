f51289a8ccb6992bc9a3852f391d2799a03e1906
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-12-21 14:51:55 +0900

    test: add test

--- test/reporters/tests/default.test.ts
+++ test/reporters/tests/default.test.ts
@@ -54,4 +54,53 @@ describe('default reporter', async () => {
     expect(vitest.stdout).not.toContain('✓ b1 test')
     expect(vitest.stdout).not.toContain('✓ b2 test')
   })
+
+  test('autocomplete', async () => {
+    const vitest = await run([], true, '-t', 'passed')
+    vitest.resetOutput()
+
+    // show all when no filter
+    vitest.write('p')
+    expect(await getAutocomplete(vitest)).toEqual(['a.test.ts', 'b1.test.ts', 'b2.test.ts'])
+
+    // filter "a"
+    vitest.write('a')
+    expect(await getAutocomplete(vitest)).toEqual(['a.test.ts'])
+
+    // run test
+    vitest.resetOutput()
+    vitest.write('\n')
+    await vitest.waitForStdout('Waiting for file changes...')
+
+    // last filter "a" is preserved
+    vitest.write('p')
+    expect(await getAutocomplete(vitest)).toEqual(['a.test.ts'])
+
+    // filter "b"
+    vitest.write('\bb')
+    expect(await getAutocomplete(vitest)).toEqual(['b1.test.ts', 'b2.test.ts'])
+  })
 }, 120000)
+
+async function getAutocomplete(vitest: { stdout: string }) {
+  await waitForStableOutput(vitest)
+
+  // get entries after the last prompt
+  const last = vitest.stdout.split(/.*Input filename pattern.*/).at(-1) ?? ''
+  return last.trim().split('\n').map(line => line.split('/').at(-1) ?? '')
+}
+
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+
+// TOOD: to test-utils?
+async function waitForStableOutput(vitest: { stdout: string }) {
+  let last = vitest.stdout
+  for (let i = 0; i < 20; i++) {
+    await sleep(500)
+    if (last === vitest.stdout)
+      return
+
+    last = vitest.stdout
+  }
+  throw new Error('waitForStableOutput')
+}
