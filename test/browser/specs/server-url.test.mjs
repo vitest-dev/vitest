d4b67765705e41fc38cc7678cc425274e6c2c442
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-01-03 18:54:57 +0900

    test: better assert

--- test/browser/specs/server-url.test.mjs
+++ test/browser/specs/server-url.test.mjs
@@ -9,12 +9,12 @@ test('server-url http', async () => {
       NO_COLOR: '1',
     },
   })
-  assert.ok(result.stdout.includes('Browser runner started at http://localhost:5173/'))
-  assert.ok(result.stdout.includes('Test Files  1 passed'))
+  assert.match(result.stdout, /Browser runner started at http:\/\/localhost:5173\//);
+  assert.match(result.stdout, /Test Files  1 passed/);
 })
 
 // the test is skipped since browser warns self-signed https and it requires manual interaction.
-// please comment out this "skip" to verify manually.
+// you can toggle this "skip" to verify this test locally.
 test('server-url https', { skip: true }, async () => {
   const result = await execa('npx', ['vitest', 'run', '--root=./fixtures/server-url'], {
     env: {
@@ -22,6 +22,6 @@ test('server-url https', { skip: true }, async () => {
       TEST_HTTPS: '1',
     },
   })
-  assert.ok(result.stdout.includes('Browser runner started at https://localhost:5173/'))
-  assert.ok(result.stdout.includes('Test Files  1 passed'))
+  assert.match(result.stdout, /Browser runner started at https:\/\/localhost:5173\//);
+  assert.match(result.stdout, /Test Files  1 passed/);
 })
