3eb015bf81b993745c889c9154198cebaa02b2c0
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-01-04 17:18:57 +0100

    chore: cleanup

--- test/vite-node/vitest.config.ts
+++ test/vite-node/vitest.config.ts
@@ -4,5 +4,9 @@ export default defineConfig({
   test: {
     clearMocks: true,
     testTimeout: process.env.CI ? 120_000 : 5_000,
+    onConsoleLog(log) {
+      if (log.includes('Port is already'))
+        return false
+    },
   },
 })
