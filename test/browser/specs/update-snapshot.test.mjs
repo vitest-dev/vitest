5044910de3d1f4436b08d93e17702c8cb7062e94
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-01-04 18:36:21 +0900

    test: test update snapshot

---
+++ test/browser/specs/update-snapshot.test.mjs
@@ -0,0 +1,46 @@
+import assert from 'node:assert'
+import fs from 'node:fs'
+import test from 'node:test'
+import { startVitest } from 'vitest/node'
+
+test('update snapshot', async () => {
+  // setup wrong snapshot value
+  const snapshotPath = './fixtures/update-snapshot/__snapshots__/basic.test.ts.snap'
+  await editFile(snapshotPath, data => data.replace('`1`', '`2`'))
+
+  // run vitest watch mode
+  const vitest = await startVitest('test', [], {
+    watch: true,
+    root: './fixtures/update-snapshot',
+    browser: { headless: true },
+  })
+  assert.ok(vitest)
+
+  // test fails
+  assert.equal(vitest.state.getFiles()[0].result.state, 'fail')
+
+  // updateSnapshot API to simulate "u" commmand
+  await vitest.updateSnapshot()
+
+  // verify snapshot value is updated
+  const snapshotData = await fs.promises.readFile(snapshotPath, 'utf-8')
+  assert.match(snapshotData, /`1`/)
+
+  // test passes
+  assert.equal(vitest.state.getFiles()[0].result.state, 'pass')
+
+  // force exitCode mutated by vitest
+  process.exitCode = 0
+  await vitest.close()
+})
+
+/**
+ *
+ * @param {string} filepath
+ * @param {(data: string) => string | Promise<string>} edit
+ */
+async function editFile(filepath, edit) {
+  let data = await fs.promises.readFile(filepath, 'utf-8')
+  data = await edit(data)
+  await fs.promises.writeFile(filepath, data)
+}
