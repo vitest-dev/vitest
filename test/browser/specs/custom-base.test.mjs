f1be9064c132c1a18608e960b24374c89765741a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-01-05 13:34:44 +0900

    test: split fixtures for browser test

---
+++ test/browser/specs/custom-base.test.mjs
@@ -0,0 +1,15 @@
+import assert from 'node:assert'
+import test from 'node:test'
+import { execa } from 'execa'
+
+test('custom-base', async () => {
+  const result = await execa('npx', [
+    'vitest',
+    'run',
+    '--root=fixtures/custom-base',
+    '--browser.headless',
+    '--reporter=tap-flat',
+  ])
+  assert.match(result.stdout, /\n1\.\.1\nok 1/)
+  assert.equal(result.stderr, '')
+})
