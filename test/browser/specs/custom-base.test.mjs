742830a85ed844e55419ac08f69baa76571dec94
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-01-05 18:39:08 +0900

    chore: back to execa to avoid process.exit

--- test/browser/specs/custom-base.test.mjs
+++ test/browser/specs/custom-base.test.mjs
@@ -1,13 +1,15 @@
 import assert from 'node:assert'
 import test from 'node:test'
-import { startVitest } from 'vitest/node'
+import { execa } from 'execa'
 
 test('custom-base', async () => {
-  const vitest = await startVitest('test', [], {
-    watch: false,
-    root: 'fixtures/custom-base',
-    browser: { headless: true },
-    reporters: ['tap'],
-  })
-  assert.equal(vitest.state.getFiles()[0].result.state, 'pass')
+  const result = await execa('npx', [
+    'vitest',
+    'run',
+    '--root=fixtures/custom-base',
+    '--browser.headless',
+    '--reporter=tap-flat',
+  ])
+  assert.match(result.stdout, /\n1\.\.1\nok 1/)
+  assert.equal(result.stderr, '')
 })
