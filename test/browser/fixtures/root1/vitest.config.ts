ebffb6860890634739d9d57d278cf4de1dd92f3b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-01-08 19:11:53 +0900

    fix: separate cacheDir (workaround)

--- test/browser/fixtures/root1/vitest.config.ts
+++ test/browser/fixtures/root1/vitest.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vitest/config'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 
 const provider = process.env.PROVIDER || 'webdriverio';
 const browser = process.env.BROWSER || (provider === 'playwright' ? 'chromium' : 'chrome');
 
 export default defineConfig({
+  cacheDir: path.join(path.dirname(fileURLToPath(import.meta.url)), "node_modules/.vite"),
   test: {
     browser: {
       enabled: true,
