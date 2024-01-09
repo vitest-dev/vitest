743795eb40f5645e8c5135e0a1e82debc82eb6ed
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-01-09 16:41:25 +0100

    chore: throw mocker error if pool is vmThreads

--- packages/vitest/src/runtime/mocker.ts
+++ packages/vitest/src/runtime/mocker.ts
@@ -415,7 +415,7 @@ export class VitestMocker {
     const isIsolatedForks = config.pool === 'forks' && (config.poolOptions?.forks?.isolate ?? true)
 
     // TODO: find a good way to throw this error even in non-isolated mode
-    if (throwIfExists && (isIsolatedThreads || isIsolatedForks)) {
+    if (throwIfExists && (isIsolatedThreads || isIsolatedForks || config.pool === 'vmThreads')) {
       const cached = this.moduleCache.has(id) && this.moduleCache.getByModuleId(id)
       if (cached && cached.importers.size)
         throw new Error(`[vitest] Cannot mock "${originalId}" because it is already loaded by "${[...cached.importers.values()].map(i => relative(this.root, i)).join('", "')}".\n\nPlease, remove the import if you want static imports to be mocked, or clear module cache by calling "vi.resetModules()" before mocking if you are going to import the file again. See: https://vitest.dev/guide/common-errors.html#cannot-mock-mocked-file-js-because-it-is-already-loaded`)
