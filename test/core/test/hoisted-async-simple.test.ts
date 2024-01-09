ca62f37a8831986207be4c0fbd4e057feffc6811
Vladimir <sleuths.slews0s@icloud.com>
2024-01-09 16:39:27 +0100

    fix(vitest): don't throw SyntaxError when "await vi.hoisted" is used (#4915)

--- test/core/test/hoisted-async-simple.test.ts
+++ test/core/test/hoisted-async-simple.test.ts
@@ -6,6 +6,8 @@ import { value } from '../src/rely-on-hoisted'
 const globalValue = await vi.hoisted(async () => {
   // @ts-expect-error not typed global
   globalThis.someGlobalValue = 'globalValue'
+  // @ts-expect-error not typed global
+  ;(globalThis._order ??= []).push(1)
   return 'globalValue'
 })
 
@@ -14,6 +16,26 @@ afterAll(() => {
   delete globalThis.someGlobalValue
 })
 
+// _order is set in the hoisted function before tests are collected
+// @ts-expect-error not typed global
+expect(globalThis._order).toEqual([1, 2, 3])
+
 it('imported value is equal to returned from hoisted', () => {
   expect(value).toBe(globalValue)
+})
+
+it('hoists async "vi.hoisted", but leaves the wrapper alone', async () => {
+  expect.assertions(1)
+  await (async () => {
+    expect(1).toBe(1)
+    vi.hoisted(() => {
+      // @ts-expect-error not typed global
+      ;(globalThis._order ??= []).push(2)
+    })
+  })()
+})
+
+await vi.hoisted(async () => {
+  // @ts-expect-error not typed global
+  ;(globalThis._order ??= []).push(3)
 })
