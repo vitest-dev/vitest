9846889bd6c387088a91d0cf836dfb0451f1388d
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-01-13 15:10:12 +0900

    test: tweak

--- test/workspaces-browser/space_browser_inline/test/basic.test.ts
+++ test/workspaces-browser/space_browser_inline/test/basic.test.ts
@@ -4,7 +4,7 @@ test('window is defined', () => {
   expect(typeof window).toBe('object')
 })
 
-test('"defined" from workspace inline config', () => {
+test('"define" from workspace inline config', () => {
   // @ts-expect-error vite define
   expect(TEST_DEFINE).toBe('hello')
 })
