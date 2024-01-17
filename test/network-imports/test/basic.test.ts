74cce1f8343372cefb93357633f232b0ce27916c
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-01-17 12:22:15 +0900

    test: tweak

--- test/network-imports/test/basic.test.ts
+++ test/network-imports/test/basic.test.ts
@@ -3,6 +3,6 @@ import { expect, test } from 'vitest'
 // @ts-expect-error network imports
 import slash from 'https://esm.sh/slash@5.1.0'
 
-test('network imports', async () => {
+test('network imports', () => {
   expect(slash('foo\\bar')).toBe('foo/bar')
 })
