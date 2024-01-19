c0ac448997c07fe4ad6d710c2d0aef5220e6721f
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-01-19 10:28:25 +0100

    chore: cleanup

--- packages/vitest/src/runtime/vm/commonjs-executor.ts
+++ packages/vitest/src/runtime/vm/commonjs-executor.ts
@@ -76,7 +76,7 @@ export class CommonjsExecutor {
         return _require
       }
 
-      register() {
+      static register() {
         throw new Error(`[vitest] "register" is not available when running in Vitest.`)
       }
 
