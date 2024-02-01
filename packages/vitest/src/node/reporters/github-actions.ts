4ff17e542f3450ee811df7c931bb0d486280f904
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-02-02 08:39:46 +0900

    fix: handle unhandled errors

--- packages/vitest/src/node/reporters/github-actions.ts
+++ packages/vitest/src/node/reporters/github-actions.ts
@@ -15,36 +15,54 @@ export class GithubActionsReporter implements Reporter {
     this.ctx = ctx
   }
 
-  async onFinished(files: File[] = []) {
+  async onFinished(files: File[] = [], errors: unknown[] = []) {
+    // collect all errors and associate them with projects
+    const projectErrors = new Array<{ project: WorkspaceProject; title: string; error: unknown }>()
+    for (const error of errors) {
+      projectErrors.push({
+        project: this.ctx.getCoreWorkspaceProject(),
+        title: 'Unhandled error',
+        error,
+      })
+    }
     for (const file of files) {
       const tasks = getTasks(file)
       const project = this.ctx.getProjectByTaskId(file.id)
       for (const task of tasks) {
+        const title = getFullName(task, ' > ')
         for (const error of task.result?.errors ?? []) {
-          const result = await printErrorWrapper(error, this.ctx, project)
-          const stack = result?.nearest
-          if (!stack)
-            continue
-
-          const formatted = formatMessage({
-            command: 'error',
-            properties: {
-              file: stack.file,
-              title: getFullName(task, ' > '),
-              line: String(stack.line),
-              column: String(stack.column),
-            },
-            message: stripAnsi(result.output),
+          projectErrors.push({
+            project,
+            title,
+            error,
           })
-          this.ctx.logger.log(`\n${formatted}`)
         }
       }
+    }
+
+    // format errors via `printError`
+    for (const { project, title, error } of projectErrors) {
+      const result = await printErrorWrapper(error, this.ctx, project)
+      const stack = result?.nearest
+      if (!stack)
+        continue
+      const formatted = formatMessage({
+        command: 'error',
+        properties: {
+          file: stack.file,
+          title,
+          line: String(stack.line),
+          column: String(stack.column),
+        },
+        message: stripAnsi(result.output),
+      })
+      this.ctx.logger.log(`\n${formatted}`)
     }
   }
 }
 
-// use Logger with custom Console to extract messgage from `processError` util.
-// TODO: refactor `processError` to require single function `(message: string) => void` instead of full Logger?
+// use Logger with custom Console to extract messgage from `processError` util
+// TODO: maybe refactor `processError` to require single function `(message: string) => void` instead of full Logger?
 async function printErrorWrapper(error: unknown, ctx: Vitest, project: WorkspaceProject) {
   let output = ''
   const writable = new Writable({
@@ -63,7 +81,6 @@ async function printErrorWrapper(error: unknown, ctx: Vitest, project: Workspace
 // workflow command formatting based on
 // https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-error-message
 // https://github.com/actions/toolkit/blob/f1d9b4b985e6f0f728b4b766db73498403fd5ca3/packages/core/src/command.ts#L80-L85
-
 function formatMessage({
   command,
   properties,
