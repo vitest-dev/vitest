a7ced15a58163eb45ff36a5c4dcf51e37e4b139c
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-02-01 18:37:21 +0900

    test: windows finally?

--- test/reporters/tests/github-actions.test.ts
+++ test/reporters/tests/github-actions.test.ts
@@ -8,7 +8,7 @@ test(GithubActionsReporter, async () => {
     { reporters: new GithubActionsReporter(), root: './fixtures' },
     ['some-failing.test.ts'],
   )
-  stdout = stdout.replace(resolve(__dirname, '..'), '__TEST_DIR__')
+  stdout = stdout.replace(resolve(__dirname.replace(/:/g, '%3A'), '..'), '__TEST_DIR__')
   expect(stdout).toMatchInlineSnapshot(`
     "
     ::error file=__TEST_DIR__/fixtures/some-failing.test.ts,title=some-failing.test.ts > 3 + 3 = 7,line=8,column=17::AssertionError: expected 6 to be 7 // Object.is equality%0A%0A- Expected%0A+ Received%0A%0A- 7%0A+ 6%0A%0A ❯ some-failing.test.ts:8:17%0A%0A
