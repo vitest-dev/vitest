939d687f3643fbeea7fde776ad66f4b3e0585734
Han Feng <fenghan770@gmail.com>
2024-02-02 13:44:15 +0800

    test: add tests

---
+++ test/core/test/fixture-options.test.ts
@@ -0,0 +1,41 @@
+import { afterAll, beforeEach, describe, expect, test, vi } from 'vitest'
+
+const mockServer = { setup: vi.fn(), teardown: vi.fn() }
+const FnA = vi.fn()
+
+export const myTest = test.extend({
+  autoFixture: [async ({}, use) => {
+    await mockServer.setup()
+    await use()
+    await mockServer.teardown()
+  }, { auto: true }],
+
+  normalFixture: [async ({}, use) => {
+    await FnA()
+    await use()
+  }, {}],
+})
+
+describe('fixture with options', () => {
+  describe('automatic fixture', () => {
+    beforeEach(() => {
+      expect(mockServer.setup).toBeCalledTimes(1)
+    })
+
+    afterAll(() => {
+      expect(mockServer.setup).toBeCalledTimes(1)
+      expect(mockServer.teardown).toBeCalledTimes(1)
+    })
+
+    myTest('should setup mock server', () => {
+      expect(mockServer.setup).toBeCalledTimes(1)
+    })
+  })
+
+  describe('normal fixture', () => {
+    myTest('it is not a fixture with options', ({ normalFixture }) => {
+      expect(FnA).not.toBeCalled()
+      expect(normalFixture).toBeInstanceOf(Array)
+    })
+  })
+})
