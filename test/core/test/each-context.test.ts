c6eae14655600c1f60d47e78c6e245db3e6900ed
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-02-13 10:40:37 +0900

    test: simplify

--- test/core/test/each-context.test.ts
+++ test/core/test/each-context.test.ts
@@ -1,7 +1,5 @@
 import { expect, expectTypeOf, test } from 'vitest'
 
-const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
-
 const myTest = test.extend<{ myFixture: number }>({
   myFixture: async ({}, use) => {
     await use(1234)
@@ -18,9 +16,8 @@ myTest.concurrent.each(['hello', 'hi'])(
 
 myTest.concurrent.each(['hello', 'hi'])(
   'single context true %s',
-  async (arg, { expect, myFixture }) => {
+  (arg, { expect, myFixture }) => {
     arg satisfies string
-    await sleep(50)
     expect({ arg, myFixture }).toMatchSnapshot()
   },
   { context: true },
@@ -36,8 +33,7 @@ myTest.concurrent.each([['hello'], ['hi']])(
 
 myTest.concurrent.each([['hello'], ['hi']])(
   'array 1 context true %s',
-  async (arg, { expect, myFixture }) => {
-    await sleep(50)
+  (arg, { expect, myFixture }) => {
     expect({ arg, myFixture }).toMatchSnapshot()
   },
   { context: true },
@@ -54,10 +50,9 @@ myTest.concurrent.each([['hello', 2], ['hi', 3]])(
 
 myTest.concurrent.each([['hello', 2], ['hi', 3]])(
   'array 2 context true %s %s',
-  async (argString, argNumber, { expect, myFixture }) => {
+  (argString, argNumber, { expect, myFixture }) => {
     argString satisfies string
     argNumber satisfies number
-    await sleep(50)
     expect({ argString, argNumber, myFixture }).toMatchSnapshot()
     expect(argString).toBeTypeOf('string')
     expect(argNumber).toBeTypeOf('number')
@@ -77,10 +72,9 @@ myTest.concurrent.each([['hello', 2], ['hi', 3]] as const)(
 
 myTest.concurrent.each([['hello', 2], ['hi', 3]] as const)(
   'array 3 context true %s %s',
-  async (argString, argNumber, { expect, myFixture }) => {
+  (argString, argNumber, { expect, myFixture }) => {
     argString satisfies string
     argNumber satisfies number
-    await sleep(50)
     expect({ argString, argNumber, myFixture }).toMatchSnapshot()
     expect(argString).toBeTypeOf('string')
     expect(argNumber).toBeTypeOf('number')
@@ -91,11 +85,10 @@ myTest.concurrent.each([['hello', 2], ['hi', 3]] as const)(
 // type is correct but it's not possible to access fixture
 myTest.concurrent.each([['hello', 2], ['hi', 3]])(
   'array 4 context true %s %s',
-  async (...args) => {
+  (...args) => {
     args[0] satisfies string
     args[1] satisfies number
     args[2] satisfies { expect: unknown; myFixture: number }
-    await sleep(50)
     expect(args[0]).toBeTypeOf('string')
     expect(args[1]).toBeTypeOf('number')
     args[2].expect({ argString: args[0], argNumber: args[1] }).toMatchSnapshot()
@@ -108,7 +101,7 @@ myTest.concurrent.each([
   [2, 3, 4],
 ])(
   'array 5 context false %s',
-  async (...args) => {
+  (...args) => {
     args satisfies number[]
     expect(args[0]).toBeTypeOf('number')
     expect(args[1]).toBeTypeOf('number')
@@ -121,7 +114,7 @@ myTest.concurrent.each([
   [2, 3, 4],
 ])(
   'array 5 context true %s',
-  async (...args) => {
+  (...args) => {
     args satisfies unknown[]
     expect(args[0]).toBeTypeOf('number')
     expect(args[1]).toBeTypeOf('number')
@@ -135,7 +128,7 @@ myTest.concurrent.each`
   ${'hi'}    | ${3}
 `(
   'template context false $a $b',
-  async (...args) => {
+  (...args) => {
     expectTypeOf(args).toEqualTypeOf<[any]>()
     expect(args[0].a).toBeTypeOf('string')
     expect(args[0].b).toBeTypeOf('number')
@@ -148,9 +141,8 @@ myTest.concurrent.each`
   ${'hi'}    | ${3}
 `(
   'template context true $a $b',
-  async (arg, { expect, myFixture }) => {
+  (arg, { expect, myFixture }) => {
     expectTypeOf(arg).toEqualTypeOf<any>()
-    await sleep(50)
     expect({ arg, myFixture }).toMatchSnapshot()
   },
   { context: true },
