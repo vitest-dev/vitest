1aecd650e221e83c0df9a13a1e3ac6687c0bf932
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-02-16 22:37:55 +0900

    fix(vitest): fix `optimizeDeps.disabled` warnings on Vite 5.1 (#5215)

---
+++ test/optimize-deps/vitest.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  optimizeDeps: {
+    include: ['@vitest/test-dep-url'],
+  },
+  ssr: {
+    optimizeDeps: {
+      include: ['@vitest/test-dep-url'],
+    },
+  },
+  test: {
+    chaiConfig: {
+      truncateThreshold: 1000,
+    },
+    deps: {
+      optimizer: {
+        web: {
+          enabled: true,
+        },
+        ssr: {
+          enabled: true,
+        },
+      },
+    },
+  },
+  // use dummy ssrLoadModule to trigger ssr.optimizeDeps.
+  // this will be unnecessary from Vite 5.1
+  // cf. https://github.com/vitejs/vite/pull/15561
+  plugins: [
+    {
+      name: 'force-ssr-optimize-deps',
+      configureServer(server) {
+        return async () => {
+          await server.ssrLoadModule('/package.json')
+        }
+      },
+    },
+  ],
+})
