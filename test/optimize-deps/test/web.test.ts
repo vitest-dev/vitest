d41a5ceed45a5a451fb72a61256eba8e8e9ac5c0
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-02-16 16:10:56 +0900

    test: test web/ssr optimizeDeps

---
+++ test/optimize-deps/test/web.test.ts
@@ -0,0 +1,10 @@
+// @vitest-environment happy-dom
+
+import { expect, test } from 'vitest'
+
+// @ts-expect-error untyped
+import { importMetaUrl } from '@vitest/test-dep-url'
+
+test('import.meta.url', () => {
+  expect(importMetaUrl).toContain('/node_modules/.vitest/deps/')
+})
---
+++ test/optimize-deps/vitest.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig } from 'vitest/config'
+
+// cf.
+// https://stackblitz.com/edit/vitest-dev-vitest-8wf26p?file=test%2Fssr.test.ts
+
+export default defineConfig({
+  // customLogger: console as any,
+  optimizeDeps: {
+    include: ['@vitest/test-dep-url'],
+  },
+  ssr: {
+    optimizeDeps: {
+      include: ['@vitest/test-dep-url'],
+    },
+  },
+  test: {
+    deps: {
+      optimizer: {
+        web: {
+          enabled: true,
+        },
+        ssr: {
+          enabled: true,
+        },
+      },
+    },
+  },
+  // use dummy ssrLoadModule to trigger ssr.optimizeDeps.
+  // this will be unnecessary from Vite 5.1
+  // cf. https://github.com/vitejs/vite/pull/15561
+  plugins: [
+    {
+      name: 'force-ssr-optimize-deps',
+      configureServer(server) {
+        return async () => {
+          await server.ssrLoadModule('/package.json')
+        }
+      },
+    },
+  ],
+})
