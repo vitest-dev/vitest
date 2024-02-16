18aea1adc28f5f155038183bbf76f0c62b8329b9
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-02-16 18:13:27 +0900

    test: ssr is the flaky one

--- test/optimize-deps/test/ssr.test.ts
+++ test/optimize-deps/test/ssr.test.ts
@@ -5,6 +5,8 @@ import { expect, test } from 'vitest'
 // @ts-expect-error untyped
 import { importMetaUrl } from '@vitest/test-dep-url'
 
-test('import.meta.url', () => {
+// TODO: flaky on Windows
+// https://github.com/vitest-dev/vitest/pull/5215#discussion_r1492066033
+test.skipIf(process.platform === 'win32')('import.meta.url', () => {
   expect(importMetaUrl).toContain('/node_modules/.vitest/deps_ssr/')
 })
--- test/optimize-deps/test/web.test.ts
+++ test/optimize-deps/test/web.test.ts
@@ -5,8 +5,6 @@ import { expect, test } from 'vitest'
 // @ts-expect-error untyped
 import { importMetaUrl } from '@vitest/test-dep-url'
 
-// TODO: flaky on Windows
-// https://github.com/vitest-dev/vitest/pull/5215#discussion_r1492066033
-test.skipIf(process.platform === 'win32')('import.meta.url', () => {
+test('import.meta.url', () => {
   expect(importMetaUrl).toContain('/node_modules/.vitest/deps/')
 })
