18dcc0b424d0fc05f38c741bca2ae36aa8a9a1b5
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-14 16:07:01 +0900

    chore: comment

--- test/ws-api/fixtures/server-url/vitest.config.ts
+++ test/ws-api/fixtures/server-url/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // test https by
-//   TEST_HTTPS=1 pnpm test-fixtures --root fixtures/server-url
+//   TEST_HTTPS=1 pnpm test --root fixtures/server-url --api
 
 export default defineConfig({
   plugins: [
