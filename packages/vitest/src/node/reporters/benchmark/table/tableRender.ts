22c3b4d7716e7e9310d6ca063e2728602abafe57
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-19 18:11:27 +0900

    chore: remove unused

--- packages/vitest/src/node/reporters/benchmark/table/tableRender.ts
+++ packages/vitest/src/node/reporters/benchmark/table/tableRender.ts
@@ -1,19 +1,19 @@
 import c from 'picocolors'
 import cliTruncate from 'cli-truncate'
 import stripAnsi from 'strip-ansi'
-import type { Benchmark, BenchmarkResult, Task } from '../../../../types'
+import type { BenchmarkResult, Task } from '../../../../types'
 import { getTests, notNullish } from '../../../../utils'
 import { F_RIGHT } from '../../../../utils/figures'
 import type { Logger } from '../../../logger'
 import { getCols, getStateSymbol } from '../../renderers/utils'
-import { TableBenchmarkOutput } from '.'
+import type { TableBenchmarkOutput } from '.'
 
 export interface TableRendererOptions {
   renderSucceed?: boolean
   logger: Logger
   showHeap: boolean
   slowTestThreshold: number
-  compare?: TableBenchmarkOutput;
+  compare?: TableBenchmarkOutput
 }
 
 const outputMap = new WeakMap<Task, string>()
@@ -37,19 +37,6 @@ function formatNumber(number: number) {
 
 const tableHead = ['name', 'hz', 'min', 'max', 'mean', 'p75', 'p99', 'p995', 'p999', 'rme', 'samples']
 
-function renderTableHead(tasks: Task[]) {
-  const benches = tasks
-    .map(i => i.meta?.benchmark ? i.result?.benchmark : undefined)
-    .filter(notNullish)
-  const allItems = benches.map(renderBenchmarkItems).concat([tableHead])
-  return `${' '.repeat(3)}${tableHead.map((i, idx) => {
-    const width = Math.max(...allItems.map(i => i[idx].length))
-    return idx
-      ? i.padStart(width, ' ')
-      : i.padEnd(width, ' ') // name
-  }).map(c.bold).join('  ')}`
-}
-
 function renderBenchmarkItems(result: BenchmarkResult) {
   return [
     result.name,
@@ -63,93 +50,9 @@ function renderBenchmarkItems(result: BenchmarkResult) {
     formatNumber(result.p999 || 0),
     `±${(result.rme || 0).toFixed(2)}%`,
     // TODO: persist only sampleCount?
-    result.samples.length ? result.samples.length.toString() : "-",
+    result.samples.length ? result.samples.length.toString() : '-',
   ]
 }
-function renderBenchmark(task: Benchmark, tasks: Task[]): string {
-  const result = task.result?.benchmark
-  if (!result)
-    return task.name
-
-  const benches = tasks
-    .map(i => i.meta?.benchmark ? i.result?.benchmark : undefined)
-    .filter(notNullish)
-  const allItems = benches.map(renderBenchmarkItems).concat([tableHead])
-  const items = renderBenchmarkItems(result)
-  const padded = items.map((i, idx) => {
-    const width = Math.max(...allItems.map(i => i[idx].length))
-    return idx
-      ? i.padStart(width, ' ')
-      : i.padEnd(width, ' ') // name
-  })
-
-  return [
-    padded[0], // name
-    c.blue(padded[1]), // hz
-    c.cyan(padded[2]), // min
-    c.cyan(padded[3]), // max
-    c.cyan(padded[4]), // mean
-    c.cyan(padded[5]), // p75
-    c.cyan(padded[6]), // p99
-    c.cyan(padded[7]), // p995
-    c.cyan(padded[8]), // p999
-    c.dim(padded[9]), // rem
-    c.dim(padded[10]), // sample
-    // TODO: fix or hide fastest/slowest during compare
-    //       show "change: 0.98x ⇓" compared to baseline
-    result.rank === 1
-      ? c.bold(c.green(' fastest'))
-      : (result.rank === benches.length && benches.length > 2)
-          ? c.bold(c.gray(' slowest'))
-          : '',
-  ].join('  ')
-}
-
-// TODO: load from --compare
-const baselineResults = {
-  "-307977539_0_0": {
-    "name": "normal",
-    "rank": 1,
-    "rme": 1.1364415465735003,
-    "totalTime": 513.9135180000001,
-    "min": 14.029334999999946,
-    "max": 15.713413999999943,
-    "hz": 58.1048440527692,
-    "period": 14.683243371428574,
-    "mean": 14.683243371428574,
-    "variance": 0.23565419608982463,
-    "sd": 0.4854422685447,
-    "sem": 0.08205471973712326,
-    "df": 34,
-    "critical": 2.0336,
-    "moe": 0.16686647805741386,
-    "p75": 15.116964999999936,
-    "p99": 15.713413999999943,
-    "p995": 15.713413999999943,
-    "p999": 15.713413999999943
-  },
-  "-307977539_0_1": {
-    "name": "reverse",
-    "rank": 2,
-    "rme": 25.34288206604909,
-    "totalTime": 532.4783659999998,
-    "min": 17.00932499999999,
-    "max": 81.15657699999997,
-    "hz": 22.536126848015464,
-    "period": 44.37319716666665,
-    "mean": 44.37319716666665,
-    "variance": 313.25254577674025,
-    "sd": 17.698941939470288,
-    "sem": 5.109244446562364,
-    "df": 11,
-    "critical": 2.201,
-    "moe": 11.245447026883765,
-    "p75": 50.36103299999991,
-    "p99": 81.15657699999997,
-    "p995": 81.15657699999997,
-    "p999": 81.15657699999997
-  }
-} as any as Record<string, BenchmarkResult>;
 
 function computeColumnWidths(results: BenchmarkResult[]): number[] {
   const rows = [
@@ -158,24 +61,24 @@ function computeColumnWidths(results: BenchmarkResult[]): number[] {
   ]
   return Array.from(
     tableHead,
-    (_, i) => Math.max(...rows.map(row => stripAnsi(row[i]).length))
+    (_, i) => Math.max(...rows.map(row => stripAnsi(row[i]).length)),
   )
 }
 
 function padRow(row: string[], widths: number[]) {
   return row.map((v, i) =>
     i
-    ? v.padStart(widths[i], ' ')
-    : v.padEnd(widths[i], ' ') // name
+      ? v.padStart(widths[i], ' ')
+      : v.padEnd(widths[i], ' '), // name
   )
 }
 
 function renderTableHead2(widths: number[]) {
-  return " ".repeat(3) + padRow(tableHead, widths).map(c.bold).join('  ')
+  return ' '.repeat(3) + padRow(tableHead, widths).map(c.bold).join('  ')
 }
 
 function renderBenchmark2(result: BenchmarkResult, widths: number[]) {
-  const padded = padRow(renderBenchmarkItems(result), widths);
+  const padded = padRow(renderBenchmarkItems(result), widths)
   return [
     padded[0], // name
     c.blue(padded[1]), // hz
@@ -194,7 +97,7 @@ function renderBenchmark2(result: BenchmarkResult, widths: number[]) {
 function renderTree(tasks: Task[], options: TableRendererOptions, level = 0): string {
   const output: string[] = []
 
-  const benchMap: Record<string, { current: BenchmarkResult, baseline?: BenchmarkResult }> = {};
+  const benchMap: Record<string, { current: BenchmarkResult; baseline?: BenchmarkResult }> = {}
   for (const t of tasks) {
     if (t.meta.benchmark && t.result?.benchmark) {
       benchMap[t.id] = {
@@ -204,19 +107,18 @@ function renderTree(tasks: Task[], options: TableRendererOptions, level = 0): st
         benchMap[t.id].baseline = {
           ...options.compare[t.id],
           samples: [],
-          name: c.gray("(prev.)"),
+          name: c.gray('(prev.)'),
         }
-        options.compare[t.id]
       }
     }
   }
-  const benchCount = Object.entries(benchMap).length;
+  const benchCount = Object.entries(benchMap).length
 
   // compute column widths
   const columnWidths = computeColumnWidths(
     Object.values(benchMap)
       .flatMap(v => [v.current, v.baseline])
-      .filter(notNullish)
+      .filter(notNullish),
   )
 
   let idx = 0
@@ -251,33 +153,34 @@ function renderTree(tasks: Task[], options: TableRendererOptions, level = 0): st
 
     const bench = benchMap[task.id]
     if (bench) {
-      let body = renderBenchmark2(bench.current, columnWidths);
+      let body = renderBenchmark2(bench.current, columnWidths)
       if (options.compare && bench.baseline) {
         if (bench.current.hz) {
-          const diff = bench.current.hz / bench.baseline.hz;
-          const diffFixed = diff.toFixed(2);
-          if (diffFixed === "1.0.0") {
-            body += `  ` + c.gray(`[${diffFixed}x]`)
-          } if (diff > 1) {
-            body += `  ` + c.blue(`[${diffFixed}x] ⇑`)
-          } else {
-            body += `  ` + c.red(`[${diffFixed}x] ⇓`)
-          }
+          const diff = bench.current.hz / bench.baseline.hz
+          const diffFixed = diff.toFixed(2)
+          if (diffFixed === '1.0.0')
+            body += `  ${c.gray(`[${diffFixed}x]`)}`
+          if (diff > 1)
+            body += `  ${c.blue(`[${diffFixed}x] ⇑`)}`
+          else
+            body += `  ${c.red(`[${diffFixed}x] ⇓`)}`
         }
         output.push(padding + prefix + body + suffix)
-        const body2 = renderBenchmark2(bench.baseline, columnWidths);
-        output.push(padding + `   ` + body2 + suffix)
-      } else {
-        if (bench.current.rank === 1) {
-          body += `  ` + c.bold(c.green(' fastest'));
-        }
-        if (bench.current.rank === benchCount && benchCount > 2) {
-          body += `  ` + c.bold(c.gray(' slowest'));
-        }
+        const body2 = renderBenchmark2(bench.baseline, columnWidths)
+        output.push(`${padding}   ${body2}${suffix}`)
+      }
+      else {
+        if (bench.current.rank === 1)
+          body += `  ${c.bold(c.green(' fastest'))}`
+
+        if (bench.current.rank === benchCount && benchCount > 2)
+          body += `  ${c.bold(c.gray(' slowest'))}`
+
         output.push(padding + prefix + body + suffix)
       }
-    } else {
-      output.push(padding + prefix + name + suffix);
+    }
+    else {
+      output.push(padding + prefix + name + suffix)
     }
     // // const body = task.meta?.benchmark
     // //   ? renderBenchmark(task as Benchmark, tasks)
