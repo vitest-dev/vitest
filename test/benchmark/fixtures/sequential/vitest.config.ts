e34c1b451bbadbd578cdda05a2eb960b007ea73e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-28 12:18:58 +0900

    chore: comment

--- test/benchmark/fixtures/sequential/vitest.config.ts
+++ test/benchmark/fixtures/sequential/vitest.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vitest/config"
 
 // to see the difference better, increase sleep time and iterations e.g. by
-// SLEEP_BENCH_MS=100 pnpm -C test/benchmark2 test bench -- --root fixtures/sequential --fileParallelism
+// SLEEP_BENCH_MS=100 pnpm -C test/benchmark test bench -- --root fixtures/sequential --fileParallelism
 
 export default defineConfig({
   test: {
