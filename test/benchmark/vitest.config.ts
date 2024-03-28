9fbaf46049a370a9893612f2c65d78ae4e1d162c
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-28 12:18:00 +0900

    test: move to test/benchmark

--- test/benchmark/vitest.config.ts
+++ test/benchmark/vitest.config.ts
@@ -1,25 +1,7 @@
 import { defineConfig } from 'vitest/config'
 
-function noop() {}
-
 export default defineConfig({
   test: {
-    update: false,
-    allowOnly: true,
-    benchmark: {
-      outputFile: './bench.json',
-      reporters: ['json', {
-        onInit: noop,
-        onPathsCollected: noop,
-        onCollected: noop,
-        onFinished: noop,
-        onTaskUpdate: noop,
-        onTestRemoved: noop,
-        onWatcherStart: noop,
-        onWatcherRerun: noop,
-        onServerRestart: noop,
-        onUserConsoleLog: noop,
-      }],
-    },
+    fileParallelism: false,
   },
 })
