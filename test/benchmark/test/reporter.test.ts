c5232bad25af5ac9f9b225fb3c331522c04eba85
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-04-05 08:12:33 +0900

    test: tweak

--- test/benchmark/test/reporter.test.ts
+++ test/benchmark/test/reporter.test.ts
@@ -6,16 +6,17 @@ it('non-tty', async () => {
   const root = pathe.join(import.meta.dirname, '../fixtures/basic')
   const result = await runVitest({ root }, ['base.bench.ts'], 'benchmark')
   const lines = result.stdout.split('\n').slice(3).slice(0, 10)
-  expect(lines).toMatchObject([
-    ' ✓ base.bench.ts > sort',
-    '     name',
-    '   · normal',
-    '   · reverse',
-    ' ✓ base.bench.ts > timeout',
-    '     name',
-    '   · timeout100',
-    '   · timeout75',
-    '   · timeout50',
-    '   · timeout25',
-  ].map(s => expect.stringContaining(s)))
+  const expected = `\
+ ✓ base.bench.ts > sort
+     name
+   · normal
+   · reverse
+ ✓ base.bench.ts > timeout
+     name
+   · timeout100
+   · timeout75
+   · timeout50
+   · timeout25
+`
+  expect(lines).toMatchObject(expected.trim().split('\n').map(s => expect.stringContaining(s)))
 })
