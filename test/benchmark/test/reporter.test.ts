db98145f4f6426b45ba8682f23fca6944ae311bd
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-04-05 20:00:28 +0900

    fix(benchmark): fix benchmark summary of single bench suite (#5489)

---
+++ test/benchmark/test/reporter.test.ts
@@ -0,0 +1,10 @@
+import { expect, it } from 'vitest'
+import * as pathe from 'pathe'
+import { runVitest } from '../../test-utils'
+
+it('summary', async () => {
+  const root = pathe.join(import.meta.dirname, '../fixtures/reporter')
+  const result = await runVitest({ root }, ['summary.bench.ts'], 'benchmark')
+  expect(result.stdout).not.toContain('NaNx')
+  expect(result.stdout.split('BENCH  Summary')[1].replaceAll(/\d/g, '?')).toMatchSnapshot()
+})
