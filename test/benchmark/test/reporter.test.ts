bea234b1c4c52e68c2d754fb9850df38f165883d
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-04-06 16:31:30 +0900

    fix(benchmark): table reporter for non TTY output (#5484)

--- test/benchmark/test/reporter.test.ts
+++ test/benchmark/test/reporter.test.ts
@@ -8,3 +8,22 @@ it('summary', async () => {
   expect(result.stdout).not.toContain('NaNx')
   expect(result.stdout.split('BENCH  Summary')[1].replaceAll(/\d/g, '?')).toMatchSnapshot()
 })
+
+it('non-tty', async () => {
+  const root = pathe.join(import.meta.dirname, '../fixtures/basic')
+  const result = await runVitest({ root }, ['base.bench.ts'], 'benchmark')
+  const lines = result.stdout.split('\n').slice(3).slice(0, 10)
+  const expected = `\
+ ✓ base.bench.ts > sort
+     name
+   · normal
+   · reverse
+ ✓ base.bench.ts > timeout
+     name
+   · timeout100
+   · timeout75
+   · timeout50
+   · timeout25
+`
+  expect(lines).toMatchObject(expected.trim().split('\n').map(s => expect.stringContaining(s)))
+})
