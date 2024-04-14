3855ec9c3e7f5fb48a7bec765821830ce8177b22
Vladimir <sleuths.slews0s@icloud.com>
2024-04-14 13:31:22 +0200

    docs: fix Badge display

--- docs/.vitepress/components.d.ts
+++ docs/.vitepress/components.d.ts
@@ -15,5 +15,6 @@ declare module 'vue' {
     NonProjectOption: typeof import('./components/NonProjectOption.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    Version: typeof import('./components/Version.vue')['default']
   }
 }
