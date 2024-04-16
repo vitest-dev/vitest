4c840e5ea19f962ad480dc879165c0c11b5946a0
Adithya Haridas <34602379+surc54@users.noreply.github.com>
2024-04-16 22:04:47 +0000

    Update close timeout to check servers on resolved projects

--- packages/vitest/src/node/core.ts
+++ packages/vitest/src/node/core.ts
@@ -905,7 +905,7 @@ export class Vitest {
         this.state.getProcessTimeoutCauses().forEach(cause => console.warn(cause))
 
         if (!this.pool) {
-          const runningServers = [this.server, ...this.projects.map(p => p.server)].filter(Boolean).length
+          const runningServers = [this.server, ...this.resolvedProjects.map(p => p.server)].filter(Boolean).length
 
           if (runningServers === 1)
             console.warn('Tests closed successfully but something prevents Vite server from exiting')
