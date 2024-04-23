f71a1b265ad286eec3ec013a3d5aed1b3425af8f
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-04-23 17:33:38 +0200

    test: move filters test to cli

--- test/cli/test/testname-pattern.test.ts
+++ test/cli/test/testname-pattern.test.ts
@@ -6,18 +6,18 @@ import { runVitest } from '../../test-utils'
 test.each([
   { filter: 'example' },
   { filter: '/example' },
-  { filter: resolve('./fixtures/test/example') },
+  { filter: resolve('./fixtures/filters/test/example') },
 ])('match by partial pattern $filter', async ({ filter }) => {
-  const { stdout } = await runVitest({ root: './fixtures' }, [filter])
+  const { stdout } = await runVitest({ root: './fixtures/filters' }, [filter])
 
   expect(stdout).toMatch('✓ test/example.test.ts > this will pass')
   expect(stdout).toMatch('Test Files  1 passed (1)')
-  expect(stdout).not.toMatch('test/filters.test.ts')
+  expect(stdout).not.toMatch('test/cli.test.ts')
 })
 
 test('match by full test file name', async () => {
-  const filename = resolve('./fixtures/test/example.test.ts')
-  const { stdout } = await runVitest({ root: './fixtures' }, [filename])
+  const filename = resolve('./fixtures/filters/test/example.test.ts')
+  const { stdout } = await runVitest({ root: './fixtures/filters' }, [filename])
 
   expect(stdout).toMatch('✓ test/example.test.ts > this will pass')
   expect(stdout).toMatch('Test Files  1 passed (1)')
@@ -25,12 +25,12 @@ test('match by full test file name', async () => {
 })
 
 test('match by pattern that also matches current working directory', async () => {
-  const filter = 'filters'
+  const filter = 'cli'
   expect(process.cwd()).toMatch(filter)
 
-  const { stdout } = await runVitest({ root: './fixtures' }, [filter])
+  const { stdout } = await runVitest({ root: './fixtures/filters' }, [filter])
 
-  expect(stdout).toMatch('✓ test/filters.test.ts > this will pass')
+  expect(stdout).toMatch('✓ test/cli.test.ts > this will pass')
   expect(stdout).toMatch('Test Files  1 passed (1)')
   expect(stdout).not.toMatch('test/example.test.ts')
 })
@@ -40,9 +40,9 @@ test.each([
   ['the parent of CWD with slash', join(resolve(process.cwd(), '..'), '/')],
   ['the parent of a parent of CWD', resolve(process.cwd(), '..', '..')],
 ])('match by pattern that also matches %s: %s', async (_, filter) => {
-  const { stdout } = await runVitest({ root: './fixtures' }, [filter])
+  const { stdout } = await runVitest({ root: './fixtures/filters' }, [filter])
 
-  expect(stdout).toMatch('✓ test/filters.test.ts > this will pass')
+  expect(stdout).toMatch('✓ test/cli.test.ts > this will pass')
   expect(stdout).toMatch('× test/dont-run-this.test.ts > this will fail')
   expect(stdout).toMatch('✓ test/example.test.ts > this will pass')
 })
@@ -79,7 +79,7 @@ test.each([
     ],
   },
 ])('filter with slash $filter', async ({ filter, files }) => {
-  const { stdout } = await runVitest({ root: './fixtures-slash' }, [filter])
+  const { stdout } = await runVitest({ root: './fixtures/filters-slash' }, [filter])
   expect(stdout).toMatch(`Test Files  ${files.length} passed (${files.length})`)
   for (const file of files)
     expect(stdout).toMatch(`✓ ${file}`)
