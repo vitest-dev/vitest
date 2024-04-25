f09926aea79bfc4b1e4c4b3e9b7ac3708d51939c
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-04-25 15:13:19 +0200

    chore: experiment

--- test/cli/test/dotted-files.test.ts
+++ test/cli/test/dotted-files.test.ts
@@ -1,9 +1,10 @@
+import { resolve } from 'node:path'
 import { expect, it } from 'vitest'
 import { runVitest } from '../../test-utils'
 
 it('run tests even though they are inside the .cache directory', async () => {
   const { stderr } = await runVitest({
-    root: 'fixtures/dotted-files/.cache/projects/test',
+    root: resolve(process.cwd(), 'fixtures/dotted-files/.cache/projects/test'),
   })
   expect(stderr).toBe('')
 })
