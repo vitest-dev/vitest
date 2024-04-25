4294a5c13c6319688216866237e6bab495c48916
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-04-25 12:26:03 +0200

    test: move cjs/esm module tests to core

--- test/module-esm/test/prototype.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect, it } from 'vitest'
-import * as exports from '../src/prototype.mjs'
-
-it('prototype is null', () => {
-  expect(Object.getPrototypeOf(exports)).toBe(null)
-  expect({}.hasOwnProperty).toBeTypeOf('function')
-
-  expect(exports.hasOwnProperty).toBeTypeOf('undefined')
-})
