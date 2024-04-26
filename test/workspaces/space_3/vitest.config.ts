48fba19022ebb1f8f9c6f04c90cf5f362513a6a3
Vladimir <sleuths.slews0s@icloud.com>
2024-04-26 15:45:12 +0200

    fix: use package.json name for a workspace project if not provided (#5608)

--- test/workspaces/space_3/vitest.config.ts
+++ test/workspaces/space_3/vitest.config.ts
@@ -3,7 +3,6 @@ import { defineProject } from 'vitest/config'
 export default defineProject({
   test: {
     include: ['**/*.space-3-test.ts'],
-    name: 'space_3',
     environment: 'node',
     globalSetup: './localSetup.ts',
   },
