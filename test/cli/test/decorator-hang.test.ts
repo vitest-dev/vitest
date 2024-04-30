d219ac8fd44cb5d37b6b68972508d7af911b4ff3
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-04-30 09:15:53 +0200

    chore: name test

--- test/cli/test/decorator-hang.test.ts
+++ test/cli/test/decorator-hang.test.ts
@@ -2,7 +2,7 @@ import { resolve } from 'node:path'
 import { expect, it } from 'vitest'
 import { runVitestCli } from '../../test-utils'
 
-it('run tests even though they are inside the .cache directory', async () => {
+it('doesn\'t hang when processing coverage', async () => {
   const { stderr } = await runVitestCli({
     cwd: resolve(process.cwd(), 'fixtures/decorator-hang'),
   }, '--no-watch')
