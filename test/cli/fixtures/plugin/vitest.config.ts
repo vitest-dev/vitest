32ded4017cdb2c80c5adf7bfa9f9cfcc58db1ea6
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-05-02 08:22:28 +0900

    refactor: minor

--- test/cli/fixtures/plugin/vitest.config.ts
+++ test/cli/fixtures/plugin/vitest.config.ts
@@ -1,46 +1,44 @@
 import { defineConfig } from 'vitest/config'
 
-const testHooks: string[] = (globalThis as any).__testHooks ??= [];
+function logHook(...args: unknown[]) {
+  ((globalThis as any).__testHooks ??= []).push(args[0]);
+
+  if (process.env["LOG_HOOK"]) {
+    console.log(...args);
+  }
+}
 
 export default defineConfig({
   plugins: [
     {
       name: "test-default",
       configureServer() {
-        testHooks.push("configureServer(default)")
-        console.log("configureServer(default)")
+        logHook("configureServer(default)")
       },
       buildStart() {
-        testHooks.push("buildStart(default)")
-        console.log("buildStart(default)")
+        logHook("buildStart(default)")
       },
       resolveId(source) {
-        testHooks.push("resolveId(default)")
-        console.log("resolveId(default)", source)
+        logHook("resolveId(default)", source)
       },
       transform(_code, id) {
-        testHooks.push("transform(default)")
-        console.log("transform(default)", id)
+        logHook("transform(default)", id)
       },
     },
     {
       name: "test-pre",
       enforce: "pre",
       configureServer() {
-        testHooks.push("configureServer(pre)")
-        console.log("configureServer(pre)")
+        logHook("configureServer(pre)")
       },
       buildStart() {
-        testHooks.push("buildStart(pre)")
-        console.log("buildStart(pre)")
+        logHook("buildStart(pre)")
       },
       resolveId(source) {
-        testHooks.push("resolveId(pre)")
-        console.log("resolveId(pre)", source)
+        logHook("resolveId(pre)", source)
       },
       transform(_code, id) {
-        testHooks.push("transform(pre)")
-        console.log("transform(pre)", id)
+        logHook("transform(pre)", id)
       },
     }
   ]
