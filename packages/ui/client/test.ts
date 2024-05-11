5e31d2564a2dcfcf2460b59dce6c302f5cfeaf1c
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-05-11 17:20:57 +0200

    chore: start rewriting UI tests

---
+++ packages/ui/client/test.ts
@@ -0,0 +1,15 @@
+import { type RenderOptions, render as _render } from '@testing-library/vue'
+import { VTooltip } from 'floating-vue'
+
+export function render(component: any, options?: RenderOptions) {
+  return _render(component, {
+    ...options,
+    global: {
+      directives: {
+        tooltip: VTooltip,
+      },
+    },
+  })
+}
+
+export { within } from '@testing-library/vue'
