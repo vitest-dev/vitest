bdb740189be8d4d23da67198113c89a0d30e5a29
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-05-13 15:42:16 +0200

    chore: fix test error

--- test/config/test/failures.test.ts
+++ test/config/test/failures.test.ts
@@ -16,49 +16,49 @@ function runVitestCli(...cliArgs: string[]) {
 test('shard cannot be used with watch mode', async () => {
   const { stderr } = await runVitest({ watch: true, shard: '1/2' })
 
-  expect(stderr).toMatch('Error: You cannot use --shard option with enabled watch')
+  expect(stderr).toMatch('You cannot use --shard option with enabled watch')
 })
 
 test('shard must be positive number', async () => {
   const { stderr } = await runVitest({ shard: '-1' })
 
-  expect(stderr).toMatch('Error: --shard <count> must be a positive number')
+  expect(stderr).toMatch('--shard <count> must be a positive number')
 })
 
 test('shard index must be smaller than count', async () => {
   const { stderr } = await runVitest({ shard: '2/1' })
 
-  expect(stderr).toMatch('Error: --shard <index> must be a positive number less then <count>')
+  expect(stderr).toMatch('--shard <index> must be a positive number less then <count>')
 })
 
 test('inspect requires changing pool and singleThread/singleFork', async () => {
   const { stderr } = await runVitest({ inspect: true })
 
-  expect(stderr).toMatch('Error: You cannot use --inspect without "--no-file-parallelism", "poolOptions.threads.singleThread" or "poolOptions.forks.singleFork"')
+  expect(stderr).toMatch('You cannot use --inspect without "--no-file-parallelism", "poolOptions.threads.singleThread" or "poolOptions.forks.singleFork"')
 })
 
 test('inspect cannot be used with multi-threading', async () => {
   const { stderr } = await runVitest({ inspect: true, pool: 'threads', poolOptions: { threads: { singleThread: false } } })
 
-  expect(stderr).toMatch('Error: You cannot use --inspect without "--no-file-parallelism", "poolOptions.threads.singleThread" or "poolOptions.forks.singleFork"')
+  expect(stderr).toMatch('You cannot use --inspect without "--no-file-parallelism", "poolOptions.threads.singleThread" or "poolOptions.forks.singleFork"')
 })
 
 test('inspect-brk cannot be used with multi processing', async () => {
   const { stderr } = await runVitest({ inspect: true, pool: 'forks', poolOptions: { forks: { singleFork: false } } })
 
-  expect(stderr).toMatch('Error: You cannot use --inspect without "--no-file-parallelism", "poolOptions.threads.singleThread" or "poolOptions.forks.singleFork"')
+  expect(stderr).toMatch('You cannot use --inspect without "--no-file-parallelism", "poolOptions.threads.singleThread" or "poolOptions.forks.singleFork"')
 })
 
 test('v8 coverage provider cannot be used with browser', async () => {
   const { stderr } = await runVitest({ coverage: { enabled: true }, browser: { enabled: true, name: 'chrome' } })
 
-  expect(stderr).toMatch('Error: @vitest/coverage-v8 does not work with --browser. Use @vitest/coverage-istanbul instead')
+  expect(stderr).toMatch('@vitest/coverage-v8 does not work with --browser. Use @vitest/coverage-istanbul instead')
 })
 
 test('v8 coverage provider cannot be used with browser in workspace', async () => {
   const { stderr } = await runVitest({ coverage: { enabled: true }, workspace: './fixtures/workspace/browser/workspace-with-browser.ts' })
 
-  expect(stderr).toMatch('Error: @vitest/coverage-v8 does not work with --browser. Use @vitest/coverage-istanbul instead')
+  expect(stderr).toMatch('@vitest/coverage-v8 does not work with --browser. Use @vitest/coverage-istanbul instead')
 })
 
 test('coverage reportsDirectory cannot be current working directory', async () => {
@@ -77,7 +77,7 @@ test('coverage reportsDirectory cannot be current working directory', async () =
   })
 
   const directory = normalize(resolve('./'))
-  expect(stderr).toMatch(`Error: You cannot set "coverage.reportsDirectory" as ${directory}. Vitest needs to be able to remove this directory before test run`)
+  expect(stderr).toMatch(`You cannot set "coverage.reportsDirectory" as ${directory}. Vitest needs to be able to remove this directory before test run`)
 })
 
 test('coverage reportsDirectory cannot be root', async () => {
@@ -96,7 +96,7 @@ test('coverage reportsDirectory cannot be root', async () => {
   })
 
   const directory = normalize(resolve('./fixtures/test'))
-  expect(stderr).toMatch(`Error: You cannot set "coverage.reportsDirectory" as ${directory}. Vitest needs to be able to remove this directory before test run`)
+  expect(stderr).toMatch(`You cannot set "coverage.reportsDirectory" as ${directory}. Vitest needs to be able to remove this directory before test run`)
 })
 
 test('version number is printed when coverage provider fails to load', async () => {
@@ -109,7 +109,7 @@ test('version number is printed when coverage provider fails to load', async () 
   })
 
   expect(stdout).toMatch(`RUN  v${version}`)
-  expect(stderr).toMatch('Error: Failed to load custom CoverageProviderModule from ./non-existing-module.ts')
+  expect(stderr).toMatch('Failed to load custom CoverageProviderModule from ./non-existing-module.ts')
 })
 
 test('coverage.autoUpdate cannot update thresholds when configuration file doesnt define them', async () => {
@@ -123,16 +123,16 @@ test('coverage.autoUpdate cannot update thresholds when configuration file doesn
     },
   })
 
-  expect(stderr).toMatch('Error: Unable to parse thresholds from configuration file: Expected config.test.coverage.thresholds to be an object')
+  expect(stderr).toMatch('Unable to parse thresholds from configuration file: Expected config.test.coverage.thresholds to be an object')
 })
 
 test('boolean flag 100 should not crash CLI', async () => {
   const { stderr } = await runVitestCli('--coverage.enabled', '--coverage.thresholds.100')
 
-  expect(stderr).toMatch('ERROR: Coverage for lines (0%) does not meet global threshold (100%)')
-  expect(stderr).toMatch('ERROR: Coverage for functions (0%) does not meet global threshold (100%)')
-  expect(stderr).toMatch('ERROR: Coverage for statements (0%) does not meet global threshold (100%)')
-  expect(stderr).toMatch('ERROR: Coverage for branches (0%) does not meet global threshold (100%)')
+  expect(stderr).toMatch('Coverage for lines (0%) does not meet global threshold (100%)')
+  expect(stderr).toMatch('Coverage for functions (0%) does not meet global threshold (100%)')
+  expect(stderr).toMatch('Coverage for statements (0%) does not meet global threshold (100%)')
+  expect(stderr).toMatch('Coverage for branches (0%) does not meet global threshold (100%)')
 })
 
 test('nextTick cannot be mocked inside child_process', async () => {
@@ -141,7 +141,7 @@ test('nextTick cannot be mocked inside child_process', async () => {
     include: ['./fake-timers.test.ts'],
   })
 
-  expect(stderr).toMatch('Error: vi.useFakeTimers({ toFake: ["nextTick"] }) is not supported in node:child_process. Use --pool=threads if mocking nextTick is required.')
+  expect(stderr).toMatch('vi.useFakeTimers({ toFake: ["nextTick"] }) is not supported in node:child_process. Use --pool=threads if mocking nextTick is required.')
 })
 
 test('nextTick can be mocked inside worker_threads', async () => {
