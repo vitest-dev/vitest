adf3924ff2f8905aced41a9b7eb426bbba002b04
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-05-14 11:56:33 +0200

    chore: remove cypress

---
+++ packages/ui/client/components/views/ViewConsoleOutputEntry.spec.ts
@@ -0,0 +1,26 @@
+import Filter from 'ansi-to-html'
+import { describe, expect, it } from 'vitest'
+import ViewConsoleOutputEntry from './ViewConsoleOutputEntry.vue'
+import { render } from '~/test'
+
+describe('ViewConsoleOutputEntry', () => {
+  it('test html entry', () => {
+    const now = new Date().toISOString()
+    const content = new Filter().toHtml(`\x1B[33m${now}\x1B[0m`)
+    const { container } = render(ViewConsoleOutputEntry, {
+      props: {
+        taskName: 'test/html',
+        type: 'stderr',
+        time: Date.now(),
+        content,
+      },
+    })
+
+    const spans = container.querySelectorAll('span')
+    expect(spans).toHaveLength(1)
+    const span = spans[0]
+
+    expect(span.textContent, 'the message has the correct message').toBe(now)
+    expect(span.getAttribute('style'), 'the message has the correct text color').toBe('color:#A50')
+  })
+})
