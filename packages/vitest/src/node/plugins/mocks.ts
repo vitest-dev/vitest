5e904df52eca0cb079dfa40811fcadee912a589e
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-05-24 20:39:34 +0200

    chore: only ignore the Vitest dist

--- packages/vitest/src/node/plugins/mocks.ts
+++ packages/vitest/src/node/plugins/mocks.ts
@@ -1,20 +1,13 @@
-import type { Plugin, ViteDevServer } from 'vite'
+import type { Plugin } from 'vite'
 import { hoistMocks } from '../hoistMocks'
 import { distDir } from '../../paths'
 
 export function MocksPlugin(): Plugin {
-  let server: ViteDevServer
   return {
     name: 'vitest:mocks',
     enforce: 'post',
-    configureServer(_server) {
-      server = _server
-    },
     transform(code, id) {
-      if (
-        id.includes(server.config.cacheDir)
-        || id.includes(distDir)
-      )
+      if (id.includes(distDir))
         return
       return hoistMocks(code, id, this.parse)
     },
