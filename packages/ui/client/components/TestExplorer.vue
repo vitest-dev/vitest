b7885c138ac1a3c138042965471913c4cf93fc4c
userquin <userquin@gmail.com>
2024-06-07 17:43:37 +0200

    chore: include tests status in details panel summary

--- packages/ui/client/components/TestExplorer.vue
+++ packages/ui/client/components/TestExplorer.vue
@@ -4,6 +4,7 @@ import type { File, Task } from 'vitest'
 import { files, findById } from '~/composables/client'
 import { activeFileId } from '~/composables/params'
 import { caseInsensitiveMatch, isSuite } from '~/utils/task'
+import { testStatus } from '~/composables/summary'
 
 const { onItemClick } = defineProps<{
   onItemClick?: (task: Task) => void
@@ -24,9 +25,14 @@ const filtered = computed(() => {
   if (!search.value.trim())
     return files.value
 
-  return files.value.filter(task => matchTasks([task as Task], search.value))
+  return files.value.filter(task => matchTasks([findById(task.id) as Task], search.value))
 })
 const filteredTests: ComputedRef<File[]> = computed(() => isFiltered.value ? filtered.value.map(task => findById(task.id)!).filter(Boolean) : [])
+
+const failed = computed(() => isFiltered.value ? filteredTests.value.filter(task => task.result?.state === 'fail').length : testStatus.filesFailed)
+const success = computed(() => isFiltered.value ? filteredTests.value.filter(task => task.result?.state === 'pass').length : testStatus.filesSuccess)
+const skipped = computed(() => isFiltered.value ? filteredTests.value.filter(task => task.mode === 'skip' || task.mode === 'todo').length : testStatus.filesSkipped)
+const running = computed(() => isFiltered.value ? filteredTests.value.length - failed.value - success.value - skipped.value : testStatus.filesRunning)
 
 const { list, containerProps, wrapperProps } = useVirtualList(filtered, {
   itemHeight: 28,
@@ -101,16 +107,56 @@ function matchTasks(tasks: Task[], search: string): boolean {
     </div>
     <div class="scrolls" flex-auto py-1 v-bind="containerProps">
       <DetailsPanel v-bind="wrapperProps">
-        <TaskTree
-          v-for="file in list"
-          :key="file.index"
-          nested
-          :task-id="file.data.id"
-          :search="search"
-          :opened="isFiltered"
-          :class="activeFileId === file.data.id ? 'bg-active' : ''"
-          :on-item-click="onItemClick"
-        />
+        <template #summary>
+          <div grid="~ items-center gap-x-1 cols-[auto_min-content_auto] rows-[min-content_min-content]">
+            <span text-red5>
+              FAIL ({{ failed }})
+            </span>
+            <span>/</span>
+            <span text-yellow5>
+              RUNNING ({{ running }})
+            </span>
+            <span text-green5>
+              PASS ({{ success }})
+            </span>
+            <span>/</span>
+            <span class="text-purple5:50">
+              SKIP ({{ skipped }})
+            </span>
+          </div>
+        </template>
+        <!-- empty-state -->
+        <template v-if="isFiltered && filtered.length === 0">
+          <div flex="~ col" items-center p="x4 y4" font-light>
+            <div op30>
+              No matched test
+            </div>
+            <button
+              type="button"
+              font-light
+              op="50 hover:100"
+              text-sm
+              border="~ gray-400/50 rounded"
+              p="x2 y0.5"
+              m="t2"
+              @click.passive="clearSearch(true)"
+            >
+              Clear Search
+            </button>
+          </div>
+        </template>
+        <template v-else>
+          <TaskTree
+            v-for="file in list"
+            :key="file.index"
+            nested
+            :task-id="file.data.id"
+            :search="search"
+            :opened="isFiltered"
+            :class="activeFileId === file.data.id ? 'bg-active' : ''"
+            :on-item-click="onItemClick"
+          />
+        </template>
       </DetailsPanel>
     </div>
   </div>
