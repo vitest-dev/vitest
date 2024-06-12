f75c5c557ad2fcb9ffcc0362073e774276851271
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-06-12 09:01:43 +0200

    chore: remove msw file

--- packages/browser/rollup.config.js
+++ packages/browser/rollup.config.js
@@ -4,7 +4,6 @@ import dts from 'rollup-plugin-dts'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
-import copy from 'rollup-plugin-copy'
 
 const require = createRequire(import.meta.url)
 const pkg = require('./package.json')
@@ -25,11 +24,6 @@ const plugins = [
   commonjs(),
   esbuild({
     target: 'node18',
-  }),
-  copy({
-    targets: [
-      { src: './msw/sw.js', dest: 'dist', rename: 'mocker-worker.js' },
-    ],
   }),
 ]
 
