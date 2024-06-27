8e437b5b8426333fdff520f5eb08af7c4a80d7fe
userquin <userquin@gmail.com>
2024-06-27 14:05:08 +0200

    chore: remove `registerInitializeListener` from auto imports dts

--- packages/ui/client/auto-imports.d.ts
+++ packages/ui/client/auto-imports.d.ts
@@ -109,7 +109,6 @@ declare global {
   const refDefault: typeof import('@vueuse/core')['refDefault']
   const refThrottled: typeof import('@vueuse/core')['refThrottled']
   const refWithControl: typeof import('@vueuse/core')['refWithControl']
-  const registerInitializeListener: typeof import('./composables/codemirror')['registerInitializeListener']
   const registerResizingListener: typeof import("./composables/browser")["registerResizingListener"]
   const resolveComponent: typeof import('vue')['resolveComponent']
   const resolveRef: typeof import('@vueuse/core')['resolveRef']
