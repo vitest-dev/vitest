94d1a399e06795ca61f200beb8c6564c69087e18
syi0808 <syi9397@naver.com>
2024-06-30 23:28:06 +0900

    test: hoist mock function and remove type assertion

--- test/config/test/workers-option.test.ts
+++ test/config/test/workers-option.test.ts
@@ -3,22 +3,12 @@ import { type UserConfig, describe, expect, test, vi } from 'vitest'
 import { getWorkersCountByPercentage } from 'vitest/src/utils/workers.js'
 import * as testUtils from '../../test-utils'
 
-function runVitest(config: UserConfig) {
-  return testUtils.runVitest({ ...config, root: './fixtures/workers-option' })
-}
-
-test('workers percent argument should not throw error', async () => {
-  const { stderr } = await runVitest({ maxWorkers: '100%', minWorkers: '10%' })
-
-  expect(stderr).toBe('')
-})
+vi.mock(import('node:os'), async importOriginal => ({
+  ...await importOriginal(),
+  cpus: () => Array.from({ length: 10 }),
+}))
 
 describe('workers util', () => {
-  vi.mock('node:os', async importOriginal => ({
-    ...(await importOriginal() as Record<any, any>),
-    cpus: () => Array.from({ length: 10 }),
-  }))
-
   test('percent=50% should return 5', () => {
     expect(getWorkersCountByPercentage('50%')).toBe(5)
   })
@@ -30,4 +20,14 @@ describe('workers util', () => {
   test('percent=110% should return 10', () => {
     expect(getWorkersCountByPercentage('110%')).toBe(10)
   })
+})
+
+function runVitest(config: UserConfig) {
+  return testUtils.runVitest({ ...config, root: './fixtures/workers-option' })
+}
+
+test('workers percent argument should not throw error', async () => {
+  const { stderr } = await runVitest({ maxWorkers: '100%', minWorkers: '10%' })
+
+  expect(stderr).toBe('')
 })
