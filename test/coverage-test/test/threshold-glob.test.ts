5cf3748d7de6e130833b90a1c446b704b4f33969
Ari Perkkiö <ari.perkkio@gmail.com>
2024-07-20 09:56:36 +0300

    test: adjust thresholds

--- test/coverage-test/test/threshold-glob.test.ts
+++ test/coverage-test/test/threshold-glob.test.ts
@@ -1,32 +1,33 @@
 import { expect } from 'vitest'
 import { coverageTest, isV8Provider, normalizeURL, runVitest, test } from '../utils'
 import { sum } from '../fixtures/src/math'
+import { isEven, isOdd } from '../fixtures/src/even'
 
 test('threshold glob patterns count in global coverage', async () => {
-  const { exitCode } = await runVitest({
+  await runVitest({
     include: [normalizeURL(import.meta.url)],
     coverage: {
       all: false,
-      include: ['**/fixtures/src/math.ts'],
+      include: ['**/fixtures/src/**'],
       thresholds: {
         'branches': 100,
-        'functions': 25,
-        'lines': isV8Provider() ? 50 : 25,
-        'statements': isV8Provider() ? 50 : 25,
+        'functions': 50,
+        'lines': isV8Provider() ? 66 : 50,
+        'statements': isV8Provider() ? 66 : 50,
 
-        '**/fixtures/src/math.ts': {
+        '**/fixtures/src/even.ts': {
           branches: 100,
-          functions: 25,
-          lines: isV8Provider() ? 50 : 25,
-          statements: isV8Provider() ? 50 : 25,
+          functions: 100,
+          lines: 100,
+          statements: 100,
         },
       },
     },
-  }, { throwOnError: false })
-
-  expect(exitCode).toBe(0)
+  })
 })
 
 coverageTest('cover some lines, but not too much', () => {
   expect(sum(1, 2)).toBe(3)
+  expect(isEven(4)).toBe(true)
+  expect(isOdd(4)).toBe(false)
 })
