62d11fa021d2c8744552d02a460fbca897c4272f
Ari Perkkiö <ari.perkkio@gmail.com>
2024-07-21 17:05:39 +0300

    test: coverage glob thresholds

--- test/coverage-test/test/threshold-glob.test.ts
+++ test/coverage-test/test/threshold-glob.test.ts
@@ -26,6 +26,45 @@ test('threshold glob patterns count in global coverage', async () => {
   })
 })
 
+test('{ thresholds: { 100: true } } on glob pattern', async () => {
+  const { stderr, exitCode } = await runVitest({
+    include: [normalizeURL(import.meta.url)],
+    coverage: {
+      include: [
+        '**/fixtures/src/even.ts',
+        '**/fixtures/src/math.ts',
+      ],
+      thresholds: {
+        '**/fixtures/src/even.ts': {
+          100: true,
+        },
+        '**/fixtures/src/math.ts': {
+          100: true,
+        },
+      },
+    },
+  }, { throwOnError: false })
+
+  expect(exitCode).toBe(1)
+
+  if (isV8Provider()) {
+    expect(stderr).toMatchInlineSnapshot(`
+      "ERROR: Coverage for lines (50%) does not meet "**/fixtures/src/math.ts" threshold (100%)
+      ERROR: Coverage for functions (25%) does not meet "**/fixtures/src/math.ts" threshold (100%)
+      ERROR: Coverage for statements (50%) does not meet "**/fixtures/src/math.ts" threshold (100%)
+      "
+    `)
+  }
+  else {
+    expect(stderr).toMatchInlineSnapshot(`
+      "ERROR: Coverage for lines (25%) does not meet "**/fixtures/src/math.ts" threshold (100%)
+      ERROR: Coverage for functions (25%) does not meet "**/fixtures/src/math.ts" threshold (100%)
+      ERROR: Coverage for statements (25%) does not meet "**/fixtures/src/math.ts" threshold (100%)
+      "
+    `)
+  }
+})
+
 coverageTest('cover some lines, but not too much', () => {
   expect(sum(1, 2)).toBe(3)
   expect(isEven(4)).toBe(true)
