b76a9279125c53e85bb0ab31b62863fc67335fa2
Vladimir <sleuths.slews0s@icloud.com>
2024-07-24 16:48:56 +0200

    refactor(vitest): move public exports to public folder (#6218)

---
+++ packages/vitest/src/public/workers.ts
@@ -0,0 +1,10 @@
+export {
+  createForksRpcOptions,
+  createThreadsRpcOptions,
+  unwrapSerializableConfig,
+} from '../runtime/workers/utils'
+export { provideWorkerState } from '../runtime/utils'
+export { run as runVitestWorker, collect as collectVitestWorkerTests } from '../runtime/worker'
+export { runVmTests } from '../runtime/workers/vm'
+export { runBaseTests } from '../runtime/workers/base'
+export type { WorkerRpcOptions, VitestWorker } from '../runtime/workers/types'
