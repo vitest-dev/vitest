94334b8f8c8babc36f6464c3ca58c2551d9529bd
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-08-01 13:59:43 +0200

    chore: cleanup

--- packages/vitest/src/typecheck/collect.ts
+++ packages/vitest/src/typecheck/collect.ts
@@ -55,7 +55,7 @@ export async function collectTests(
   const ast = await parseAstAsync(request.code)
   const testFilepath = relative(ctx.config.root, filepath)
   const projectName = ctx.getName()
-  const typecheckSubprojectName = projectName ? `${projectName}:typecheck` : 'typecheck'
+  const typecheckSubprojectName = projectName ? `${projectName}:__typecheck__` : '__typecheck__'
   const file: ParsedFile = {
     filepath,
     type: 'suite',
