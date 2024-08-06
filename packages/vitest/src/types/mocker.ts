edb75a9264a45302bbd45eca88f511e37edd05fa
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-08-06 16:41:32 +0200

    feat(vitest): allow auto spying on all exports

--- packages/vitest/src/types/mocker.ts
+++ packages/vitest/src/types/mocker.ts
@@ -2,13 +2,19 @@ export type MockFactoryWithHelper<M = unknown> = (
   importOriginal: <T extends M>() => Promise<T>
 ) => any
 export type MockFactory = () => any
+export interface MockOptions {
+  spy?: boolean
+}
 
 export type MockMap = Map<string, Record<string, string | null | MockFactory>>
+
+export type MockBehaviour = 'autospy' | 'automock' | 'manual'
 
 export interface PendingSuiteMock {
   id: string
   importer: string
   type: 'mock' | 'unmock'
   throwIfCached: boolean
+  behaviour?: MockBehaviour
   factory?: MockFactory
 }
