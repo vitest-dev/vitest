0c197f5b84e030a8000c207a84c4c00f9d6540bb
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-08 09:52:05 +0900

    chore: lint

--- test/config/test/fs-cached-check.test.ts
+++ test/config/test/fs-cached-check.test.ts
@@ -1,12 +1,12 @@
-import { expect, test } from "vitest";
-import { runVitest } from "../../test-utils";
-import fs from "node:fs"
-import path from "node:path"
+import fs from 'node:fs'
+import path from 'node:path'
+import { expect, test } from 'vitest'
+import { runVitest } from '../../test-utils'
 
-test("fs cache", async () => {
-  // ensure removed
-  const root = path.resolve("fixtures/fs-cached-check")
-  await fs.promises.rm(path.join(root, "dist"), { recursive: true, force: true })
+test('import a generated file', async () => {
+  // ensure removed first
+  const root = path.resolve('fixtures/fs-cached-check')
+  await fs.promises.rm(path.join(root, 'dist'), { recursive: true, force: true })
 
   const { stderr, exitCode } = await runVitest(
     {
@@ -21,7 +21,7 @@ test("fs cache", async () => {
         },
       },
     },
-  );
+  )
   expect(stderr).toBe('')
   expect(exitCode).toBe(0)
-});
+})
