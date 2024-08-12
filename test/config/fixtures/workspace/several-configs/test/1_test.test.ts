b1d60cc2db31dd79c75091d6a52dab993dc1c268
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-08-12 18:00:49 +0200

    test: workspace runs correctly with several configs

---
+++ test/config/fixtures/workspace/several-configs/test/1_test.test.ts
@@ -0,0 +1,5 @@
+import { expect, test } from 'vitest';
+
+test('1 + 1 = 2', () => {
+  expect(1 + 1).toBe(2);
+})
--- test/config/test/workspace.test.ts
+++ test/config/test/workspace.test.ts
@@ -10,3 +10,16 @@ it('correctly runs workspace tests when workspace config path is specified', asy
   expect(stdout).toContain('1 + 1 = 2')
   expect(stdout).not.toContain('2 + 2 = 4')
 })
+
+it('runs the workspace if there are several vitest config files', async () => {
+  const { stderr, stdout } = await runVitest({
+    root: 'fixtures/workspace/several-configs',
+    workspace: './fixtures/workspace/several-configs/vitest.workspace.ts',
+  })
+  expect(stderr).toBe('')
+  expect(stdout).toContain('workspace/several-configs')
+  expect(stdout).toContain('| 1_test')
+  expect(stdout).toContain('| 2_test')
+  expect(stdout).toContain('1 + 1 = 2')
+  expect(stdout).toContain('2 + 2 = 4')
+})
