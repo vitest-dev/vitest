f91eba21934f972c7628b6e7386c0b9f78e464cc
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-13 14:38:23 +0900

    fix(snapshot): reject toMatchInlineSnapshot in loop

---
+++ test/cli/fixtures/fails/inline-snapshop-inside-loop.test.ts
@@ -0,0 +1,31 @@
+import {test, expect} from "vitest";
+
+test("ok", () => {
+  expect("ok").toMatchInlineSnapshot(`"ok"`);
+});
+
+test("fail 1", () => {
+  for (const str of ["foo", "bar"]) {
+    expect(str).toMatchInlineSnapshot();
+  }
+});
+
+// this test causes infinite re-run when --watch and --update
+// since snapshot update switches between "foo" and "bar" forever.
+test("fail 2", () => {
+  for (const str of ["foo", "bar"]) {
+    expect(str).toMatchInlineSnapshot(`"bar"`);
+  }
+});
+
+test("fail 3", () => {
+  for (const str of ["ok", "ok"]) {
+    expect(str).toMatchInlineSnapshot();
+  }
+});
+
+test("somehow ok", () => {
+  for (const str of ["ok", "ok"]) {
+    expect(str).toMatchInlineSnapshot(`"ok"`);
+  }
+});
