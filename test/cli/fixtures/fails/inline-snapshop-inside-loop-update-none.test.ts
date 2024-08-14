29234f0f72e4fbd59f2f87f08ea651b60d8c2d6b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-14 09:27:15 +0900

    fix(snapshot): improve inline snapshot loop detection

--- test/cli/fixtures/fails/inline-snapshop-inside-loop-update-none.test.ts
+++ test/cli/fixtures/fails/inline-snapshop-inside-loop-update-none.test.ts
@@ -1,9 +1,1 @@
-import {test, expect} from "vitest";
-
-// this test causes infinite re-run when --watch and --update
-// since snapshot update switches between "foo" and "bar" forever.
-test("fail 2", () => {
-  for (const str of ["foo", "bar"]) {
-    expect(str).toMatchInlineSnapshot(`"bar"`);
-  }
-});
+import "./inline-snapshop-inside-loop-update-all.test";
