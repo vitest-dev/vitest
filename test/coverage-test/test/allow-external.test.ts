9b463d66285b1068e75c9f4c72dd3c32af44d159
Ari Perkkiö <ari.perkkio@gmail.com>
2024-08-20 20:31:22 +0300

    test(coverage): uncovered files with `allowExternal`

--- test/coverage-test/test/allow-external.test.ts
+++ test/coverage-test/test/allow-external.test.ts
@@ -4,13 +4,21 @@ import { readCoverageMap, runVitest, test } from '../utils'
 test('{ allowExternal: true } includes files outside project root', async () => {
   await runVitest({
     include: ['fixtures/test/allow-external-fixture.test.ts'],
-    coverage: { allowExternal: true, reporter: 'json', include: ['**/fixtures/**'], all: false },
+    coverage: {
+      allowExternal: true,
+      reporter: 'json',
+      include: ['**/fixtures/src/math.ts', '**/test/test-utils/fixtures/**'],
+      all: true,
+    },
   })
   const coverageMap = await readCoverageMap()
   const files = coverageMap.files()
 
   // File outside project root
   expect(files).toContain('<project-root>/test/test-utils/fixtures/external-math.ts')
+
+  // Uncovered files outside project root should also be included
+  expect(files).toContain('<project-root>/test/test-utils/fixtures/uncovered.ts')
 
   // Files inside project root should always be included
   expect(files).toContain('<process-cwd>/fixtures/src/math.ts')
@@ -19,13 +27,18 @@ test('{ allowExternal: true } includes files outside project root', async () => 
 test('{ allowExternal: false } excludes files outside project root', async () => {
   await runVitest({
     include: ['fixtures/test/allow-external-fixture.test.ts'],
-    coverage: { allowExternal: false, reporter: 'json', include: ['**/fixtures/**'] },
+    coverage: {
+      allowExternal: false,
+      reporter: 'json',
+      include: ['**/fixtures/src/math.ts', '**/test/test-utils/fixtures/**'],
+    },
   })
   const coverageMap = await readCoverageMap()
   const files = coverageMap.files()
 
   // File outside project root
   expect(files.find(file => file.includes('test-utils/fixtures/external-math.ts'))).toBeFalsy()
+  expect(files.find(file => file.includes('test-utils/fixtures/uncovered.ts'))).toBeFalsy()
 
   // Files inside project root should always be included
   expect(files).toContain('<process-cwd>/fixtures/src/math.ts')
---
+++ test/test-utils/fixtures/uncovered.ts
@@ -0,0 +1,3 @@
+export function uncovered() {
+  return "This file is not covered"
+}
