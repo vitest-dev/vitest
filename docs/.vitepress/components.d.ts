069d856e38cbedf9e70ff359c6b2b678a1e453de
patak-dev <matias.capeletto@gmail.com>
2024-08-27 18:32:52 +0200

    docs: viteconf 24 cta

--- docs/.vitepress/components.d.ts
+++ docs/.vitepress/components.d.ts
@@ -7,6 +7,7 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    AsideViteConf: typeof import('./components/AsideViteConf.vue')['default']
     Contributors: typeof import('./components/Contributors.vue')['default']
     CourseLink: typeof import('./components/CourseLink.vue')['default']
     FeaturesList: typeof import('./components/FeaturesList.vue')['default']
