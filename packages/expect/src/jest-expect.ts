f7da6199d11ac0397f101a32f88758e5273cd0d1
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-09-25 17:30:46 +0900

    fix(expect): fix `toBeDefined` with `expect.poll` (#6562)

--- packages/expect/src/jest-expect.ts
+++ packages/expect/src/jest-expect.ts
@@ -384,14 +384,13 @@ export const JestChaiExpect: ChaiPlugin = (chai, utils) => {
     return this.be.null
   })
   def('toBeDefined', function () {
-    const negate = utils.flag(this, 'negate')
-    utils.flag(this, 'negate', false)
-
-    if (negate) {
-      return this.be.undefined
-    }
-
-    return this.not.be.undefined
+    const obj = utils.flag(this, 'object')
+    this.assert(
+      typeof obj !== 'undefined',
+      'expected #{this} to be defined',
+      'expected #{this} to be undefined',
+      obj,
+    )
   })
   def(
     'toBeTypeOf',
