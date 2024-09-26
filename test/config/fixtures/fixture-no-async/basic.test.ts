e2ccba45a24795cf861925956ce3a8ce1ae9799f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-09-26 09:25:17 +0900

    fix(runner): fix fixture parsing with lowered non arrow async function

--- test/config/fixtures/fixture-no-async/basic.test.ts
+++ test/config/fixtures/fixture-no-async/basic.test.ts
@@ -3,6 +3,9 @@ import { test as base, expect } from "vitest";
 type Fixture = {
   simple: string,
   nested: string,
+  notArrow1: string,
+  notArrow2: string,
+  notArrow3: string,
 }
 
 const test = base.extend<Fixture>({
@@ -12,6 +15,15 @@ const test = base.extend<Fixture>({
   nested: async ({ simple }, use) => {
     await use("nested:" + simple);
   },
+  async notArrow1({}, use) {
+    await use("notArrow1");
+  },
+  notArrow2: async function({}, use) {
+    await use("notArrow2");
+  },
+  notArrow3: async function notArrow3({}, use) {
+    await use("notArrow3");
+  }
 });
 
 test("test sync", ({ simple, nested }) => {
@@ -22,6 +34,12 @@ test("test sync", ({ simple, nested }) => {
 test("test async", async ({ simple, nested }) => {
   expect(simple).toBe("simple");
   expect(nested).toBe("nested:simple")
+});
+
+test("test notArrow", async function ({ notArrow1, notArrow2, notArrow3 }) {
+  expect(notArrow1).toMatchInlineSnapshot(`"notArrow1"`)
+  expect(notArrow2).toMatchInlineSnapshot(`"notArrow2"`)
+  expect(notArrow3).toMatchInlineSnapshot(`"notArrow3"`)
 });
 
 test.for([1, 2])("test.for sync %i", (i, { expect, simple, nested }) => {
