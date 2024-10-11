20afb7d5a06027e21557127f8be29d305b3d238c
Vladimir <sleuths.slews0s@icloud.com>
2024-10-11 16:50:12 +0200

    fix: also check for file:// imports

--- packages/vite-node/src/server.ts
+++ packages/vite-node/src/server.ts
@@ -1,6 +1,7 @@
 import { performance } from 'node:perf_hooks'
 import { existsSync } from 'node:fs'
 import assert from 'node:assert'
+import { pathToFileURL } from 'node:url'
 import { join, normalize, relative, resolve } from 'pathe'
 import type { TransformResult, ViteDevServer } from 'vite'
 import createDebug from 'debug'
@@ -106,7 +107,13 @@ export class ViteNodeServer {
     }
 
     if (options.deps.inlineFiles) {
-      options.deps.inlineFiles = options.deps.inlineFiles.map(file => resolve(file))
+      options.deps.inlineFiles = options.deps.inlineFiles.flatMap((file) => {
+        if (file.startsWith('file://')) {
+          return file
+        }
+        const resolvedId = resolve(file)
+        return [resolvedId, pathToFileURL(resolvedId).href]
+      })
     }
 
     options.deps.moduleDirectories ??= []
