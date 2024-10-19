bfecf85592b45576af320721e34aa331fd8af573
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-10-19 10:23:26 +0900

    test: tweak

--- test/config/test/diff.test.ts
+++ test/config/test/diff.test.ts
@@ -1,18 +1,15 @@
-import { describe } from 'node:test'
 import { expect, test } from 'vitest'
 import { runVitest } from '../../test-utils'
 
-describe('inline diff options', () => {
-  test.for([
-    [undefined],
-    [{ expand: false }],
-  ])('options %o', async ([options]) => {
-    const { stdout } = await runVitest({
-      root: './fixtures/diff',
-      diff: options,
-      reporters: ['junit'],
-    })
-    const matches = stdout.matchAll(/<failure[^>]*>(.*)<\/failure>/gs)
-    expect([...matches].map(m => m[1])).matchSnapshot()
+test.for([
+  [undefined],
+  [{ expand: false }],
+])('inline diff options %o', async ([options]) => {
+  const { stdout } = await runVitest({
+    root: './fixtures/diff',
+    diff: options,
+    reporters: ['junit'],
   })
+  const matches = stdout.matchAll(/<failure[^>]*>(.*)<\/failure>/gs)
+  expect([...matches].map(m => m[1])).matchSnapshot()
 })
