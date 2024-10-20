745c9e5b431913b8266f0448b47b4428721b4771
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-10-20 14:31:32 +0900

    test: tweak

--- test/config/test/diff.test.ts
+++ test/config/test/diff.test.ts
@@ -1,15 +1,21 @@
+import { stripVTControlCharacters } from 'node:util'
 import { expect, test } from 'vitest'
 import { runVitest } from '../../test-utils'
 
 test.for([
   [undefined],
   [{ expand: false }],
-])('inline diff options %o', async ([options]) => {
-  const { stdout } = await runVitest({
+])(`inline diff options: %o`, async ([options]) => {
+  const { ctx } = await runVitest({
     root: './fixtures/diff',
     diff: options,
-    reporters: ['junit'],
   })
-  const matches = stdout.matchAll(/<failure[^>]*>(.*)<\/failure>/gs)
-  expect([...matches].map(m => m[1])).matchSnapshot()
+  const errors = ctx!.state
+    .getFiles()
+    .flatMap(f =>
+      f.tasks.flatMap(t => t.result?.errors ?? []),
+    )
+  expect(
+    errors.map(e => e.diff && stripVTControlCharacters(e.diff)),
+  ).matchSnapshot()
 })
