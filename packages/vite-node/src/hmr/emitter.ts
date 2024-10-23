bce9ff8c6661fa50ab1135883a801fb247279e21
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-10-23 18:00:07 +0900

    fix(vite-node): invalidate source map cache

--- packages/vite-node/src/hmr/emitter.ts
+++ packages/vite-node/src/hmr/emitter.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from 'node:events'
 import type { HMRPayload, Plugin } from 'vite'
+import { sourceMapCache } from '../source-map-cache'
 
 export type EventType = string | symbol
 export type Handler<T = unknown> = (event: T) => void
@@ -72,6 +73,10 @@ export function viteNodeHmrPlugin(): Plugin {
           emitter.emit('message', payload)
         }
       }
+    },
+
+    handleHotUpdate(ctx) {
+      delete sourceMapCache[ctx.file]
     },
   }
 }
