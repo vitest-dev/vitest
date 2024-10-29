a34b4f58d7cd3ffb3978da3554a30c3fa7485625
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-10-29 14:50:23 +0100

    test: add a test with a manual rerun

--- test/watch/test/global-setup-rerun.test.ts
+++ test/watch/test/global-setup-rerun.test.ts
@@ -3,7 +3,7 @@ import { editFile, runVitest } from '../../test-utils'
 
 const testFile = 'fixtures/math.test.ts'
 
-test('global setup calls hooks correctly', async () => {
+test('global setup calls hooks correctly when file changes', async () => {
   process.env.TEST_GLOBAL_SETUP = 'true'
   const { vitest, ctx } = await runVitest({
     root: 'fixtures',
@@ -17,6 +17,29 @@ test('global setup calls hooks correctly', async () => {
   expect(calls).toEqual(['start'])
 
   editFile(testFile, testFileContent => `${testFileContent}\n\n`)
+
+  await vitest.waitForStdout('RERUN')
+  expect(calls).toEqual(['start', 'rerun'])
+
+  await ctx?.close()
+
+  expect(calls).toEqual(['start', 'rerun', 'end'])
+})
+
+test('global setup calls hooks correctly with a manual rerun', async () => {
+  process.env.TEST_GLOBAL_SETUP = 'true'
+  const { vitest, ctx } = await runVitest({
+    root: 'fixtures',
+    watch: true,
+    include: ['math.test.ts'],
+  })
+
+  await vitest.waitForStdout('Waiting for file changes')
+
+  const calls = (globalThis as any).__CALLS as string[]
+  expect(calls).toEqual(['start'])
+
+  vitest.write('r')
 
   await vitest.waitForStdout('RERUN')
   expect(calls).toEqual(['start', 'rerun'])
