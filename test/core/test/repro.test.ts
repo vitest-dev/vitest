047badf842ce20e23d9e598e75641ce1b53ad768
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-10-30 14:14:22 +0900

    wip: test

---
+++ test/core/test/repro.test.ts
@@ -0,0 +1,20 @@
+import { expect, test } from 'vitest'
+
+test('repro', () => {
+  expect
+})
+
+// test('inline repeats', { repeats: 1 }, () => {
+//   expect('foo').toMatchInlineSnapshot(`"foo"`);
+// });
+
+// test('inline retry', { retry: 1 }, (ctx) => {
+//   expect('foo').toMatchInlineSnapshot(`"foo"`)
+//   if (ctx.task.result?.retryCount === 0) {
+//     throw new Error('boom')
+//   }
+// })
+
+// test('foo', { retry: 1 }, () => {
+//   expect('foo').toMatchSnapshot()
+// });
