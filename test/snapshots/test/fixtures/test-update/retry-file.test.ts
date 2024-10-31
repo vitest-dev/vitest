e0768a10816f2ce24e8f713ea5c1bc07cefd5e57
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-10-31 17:26:29 +0900

    test: cleanup

--- test/snapshots/test/fixtures/test-update/retry-file.test.ts
+++ test/snapshots/test/fixtures/test-update/retry-file.test.ts
@@ -1,20 +1,5 @@
 import { expect, test } from 'vitest'
 
-test('repro', () => {
-  expect(0).toBe(0)
-})
-
-test('inline repeats', { repeats: 1 }, () => {
-  expect('foo').toMatchInlineSnapshot(`"foo"`)
-})
-
-test('inline retry', { retry: 1 }, (ctx) => {
-  expect('foo').toMatchInlineSnapshot(`"foo"`)
-  if (ctx.task.result?.retryCount === 0) {
-    throw new Error('boom')
-  }
-})
-
 test('file repeats', { repeats: 1 }, () => {
   expect('foo').toMatchSnapshot()
 })
--- test/snapshots/test/test-update.test.ts
+++ test/snapshots/test/test-update.test.ts
@@ -4,7 +4,7 @@ import { expect, test } from 'vitest'
 
 const dir = join(import.meta.dirname, '../test-update')
 const dirents = await fs.readdir(dir, { withFileTypes: true })
-const files = dirents.filter(f => f.isFile()).map(f => f.name)
+const files = dirents.filter(f => f.isFile() && f.name.includes('inline')).map(f => f.name)
 test.for(files)('%s', async (file) => {
   const content = await fs.readFile(join(dir, file), 'utf8')
   expect(content).toMatchSnapshot()
