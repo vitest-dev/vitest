63d981752d75440810b80b7b29a5f7aac7a3ee2e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-11-04 16:32:27 +0900

    test: use import.meta.glob

--- test/snapshots/test/test-update.test.ts
+++ test/snapshots/test/test-update.test.ts
@@ -1,11 +1,8 @@
-import fs from 'node:fs/promises'
-import { join } from 'node:path'
 import { expect, test } from 'vitest'
 
-const dir = join(import.meta.dirname, '../test-update')
-const dirents = await fs.readdir(dir, { withFileTypes: true })
-const files = dirents.filter(f => f.isFile() && f.name.includes('inline')).map(f => f.name)
-test.for(files)('%s', async (file) => {
-  const content = await fs.readFile(join(dir, file), 'utf8')
-  expect(content).toMatchSnapshot()
-})
+const entries = import.meta.glob('../test-update/*inline*', { eager: true, query: 'raw' })
+for (const [file, mod] of Object.entries(entries)) {
+  test(file.split('/').at(-1)!, () => {
+    expect((mod as any).default).toMatchSnapshot()
+  })
+}
