bce4c191aec8fb1590f39189481ba137ae41f1b4
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-11-07 14:51:29 +0900

    ci: debug

--- test/attest/fixtures/setup-attest-analyze.ts
+++ test/attest/fixtures/setup-attest-analyze.ts
@@ -1,5 +1,5 @@
 import type { GlobalSetupContext } from 'vitest/node'
-import { execSync } from 'node:child_process'
+import { execFileSync } from 'node:child_process'
 import { mkdirSync } from 'node:fs'
 import { getConfig } from '@ark/attest/internal/config.js'
 
@@ -11,7 +11,7 @@ const config = getConfig()
 
 async function setup2() {
   mkdirSync('.attest/assertions', { recursive: true })
-  execSync('attest precache .attest/assertions/typescript.json', {
+  execFileSync('../node_modules/.bin/attest', ['precache', '.attest/assertions/typescript.json'], {
     stdio: 'inherit',
   })
 }
