56e95dd585e660d1b4d39ab7d2a805edbfa7c89f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-11-08 09:49:47 +0900

    test: tweak

--- test/browser/fixtures/user-event/keyboard.test.ts
+++ test/browser/fixtures/user-event/keyboard.test.ts
@@ -3,14 +3,34 @@ import { userEvent, page, server } from '@vitest/browser/context'
 
 test('non US keys', async () => {
   document.body.innerHTML = `
-    <input placeholder="#7396" />
-    <input placeholder="emoji" />
+    <input placeholder="type-#7396" />
+    <input placeholder="fill-#7396" />
+    <input placeholder="type-emoji" />
+    <input placeholder="fill-emoji" />
   `;
-  await userEvent.type(page.getByPlaceholder("#7396"), 'éèù')
-  await expect.element(page.getByPlaceholder("#7396")).toHaveValue('éèù')
 
-  if (server.provider !== 'webdriverio') {
-    await userEvent.type(page.getByPlaceholder("emoji"), '😊')
-    await expect.element(page.getByPlaceholder("emoji")).toHaveValue('😊')
+  await userEvent.type(page.getByPlaceholder("type-#7396"), 'éèù')
+  await expect.element(page.getByPlaceholder("type-#7396")).toHaveValue('éèù')
+  await userEvent.fill(page.getByPlaceholder("fill-#7396"), 'éèù')
+  await expect.element(page.getByPlaceholder("fill-#7396")).toHaveValue('éèù')
+
+  // playwright: garbled characters
+  // webdriverio: error
+  // preview: ok
+  try {
+    await userEvent.fill(page.getByPlaceholder("type-emoji"), '😊😍')
+    await expect.element(page.getByPlaceholder("type-emoji")).toHaveValue('😊😍')
+  } catch (e) {
+    console.error(e)
+  }
+
+  // playwright: ok
+  // webdriverio: error
+  // preview: ok
+  try {
+    await userEvent.type(page.getByPlaceholder("fill-emoji"), '😊😍')
+    await expect.element(page.getByPlaceholder("fill-emoji")).toHaveValue('😊😍')
+  } catch (e) {
+    console.error(e)
   }
 })
