5403ffbfbad3fd422c4303cf5d26f0848d225f8b
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-11-08 15:42:07 +0100

    ci: add type lint check

---
+++ tsslint.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from '@tsslint/config'
+import { loadPluginRules } from '@tsslint/eslint'
+
+export default defineConfig({
+  include: ['./packages/**/*.ts'],
+  rules: {
+    ...loadPluginRules({
+      '@typescript-eslint/use-unknown-in-catch-callback-variable': 'error',
+      '@typescript-eslint/return-await': 'error',
+      '@typescript-eslint/restrict-plus-operands': 'error',
+      '@typescript-eslint/require-await': 'error',
+      '@typescript-eslint/prefer-return-this-type': 'error',
+      '@typescript-eslint/prefer-regexp-exec': 'error',
+      '@typescript-eslint/prefer-optional-chain': 'error',
+      '@typescript-eslint/prefer-nullish-coalescing': 'error',
+      '@typescript-eslint/prefer-find': 'error',
+      '@typescript-eslint/non-nullable-type-assertion-style': 'error',
+      '@typescript-eslint/no-unsafe-return': 'error',
+      '@typescript-eslint/no-unsafe-argument': 'error',
+      '@typescript-eslint/no-unsafe-assignment': 'error',
+      '@typescript-eslint/no-unnecessary-condition': 'error',
+      '@typescript-eslint/no-misused-promises': 'error',
+      '@typescript-eslint/await-thenable': 'error',
+      '@typescript-eslint/no-floating-promises': 'error',
+    }),
+  },
+})
