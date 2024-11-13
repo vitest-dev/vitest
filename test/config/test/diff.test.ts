04ec5fa451792a83bdca3e4ca63a6d822204c463
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-11-13 09:52:25 +0900

    chore: lint

--- test/config/test/diff.test.ts
+++ test/config/test/diff.test.ts
@@ -6,11 +6,9 @@ test('diff', async () => {
   const { ctx } = await runVitest({
     root: './fixtures/diff',
   })
-  const errors = ctx!.state
-    .getFiles()
-    .flatMap(f =>
-      f.tasks.flatMap(t => t.result?.errors ?? []),
-    )
+  const errors = ctx!.state.getFiles().flatMap(f =>
+    f.tasks.flatMap(t => t.result?.errors ?? []),
+  )
   expect(
     errors.map(e => e.diff && stripVTControlCharacters(e.diff)),
   ).matchSnapshot()
