52d5e3b4a928a3d3c1b8c6ce847de8629f3ec1c2
mzhubail <mohamed.hubail@array.world>
2024-11-21 12:08:50 +0300

    Add run command tests

--- test/cli/test/location-filters.test.ts
+++ test/cli/test/location-filters.test.ts
@@ -72,80 +72,81 @@ describe('location filter with list command', () => {
   })
 })
 
-// describe('location filter with run command', () => {
-//   test('finds test at correct line number', async () => {
-//     const { stdout, stderr } = await runVitestCli(
-//       'run',
-//       '-r=./fixtures/location-filters',
-//       '--config=vitest.config.ts',
-//       'basic.test.ts:6',
-//       // 'basic.test.ts:15',
-//       // 'math.test.ts:3',
-//     )
+describe('location filter with run command', () => {
+  test('handles file with a dash in the name', async () => {
+    const { stdout, stderr } = await runVitestCli(
+      'run',
+      '-r=./fixtures/location-filters',
+      'math-with-dashes-in-name.test.ts:3',
+    )
+
+    expect(stdout).contain('1 passed')
+    expect(stdout).contain('1 skipped')
+    expect(stderr).toEqual('')
+  })
+
+  test('reports not found test', async () => {
+    const { stdout, stderr } = await runVitestCli(
+      'run',
+      '-r=./fixtures/location-filters',
+      'basic.test.ts:99',
+    )
+
+    expect(stdout).toContain('4 skipped')
+    expect(stderr).toContain('Error: No test found in basic.test.ts in line 99')
+  })
+
+  test('reports multiple not found tests', async () => {
+    const { stdout, stderr } = await runVitestCli(
+      'run',
+      '-r=./fixtures/location-filters',
+      'basic.test.ts:5',
+      'basic.test.ts:12',
+      'basic.test.ts:99',
+    )
+
+    expect(stdout).toContain('4 skipped')
+    expect(stderr).toContain('Error: No test found in basic.test.ts in lines 12, 99')
+  })
+
+  test('errors if range location is provided', async () => {
+    const { stderr } = await runVitestCli(
+      'run',
+      '-r=./fixtures/location-filters',
+      'a/file/that/doesnt/exit:10-15',
+    )
+
+    expect(stderr).toContain('Error: Found "-"')
+  })
+
+  test('errors if includeTaskLocation is not enabled', async () => {
+    const { stderr } = await runVitestCli(
+      'run',
+      '-r=./fixtures/location-filters',
+      '--config=no-task-location.config.ts',
+      'a/file/that/doesnt/exist:5',
+    )
+
+    expect(stderr).toMatchInlineSnapshot(`
+      "Error: Recieved line number filters while \`includeTaskLocation\` option is disabled
+      "
+    `)
+  })
+
+  test('fails on part of filename with location filter', async () => {
+    const { stdout, stderr } = await runVitestCli(
+      'run',
+      '-r=./fixtures/location-filters',
+      'math:999',
+    )
+
+    expect(stdout).contain('math.test.ts')
+    expect(stdout).contain('math-with-dashes-in-name.test.ts')
+    expect(stderr).toMatchInlineSnapshot(`
+      "Error: Couldn't find file "math". Ignoring test location filter.
+      Note when specifying the test location you have to specify the full test filename.
+      "
+    `)
+  })
+})
 //
-//     expect(`${stdout} ${stderr}`).toEqual('')
-//
-//     expect(stdout).toMatchInlineSnapshot(`
-//       "[custom] basic.test.ts > basic suite > inner suite > some test
-//       "
-//     `)
-//     expect(stderr).toEqual('')
-//   })
-//
-//   // test('reports not found test', async () => {
-//   //   const { stdout, stderr } = await runVitestCli(
-//   //     'list',
-//   //     '-r=./fixtures/location-filters',
-//   //     '--config=custom.config.ts',
-//   //     'basic.test.ts:99',
-//   //   )
-//   //
-//   //   expect(stdout).toEqual('')
-//   //   expect(stderr).toMatchInlineSnapshot(`
-//   //     "Error: No test found in basic.test.ts in line 99
-//   //     "
-//   //   `)
-//   // })
-//   //
-//   // test('reports multiple not found tests', async () => {
-//   //   const { stdout, stderr } = await runVitestCli(
-//   //     'list',
-//   //     '-r=./fixtures/location-filters',
-//   //     '--config=custom.config.ts',
-//   //     'basic.test.ts:5',
-//   //     'basic.test.ts:12',
-//   //     'basic.test.ts:99',
-//   //   )
-//   //
-//   //   expect(stdout).toEqual('')
-//   //   expect(stderr).toMatchInlineSnapshot(`
-//   //     "Error: No test found in basic.test.ts in lines 12, 99
-//   //     "
-//   //   `)
-//   // })
-//   //
-//   // test('errors if range location is provided', async () => {
-//   //   const { stdout, stderr } = await runVitestCli(
-//   //     'list',
-//   //     '-r=./fixtures/location-filters',
-//   //     'a/file/that/doesnt/exit:10-15',
-//   //   )
-//   //
-//   //   expect(stdout).toEqual('')
-//   //   expect(stderr).toContain('Collect Error')
-//   //   expect(stderr).toContain('RangeLocationFilterProvidedError')
-//   // })
-//   //
-//   // test('erorrs if includeTaskLocation is not enabled', async () => {
-//   //   const { stdout, stderr } = await runVitestCli(
-//   //     'list',
-//   //     '-r=./fixtures/location-filters',
-//   //     '--config=no-task-location.config.ts',
-//   //     'a/file/that/doesnt/exist:5',
-//   //   )
-//   //
-//   //   expect(stdout).toEqual('')
-//   //   expect(stderr).toContain('Collect Error')
-//   //   expect(stderr).toContain('IncludeTaskLocationDisabledError')
-//   // })
-// })
