ce63c397f7a4677dbf2dc996c91d7e58ada4e9f7
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-12-01 18:11:40 +0900

    chore: lint

--- test/dts-playwright/src/basic.test.ts
+++ test/dts-playwright/src/basic.test.ts
@@ -1,8 +1,8 @@
+import { page, userEvent } from '@vitest/browser/context'
 /// <reference types="@vitest/browser/providers/playwright" />
 import { test } from 'vitest'
-import { userEvent, page } from "@vitest/browser/context"
 
-test("basic", async () => {
-  document.body.innerHTML = `<button>hello</button>`;
-  await userEvent.click(page.getByRole('button'), { force: true });
+test('basic', async () => {
+  document.body.innerHTML = `<button>hello</button>`
+  await userEvent.click(page.getByRole('button'), { force: true })
 })
