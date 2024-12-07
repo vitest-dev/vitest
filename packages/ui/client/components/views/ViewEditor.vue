29bcbeb97c82d97c93be66d130886efa75a5d81f
userquin <userquin@gmail.com>
2024-12-07 05:39:22 +0100

    fix(ui): add errors and draft state (*) to the code editor

--- packages/ui/client/components/views/ViewEditor.vue
+++ packages/ui/client/components/views/ViewEditor.vue
@@ -3,6 +3,7 @@ import type CodeMirror from 'codemirror'
 import type { ErrorWithDiff, File } from 'vitest'
 import { createTooltip, destroyTooltip } from 'floating-vue'
 import { client, isReport } from '~/composables/client'
+import { finished } from '~/composables/client/state'
 import { codemirrorRef } from '~/composables/codemirror'
 import { openInEditor } from '~/composables/error'
 import { lineNumber } from '~/composables/params'
@@ -17,6 +18,9 @@ const code = ref('')
 const serverCode = shallowRef<string | undefined>(undefined)
 const draft = ref(false)
 const loading = ref(true)
+// finished.value is true when saving the file, we need it to restore the caret position
+const saving = ref(true)
+const currentPosition = ref<CodeMirror.Position | undefined>()
 
 watch(
   () => props.file,
@@ -34,10 +38,15 @@ watch(
       serverCode.value = code.value
       draft.value = false
     }
-    finally {
-      // fire focusing editor after loading
-      nextTick(() => (loading.value = false))
+    catch (e) {
+      console.error('cannot fetch file', e)
     }
+
+    await nextTick()
+
+    // fire focusing editor after loading
+    loading.value = false
+    saving.value = false
   },
   { immediate: true },
 )
@@ -65,13 +74,9 @@ watch(() => [loading.value, props.file, lineNumber.value] as const, ([loadingFil
 const ext = computed(() => props.file?.filepath?.split(/\./g).pop() || 'js')
 const editor = ref<any>()
 
-const cm = computed<CodeMirror.EditorFromTextArea | undefined>(
-  () => editor.value?.cm,
-)
 const failed = computed(
   () => props.file?.tasks.filter(i => i.result?.state === 'fail') || [],
 )
-
 const widgets: CodeMirror.LineWidget[] = []
 const handles: CodeMirror.LineHandle[] = []
 const listeners: [el: HTMLSpanElement, l: EventListener, t: () => void][] = []
@@ -134,46 +139,103 @@ function createErrorElement(e: ErrorWithDiff) {
   const el: EventListener = async () => {
     await openInEditor(stack.file, stack.line, stack.column)
   }
+  span.addEventListener('click', el)
   div.appendChild(span)
   listeners.push([span, el, () => destroyTooltip(span)])
   handles.push(codemirrorRef.value!.addLineClass(stack.line - 1, 'wrap', 'bg-red-500/10'))
   widgets.push(codemirrorRef.value!.addLineWidget(stack.line - 1, div))
 }
 
-watch(
-  [cm, failed],
-  ([cmValue]) => {
+const { pause, resume } = watch(
+  [codemirrorRef, failed, finished, saving] as const,
+  ([cmValue, f, end, s]) => {
     if (!cmValue) {
+      widgets.length = 0
+      handles.length = 0
       clearListeners()
       return
     }
 
-    setTimeout(() => {
-      clearListeners()
-      widgets.forEach(widget => widget.clear())
-      handles.forEach(h => codemirrorRef.value?.removeLineClass(h, 'wrap'))
-      widgets.length = 0
-      handles.length = 0
+    // if still running or saving return
+    if (!end || s) {
+      return
+    }
 
-      cmValue.on('changes', codemirrorChanges)
+    cmValue.off('changes', codemirrorChanges)
 
-      failed.value.forEach((i) => {
-        i.result?.errors?.forEach(createErrorElement)
-      })
-      if (!hasBeenEdited.value) {
-        cmValue.clearHistory()
-      } // Prevent getting access to initial state
-    }, 100)
+    // cleanup previous data
+    clearListeners()
+    widgets.forEach(widget => widget.clear())
+    handles.forEach(h => cmValue?.removeLineClass(h, 'wrap'))
+    widgets.length = 0
+    handles.length = 0
+
+    // add new data
+    f.forEach((i) => {
+      i.result?.errors?.forEach(createErrorElement)
+    })
+
+    // Prevent getting access to initial state
+    if (!hasBeenEdited.value) {
+      cmValue.clearHistory()
+    }
+
+    cmValue.on('changes', codemirrorChanges)
+
+    // restore caret position
+    const { ch, line } = currentPosition.value ?? {}
+    console.log('WTF: ', currentPosition.value)
+    console.error('WTF', new Error('WTF'))
+    if (typeof ch === 'number' && typeof line === 'number') {
+      currentPosition.value = undefined
+    }
   },
   { flush: 'post' },
 )
 
+watchDebounced(() => [finished.value, saving.value, currentPosition.value] as const, ([f, s], old) => {
+  if (f && !s && old && old[2]) {
+    codemirrorRef.value?.setCursor(old[2])
+  }
+}, { debounce: 100, flush: 'post' })
+
 async function onSave(content: string) {
-  hasBeenEdited.value = true
-  await client.rpc.saveTestFile(props.file!.filepath, content)
-  serverCode.value = content
-  draft.value = false
+  if (saving.value) {
+    return
+  }
+  pause()
+  saving.value = true
+  await nextTick()
+  try {
+    currentPosition.value = codemirrorRef.value?.getCursor()
+    hasBeenEdited.value = true
+    // save the file changes
+    await client.rpc.saveTestFile(props.file!.filepath, content)
+    // update original server code
+    serverCode.value = content
+    // update draft indicator in the tab title (</> * Code)
+    draft.value = false
+    // the server will send 3 events in a row
+    // await first change in the state
+    await until(finished).toBe(false, { flush: 'sync', timeout: 1000, throwOnTimeout: false })
+    // await second change in the state
+    await until(finished).toBe(true, { flush: 'sync', timeout: 1000, throwOnTimeout: false })
+    // await last change in the state
+    await until(finished).toBe(false, { flush: 'sync', timeout: 1000, throwOnTimeout: false })
+  }
+  catch (e) {
+    console.error('error saving file', e)
+  }
+
+  // activate watcher
+  resume()
+  await nextTick()
+  // enable adding the errors if present
+  saving.value = false
 }
+
+// we need to remove listeners before unmounting the component: the watcher will not be called
+onBeforeUnmount(clearListeners)
 </script>
 
 <template>
@@ -181,7 +243,7 @@ async function onSave(content: string) {
     ref="editor"
     v-model="code"
     h-full
-    v-bind="{ lineNumbers: true, readOnly: isReport }"
+    v-bind="{ lineNumbers: true, readOnly: isReport, saving }"
     :mode="ext"
     data-testid="code-mirror"
     @save="onSave"
