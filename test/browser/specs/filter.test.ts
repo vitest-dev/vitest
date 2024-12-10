59b4a4002ce22548bb8a1d164b4874ab1eb881ff
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-12-10 16:27:11 +0100

    test: fix project name

--- test/browser/specs/filter.test.ts
+++ test/browser/specs/filter.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
-import { runBrowserTests } from './utils'
+import { browser, runBrowserTests } from './utils'
 
 test('filter', async () => {
   const { stderr, stdout } = await runBrowserTests({
@@ -8,7 +8,7 @@ test('filter', async () => {
   }, ['test/basic.test.ts'])
 
   expect(stderr).toBe('')
-  expect(stdout).toContain('✓ |chromium| test/basic.test.ts > basic 2')
+  expect(stdout).toContain(`✓ |${browser}| test/basic.test.ts > basic 2`)
   expect(stdout).toContain('Test Files  1 passed')
   expect(stdout).toContain('Tests  1 passed | 3 skipped')
 })
