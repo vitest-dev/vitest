62696fd76ddc9da2eb39f4b6c3d8af27709cbd57
Ari Perkkiö <ari.perkkio@gmail.com>
2024-12-17 10:21:15 +0200

    fix: colors on forks pool

--- test/config/test/console-color.test.ts
+++ test/config/test/console-color.test.ts
@@ -29,3 +29,35 @@ test('without color', async () => {
   expect(stdout).toContain('true\n')
   expect(stdout).not.toContain('\x1B[33mtrue\x1B[39m\n')
 })
+
+test('without color, forks pool in non-TTY parent', async () => {
+  const { stdout } = await runVitest({
+    root: 'fixtures/console-color',
+    env: {
+      CI: undefined,
+      FORCE_COLOR: undefined,
+      NO_COLOR: undefined,
+      GITHUB_ACTIONS: undefined,
+
+      // Overrides current process's value, since we are running Vitest in Vitest here
+      FORCE_TTY: undefined,
+    },
+  }, undefined, undefined, undefined, { preserveAnsi: true })
+
+  expect(stdout).toContain('true\n')
+  expect(stdout).not.toContain('\x1B[33mtrue\x1B[39m\n')
+})
+
+test('with color, forks pool in TTY parent', async () => {
+  const { stdout } = await runVitest({
+    root: 'fixtures/console-color',
+    env: {
+      CI: undefined,
+      FORCE_COLOR: undefined,
+      NO_COLOR: undefined,
+      GITHUB_ACTIONS: undefined,
+    },
+  }, undefined, undefined, undefined, { preserveAnsi: true })
+
+  expect(stdout).toContain('\x1B[33mtrue\x1B[39m\n')
+})
