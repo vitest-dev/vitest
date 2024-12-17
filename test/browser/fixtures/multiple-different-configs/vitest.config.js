8de42d265418970c22247dfc82430588a250ffc5
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-12-17 16:31:50 +0100

    test: add webdriverio test

--- test/browser/fixtures/multiple-different-configs/vitest.config.js
+++ test/browser/fixtures/multiple-different-configs/vitest.config.js
@@ -1,22 +1,25 @@
 import { defineConfig } from 'vitest/config';
 import { fileURLToPath } from 'node:url'
 
+const provider = process.env.PROVIDER || 'playwright'
+
 export default defineConfig({
   clearScreen: false,
   cacheDir: fileURLToPath(new URL("./node_modules/.vite", import.meta.url)),
   test: {
     browser: {
-      provider: 'playwright',
+      provider: provider,
       enabled: true,
       headless: true,
       screenshotFailures: false,
       instances: [
         {
-          browser: 'chromium',
+          browser: provider === 'playwright' ? 'chromium' : 'chrome',
           testerHtmlPath: './customTester.html',
           provide: {
             providedVar: true,
           },
+          name: 'chromium',
         },
         {
           browser: 'firefox',
--- test/browser/specs/multiple-different-configs.test.ts
+++ test/browser/specs/multiple-different-configs.test.ts
@@ -12,3 +12,14 @@ test.runIf(provider === 'playwright')('[playwright] runs multiple different conf
   expect(stdout).toContain('[chromium] HTML_INJECTED_VAR is true')
   expect(stdout).toContain('[firefox] HTML_INJECTED_VAR is undefined')
 })
+
+test.runIf(provider === 'webdriverio')('[webdriverio] runs multiple different configurations correctly', async () => {
+  const { stdout, exitCode, stderr } = await runBrowserTests({
+    root: './fixtures/multiple-different-configs',
+  })
+
+  expect(stderr).toBe('')
+  expect(exitCode).toBe(0)
+  expect(stdout).toContain('[chromium] HTML_INJECTED_VAR is true')
+  expect(stdout).toContain('[firefox] HTML_INJECTED_VAR is undefined')
+})
