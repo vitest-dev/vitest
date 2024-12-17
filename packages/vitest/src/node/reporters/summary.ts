5f8d2091215ca867828aa376a2beeeffbdcbfd6b
Vladimir <sleuths.slews0s@icloud.com>
2024-12-17 16:20:32 +0100

    feat(runner): add "queued" state (#6931)
    Co-authored-by: Ari Perkkiö <ari.perkkio@gmail.com>

--- packages/vitest/src/node/reporters/summary.ts
+++ packages/vitest/src/node/reporters/summary.ts
@@ -1,6 +1,7 @@
 import type { File, Test } from '@vitest/runner'
 import type { Vitest } from '../core'
 import type { Reporter } from '../types/reporter'
+import type { TestModule } from './reported-tasks'
 import type { HookOptions } from './task-parser'
 import { getTests } from '@vitest/runner/utils'
 import c from 'tinyrainbow'
@@ -87,6 +88,10 @@ export class SummaryReporter extends TaskParser implements Reporter {
     })
   }
 
+  onTestModuleQueued(module: TestModule) {
+    this.onTestFilePrepare(module.task)
+  }
+
   onPathsCollected(paths?: string[]) {
     this.suites.total = (paths || []).length
   }
@@ -111,7 +116,18 @@ export class SummaryReporter extends TaskParser implements Reporter {
   }
 
   onTestFilePrepare(file: File) {
-    if (this.allFinishedTests.has(file.id) || this.runningTests.has(file.id)) {
+    if (this.runningTests.has(file.id)) {
+      const stats = this.runningTests.get(file.id)!
+      // if there are no tests, it means the test was queued but not collected
+      if (!stats.total) {
+        const total = getTests(file).length
+        this.tests.total += total
+        stats.total = total
+      }
+      return
+    }
+
+    if (this.allFinishedTests.has(file.id)) {
       return
     }
 
@@ -266,7 +282,7 @@ export class SummaryReporter extends TaskParser implements Reporter {
     const file = test.file
     let stats = this.runningTests.get(file.id)
 
-    if (!stats) {
+    if (!stats || stats.total === 0) {
       // It's possible that that test finished before it's preparation was even reported
       this.onTestFilePrepare(test.file)
       stats = this.runningTests.get(file.id)!
@@ -303,7 +319,9 @@ export class SummaryReporter extends TaskParser implements Reporter {
         c.bold(c.yellow(` ${F_POINTER} `))
         + formatProjectName(testFile.projectName)
         + testFile.filename
-        + c.dim(` ${testFile.completed}/${testFile.total}`),
+        + c.dim(!testFile.completed && !testFile.total
+          ? ' [queued]'
+          : ` ${testFile.completed}/${testFile.total}`),
       )
 
       const slowTasks = [
