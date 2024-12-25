aceb1f0dfe94ca87960fa876eafba5aeb540f9fc
nopeless <38830903+nopeless@users.noreply.github.com>
2024-12-25 02:07:16 -0600

    partial revert of 5f4774

--- packages/expect/rollup.config.js
+++ packages/expect/rollup.config.js
@@ -1,6 +1,5 @@
 import { builtinModules, createRequire } from 'node:module'
 import { defineConfig } from 'rollup'
-import copy from 'rollup-plugin-copy'
 import dts from 'rollup-plugin-dts'
 import esbuild from 'rollup-plugin-esbuild'
 
@@ -17,15 +16,6 @@ const external = [
 const plugins = [
   esbuild({
     target: 'node14',
-  }),
-  copy({
-    targets: [
-      {
-        src: 'node_modules/@types/chai/index.d.ts',
-        dest: 'dist',
-        rename: 'chai.d.cts',
-      },
-    ],
   }),
 ]
 
