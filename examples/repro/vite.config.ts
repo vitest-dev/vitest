bab9c505e5e935c588f03dbc368de1bef6984134
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-01-08 16:08:01 +0900

    chore: more repro

--- examples/repro/vite.config.ts
+++ examples/repro/vite.config.ts
@@ -1,3 +1,4 @@
+import MagicString from 'magic-string'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
@@ -6,4 +7,20 @@ export default defineConfig({
       reporter: ['text', 'text-summary', 'html', 'clover', 'json'],
     },
   },
+  plugins: [
+    {
+      name: 'repro',
+      transform(code, id, _options) {
+        if (id.endsWith('/basic.ts')) {
+          const output = new MagicString(code)
+          output.prepend(`function prepended(){};`)
+          output.append(`;function appended(){};`)
+          return {
+            code: output.toString(),
+            map: output.generateMap({ hires: 'boundary' }),
+          }
+        }
+      },
+    },
+  ],
 })
