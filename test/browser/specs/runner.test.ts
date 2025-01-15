4d1728dc23cf3d84be92db6d23a235e060df76c2
Lucas Correia <tsirlucas@gmail.com>
2025-01-15 12:03:22 -0300

    bump test counter

--- test/browser/specs/runner.test.ts
+++ test/browser/specs/runner.test.ts
@@ -71,7 +71,7 @@ describe('running browser tests', async () => {
     // This should match the number of actual tests from browser.json
     // if you added new tests, these assertion will fail and you should
     // update the numbers
-    expect(browserResultJson.testResults).toHaveLength(21 * instances.length)
+    expect(browserResultJson.testResults).toHaveLength(22 * instances.length)
     expect(passedTests).toHaveLength(19 * instances.length)
     expect(failedTests).toHaveLength(2 * instances.length)
 
