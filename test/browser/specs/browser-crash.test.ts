346c2efb27a8601074406aa7b9a664a94cf1afb5
Lucas Correia <tsirlucas@gmail.com>
2025-01-15 19:54:57 -0300

    dont test crash on webkit

--- test/browser/specs/browser-crash.test.ts
+++ test/browser/specs/browser-crash.test.ts
@@ -1,10 +1,14 @@
 import { expect, test } from 'vitest'
-import { runBrowserTests } from './utils'
+import { instances, runBrowserTests } from './utils'
 
 test('fails gracefully when browser crashes', async () => {
   const { stderr } = await runBrowserTests({
     root: './fixtures/browser-crash',
     reporters: [['verbose', { isTTY: false }]],
+    browser: {
+      // webkit has no support for simulating browser crash
+      instances: instances.filter(item => item.name !== 'webkit'),
+    },
   })
 
   expect(stderr).contains('Page crashed when executing tests')
