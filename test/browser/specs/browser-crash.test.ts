891036d2fe234892468f4d25b037a6661194ee0f
Lucas Correia <tsirlucas@gmail.com>
2025-01-15 20:36:00 -0300

    adjust test so test counter is not dynamic

--- test/browser/specs/browser-crash.test.ts
+++ test/browser/specs/browser-crash.test.ts
@@ -1,15 +1,17 @@
 import { expect, test } from 'vitest'
 import { instances, provider, runBrowserTests } from './utils'
 
-test.runIf(provider === 'playwright')('fails gracefully when browser crashes', async () => {
-  const { stderr } = await runBrowserTests({
-    root: './fixtures/browser-crash',
-    reporters: [['verbose', { isTTY: false }]],
-    browser: {
-      // webkit has no support for simulating browser crash
-      instances: instances.filter(item => item.name !== 'webkit'),
-    },
-  })
+test('fails gracefully when browser crashes', async () => {
+  if (provider === 'playwright') {
+    const { stderr } = await runBrowserTests({
+      root: './fixtures/browser-crash',
+      reporters: [['verbose', { isTTY: false }]],
+      browser: {
+        // webkit has no support for simulating browser crash
+        instances: instances.filter(item => item.name !== 'webkit'),
+      },
+    })
 
-  expect(stderr).contains('Page crashed when executing tests')
+    expect(stderr).contains('Page crashed when executing tests')
+  }
 })
