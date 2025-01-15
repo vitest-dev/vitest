9c2f84d620c82c6e3a1345f9d30b3f6b80039c2c
Lucas Correia <tsirlucas@gmail.com>
2025-01-15 12:36:31 -0300

    update test counters

--- test/browser/specs/runner.test.ts
+++ test/browser/specs/runner.test.ts
@@ -71,8 +71,8 @@ describe('running browser tests', async () => {
     // This should match the number of actual tests from browser.json
     // if you added new tests, these assertion will fail and you should
     // update the numbers
-    expect(browserResultJson.testResults).toHaveLength(22 * instances.length)
-    expect(passedTests).toHaveLength(19 * instances.length)
+    expect(browserResultJson.testResults).toHaveLength(20 * instances.length)
+    expect(passedTests).toHaveLength(18 * instances.length)
     expect(failedTests).toHaveLength(2 * instances.length)
 
     expect(stderr).not.toContain('optimized dependencies changed')
