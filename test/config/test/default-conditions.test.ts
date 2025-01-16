0a6f843610968782c534bc8bdbd94616673dc3fb
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-01-16 10:22:22 +0900

    test: test inline deps too

--- test/config/test/default-conditions.test.ts
+++ test/config/test/default-conditions.test.ts
@@ -1,9 +1,22 @@
 import { expect, test } from 'vitest'
 import { runVitest } from '../../test-utils'
 
-test('correctly imports "module" dependency with default resolve.conditions', async () => {
+test('"module" condition for external dep', async () => {
   const { stderr } = await runVitest({
     root: 'fixtures/default-conditions',
+  })
+
+  expect(stderr).toBe('')
+})
+
+test('"module" condition for inline dep', async () => {
+  const { stderr } = await runVitest({
+    root: 'fixtures/default-conditions',
+    server: {
+      deps: {
+        inline: ['@vitest/test-dep-conditions'],
+      },
+    },
   })
 
   expect(stderr).toBe('')
