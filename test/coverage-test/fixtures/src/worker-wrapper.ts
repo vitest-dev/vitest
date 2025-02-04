27b97c8a8d5f5625791a486b561866c0a3b7eb5b
Ari Perkkiö <ari.perkkio@gmail.com>
2025-02-04 12:54:56 +0200

    test: failing test for #7379

---
+++ test/coverage-test/fixtures/src/worker-wrapper.ts
@@ -0,0 +1,31 @@
+export async function sumInBackground(a: number, b: number) {
+  const worker = new Worker(new URL('./worker', import.meta.url), {
+    type: 'module',
+  });
+
+
+  const promise = new Promise<MessageEvent>(resolve => {
+    worker.onmessage = resolve;
+  });
+
+  function uncovered() {
+    return "This is uncovered"
+  }
+
+  worker.postMessage({ a, b });
+
+  const result = await promise;
+  covered();
+  worker.terminate();
+
+  return result.data;
+}
+
+
+function covered() {
+  return "This is covered"
+}
+
+export function uncovered() {
+  return "This is uncovered"
+}
