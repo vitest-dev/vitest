c8971a73470b5c42e8d9804be49df32dcebdefad
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-02-04 15:46:32 +0900

    test: add test

--- test/browser/fixtures/mocking-out-of-root/project1/vitest.config.ts
+++ test/browser/fixtures/mocking-out-of-root/project1/vitest.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vitest/config';
 import { fileURLToPath } from 'node:url'
+import { instances, provider } from '../../../settings'
 
-// pnpm -C test/browser test-fixtures --root fixtures/mocking-out-of-root/project1
-
-const provider = process.env.PROVIDER || 'playwright'
+// BROWSER=chromium pnpm -C test/browser test-fixtures --root fixtures/mocking-out-of-root/project1
 
 export default defineConfig({
   cacheDir: fileURLToPath(new URL("./node_modules/.vite", import.meta.url)),
@@ -13,12 +12,7 @@ export default defineConfig({
       enabled: true,
       provider: provider,
       screenshotFailures: false,
-      instances: [
-        {
-          name: 'chromium',
-          browser: provider === 'playwright' ? 'chromium' : 'chrome',
-        },
-      ],
+      instances,
     },
   },
 })
