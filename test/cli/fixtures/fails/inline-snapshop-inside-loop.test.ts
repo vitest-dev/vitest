ebe1a067cd6a8ce5270d2f51adf5d5c32558dbe1
jycouet <jycouet@gmail.com>
2025-02-11 22:37:09 +0100

    Keep 1 failing test

--- test/cli/fixtures/fails/inline-snapshop-inside-loop.test.ts
+++ test/cli/fixtures/fails/inline-snapshop-inside-loop.test.ts
@@ -5,15 +5,3 @@ test("fail 1", () => {
     expect(str).toMatchInlineSnapshot(`"foo"`);
   }
 });
-
-test("fail 2.1", () => {
-  for (const str of ["foo", "bar"]) {
-    expect(str).toMatchInlineSnapshot(`"bar"`);
-  }
-});
-
-test("fail 2.2", () => {
-  for (const str of ["foo", "bar"]) {
-    expect(str).toMatchInlineSnapshot(`"foo"`);
-  }
-});
