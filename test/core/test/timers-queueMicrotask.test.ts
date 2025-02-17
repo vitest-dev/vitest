eabf90193e318ae7ab652fcc250ca79f6372536e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-02-17 11:08:42 +0900

    test: tweak

--- test/core/test/timers-queueMicrotask.test.ts
+++ test/core/test/timers-queueMicrotask.test.ts
@@ -2,7 +2,7 @@ import { expect, onTestFinished, test, vi } from 'vitest'
 
 test(`node fetch works without fake timers`, async () => {
   const response = Response.json('ok')
-  expect(await response.json()).toMatchInlineSnapshot(`"ok"`)
+  expect(await response.json()).toBe('ok')
 })
 
 test(`node fetch works with fake timers`, async () => {
@@ -10,7 +10,20 @@ test(`node fetch works with fake timers`, async () => {
   onTestFinished(() => {
     vi.useRealTimers()
   })
-
   const response = Response.json('ok')
-  expect(await response.json()).toMatchInlineSnapshot(`"ok"`)
+  expect(await response.json()).toBe('ok')
+})
+
+test(`node fetch timeouts with fake queueMicrotask`, async () => {
+  vi.useFakeTimers({ toFake: ['queueMicrotask'] })
+  onTestFinished(() => {
+    vi.useRealTimers()
+  })
+  const response = Response.json('ok')
+  expect(
+    await Promise.race([
+      new Promise(r => setTimeout(() => r('timeout'), 200)),
+      response.json(),
+    ]),
+  ).toBe('timeout')
 })
