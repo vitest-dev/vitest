027ce9bbf73c560006c372d68bcde867c80f9c2d
Ari Perkkiö <ari.perkkio@gmail.com>
2025-02-18 09:35:03 +0200

    fix(reporters): render tasks in tree when in TTY (#7503)

--- packages/vitest/src/node/reporters/verbose.ts
+++ packages/vitest/src/node/reporters/verbose.ts
@@ -1,5 +1,5 @@
 import type { Task } from '@vitest/runner'
-import { getFullName } from '@vitest/runner/utils'
+import { getFullName, getTests } from '@vitest/runner/utils'
 import c from 'tinyrainbow'
 import { DefaultReporter } from './default'
 import { F_RIGHT } from './renderers/figures'
@@ -45,4 +45,33 @@ export class VerboseReporter extends DefaultReporter {
       task.result.errors?.forEach(error => this.log(c.red(`   ${F_RIGHT} ${error?.message}`)))
     }
   }
+
+  protected printSuite(task: Task): void {
+    const indentation = '  '.repeat(getIndentation(task))
+    const tests = getTests(task)
+    const state = getStateSymbol(task)
+
+    this.log(` ${indentation}${state} ${task.name} ${c.dim(`(${tests.length})`)}`)
+  }
+
+  protected getTestName(test: Task): string {
+    return test.name
+  }
+
+  protected getTestIndentation(test: Task): string {
+    return '  '.repeat(getIndentation(test))
+  }
+
+  protected formatShortError(): string {
+    // Short errors are not shown in tree-view
+    return ''
+  }
+}
+
+function getIndentation(suite: Task, level = 1): number {
+  if (suite.suite && !('filepath' in suite.suite)) {
+    return getIndentation(suite.suite, level + 1)
+  }
+
+  return level
 }
--- test/reporters/tests/verbose.test.ts
+++ test/reporters/tests/verbose.test.ts
@@ -1,18 +1,18 @@
+import type { TestSpecification } from 'vitest/node'
 import { expect, test } from 'vitest'
 import { runVitest } from '../../test-utils'
 
 test('duration', async () => {
-  const result = await runVitest({
+  const { stdout } = await runVitest({
     root: 'fixtures/duration',
     reporters: 'verbose',
     env: { CI: '1' },
   })
 
-  const output = result.stdout.replace(/\d+ms/g, '[...]ms')
-  expect(output).toContain(`
+  expect(trimReporterOutput(stdout)).toContain(`
  ✓ basic.test.ts > fast
- ✓ basic.test.ts > slow [...]ms
-`)
+ ✓ basic.test.ts > slow [...]ms`,
+  )
 })
 
 test('prints error properties', async () => {
@@ -72,3 +72,89 @@ test('prints repeat count', async () => {
   expect(stdout).toContain('1 passed')
   expect(stdout).toContain('✓ repeat couple of times (repeat x3)')
 })
+
+test('renders tree when in TTY', async () => {
+  const { stdout } = await runVitest({
+    include: ['fixtures/verbose/*.test.ts'],
+    reporters: [['verbose', { isTTY: true, summary: false }]],
+    config: false,
+    fileParallelism: false,
+    sequence: {
+      sequencer: class StableTestFileOrderSorter {
+        sort(files: TestSpecification[]) {
+          return files.sort((a, b) => a.moduleId.localeCompare(b.moduleId))
+        }
+
+        shard(files: TestSpecification[]) {
+          return files
+        }
+      },
+    },
+  })
+
+  expect(trimReporterOutput(stdout)).toMatchInlineSnapshot(`
+    "❯ fixtures/verbose/example-1.test.ts (10 tests | 1 failed | 4 skipped) [...]ms
+       ✓ test pass in root
+       ↓ test skip in root
+       ❯ suite in root (5)
+         ✓ test pass in 1. suite #1
+         ✓ test pass in 1. suite #2
+         ❯ suite in suite (3)
+           ✓ test pass in nested suite #1
+           ✓ test pass in nested suite #2
+           ❯ suite in nested suite (1)
+             × test failure in 2x nested suite [...]ms
+       ↓ suite skip in root (3)
+         ↓ test 1.3
+         ↓ suite in suite (2)
+           ↓ test in nested suite
+           ↓ test failure in nested suite of skipped suite
+     ✓ fixtures/verbose/example-2.test.ts (3 tests | 1 skipped) [...]ms
+       ✓ test 0.1
+       ↓ test 0.2
+       ✓ suite 1.1 (1)
+         ✓ test 1.1"
+  `)
+})
+
+test('does not render tree when in non-TTY', async () => {
+  const { stdout } = await runVitest({
+    include: ['fixtures/verbose/*.test.ts'],
+    reporters: [['verbose', { isTTY: false, summary: false }]],
+    config: false,
+    fileParallelism: false,
+    sequence: {
+      sequencer: class StableTestFileOrderSorter {
+        sort(files: TestSpecification[]) {
+          return files.sort((a, b) => a.moduleId.localeCompare(b.moduleId))
+        }
+
+        shard(files: TestSpecification[]) {
+          return files
+        }
+      },
+    },
+  })
+
+  expect(trimReporterOutput(stdout)).toMatchInlineSnapshot(`
+    "✓ fixtures/verbose/example-1.test.ts > test pass in root
+     ✓ fixtures/verbose/example-1.test.ts > suite in root > test pass in 1. suite #1
+     ✓ fixtures/verbose/example-1.test.ts > suite in root > test pass in 1. suite #2
+     ✓ fixtures/verbose/example-1.test.ts > suite in root > suite in suite > test pass in nested suite #1
+     ✓ fixtures/verbose/example-1.test.ts > suite in root > suite in suite > test pass in nested suite #2
+     × fixtures/verbose/example-1.test.ts > suite in root > suite in suite > suite in nested suite > test failure in 2x nested suite
+       → expected 'should fail' to be 'as expected' // Object.is equality
+     ✓ fixtures/verbose/example-2.test.ts > test 0.1
+     ✓ fixtures/verbose/example-2.test.ts > suite 1.1 > test 1.1"
+  `)
+})
+
+function trimReporterOutput(report: string) {
+  const rows = report.replace(/\d+ms/g, '[...]ms').split('\n')
+
+  // Trim start and end, capture just rendered tree
+  rows.splice(0, rows.findIndex(row => row.includes('fixtures/verbose/example-')))
+  rows.splice(rows.findIndex(row => row.includes('Test Files')))
+
+  return rows.join('\n').trim()
+}
