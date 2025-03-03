34616529de082afd283b1dc4332fb010c94b6988
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-03-03 15:07:19 +0900

    test: also confirm after

--- test/cli/fixtures/custom-runner/test-runner.ts
+++ test/cli/fixtures/custom-runner/test-runner.ts
@@ -1,21 +1,13 @@
 import type { Suite, TestContext } from '@vitest/runner'
 import { VitestTestRunner } from 'vitest/runners'
+import { getSuiteNames } from './utils';
 
 class CustomTestRunner extends VitestTestRunner {
 	extendTaskContext(context: TestContext) {
     super.extendTaskContext(context);
-		(context as any).__suiteNames = getSuiteNames(context.task.suite);
+		(context as any).__suiteNames = getSuiteNames(context.task.suite)
     return context
 	}
-}
-
-function getSuiteNames(suite?: Suite) {
-	const names = []
-	while (suite) {
-		names.push(suite.name)
-		suite = suite.suite
-	}
-	return names
 }
 
 export default CustomTestRunner
