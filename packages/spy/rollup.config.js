340f7d3e881fc11adef6cbc39592a98db4d9d549
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-03-05 17:17:58 +0900

    chore: spy

--- packages/spy/rollup.config.js
+++ packages/spy/rollup.config.js
@@ -1,7 +1,7 @@
 import { builtinModules, createRequire } from 'node:module'
 import { defineConfig } from 'rollup'
-import dts from 'rollup-plugin-dts'
 import esbuild from 'rollup-plugin-esbuild'
+import { rollupDtsHelper } from '../ui/rollup.config.js'
 
 const require = createRequire(import.meta.url)
 const pkg = require('./package.json')
@@ -12,7 +12,10 @@ const external = [
   ...Object.keys(pkg.peerDependencies || {}),
 ]
 
+const dtsHelper = rollupDtsHelper()
+
 const plugins = [
+  dtsHelper.isolatedDecl(),
   esbuild({
     target: 'node14',
   }),
@@ -32,14 +35,14 @@ export default defineConfig([
     onwarn,
   },
   {
-    input: 'src/index.ts',
+    input: dtsHelper.dtsInput('src/index.ts'),
     output: {
       dir: 'dist',
       entryFileNames: '[name].d.ts',
       format: 'esm',
     },
     external,
-    plugins: [dts({ respectExternal: true })],
+    plugins: [dtsHelper.dts()],
     onwarn,
   },
 ])
