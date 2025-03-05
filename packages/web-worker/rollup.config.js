8474880e86657cc2cb9fd8533cab67b2e6e26ea6
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-03-05 12:42:53 +0900

    chore: do packages/web-worker

--- packages/web-worker/rollup.config.js
+++ packages/web-worker/rollup.config.js
@@ -1,9 +1,11 @@
+import fs from 'node:fs'
 import { createRequire } from 'node:module'
 import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import dts from 'rollup-plugin-dts'
 import esbuild from 'rollup-plugin-esbuild'
+import isolatedDecl from 'unplugin-isolated-decl/rollup'
 
 const require = createRequire(import.meta.url)
 const pkg = require('./package.json')
@@ -22,6 +24,10 @@ const external = [
 
 const plugins = [
   json(),
+  isolatedDecl({
+    transformer: 'oxc',
+    extraOutdir: '.types',
+  }),
   nodeResolve(),
   commonjs(),
   esbuild({
@@ -40,13 +46,21 @@ export default () => [
     plugins,
   },
   {
-    input: 'src/pure.ts',
+    input: 'dist/.types/pure.d.ts',
     output: {
-      dir: process.cwd(),
-      entryFileNames: 'dist/[name].d.ts',
+      dir: 'dist',
+      entryFileNames: '[name].ts',
       format: 'esm',
     },
     external,
-    plugins: [dts({ respectExternal: true })],
+    plugins: [
+      dts({ respectExternal: true }),
+      {
+        name: 'cleanup-types',
+        buildEnd() {
+          fs.rmSync('./dist/.types', { recursive: true, force: true })
+        },
+      },
+    ],
   },
 ]
