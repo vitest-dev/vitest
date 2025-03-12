f7114c999e670f7474c94fddcd44ccbe586c1ba8
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-03-12 16:50:12 +0900

    test: update snapshot

--- test/coverage-test/test/pre-transpiled-source.test.ts
+++ test/coverage-test/test/pre-transpiled-source.test.ts
@@ -1,6 +1,6 @@
 import libCoverage from 'istanbul-lib-coverage'
 import { expect } from 'vitest'
-import { isV8Provider, readCoverageJson, runVitest, test } from '../utils'
+import { isBrowser, isV8Provider, readCoverageJson, runVitest, test } from '../utils'
 
 test('pre-transpiled code with source maps to original (#5341)', async () => {
   await runVitest({
@@ -22,5 +22,9 @@ test('pre-transpiled code with source maps to original (#5341)', async () => {
   expect(files.find(file => file.includes('transpiled.ts'))).toBeFalsy()
   expect(files.find(file => file.includes('transpiled.d.ts'))).toBeFalsy()
 
-  await expect(JSON.stringify(coverageJson, null, 2)).toMatchFileSnapshot(`__snapshots__/pre-transpiled-${isV8Provider() ? 'v8' : 'istanbul'}.snapshot.json`)
+  await expect(JSON.stringify(coverageJson, null, 2)).toMatchFileSnapshot(
+    `__snapshots__/pre-transpiled-${
+      isV8Provider() ? (isBrowser() ? 'v8-browser' : 'v8') : 'istanbul'
+    }.snapshot.json`,
+  )
 })
