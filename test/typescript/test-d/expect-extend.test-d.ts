c848498680f9c01edba77efbf0ebc0f2a3977344
Artem Zakharchenko <kettanaito@gmail.com>
2025-03-24 11:33:56 +0100

    test: add type tests for matcher declaration

---
+++ test/typescript/test-d/expect-extend.test-d.ts
@@ -0,0 +1,47 @@
+import { expect, expectTypeOf, test } from 'vitest'
+
+interface CustomMatchers<R = unknown> {
+  toMatchSchema: (schema: { a: string }) => R
+  toEqualMultiple: (a: string, b: number) => R
+}
+
+declare module 'vitest' {
+  interface Matchers extends CustomMatchers {}
+}
+
+test('infers matcher declaration type from a matcher with a single argument', () => {
+  expect.extend({
+    toMatchSchema(received, expected) {
+      expectTypeOf(received).toBeAny()
+      expectTypeOf(expected).toEqualTypeOf<{ a: string }>()
+
+      return { pass: true, message: () => '' }
+    },
+    toEqualMultiple(received, a, b) {
+      expectTypeOf(received).toBeAny()
+      expectTypeOf(a).toBeString()
+      expectTypeOf(b).toBeNumber()
+
+      return { pass: true, message: () => '' }
+    },
+  })
+
+  expect({ a: 1, b: '2' }).toMatchSchema({ a: '1' })
+  expect('a').toEqualMultiple('a', 1)
+})
+
+test('errors on missing matcher declarations', () => {
+  expect.extend(
+    // @ts-expect-error Missing declaration for all custom matchers.
+    {},
+  )
+
+  expect.extend(
+    // @ts-expect-error Missing declaration for some custom matchers.
+    {
+      toMatchSchema(_received, _expected) {
+        return { pass: true, message: () => '' }
+      },
+    },
+  )
+})
