b12914215071a14ecb8f8ec009b906bf685a79fe
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2025-03-27 16:21:39 +0100

    test: add hideSkipped in versbose reporter

--- test/reporters/tests/verbose.test.ts
+++ test/reporters/tests/verbose.test.ts
@@ -167,6 +167,39 @@ test('does not render tree when in non-TTY', async () => {
   `)
 })
 
+test('hides skipped tests when --hideSkippedTests and in non-TTY', async () => {
+  const { stdout } = await runVitest({
+    include: ['fixtures/verbose/*.test.ts'],
+    reporters: [['verbose', { isTTY: false, summary: false }]],
+    hideSkippedTests: true,
+    config: false,
+    fileParallelism: false,
+    sequence: {
+      sequencer: class StableTestFileOrderSorter {
+        sort(files: TestSpecification[]) {
+          return files.sort((a, b) => a.moduleId.localeCompare(b.moduleId))
+        }
+
+        shard(files: TestSpecification[]) {
+          return files
+        }
+      },
+    },
+  })
+
+  expect(trimReporterOutput(stdout)).toMatchInlineSnapshot(`
+    "✓ fixtures/verbose/example-1.test.ts > test pass in root [...]ms
+     ✓ fixtures/verbose/example-1.test.ts > suite in root > test pass in 1. suite #1 [...]ms
+     ✓ fixtures/verbose/example-1.test.ts > suite in root > test pass in 1. suite #2 [...]ms
+     ✓ fixtures/verbose/example-1.test.ts > suite in root > suite in suite > test pass in nested suite #1 [...]ms
+     ✓ fixtures/verbose/example-1.test.ts > suite in root > suite in suite > test pass in nested suite #2 [...]ms
+     × fixtures/verbose/example-1.test.ts > suite in root > suite in suite > suite in nested suite > test failure in 2x nested suite [...]ms
+       → expected 'should fail' to be 'as expected' // Object.is equality
+     ✓ fixtures/verbose/example-2.test.ts > test 0.1 [...]ms
+     ✓ fixtures/verbose/example-2.test.ts > suite 1.1 > test 1.1 [...]ms"
+  `)
+})
+
 function trimReporterOutput(report: string) {
   const rows = report.replace(/\d+ms/g, '[...]ms').split('\n')
 
