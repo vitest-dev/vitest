5a435ca6794902585b6662c0b387207c0aa1caa2
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-04-04 10:15:51 +0900

    test: tweak

--- test/browser/fixtures/mocking-out-of-root/project1/basic.test.js
+++ test/browser/fixtures/mocking-out-of-root/project1/basic.test.js
@@ -1,5 +1,6 @@
 import { test, expect, vi } from 'vitest';
 import project2 from "../project2/index.js"
+import "./imported-test.js"
 import "../project3/imported-test.js"
 
 vi.mock("../project2/index.js", () => ({
