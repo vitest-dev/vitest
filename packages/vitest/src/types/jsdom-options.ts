61935e3fa8820f61d0e70022542b27d7d5f9d7a3
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-04-04 10:50:25 +0900

    fix: jsdom too

--- packages/vitest/src/types/jsdom-options.ts
+++ packages/vitest/src/types/jsdom-options.ts
@@ -1,15 +1,16 @@
-export interface JSDOMOptions {
+// eslint-disable-next-line ts/ban-ts-comment
+// @ts-ignore `jsdom` is optional peeer dep
+import type { ConstructorOptions } from 'jsdom'
+
+export type JSDOMOptions = ConstructorOptionsOverride & Omit<ConstructorOptions, keyof ConstructorOptionsOverride>
+
+interface ConstructorOptionsOverride {
   /**
    * The html content for the test.
    *
    * @default '<!DOCTYPE html>'
    */
   html?: string | ArrayBufferLike
-  /**
-   * referrer just affects the value read from document.referrer.
-   * It defaults to no referrer (which reflects as the empty string).
-   */
-  referrer?: string
   /**
    * userAgent affects the value read from navigator.userAgent, as well as the User-Agent header sent while fetching subresources.
    *
@@ -24,21 +25,6 @@ export interface JSDOMOptions {
    * @default 'http://localhost:3000'.
    */
   url?: string
-  /**
-   * contentType affects the value read from document.contentType, and how the document is parsed: as HTML or as XML.
-   * Values that are not "text/html" or an XML mime type will throw.
-   *
-   * @default 'text/html'.
-   */
-  contentType?: string
-  /**
-   * The maximum size in code units for the separate storage areas used by localStorage and sessionStorage.
-   * Attempts to store data larger than this limit will cause a DOMException to be thrown. By default, it is set
-   * to 5,000,000 code units per origin, as inspired by the HTML specification.
-   *
-   * @default 5_000_000
-   */
-  storageQuota?: number
   /**
    * Enable console?
    *
@@ -55,20 +41,6 @@ export interface JSDOMOptions {
    * @default true
    */
   pretendToBeVisual?: boolean
-  /**
-   * `includeNodeLocations` preserves the location info produced by the HTML parser,
-   * allowing you to retrieve it with the nodeLocation() method (described below).
-   *
-   * It defaults to false to give the best performance,
-   * and cannot be used with an XML content type since our XML parser does not support location info.
-   *
-   * @default false
-   */
-  includeNodeLocations?: boolean | undefined
-  /**
-   * @default 'dangerously'
-   */
-  runScripts?: 'dangerously' | 'outside-only'
   /**
    * Enable CookieJar
    *
