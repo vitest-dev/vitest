1340e8475743f57e0d91715ab5425dff8e11f687
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2025-04-17 23:39:04 +0200

    chore: cleanup

--- examples/lit/test/basic.test.ts
+++ examples/lit/test/basic.test.ts
@@ -3,8 +3,7 @@ import { beforeEach, describe, expect, it } from 'vitest'
 
 import '../src/my-button.js'
 
-// TODO: wait until rolldown has https://github.com/oxc-project/oxc/issues/9129
-describe.skip('Button with increment', async () => {
+describe('Button with increment', async () => {
   beforeEach(() => {
     document.body.innerHTML = '<my-button name="World"></my-button>'
   })
