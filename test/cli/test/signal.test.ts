d0b3fe6aa110aa05ea3e3ac3649a624ccd9f4f49
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2025-04-24 17:11:55 +0200

    test: add signal tests

---
+++ test/cli/test/signal.test.ts
@@ -0,0 +1,159 @@
+import type { UserConsoleLog } from 'vitest'
+import type { Reporter, Vitest } from 'vitest/node'
+import { expect, test } from 'vitest'
+import { runInlineTests } from '../../test-utils'
+
+test('timeout aborts the signal without fixtures', async () => {
+  const { stderr, results } = await runInlineTests({
+    'basic.test.ts': /* ts */`
+      import { test } from 'vitest'
+      import { setTimeout } from 'node:timers/promises'
+      test('timeouts', async ({ signal, task, example }) => {
+        signal.addEventListener('abort', () => {
+          task.meta.aborted = true
+        })
+        await setTimeout(100)
+      }, 10)
+    `,
+  })
+  expect(stderr).toContain('Test timed out in 10ms.')
+  expect(results).toHaveLength(1)
+  expect(results[0].children.at(0)?.meta()).toEqual({
+    aborted: true,
+  })
+})
+
+test('timeout aborts the signal', async () => {
+  const { stderr, results } = await runInlineTests({
+    'basic.test.ts': /* ts */`
+      import { test } from 'vitest'
+      import { setTimeout } from 'node:timers/promises'
+      test.extend({
+        // .extend to force fixture initialisation
+        example: true,
+      })('timeouts', async ({ signal, task, example }) => {
+        signal.addEventListener('abort', () => {
+          task.meta.aborted = true
+        })
+        await setTimeout(100)
+      }, 10)
+    `,
+  })
+  expect(stderr).toContain('Test timed out in 10ms.')
+  expect(results).toHaveLength(1)
+  expect(results[0].children.at(0)?.meta()).toEqual({
+    aborted: true,
+  })
+})
+
+test('timeout aborts all signals in concurrent tests', async () => {
+  const { stderr, results } = await runInlineTests({
+    'basic.test.ts': /* ts */`
+      import { test } from 'vitest'
+      import { setTimeout } from 'node:timers/promises'
+      test
+        // .extend to force fixture initialisation
+        .extend({ example: true })
+        .concurrent
+        .for([1, 1, 1])
+      ('timeouts', async (_, { signal, task, example }) => {
+        signal.addEventListener('abort', () => {
+          task.meta.aborted = true
+        })
+        await setTimeout(100)
+      }, 10)
+    `,
+  })
+  expect(stderr).toContain('Test timed out in 10ms.')
+  expect(results).toHaveLength(1)
+  expect(results[0].children.at(0)?.meta()).toEqual({
+    aborted: true,
+  })
+  expect(results[0].children.at(1)?.meta()).toEqual({
+    aborted: true,
+  })
+  expect(results[0].children.at(2)?.meta()).toEqual({
+    aborted: true,
+  })
+})
+
+class AbortReporter implements Reporter {
+  vitest!: Vitest
+  onInit(vitest: Vitest) {
+    this.vitest = vitest
+  }
+
+  idx = 0
+
+  onUserConsoleLog(log: UserConsoleLog) {
+    this.idx++
+    if (log.content.includes('ready')) {
+      this.vitest.cancelCurrentRun('keyboard-input')
+    }
+  }
+}
+
+test('cancelling test run aborts the signal', async () => {
+  const { results, stderr } = await runInlineTests({
+    'basic.test.ts': /* ts */ `
+      import { test } from 'vitest'
+      test('aborted', async ({ signal, task }) => {
+        return new Promise(resolve => {
+          console.log('ready')
+          signal.addEventListener('abort', () => {
+            task.meta.aborted = true
+            resolve()
+          })
+        })
+      }, Infinity)
+    `,
+  }, {
+    reporters: [
+      'default',
+      new AbortReporter(),
+    ],
+  })
+  expect(stderr).toBe('')
+  expect(results).toHaveLength(1)
+  expect(results[0].children.at(0)?.meta()).toEqual({
+    aborted: true,
+  })
+})
+
+test('cancelling test run aborts the signal in all concurrent tests', async () => {
+  const { results, stderr } = await runInlineTests({
+    'basic.test.ts': /* ts */`
+      import { test } from 'vitest'
+      test.concurrent.for([1, 2, 3])(
+        'aborted',
+        { timeout: Infinity },
+         async (number, { signal, task }) => {
+          return new Promise(resolve => {
+            if (number === 3) {
+              console.log('ready')
+            }
+            signal.addEventListener('abort', () => {
+              task.meta.aborted = true
+              resolve()
+            })
+          })
+        })
+    `,
+  }, {
+    reporters: [
+      'default',
+      new AbortReporter(),
+    ],
+  })
+  expect(stderr).toBe('')
+  expect(results).toHaveLength(1)
+  expect(results[0].children.at(0)?.meta()).toEqual({
+    aborted: true,
+  })
+  expect(results[0].children.at(1)?.meta()).toEqual({
+    aborted: true,
+  })
+  expect(results[0].children.at(2)?.meta()).toEqual({
+    aborted: true,
+  })
+})
