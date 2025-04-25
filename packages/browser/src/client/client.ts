0d42aa5d277648adc0b6df11d1c40eef4af09c7b
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2025-04-25 18:43:30 +0200

    fix: don't panic if orchestrator is not set yet

--- packages/browser/src/client/client.ts
+++ packages/browser/src/client/client.ts
@@ -2,6 +2,7 @@ import type { ModuleMocker } from '@vitest/mocker/browser'
 import type { CancelReason } from '@vitest/runner'
 import type { BirpcReturn } from 'birpc'
 import type { WebSocketBrowserEvents, WebSocketBrowserHandlers } from '../node/types'
+import type { IframeOrchestrator } from './orchestrator'
 import { createBirpc } from 'birpc'
 import { parse, stringify } from 'flatted'
 import { getBrowserState } from './utils'
@@ -35,6 +36,27 @@ export type BrowserRPC = BirpcReturn<
   WebSocketBrowserEvents
 >
 
+// ws connection can be established before the orchestrator is fully loaded
+// in very rare cases in the preview provider
+function waitForOrchestrator() {
+  return new Promise<IframeOrchestrator>((resolve, reject) => {
+    const type = getBrowserState().type
+    if (type !== 'orchestrator') {
+      reject(new TypeError('Only orchestrator can create testers.'))
+      return
+    }
+
+    function check() {
+      const orchestrator = getBrowserState().orchestrator
+      if (orchestrator) {
+        return resolve(orchestrator)
+      }
+      setTimeout(check)
+    }
+    check()
+  })
+}
+
 function createClient() {
   const autoReconnect = true
   const reconnectInterval = 2000
@@ -54,17 +76,11 @@ function createClient() {
     {
       onCancel: setCancel,
       async createTesters(options) {
-        const orchestrator = getBrowserState().orchestrator
-        if (!orchestrator) {
-          throw new TypeError('Only orchestrator can create testers.')
-        }
+        const orchestrator = await waitForOrchestrator()
         return orchestrator.createTesters(options)
       },
       async cleanupTesters() {
-        const orchestrator = getBrowserState().orchestrator
-        if (!orchestrator) {
-          throw new TypeError('Only orchestrator can cleanup testers.')
-        }
+        const orchestrator = await waitForOrchestrator()
         return orchestrator.cleanupTesters()
       },
       cdpEvent(event: string, payload: unknown) {
