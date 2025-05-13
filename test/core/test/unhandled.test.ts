f7826b22b53ddb255dbe77e0870fd61ebb17551b
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2025-05-13 15:02:44 +0200

    chore: cleanup

--- test/core/test/unhandled.test.ts
+++ test/core/test/unhandled.test.ts
@@ -1,11 +1,12 @@
-import { test } from 'vitest';
+import { test } from 'vitest'
 
 process.on('unhandledRejection', () => {
   // ignore errors
 })
 
 test('throws unhandled but not reported', () => {
+  // eslint-disable-next-line no-new
   new Promise((resolve, reject) => {
-    reject('promise error');
-  });
+    reject(new Error('promise error'))
+  })
 })
