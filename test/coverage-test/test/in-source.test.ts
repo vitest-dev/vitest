c5e4c24d0c9aaca975a028a13df34ce5ebf509d8
Ari Perkkiö <ari.perkkio@gmail.com>
2025-05-21 17:49:15 +0300

    fix(browser): source maps of in-source tests

--- test/coverage-test/test/in-source.test.ts
+++ test/coverage-test/test/in-source.test.ts
@@ -1,5 +1,5 @@
 import { expect } from 'vitest'
-import { readCoverageMap, runVitest, test } from '../utils'
+import { isV8Provider, readCoverageMap, runVitest, test } from '../utils'
 
 test('in-source tests work', async () => {
   const { stdout } = await runVitest({
@@ -18,4 +18,30 @@ test('in-source tests work', async () => {
       "<process-cwd>/fixtures/src/in-source.ts",
     ]
   `)
+
+  const fileCoverage = coverageMap.fileCoverageFor('<process-cwd>/fixtures/src/in-source.ts')
+
+  // If-branch is not taken - makes sure source maps are correct in in-source testing too
+  expect(fileCoverage.getUncoveredLines()).toContain('5')
+
+  if (isV8Provider()) {
+    expect(fileCoverage).toMatchInlineSnapshot(`
+      {
+        "branches": "2/4 (50%)",
+        "functions": "1/1 (100%)",
+        "lines": "10/12 (83.33%)",
+        "statements": "10/12 (83.33%)",
+      }
+    `)
+  }
+  else {
+    expect(fileCoverage).toMatchInlineSnapshot(`
+      {
+        "branches": "3/6 (50%)",
+        "functions": "2/2 (100%)",
+        "lines": "6/7 (85.71%)",
+        "statements": "6/7 (85.71%)",
+      }
+    `)
+  }
 })
