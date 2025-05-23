3cb84d130091b50966627aaa4020d2a468a31e3e
Nathan Sarang-Walters <nwalters512@gmail.com>
2025-05-23 11:33:51 -0700

    Improve test case

--- test/reporters/tests/github-actions.test.ts
+++ test/reporters/tests/github-actions.test.ts
@@ -18,7 +18,7 @@ describe(GithubActionsReporter, () => {
   })
 
   it('uses onWritePath to format path', async () => {
-    let { stdout, stderr } = await runVitest(
+    const { stdout, stderr } = await runVitest(
       {
         reporters: new GithubActionsReporter({
           onWritePath(path) {
@@ -30,7 +30,6 @@ describe(GithubActionsReporter, () => {
         env: { GITHUB_WORKSPACE: '/home/user/workspace' },
       },
     )
-    stdout = stdout.replace(resolve(__dirname, '..').replace(/:/g, '%3A'), '__TEST_DIR__')
     expect(stdout).toMatchInlineSnapshot(`
       "
       ::error file=__TEST_DIR__/fixtures/some-failing.test.ts,title=some-failing.test.ts > 3 + 3 = 7,line=8,column=17::AssertionError: expected 6 to be 7 // Object.is equality%0A%0A- Expected%0A+ Received%0A%0A- 7%0A+ 6%0A%0A ❯ some-failing.test.ts:8:17%0A%0A
