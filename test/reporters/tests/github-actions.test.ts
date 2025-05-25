188cf18a3d17cdc1ba6f258c73930dd6f20f14fd
Ari Perkkiö <ari.perkkio@gmail.com>
2025-05-25 11:19:01 +0300

    test: verify `onWritePath` with explicit custom path

--- test/reporters/tests/github-actions.test.ts
+++ test/reporters/tests/github-actions.test.ts
@@ -1,3 +1,4 @@
+import { sep } from 'node:path'
 import { resolve } from 'pathe'
 import { describe, expect, it } from 'vitest'
 import { GithubActionsReporter } from '../../../packages/vitest/src/node/reporters'
@@ -22,7 +23,11 @@ describe(GithubActionsReporter, () => {
       {
         reporters: new GithubActionsReporter({
           onWritePath(path) {
-            return path.replace(resolve(__dirname, '..').replace(/:/g, '%3A'), '__TEST_DIR__')
+            const normalized = path
+              .replace(resolve(__dirname, '..'), '')
+              .replaceAll(sep, '/')
+
+            return `/some-custom-path${normalized}`
           },
         }),
         root: './fixtures',
@@ -31,7 +36,7 @@ describe(GithubActionsReporter, () => {
     )
     expect(stdout).toMatchInlineSnapshot(`
       "
-      ::error file=__TEST_DIR__/fixtures/some-failing.test.ts,title=some-failing.test.ts > 3 + 3 = 7,line=8,column=17::AssertionError: expected 6 to be 7 // Object.is equality%0A%0A- Expected%0A+ Received%0A%0A- 7%0A+ 6%0A%0A ❯ some-failing.test.ts:8:17%0A%0A
+      ::error file=/some-custom-path/fixtures/some-failing.test.ts,title=some-failing.test.ts > 3 + 3 = 7,line=8,column=17::AssertionError: expected 6 to be 7 // Object.is equality%0A%0A- Expected%0A+ Received%0A%0A- 7%0A+ 6%0A%0A ❯ some-failing.test.ts:8:17%0A%0A
       "
     `)
     expect(stderr).toBe('')
