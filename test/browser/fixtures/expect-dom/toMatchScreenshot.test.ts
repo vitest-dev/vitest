b95d4be1870d7cc7e605aa9552639c00882ddd56
ursulean <paulursulean@gmail.com>
2025-05-26 00:57:15 +0200

    Add more tests

--- test/browser/fixtures/expect-dom/toMatchScreenshot.test.ts
+++ test/browser/fixtures/expect-dom/toMatchScreenshot.test.ts
@@ -1,13 +1,33 @@
 import { expect, test } from 'vitest'
+import { page } from '@vitest/browser/context'
 
-test('matches screenshot baseline', async () => {
+function addHelloWorldDiv() {
   const div = document.createElement('div')
   div.textContent = 'Hello, World!!'
+  div.className = 'hello-world'
   div.style.fontSize = '24px'
   div.style.color = 'blue'
   div.style.textAlign = 'center'
   div.style.marginTop = '20px'
   document.body.appendChild(div)
+}
+
+test('body matches screenshot', async () => {
+  addHelloWorldDiv()
 
   await expect(document.body).toMatchScreenshot()
+})
+
+test('div matches screenshot', async () => {
+  addHelloWorldDiv()
+
+  await expect(document.querySelector('.hello-world')).toMatchScreenshot()
+})
+
+test('div matches screenshot with locator', async () => {
+  addHelloWorldDiv()
+  const div = document.querySelector('.hello-world')
+
+  expect(div).toBeDefined()
+  await expect(page.elementLocator(div)).toMatchScreenshot()
 })
