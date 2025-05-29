dcc294fd5e5d7e4e3441a5486f68046203efb897
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2025-05-29 14:12:15 +0200

    chore: workspace plugin

--- packages/vitest/src/node/plugins/workspace.ts
+++ packages/vitest/src/node/plugins/workspace.ts
@@ -33,153 +33,156 @@ export function WorkspaceVitestPlugin(
   return <VitePlugin[]>[
     {
       name: 'vitest:project',
-      enforce: 'post',
+      enforce: 'pre',
       options() {
         this.meta.watchMode = false
       },
-      config(viteConfig) {
-        const defines: Record<string, any> = deleteDefineConfig(viteConfig)
+      config: {
+        order: 'post',
+        handler(viteConfig) {
+          const defines: Record<string, any> = deleteDefineConfig(viteConfig)
 
-        const testConfig = viteConfig.test || {}
+          const testConfig = viteConfig.test || {}
 
-        const root = testConfig.root || viteConfig.root || options.root
+          const root = testConfig.root || viteConfig.root || options.root
 
-        let { label: name, color } = typeof testConfig.name === 'string'
-          ? { label: testConfig.name }
-          : { label: '', ...testConfig.name }
+          let { label: name, color } = typeof testConfig.name === 'string'
+            ? { label: testConfig.name }
+            : { label: '', ...testConfig.name }
 
-        if (!name) {
-          if (typeof options.workspacePath === 'string') {
+          if (!name) {
+            if (typeof options.workspacePath === 'string') {
             // if there is a package.json, read the name from it
-            const dir = options.workspacePath.endsWith('/')
-              ? options.workspacePath.slice(0, -1)
-              : dirname(options.workspacePath)
-            const pkgJsonPath = resolve(dir, 'package.json')
-            if (existsSync(pkgJsonPath)) {
-              name = JSON.parse(readFileSync(pkgJsonPath, 'utf-8')).name
+              const dir = options.workspacePath.endsWith('/')
+                ? options.workspacePath.slice(0, -1)
+                : dirname(options.workspacePath)
+              const pkgJsonPath = resolve(dir, 'package.json')
+              if (existsSync(pkgJsonPath)) {
+                name = JSON.parse(readFileSync(pkgJsonPath, 'utf-8')).name
+              }
+              if (typeof name !== 'string' || !name) {
+                name = basename(dir)
+              }
             }
-            if (typeof name !== 'string' || !name) {
-              name = basename(dir)
+            else {
+              name = options.workspacePath.toString()
             }
           }
-          else {
-            name = options.workspacePath.toString()
-          }
-        }
 
-        const resolveOptions = getDefaultResolveOptions()
-        const config: ViteConfig = {
-          root,
-          define: {
+          const resolveOptions = getDefaultResolveOptions()
+          const config: ViteConfig = {
+            root,
+            define: {
             // disable replacing `process.env.NODE_ENV` with static string by vite:client-inject
-            'process.env.NODE_ENV': 'process.env.NODE_ENV',
-          },
-          resolve: {
-            ...resolveOptions,
-            alias: testConfig.alias,
-          },
-          esbuild: viteConfig.esbuild === false
-            ? false
-            : {
+              'process.env.NODE_ENV': 'process.env.NODE_ENV',
+            },
+            resolve: {
+              ...resolveOptions,
+              alias: testConfig.alias,
+            },
+            esbuild: viteConfig.esbuild === false
+              ? false
+              : {
                 // Lowest target Vitest supports is Node18
-                target: viteConfig.esbuild?.target || 'node18',
-                sourcemap: 'external',
-                // Enables using ignore hint for coverage providers with @preserve keyword
-                legalComments: 'inline',
-              },
-          server: {
+                  target: viteConfig.esbuild?.target || 'node18',
+                  sourcemap: 'external',
+                  // Enables using ignore hint for coverage providers with @preserve keyword
+                  legalComments: 'inline',
+                },
+            server: {
             // disable watch mode in workspaces,
             // because it is handled by the top-level watcher
-            watch: null,
-            open: false,
-            hmr: false,
-            ws: false,
-            preTransformRequests: false,
-            middlewareMode: true,
-            fs: {
-              allow: resolveFsAllow(
-                project.vitest.config.root,
-                project.vitest.vite.config.configFile,
-              ),
+              watch: null,
+              open: false,
+              hmr: false,
+              ws: false,
+              preTransformRequests: false,
+              middlewareMode: true,
+              fs: {
+                allow: resolveFsAllow(
+                  project.vitest.config.root,
+                  project.vitest.vite.config.configFile,
+                ),
+              },
             },
-          },
-          // eslint-disable-next-line ts/ban-ts-comment
-          // @ts-ignore Vite 6 compat
-          environments: {
-            ssr: {
-              resolve: resolveOptions,
+            // eslint-disable-next-line ts/ban-ts-comment
+            // @ts-ignore Vite 6 compat
+            environments: {
+              ssr: {
+                resolve: resolveOptions,
+              },
             },
-          },
-          test: {
-            name: { label: name, color },
-          },
-        }
+            test: {
+              name: { label: name, color },
+            },
+          }
 
-        ;(config.test as ResolvedConfig).defines = defines
+          ;(config.test as ResolvedConfig).defines = defines
 
-        const isUserBrowserEnabled = viteConfig.test?.browser?.enabled
-        const isBrowserEnabled = isUserBrowserEnabled ?? (viteConfig.test?.browser && project.vitest._cliOptions.browser?.enabled)
-        // keep project names to potentially filter it out
-        const workspaceNames = [name]
-        const browser = viteConfig.test!.browser || {}
-        if (isBrowserEnabled && browser.name && !browser.instances?.length) {
+          const isUserBrowserEnabled = viteConfig.test?.browser?.enabled
+          const isBrowserEnabled = isUserBrowserEnabled ?? (viteConfig.test?.browser && project.vitest._cliOptions.browser?.enabled)
+          // keep project names to potentially filter it out
+          const workspaceNames = [name]
+          const browser = viteConfig.test!.browser || {}
+          if (isBrowserEnabled && browser.name && !browser.instances?.length) {
           // vitest injects `instances` in this case later on
-          workspaceNames.push(name ? `${name} (${browser.name})` : browser.name)
-        }
+            workspaceNames.push(name ? `${name} (${browser.name})` : browser.name)
+          }
 
-        viteConfig.test?.browser?.instances?.forEach((instance) => {
+          viteConfig.test?.browser?.instances?.forEach((instance) => {
           // every instance is a potential project
-          instance.name ??= name ? `${name} (${instance.browser})` : instance.browser
-          if (isBrowserEnabled) {
-            workspaceNames.push(instance.name)
-          }
-        })
-
-        const filters = project.vitest.config.project
-        // if there is `--project=...` filter, check if any of the potential projects match
-        // if projects don't match, we ignore the test project altogether
-        // if some of them match, they will later be filtered again by `resolveWorkspace`
-        if (filters.length) {
-          const hasProject = workspaceNames.some((name) => {
-            return project.vitest.matchesProjectFilter(name)
+            instance.name ??= name ? `${name} (${instance.browser})` : instance.browser
+            if (isBrowserEnabled) {
+              workspaceNames.push(instance.name)
+            }
           })
-          if (!hasProject) {
-            throw new VitestFilteredOutProjectError()
-          }
-        }
 
-        const classNameStrategy
-          = (typeof testConfig.css !== 'boolean'
-            && testConfig.css?.modules?.classNameStrategy)
-          || 'stable'
-
-        if (classNameStrategy !== 'scoped') {
-          config.css ??= {}
-          config.css.modules ??= {}
-          if (config.css.modules) {
-            config.css.modules.generateScopedName = (
-              name: string,
-              filename: string,
-            ) => {
-              const root = project.config.root
-              return generateScopedClassName(
-                classNameStrategy,
-                name,
-                relative(root, filename),
-              )!
+          const filters = project.vitest.config.project
+          // if there is `--project=...` filter, check if any of the potential projects match
+          // if projects don't match, we ignore the test project altogether
+          // if some of them match, they will later be filtered again by `resolveWorkspace`
+          if (filters.length) {
+            const hasProject = workspaceNames.some((name) => {
+              return project.vitest.matchesProjectFilter(name)
+            })
+            if (!hasProject) {
+              throw new VitestFilteredOutProjectError()
             }
           }
-        }
-        config.customLogger = createViteLogger(
-          project.vitest.logger,
-          viteConfig.logLevel || 'warn',
-          {
-            allowClearScreen: false,
-          },
-        )
-        config.customLogger = silenceImportViteIgnoreWarning(config.customLogger)
 
-        return config
+          const classNameStrategy
+            = (typeof testConfig.css !== 'boolean'
+              && testConfig.css?.modules?.classNameStrategy)
+            || 'stable'
+
+          if (classNameStrategy !== 'scoped') {
+            config.css ??= {}
+            config.css.modules ??= {}
+            if (config.css.modules) {
+              config.css.modules.generateScopedName = (
+                name: string,
+                filename: string,
+              ) => {
+                const root = project.config.root
+                return generateScopedClassName(
+                  classNameStrategy,
+                  name,
+                  relative(root, filename),
+                )!
+              }
+            }
+          }
+          config.customLogger = createViteLogger(
+            project.vitest.logger,
+            viteConfig.logLevel || 'warn',
+            {
+              allowClearScreen: false,
+            },
+          )
+          config.customLogger = silenceImportViteIgnoreWarning(config.customLogger)
+
+          return config
+        },
       },
       async configureServer(server) {
         const options = deepMerge({}, configDefaults, server.config.test || {})
