f9470928896c0c63f852b74f6e749bc4a812e698
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2025-05-29 10:47:07 +0200

    chore: remove deprecated workspace

--- test/coverage-test/test/workspace.multi-transform.test.ts
+++ test/coverage-test/test/workspace.multi-transform.test.ts
@@ -3,7 +3,7 @@ import { isV8Provider, readCoverageMap, runVitest, test } from '../utils'
 
 test('{ all: true } includes uncovered files that require custom transform', async () => {
   await runVitest({
-    workspace: 'fixtures/configs/vitest.workspace.multi-transforms.ts',
+    config: './fixtures/configs/vitest.config.multi-transforms.ts',
     coverage: {
       all: true,
       extension: ['.ts', '.custom-1', '.custom-2'],
