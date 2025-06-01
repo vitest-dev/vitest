8cf5595d8abacf9fd22a170e10d311273f421056
Ari Perkkiö <ari.perkkio@gmail.com>
2025-06-01 19:34:30 +0300

    docs(coverage): explain v8 and istanbul differences

---
+++ docs/.vitepress/components/ArrowRight.vue
@@ -0,0 +1,26 @@
+<template>
+  <span class="arrow-right">
+    <span class="arrow-line" />
+    <span class="arrow-head" />
+  </span>
+</template>
+
+<style>
+.arrow-right {
+  display: flex;
+  align-items: center;
+}
+
+.arrow-line {
+  height: 0.2rem;
+  width: 1rem;
+  background-color: var(--vp-c-brand-1);
+}
+
+.arrow-head {
+  height: 1rem;
+  width: 1rem;
+  background-color: var(--vp-c-brand-1);
+  clip-path: polygon(0 0, 0% 100%, 100% 50%);
+}
+</style>
