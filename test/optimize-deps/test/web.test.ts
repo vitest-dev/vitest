5297f3229cc869de7bdaed56e889816416ab8e36
Vladimir <sleuths.slews0s@icloud.com>
2025-06-03 19:42:12 +0200

    test: fix tests failing in CI (#8083)

--- test/optimize-deps/test/web.test.ts
+++ test/optimize-deps/test/web.test.ts
@@ -6,5 +6,5 @@ import { importMetaUrl } from '@vitest/test-dep-url'
 import { expect, test } from 'vitest'
 
 test('import.meta.url', () => {
-  expect(importMetaUrl).toContain('/node_modules/.vite/vitest/d41d8cd98f00b204e9800998ecf8427e/deps/')
+  expect(importMetaUrl).toContain('/node_modules/.vite/vitest/da39a3ee5e6b4b0d3255bfef95601890afd80709/deps/')
 })
