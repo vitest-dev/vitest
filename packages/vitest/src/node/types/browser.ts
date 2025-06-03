0fb5cc4a4dd0cf1af08f16651aebbe09234974a6
Raul Macarie <raul@macarie.me>
2025-06-03 21:22:53 +0200

    feat: provide `attachmentsDir` to `resolve*Paths` and use it instead of `__diffs__`

--- packages/vitest/src/node/types/browser.ts
+++ packages/vitest/src/node/types/browser.ts
@@ -136,6 +136,12 @@ type ToMatchScreenshotResolvePath = (data: {
    * parent {@linkcode https://main.vitest.dev/api/#describe|describe}, sanitized.
    */
   testName: string
+  /**
+   * The value provided to
+   * {@linkcode https://main.vitest.dev/config/#attachmentsdir|attachmentsDir},
+   * if none is provided, its default value.
+   */
+  attachmentsDir: string
 }) => string
 
 export interface BrowserInstanceOption extends BrowserProviderOptions,
@@ -316,7 +322,7 @@ export interface BrowserConfigOptions {
       /**
        * Overrides default screenshot path used for diffs.
        *
-       * @default `${root}/__diffs__/${testFileDirectory}/${testFileName}/${arg}-${browserName}${ext}`
+       * @default `${root}/${attachmentsDir}/${testFileDirectory}/${testFileName}/${arg}-${browserName}${ext}`
        */
       resolveDiffPath?: ToMatchScreenshotResolvePath
     }
