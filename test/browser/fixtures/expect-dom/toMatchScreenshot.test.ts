4b0b3534e9bd1998d6a4502ddf85353e5d0236d5
Raul Macarie <raul@macarie.me>
2025-06-08 23:49:12 +0200

    test: improve testing cases

--- test/browser/fixtures/expect-dom/toMatchScreenshot.test.ts
+++ test/browser/fixtures/expect-dom/toMatchScreenshot.test.ts
@@ -1,92 +1,268 @@
-import { describe, expect, test } from 'vitest'
+import { afterEach, describe, expect, test } from 'vitest'
 import { render } from './utils'
 import { page, server } from '@vitest/browser/context'
+import { join } from 'pathe'
 
 const blockSize = 19
 const blocks = 5
+const dataTestId = 'colors-box'
+
+const extractPaths = (errorMessage: string, filename: string): string[] =>
+  // `map` on `Iterator` is only available in Node >= 22
+  Array.from(
+    errorMessage.matchAll(
+      new RegExp(`^.*?(/.*?${filename}-[\\w-]+\\.png)`, 'gm'),
+    ),
+    ([_, path]) => path,
+  )
 
 const renderTestCase = (colors: readonly [string, string, string]) =>
   render(`
-      <div style="--size: ${blockSize}px; display: flex; justify-content: center; height: var(--size); width: calc(var(--size) * ${blocks});" data-testid="rgb-box">
-        <div style="background-color: ${colors[0]}; width: var(--size);"></div>
-        <div style="background-color: ${colors[1]}; width: var(--size);"></div>
-        <div style="background-color: ${colors[2]}; width: var(--size);"></div>
-      </div>
-    `)
+    <div style="--size: ${blockSize}px; display: flex; justify-content: center; height: var(--size); width: calc(var(--size) * ${blocks});" data-testid="${dataTestId}">
+      <div style="background-color: ${colors[0]}; width: var(--size);"></div>
+      <div style="background-color: ${colors[1]}; width: var(--size);"></div>
+      <div style="background-color: ${colors[2]}; width: var(--size);"></div>
+    </div>
+  `)
 
 describe('.toMatchScreenshot', () => {
-  test('works correctly', async () => {
-    const { queryByTestId } = renderTestCase(['#ff0000', '#00ff00', '#0000ff'])
-
-    await page.viewport(blockSize * blocks, blockSize)
-
-    await expect(queryByTestId('rgb-box')).toMatchScreenshot('rgb-box')
-  })
-
-  test('throws when screenshots don\'t match', async () => {
-    const { queryByTestId } = renderTestCase(['#ff00ff', '#00ff00', '#0000ff'])
-
-    await page.viewport(blockSize * blocks, blockSize)
-
-    let errorMessage: string
-
-    try {
-      await expect(queryByTestId('rgb-box')).toMatchScreenshot('rgb-box')
-    } catch (error) {
-      errorMessage = error.message
-    }
-
-    expect(errorMessage).toMatch(new RegExp(dedent(`
-      expect\\(element\\)\\.toMatchScreenshot\\(\\)
-
-      Screenshot does not match the stored reference\\.
-      \\d+ pixels \\(ratio [01]\\.\\d{2}\\) differ\\.
-
-      Reference screenshot:
-        .*?[\\/]rgb-box-\\w+\\.png
-
-      Actual screenshot:
-        .*?[\\/]rgb-box-actual-\\w+\\.png
-    `)))
-  })
-
-  test('throws when creating a screenshot for the first time', async ({ onTestFinished }) => {
-    let path: string | undefined
+  test('compares screenshots correctly', async ({ onTestFinished }) => {
+    const filename = globalThis.crypto.randomUUID()
+    const path = join(
+      import.meta.dirname,
+      '__screenshots__',
+      'toMatchScreenshot.test.ts',
+      `${filename}-${server.browser}.png`,
+    )
 
     onTestFinished(async () => {
-      if (path) {
-        server.commands.removeFile(path)
-      }
+      await server.commands.removeFile(path)
     })
 
-    const { queryByTestId } = renderTestCase(['#ff2056', '#7ccf00', '#8e51ff'])
+    renderTestCase([
+      'oklch(39.6% 0.141 25.723)',
+      'oklch(40.5% 0.101 131.063)',
+      'oklch(37.9% 0.146 265.522)',
+    ])
+
+    const locator = page.getByTestId(dataTestId)
+
+    await locator.screenshot({
+      save: true,
+      path,
+    })
+
+    await expect(locator).toMatchScreenshot(filename)
+  })
+
+  test("throws when screenshots don't match", async ({ onTestFinished }) => {
+    const filename = globalThis.crypto.randomUUID()
+    const path = join(
+      import.meta.dirname,
+      '__screenshots__',
+      'toMatchScreenshot.test.ts',
+      `${filename}-${server.browser}.png`,
+    )
+
+    onTestFinished(async () => {
+      await server.commands.removeFile(path)
+    })
+
+    renderTestCase([
+      'oklch(39.6% 0.141 25.723)',
+      'oklch(40.5% 0.101 131.063)',
+      'oklch(37.9% 0.146 265.522)',
+    ])
+
+    const locator = page.getByTestId(dataTestId)
+
+    await locator.screenshot({
+      save: true,
+      path,
+    })
+
+    renderTestCase([
+      'oklch(84.1% 0.238 128.85)',
+      'oklch(84.1% 0.238 128.85)',
+      'oklch(84.1% 0.238 128.85)',
+    ])
 
     let errorMessage: string
 
     try {
-      await expect(queryByTestId('rgb-box')).toMatchScreenshot(Math.random().toString())
+      await expect(locator).toMatchScreenshot(filename)
     } catch (error) {
       errorMessage = error.message
     }
 
-    path = errorMessage.split('\n').at(-1).trim()
+    const [referencePath, actualPath, diffPath] = extractPaths(
+      errorMessage,
+      filename,
+    )
 
-    expect(errorMessage).toMatch(new RegExp(dedent(`
-      expect\\(element\\)\\.toMatchScreenshot\\(\\)
+    expect(referencePath).toMatch(new RegExp(`${path}$`))
+    expect(typeof actualPath).toBe('string')
+    expect(typeof diffPath).toBe('string')
 
-      No existing reference screenshot found.
+    onTestFinished(async () => {
+      await Promise.all([
+        server.commands.removeFile(actualPath),
+        server.commands.removeFile(diffPath),
+      ])
+    })
+
+    const { pixels, ratio } =
+      /(?<pixels>\d+).*?ratio (?<ratio>[01]\.\d{2})/.exec(errorMessage)
+        ?.groups ?? {}
+
+    expect(pixels).toMatch(/\d+/)
+    expect(ratio).toMatch(/[01]\.\d{2}/)
+
+    expect(errorMessage).toMatchInlineSnapshot(`
+      expect(element).toMatchScreenshot()
+
+      Screenshot does not match the stored reference.
+      ${pixels} pixels (ratio ${ratio}) differ.
 
       Reference screenshot:
-        .*?\\.png
-    `)))
+        ${referencePath}
+
+      Actual screenshot:
+        ${actualPath}
+
+      Diff screenshot:
+        ${diffPath}
+    `)
   })
+
+  test('throws when creating a screenshot for the first time', async ({
+    onTestFinished,
+    task,
+  }) => {
+    // if running with updates enabled, this test will not work
+    if (server.config.snapshotOptions.updateSnapshot === 'all') {
+      return
+    }
+
+    const { queryByTestId } = renderTestCase([
+      'oklch(37.9% 0.146 265.522)',
+      'oklch(40.5% 0.101 131.063)',
+      'oklch(39.6% 0.141 25.723)',
+    ])
+
+    let errorMessage: string
+
+    const filename = globalThis.crypto.randomUUID()
+
+    try {
+      await expect(queryByTestId(dataTestId)).toMatchScreenshot(filename)
+    } catch (error) {
+      errorMessage = error.message
+    }
+
+    const [referencePath] = extractPaths(errorMessage, filename)
+
+    expect(typeof referencePath).toBe('string')
+
+    onTestFinished(async () => {
+      await server.commands.removeFile(referencePath)
+    })
+
+    expect(errorMessage).toMatchInlineSnapshot(`
+      expect(element).toMatchScreenshot()
+
+      No existing reference screenshot found${
+        server.config.snapshotOptions.updateSnapshot === 'none'
+          ? '.'
+          : '; a new one was created. Review it before running tests again.'
+      }
+
+      Reference screenshot:
+        ${referencePath}
+    `)
+  })
+
+  test(
+    'throws when not able to capture a stable screenshot',
+    // this test un not stable
+    { retry: 5 },
+    async ({ onTestFailed }) => {
+      const filename = globalThis.crypto.randomUUID()
+
+      const { queryByTestId } = render(`
+        <div style="--size: 20px; --blocks: 10; height: var(--size); width: calc(var(--size) * var(--blocks));" data-testid="${dataTestId}">
+          <div style="height: 100%; aspect-ratio: 1; transform: translateX(calc(var(--size) * (var(--blocks) - 1))); animation: pong 4.5ms linear infinite;"></div>
+        </div>
+        <style>
+          @keyframes pong {
+            0% {
+              --blocks: 0;
+              background-color: oklch(84.1% 0.238 128.85);
+            }
+            11.11% {
+              --blocks: 1;
+              background-color: oklch(71.2% 0.194 13.428);
+            }
+            22.22% {
+              --blocks: 2;
+              background-color: oklch(84.1% 0.238 128.85);
+            }
+            33.33% {
+              --blocks: 3;
+              background-color: oklch(71.2% 0.194 13.428);
+            }
+            44.44% {
+              --blocks: 4;
+              background-color: oklch(84.1% 0.238 128.85);
+            }
+            55.55% {
+              --blocks: 5;
+              background-color: oklch(71.2% 0.194 13.428);
+            }
+            66.66% {
+              --blocks: 6;
+              background-color: oklch(84.1% 0.238 128.85);
+            }
+            77.77% {
+              --blocks: 7;
+              background-color: oklch(71.2% 0.194 13.428);
+            }
+            88.88% {
+              --blocks: 8;
+              background-color: oklch(84.1% 0.238 128.85);
+            }
+            100% {
+              --blocks: 9;
+              background-color: oklch(71.2% 0.194 13.428);
+            }
+          }
+        </style>
+      `)
+
+      let errorMessage: string
+
+      try {
+        await expect(queryByTestId(dataTestId)).toMatchScreenshot(filename, {
+          screenshotOptions: { animations: 'allow' },
+          timeout: 1_000,
+        })
+      } catch (error) {
+        errorMessage = error.message
+      }
+
+      onTestFailed(async () => {
+        const [referencePath] = extractPaths(errorMessage, filename)
+
+        if (typeof referencePath === 'string') {
+          await server.commands.removeFile(referencePath)
+        }
+      })
+
+      expect(errorMessage).toMatchInlineSnapshot(`
+        expect(element).toMatchScreenshot()
+
+        Could not capture a stable screenshot within 1000ms.
+      `)
+    },
+  )
 })
-
-/**
- * Simple dedent utility. Removes the first and last newlines.
- */
-const dedent = (input: string) => {
-  const spaces = /(?:^[\r\n]+)?(?<spaces>\s+)/.exec(input)?.groups.spaces?.length ?? 0
-
-  return input.replace(new RegExp(`^ {${spaces}}`, 'gm'), '').trim()
-}
