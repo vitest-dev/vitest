aad1474b1ad9bdcf3ffc334322a9db119162f131
Taylor Buchanan <taylor.buchanan@ewn.com>
2025-06-25 11:36:09 -0500

    Dedup project roots

--- packages/vitest/src/node/coverage.ts
+++ packages/vitest/src/node/coverage.ts
@@ -134,7 +134,7 @@ export class BaseCoverageProvider<Options extends ResolvedCoverageOptions<'istan
 
     // If --project filter is set pick only roots of resolved projects
     this.roots = ctx.config.project?.length
-      ? ctx.projects.map(project => project.config.root)
+      ? [...new Set(ctx.projects.map(project => project.config.root))]
       : [ctx.config.root]
   }
 
