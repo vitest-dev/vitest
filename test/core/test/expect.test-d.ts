598b4568bd9e972c6ee7f9194c791658aafc1119
samchungy <samchungy@gmail.com>
2025-07-06 00:30:19 +1000

    Add support for generics in Promise expects

--- test/core/test/expect.test-d.ts
+++ test/core/test/expect.test-d.ts
@@ -2,35 +2,51 @@
 
 import { expect, test } from 'vitest'
 
-test('expect.* allows asymmetrict mattchers with different types', () => {
+test('expect.* allows asymmetrict mattchers with different types', async () => {
   // types.ts examples: stringContaining
   expect('I have an apple').toEqual(expect.stringContaining('apple'))
   expect('I have an apple').toEqual<string>(expect.stringContaining('apple'))
+  await expect('I have an apple').resolves.toEqual(expect.stringContaining('apple'))
+  await expect('I have an apple').resolves.toEqual<string>(expect.stringContaining('apple'))
 
   expect({ a: 'test string' }).toEqual({ a: expect.stringContaining('test') })
   expect({ a: 'test string' }).toEqual<{ a: string }>({ a: expect.stringContaining('test') })
+  await expect({ a: 'test string' }).resolves.toEqual({ a: expect.stringContaining('test') })
+  await expect({ a: 'test string' }).resolves.toEqual<{ a: string }>({ a: expect.stringContaining('test') })
 
   // types.ts examples: objectContaining
   expect({ a: '1', b: 2 }).toEqual(expect.objectContaining({ a: '1' }))
   expect({ a: '1', b: 2 }).toEqual<{ a: string; b: string }>(expect.objectContaining({ a: '1' }))
+  await expect({ a: '1', b: 2 }).resolves.toEqual(expect.objectContaining({ a: '1' }))
+  await expect({ a: '1', b: 2 }).resolves.toEqual<{ a: string; b: string }>(expect.objectContaining({ a: '1' }))
 
   // types.ts examples: arrayContaining
   expect(['a', 'b', 'c']).toEqual(expect.arrayContaining(['b', 'a']))
   expect(['a', 'b', 'c']).toEqual<string[]>(expect.arrayContaining(['b', 'a']))
+  await expect(['a', 'b', 'c']).resolves.toEqual(expect.arrayContaining(['b', 'a']))
+  await expect(['a', 'b', 'c']).resolves.toEqual<string[]>(expect.arrayContaining(['b', 'a']))
 
   // types.ts examples: stringMatching
   expect('hello world').toEqual(expect.stringMatching(/^hello/))
   expect('hello world').toEqual<string>(expect.stringMatching(/^hello/))
+  await expect('hello world').resolves.toEqual(expect.stringMatching(/^hello/))
+  await expect('hello world').resolves.toEqual<string>(expect.stringMatching(/^hello/))
 
   expect('hello world').toEqual(expect.stringMatching('hello'))
   expect('hello world').toEqual<string>(expect.stringMatching('hello'))
+  await expect('hello world').resolves.toEqual(expect.stringMatching('hello'))
+  await expect('hello world').resolves.toEqual<string>(expect.stringMatching('hello'))
 
   // types.ts examples: closeTo
   expect(10.45).toEqual(expect.closeTo(10.5, 1))
   expect(10.45).toEqual<number>(expect.closeTo(10.5, 1))
+  await expect(10.45).resolves.toEqual(expect.closeTo(10.5, 1))
+  await expect(10.45).resolves.toEqual<number>(expect.closeTo(10.5, 1))
 
   expect(5.11).toEqual(expect.closeTo(5.12))
   expect(5.11).toEqual<number>(expect.closeTo(5.12))
+  await expect(5.11).resolves.toEqual(expect.closeTo(5.12))
+  await expect(5.11).resolves.toEqual<number>(expect.closeTo(5.12))
 
   // expect.any(String)
   // https://github.com/vitest-dev/vitest/pull/7016#issuecomment-2517674066
@@ -45,7 +61,20 @@ test('expect.* allows asymmetrict mattchers with different types', () => {
       name: 'Amelia',
     })
 
+    await expect(obj).resolves.toEqual({
+      id: expect.any(String),
+      name: 'Amelia',
+    })
+
     expect(obj).toEqual<{
+      id: string
+      name: string
+    }>({
+      id: expect.any(String),
+      name: 'Amelia',
+    })
+
+    await expect(obj).resolves.toEqual<{
       id: string
       name: string
     }>({
@@ -71,7 +100,23 @@ test('expect.* allows asymmetrict mattchers with different types', () => {
       createdAt: expect.any(Date),
     })
 
+    await expect(actual).resolves.toEqual({
+      foo: 'foo',
+      bar: 'bar',
+      createdAt: expect.any(Date),
+    })
+
     expect(actual).toEqual<{
+      foo: string
+      bar: string
+      createdAt: Date
+    }>({
+      foo: 'foo',
+      bar: 'bar',
+      createdAt: expect.any(Date),
+    })
+
+    await expect(actual).resolves.toEqual<{
       foo: string
       bar: string
       createdAt: Date
@@ -92,6 +137,18 @@ test('expect.* allows asymmetrict mattchers with different types', () => {
         createdAt: expect.any(Date),
       },
     ])
+
+    await expect(actual).resolves.toEqual<{
+      foo: string
+      bar: string
+      createdAt: Date
+    }[]>([
+      {
+        foo: 'foo',
+        bar: 'bar',
+        createdAt: expect.any(Date),
+      },
+    ])
   }
 
   // expect.arrayContaining
@@ -99,18 +156,26 @@ test('expect.* allows asymmetrict mattchers with different types', () => {
   {
     expect([1, 2, 3]).toEqual(expect.arrayContaining(['a']))
     expect([1, 2, 3]).toEqual<number[]>(expect.arrayContaining(['a']))
+    await expect([1, 2, 3]).resolves.toEqual<number[]>(expect.arrayContaining(['a']))
+    await expect([1, 2, 3]).resolves.toEqual<number[]>(expect.arrayContaining(['a']))
 
     expect([1, 2, 3]).toEqual(expect.arrayContaining([expect.any(Number)]))
     expect([1, 2, 3]).toEqual<number[]>(expect.arrayContaining([expect.any(Number)]))
+    await expect([1, 2, 3]).resolves.toEqual(expect.arrayContaining([expect.any(Number)]))
+    await expect([1, 2, 3]).resolves.toEqual<number[]>(expect.arrayContaining([expect.any(Number)]))
 
     expect([1, 2, 3]).toEqual(expect.arrayContaining([expect.anything()]))
     expect([1, 2, 3]).toEqual<number[]>(expect.arrayContaining([expect.anything()]))
+    await expect([1, 2, 3]).resolves.toEqual(expect.arrayContaining([expect.anything()]))
+    await expect([1, 2, 3]).resolves.toEqual<number[]>(expect.arrayContaining([expect.anything()]))
   }
 
   // expect.any(Array)
   // https://github.com/DefinitelyTyped/DefinitelyTyped/pull/62831/files#diff-ff7b882e4a29e7fe0e348a6bdf8b11774d606eaa221009b166b01389576d921fR1237
   expect({ list: [1, 2, 3] }).toMatchObject({ list: expect.any(Array) })
   expect({ list: [1, 2, 3] }).toMatchObject<{ list: number[] }>({ list: expect.any(Array) })
+  await expect({ list: [1, 2, 3] }).resolves.toMatchObject({ list: expect.any(Array) })
+  await expect({ list: [1, 2, 3] }).resolves.toMatchObject<{ list: number[] }>({ list: expect.any(Array) })
 
   // expect<T>
   // https://github.com/vitest-dev/vitest/issues/8081
