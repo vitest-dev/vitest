1bb900de9552964ada8284d6e91dd6c554c1f306
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2025-07-28 14:51:24 +0200

    fix(wdio): properly construct the shadow root selector if there are multiple elements

--- packages/browser/src/client/tester/locators/webdriverio.ts
+++ packages/browser/src/client/tester/locators/webdriverio.ts
@@ -60,7 +60,15 @@ class WebdriverIOLocator extends Locator {
     if (!selectors.length) {
       throw getElementError(this._pwSelector, this._container || document.body)
     }
-    return selectors.join(', ')
+    let hasShadowRoot = false
+    const newSelectors = selectors.map((selector) => {
+      if (selector.startsWith('>>>')) {
+        hasShadowRoot = true
+        return selector.slice(3)
+      }
+      return selector
+    })
+    return (hasShadowRoot ? '>>>' : '') + newSelectors.join(', ')
   }
 
   public override click(options?: UserEventClickOptions): Promise<void> {
