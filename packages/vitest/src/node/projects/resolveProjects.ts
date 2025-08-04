010fc55b527fc55982f9e12331420fff97c10f76
Shinobu Hayashi <shinobu.hayashi@dr-ubie.com>
2025-08-04 20:17:14 +0900

    fix(vitest): override `config.include` option with `config.browser.instances[].include` option if it is specified (#8260)

--- packages/vitest/src/node/projects/resolveProjects.ts
+++ packages/vitest/src/node/projects/resolveProjects.ts
@@ -303,8 +303,9 @@ function cloneConfig(project: TestProject, { browser, ...config }: BrowserInstan
     ...overrideConfig
   } = config
   const currentConfig = project.config.browser
+  const clonedConfig = deepClone(project.config)
   return mergeConfig<any, any>({
-    ...deepClone(project.config),
+    ...clonedConfig,
     browser: {
       ...project.config.browser,
       locators: locators
@@ -321,6 +322,10 @@ function cloneConfig(project: TestProject, { browser, ...config }: BrowserInstan
       providerOptions: config,
       instances: undefined, // projects cannot spawn more configs
     },
+    // If there is no include or exclude or includeSource pattern in browser.instances[], we should use the that's pattern from the parent project
+    include: (overrideConfig.include && overrideConfig.include.length > 0) ? [] : clonedConfig.include,
+    exclude: (overrideConfig.exclude && overrideConfig.exclude.length > 0) ? [] : clonedConfig.exclude,
+    includeSource: (overrideConfig.includeSource && overrideConfig.includeSource.length > 0) ? [] : clonedConfig.includeSource,
     // TODO: should resolve, not merge/override
   } satisfies ResolvedConfig, overrideConfig) as ResolvedConfig
 }
