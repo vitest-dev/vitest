9a0274cb8632a5468bab24b4be5caa165f5c4dff
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2025-08-26 11:56:28 +0200

    chore: remove pretty-format filter

--- scripts/publish-ci.ts
+++ scripts/publish-ci.ts
@@ -36,7 +36,7 @@ const releaseTag = version.includes('beta')
 console.log('Publishing version', version, 'with tag', releaseTag || 'latest')
 
 if (releaseTag) {
-  await $`pnpm -r --filter !./packages/pretty-format publish --access public --no-git-checks --tag ${releaseTag}`
+  await $`pnpm -r publish --access public --no-git-checks --tag ${releaseTag}`
 }
 else {
   await $`pnpm -r publish --access public --no-git-checks`
