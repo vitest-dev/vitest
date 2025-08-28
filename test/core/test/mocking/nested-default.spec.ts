259a2981816c6a151d7bd7f4f6e5212abe3160ac
Vladimir <sleuths.slews0s@icloud.com>
2025-08-28 11:38:47 +0200

    chore: remove `sweetalert2` from test dependencies (#8495)

--- test/core/test/mocking/nested-default.spec.ts
+++ test/core/test/mocking/nested-default.spec.ts
@@ -1,15 +1,15 @@
 // @vitest-environment jsdom
 
-import sweetalert from 'sweetalert2'
+import nestedCjs from '@vitest/test-dep-nested-cjs'
 import { expect, test, vi } from 'vitest'
 // @ts-expect-error mocked module
 import * as modDefaultCjs from '../../src/mocks/external/default-cjs.cjs'
 
-vi.mock('sweetalert2')
+vi.mock('@vitest/test-dep-nested-cjs')
 vi.mock('../../src/mocks/external/default-cjs.cjs')
 
 test('default is mocked', () => {
   expect(vi.isMockFunction(modDefaultCjs.default.fn)).toBe(true)
-  expect(vi.isMockFunction(sweetalert)).toBe(true)
-  expect(vi.isMockFunction(sweetalert.clickCancel)).toBe(true)
+  expect(vi.isMockFunction(nestedCjs)).toBe(true)
+  expect(vi.isMockFunction(nestedCjs.clickCancel)).toBe(true)
 })
