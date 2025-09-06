80d15f418ef04629db946ec903b841072c0d445a
btea <2356281422@qq.com>
2025-09-06 23:18:27 +0800

    perf: replace startsWith with strict equality

--- packages/vitest/src/utils/base.ts
+++ packages/vitest/src/utils/base.ts
@@ -24,7 +24,7 @@ export function escapeRegExp(s: string): string {
 }
 
 export function wildcardPatternToRegExp(pattern: string): RegExp {
-  const negated = pattern.startsWith('!')
+  const negated = pattern[0] === '!'
 
   if (negated) {
     pattern = pattern.slice(1)
