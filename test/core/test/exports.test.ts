fd0921f4b7da58e96705ee2d4cbc2329f8dbd44a
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2025-09-30 16:42:49 +0200

    test: ignore browser entry

--- test/core/test/exports.test.ts
+++ test/core/test/exports.test.ts
@@ -9,6 +9,9 @@ it('exports snapshot', async ({ skip, task }) => {
   const manifest = await getPackageExportsManifest({
     importMode: 'package', // or 'dist' or 'package'
     cwd: resolve(import.meta.dirname, '../../../packages/vitest'),
+    resolveExportEntries(entries) {
+      return entries.filter(([key]) => key !== './browser')
+    },
   })
 
   if (rolldownVersion) {
