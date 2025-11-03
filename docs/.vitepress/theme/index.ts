c42e9b60358a3aa61bf940201048893d9bbe0c67
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2025-11-03 17:55:00 +0100

    docs: add client-side redirect for the config

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -25,7 +25,18 @@ export default {
       'home-features-after': () => h(HomePage),
     })
   },
-  enhanceApp({ app }) {
+  enhanceApp({ app, router }) {
+    router.onBeforeRouteChange = (to) => {
+      const url = new URL(to, location.href)
+      if (!url.hash) {
+        return true
+      }
+      if (url.pathname === '/config' || url.pathname === '/config/') {
+        const [page, ...hash] = (url.hash.startsWith('#browser.') ? url.hash.slice(9) : url.hash.slice(1)).toLowerCase().split('-')
+        setTimeout(() => { router.go(`/config/${page}${hash.length ? '#' : ''}${hash.join('-')}`) })
+        return false
+      }
+    }
     app.component('Version', Version)
     app.component('CRoot', CRoot)
     app.component('Deprecated', Deprecated)
