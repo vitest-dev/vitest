168da327579a59a194f456eaea6dcc5306e4b63c
Nico Jansen <jansennico@gmail.com>
2025-11-07 15:24:59 +0100

    Update add.spec.js

--- test/bail-race/src/add.spec.js
+++ test/bail-race/src/add.spec.js
@@ -10,11 +10,3 @@ test('fails adding two numbers', () => {
   const result = add(2, 3)
   expect(result).toBe(6)
 })
-
-// test("adds two numbers 1", async () => {
-//   const result = add(2, 3);
-//   await sleep(1000);
-//   expect(result).toBe(5);
-// });
-
-// const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
