cd33b576c45b1762d242dcb38b03ecf4745be00a
Ari Perkkiö <ari.perkkio@gmail.com>
2025-11-10 09:27:34 +0200

    test: simplify test case

--- test/cli/test/bail-race.test.ts
+++ test/cli/test/bail-race.test.ts
@@ -1,85 +1,44 @@
-import type { RunnerTestCase, RunnerTestSuite } from 'vitest/node'
-import { Writable } from 'node:stream'
 import { resolve } from 'pathe'
-import { expect, it } from 'vitest'
+import { expect, test } from 'vitest'
 import { createVitest } from 'vitest/node'
 
-const root = resolve(import.meta.dirname, '../fixtures/bail-race')
+test('cancels previous run before starting new one', async () => {
+  const results: string[] = []
 
-class NoopStream extends Writable {
-  override _write(_chunk: unknown, _encoding: BufferEncoding, callback: (error?: Error | null) => void) {
-    callback()
+  const vitest = await createVitest('test', {
+    root: resolve(import.meta.dirname, '../fixtures/bail-race'),
+    bail: 1,
+    maxWorkers: 1,
+    watch: false,
+    reporters: [{
+      onTestCaseResult(testCase) {
+        const result = testCase.result()
+
+        results.push(`${result.state}${result.errors ? `: ${result.errors?.[0].message}` : ''}`)
+      },
+    }],
+  })
+
+  let rounds = 0
+
+  while (vitest.state.errorsSet.size === 0) {
+    await vitest.start()
+
+    if (rounds >= 2) {
+      break
+    }
+
+    rounds++
   }
-}
 
-it('should be able to bail fast without race conditions', async () => {
-  // Arrange
-  const abortController = new AbortController()
-  const runPromise = Promise.resolve().then(async () => {
-    const vitest = await createVitest('test', {
-      pool: 'threads',
-      coverage: { enabled: false },
-      maxWorkers: 1,
-      maxConcurrency: 1,
-      watch: false,
-      bail: 1,
-      reporters: [],
-      root,
-    }, {}, { stderr: new NoopStream(), stdout: new NoopStream() })
-
-    // Act
-    while (vitest.state.errorsSet.size === 0 && !abortController.signal.aborted) {
-      await vitest.start()
-      const tests = vitest.state.getFiles().flatMap(file => collectTestsFromSuite(file)).map(({ name, result }) => ({ name, state: result?.state }))
-      expect(tests).toEqual( // verify that nothing strange happened
-        [
-          {
-            name: 'adds two numbers',
-            state: 'pass',
-          },
-          {
-            name: 'fails adding two numbers',
-            state: 'fail',
-          },
-        ],
-      )
-    }
-    await vitest.close()
-    abortController.abort()
-    if (vitest.state.errorsSet.size > 0) {
-      const msg = [...vitest.state.errorsSet]
-        .map(err => (err as Error).message)
-        .join('\n')
-      throw new Error(`Tests failed with bail:\n${msg}`)
-    }
-  })
-
-  const timeoutPromise = new Promise<'timeout'>((res) => {
-    const timeoutId = setTimeout(() => {
-      abortController.abort()
-      res('timeout') // Success!
-    }, 5_000) // Give race condition 5 seconds to manifest
-    abortController.signal.addEventListener('abort', () => {
-      clearTimeout(timeoutId)
-    })
-  })
-
-  // Assert
-  await expect(Promise.race([runPromise, timeoutPromise])).resolves.toBe('timeout')
+  expect(results).toMatchInlineSnapshot(`
+    [
+      "passed",
+      "failed: expected 5 to be 6 // Object.is equality",
+      "passed",
+      "failed: expected 5 to be 6 // Object.is equality",
+      "passed",
+      "failed: expected 5 to be 6 // Object.is equality",
+    ]
+  `)
 })
-
-function collectTestsFromSuite(
-  suite: RunnerTestSuite,
-): RunnerTestCase[] {
-  return suite.tasks.flatMap((task) => {
-    if (task.type === 'suite') {
-      return collectTestsFromSuite(task)
-    }
-    else if (task.type === 'test') {
-      return task
-    }
-    else {
-      return []
-    }
-  })
-}
