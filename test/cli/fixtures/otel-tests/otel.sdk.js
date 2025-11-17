957bae5591c116cfb7fbcb92312699bdbf54c7c3
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2025-11-17 15:45:50 +0100

    test: don't start sdk

--- test/cli/fixtures/otel-tests/otel.sdk.js
+++ test/cli/fixtures/otel-tests/otel.sdk.js
@@ -4,5 +4,6 @@ const sdk = new NodeSDK({
   serviceName: 'vitest',
 })
 
-sdk.start()
+// sdk.start() do not start to keep traces noop,
+// but Vitest will still be able to see otel
 export default sdk
