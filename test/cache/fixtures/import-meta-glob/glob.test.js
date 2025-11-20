ff5cce42adb50fd4b25c144db89ec136eb54196c
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2025-11-20 11:25:19 +0100

    chore: remove log

--- test/cache/fixtures/import-meta-glob/glob.test.js
+++ test/cache/fixtures/import-meta-glob/glob.test.js
@@ -2,6 +2,5 @@ import { test, expect, inject } from 'vitest'
 
 test('replaced variable is the same', () => {
   const files = import.meta.glob('./generated/*')
-  console.log(files)
   expect(Object.keys(files)).toEqual(inject('generated'))
 })
