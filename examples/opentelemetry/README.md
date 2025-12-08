8057c1dc563bc3d25ac6169c225c067cccd319e3
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-12-08 12:19:40 +0900

    chore: tweak examples

--- examples/opentelemetry/README.md
+++ examples/opentelemetry/README.md
@@ -3,16 +3,14 @@
 ```sh
 # Start Jaeger service to receive otlp traces over http
 # and serve Web UI at http://localhost:16686
-docker compose up
+docker compose up -d
 
 # Run tests (exports with OTLP HTTP by deafult)
 pnpm test --experimental.openTelemetry.enabled
 
-# Use console exporter
+# Use console exporter for quick debugging
 OTEL_TRACES_EXPORTER=console pnpm test --experimental.openTelemetry.enabled
 
 # Run browser mode
 pnpm test --experimental.openTelemetry.enabled --browser.enabled
-
-# Open http://localhost:16686
 ```
--- examples/opentelemetry/otel.js
+++ examples/opentelemetry/otel.js
@@ -3,8 +3,14 @@ import { NodeSDK } from '@opentelemetry/sdk-node'
 
 const sdk = new NodeSDK({
   serviceName: 'vitest',
-  // by default, it uses "@opentelemetry/exporter-trace-otlp-proto"
-  // traceExporter
+
+  // NodeSDK uses "@opentelemetry/exporter-trace-otlp-proto" by default.
+  // you can override it via `traceExporter` options.
+  // traceExporter: ...,
+
+  // you can configure additional instrumentations such as `@opentelemetry/auto-instrumentations-node`
+  // via `instrumentations` options.
+  // instrumentations: ...
 })
 sdk.start()
 export default sdk
