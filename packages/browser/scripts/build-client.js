0cdff7524b0807c21fde9199713141ee7b7e8688
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-12-10 16:29:33 +0900

    comment

--- packages/browser/scripts/build-client.js
+++ packages/browser/scripts/build-client.js
@@ -1,4 +1,5 @@
-// wrapper script for node --watch
+// wrapper script for `node --watch`.
+// this works around some issues with `vite build --watch`.
 import { spawn } from 'node:child_process'
 
 spawn('node', ['--run', 'build:client'], {
