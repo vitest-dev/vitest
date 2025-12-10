fe91a8bcba17762f92935146db9b3a133b47c61d
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2025-12-10 16:01:16 +0100

    fix: log deprecated config.poolOptions if it's set

--- packages/vitest/src/node/config/resolveConfig.ts
+++ packages/vitest/src/node/config/resolveConfig.ts
@@ -147,6 +147,10 @@ export function resolveConfig(
     resolved.poolRunner = options.pool
   }
 
+  if ('poolOptions' in resolved) {
+    logger.deprecate('`test.poolOptions` was removed in Vitest 4. All previous `poolOptions` are now top-level options. Please, refer to the migration guide: https://vitest.dev/guide/migration#pool-rework')
+  }
+
   resolved.pool ??= 'forks'
 
   resolved.project = toArray(resolved.project)
