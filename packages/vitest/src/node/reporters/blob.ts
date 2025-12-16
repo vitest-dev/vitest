795d2b1b142864ee43e5d953682baf907d72dd57
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-12-16 13:04:12 +0900

    fix: mutate improtDurations

--- packages/vitest/src/node/reporters/blob.ts
+++ packages/vitest/src/node/reporters/blob.ts
@@ -226,9 +226,14 @@ export function optimizeFilesReport(files: File[]): CompactFiles {
     if (file.importDurations) {
       flatImportDurations[file.id] = file.importDurations
     }
-    const newFile = { ...file }
-    delete newFile.importDurations
-    newFiles.push(newFile)
+    // TODO:
+    // need to mutate File report itself to avoid flatted serialization
+    // to not include importDurations referenced in nested objects
+    delete file.importDurations
+    newFiles.push(file)
+    // const newFile = { ...file }
+    // delete newFile.importDurations
+    // newFiles.push(newFile)
   }
   return {
     files: newFiles,
