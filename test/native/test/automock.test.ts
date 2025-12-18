f8c558d86798196db0c2a08ac6bebbe5e0469b56
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2025-12-18 10:54:52 +0100

    test: add imports from basic.ts

--- test/native/test/automock.test.ts
+++ test/native/test/automock.test.ts
@@ -1,9 +1,13 @@
 import { expect, test, vi } from 'vitest'
-import { squared } from '../src/basic.ts'
+import basicDefault, { add, hello, squared } from '../src/basic.ts'
 
 vi.mock(import('../src/basic.ts'))
 
 test('squared is mocked', () => {
   expect(vi.isMockFunction(squared)).toBe(true)
+  expect(vi.isMockFunction(add)).toBe(true)
   expect(squared(2)).toBe(undefined)
+  expect(add(1, 1)).toBe(undefined)
+  expect(hello).toBe('world')
+  expect(basicDefault).toBe('hello world')
 })
--- test/native/test/autospy.test.ts
+++ test/native/test/autospy.test.ts
@@ -1,10 +1,17 @@
 import { expect, test, vi } from 'vitest'
-import { squared } from '../src/basic.ts'
+import basicDefault, { add, hello, squared } from '../src/basic.ts'
 
 vi.mock(import('../src/basic.ts'), { spy: true })
 
 test('squared is mocked', () => {
   expect(vi.isMockFunction(squared)).toBe(true)
+  expect(vi.isMockFunction(add)).toBe(true)
   expect(squared(2)).toBe(4)
   expect(squared).toHaveBeenCalled()
+
+  expect(add(1, 1)).toBe(2)
+  expect(add).toHaveBeenCalled()
+
+  expect(hello).toBe('world')
+  expect(basicDefault).toBe('hello world')
 })
