3ded894ba6d4412020386bd15c9c4d48a82204c8
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2025-12-18 10:56:37 +0100

    fix: move mock=actual resolution to worker loader

--- packages/vitest/src/runtime/nodejsWorkerLoader.ts
+++ packages/vitest/src/runtime/nodejsWorkerLoader.ts
@@ -1,5 +1,6 @@
 import type { InitializeHook, ResolveHook } from 'node:module'
 import type { MessagePort } from 'node:worker_threads'
+import { isBuiltin } from 'node:module'
 
 let port: MessagePort
 
@@ -15,8 +16,19 @@ export const initialize: InitializeHook = async ({
 
 const NOW_LENGTH = Date.now().toString().length
 const REGEXP_VITEST = new RegExp(`%3Fvitest=\\d{${NOW_LENGTH}}`)
+const REGEXP_MOCK_ACTUAL = /\?mock=actual/
 
 export const resolve: ResolveHook = (specifier, context, defaultResolve) => {
+  if (specifier.includes('mock=actual')) {
+    // url is already resolved by `importActual`
+    const moduleId = specifier.replace(REGEXP_MOCK_ACTUAL, '')
+    return {
+      url: moduleId,
+      format: isBuiltin(moduleId) ? 'builtin' : undefined,
+      shortCircuit: true,
+    }
+  }
+
   const isVitest = specifier.includes('%3Fvitest=')
   const result = defaultResolve(
     isVitest ? specifier.replace(REGEXP_VITEST, '') : specifier,
