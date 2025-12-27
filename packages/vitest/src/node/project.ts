914151a57e4888428eb773699ee167dffb92ff13
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2025-12-27 15:13:21 +0100

    refactor: rename spec.ts to test-specifications.ts

--- packages/vitest/src/node/project.ts
+++ packages/vitest/src/node/project.ts
@@ -6,7 +6,7 @@ import type { ProvidedContext } from '../types/general'
 import type { OnTestsRerunHandler, Vitest } from './core'
 import type { VitestFetchFunction } from './environments/fetchModule'
 import type { GlobalSetupFile } from './globalSetup'
-import type { TestSpecificationOptions } from './spec'
+import type { TestSpecificationOptions } from './test-specification'
 import type { ParentProjectBrowser, ProjectBrowser } from './types/browser'
 import type {
   ProjectName,
@@ -36,7 +36,7 @@ import { MocksPlugins } from './plugins/mocks'
 import { WorkspaceVitestPlugin } from './plugins/workspace'
 import { getFilePoolName } from './pool'
 import { VitestResolver } from './resolver'
-import { TestSpecification } from './spec'
+import { TestSpecification } from './test-specification'
 import { createViteServer } from './vite'
 
 export class TestProject {
--- packages/vitest/src/public/node.ts
+++ packages/vitest/src/public/node.ts
@@ -69,8 +69,8 @@ export type {
   TestSequencer,
   TestSequencerConstructor,
 } from '../node/sequencers/types'
-export type { TestSpecification } from '../node/spec'
 export { registerConsoleShortcuts } from '../node/stdin'
+export type { TestSpecification } from '../node/test-specification'
 export type { BenchmarkUserOptions } from '../node/types/benchmark'
 
 export type {
