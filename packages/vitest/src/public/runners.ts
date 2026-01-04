d13c97493dcee1f668d3145f809d6113acbcbabf
Vladimir <sheremet.va@icloud.com>
2026-01-04 13:12:15 +0100

    Update packages/vitest/src/public/runners.ts

--- packages/vitest/src/public/runners.ts
+++ packages/vitest/src/public/runners.ts
@@ -2,4 +2,4 @@ export { NodeBenchmarkRunner } from '../runtime/runners/benchmark'
 export { TestRunner as VitestTestRunner } from '../runtime/runners/test'
 export type { VitestRunner } from '@vitest/runner'
 
-console.warn('Importing from "vitest/runners" is deprecated since Vitest 4.1. Please use "vitest/runtime" instead.')
+console.warn('Importing from "vitest/runners" is deprecated since Vitest 4.1. Please use "vitest" instead.')
