5dc2d1972a261e8a8634b305dfa3567817c78037
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2026-01-14 20:03:37 +0100

    chore: use local-pkg instead

--- packages/vitest/src/utils/nativeModuleRunner.ts
+++ packages/vitest/src/utils/nativeModuleRunner.ts
@@ -1,11 +1,9 @@
 import type { TestModuleMocker } from '@vitest/mocker'
-import { createRequire } from 'node:module'
 import { pathToFileURL } from 'node:url'
 import { isBareImport } from '@vitest/utils/helpers'
+import { resolveModule } from 'local-pkg'
 import { isAbsolute, resolve } from 'pathe'
 import { ModuleRunner } from 'vite/module-runner'
-
-const __require = createRequire(import.meta.url)
 
 export class NativeModuleRunner extends ModuleRunner {
   /**
@@ -35,11 +33,9 @@ export class NativeModuleRunner extends ModuleRunner {
 
   override import(moduleId: string): Promise<any> {
     if (isBareImport(moduleId)) {
-      // better to use `import.meta.resolve`, but it's experimental
-      // this works in most cases, except where there is no `default` or `import` would be preferable
-      // if there is a commonjs condition in package.json, it will be preferred over esm
-      // TODO: revisit the solution
-      return import(__require.resolve(moduleId, { paths: [this.root] }))
+      const path = resolveModule(moduleId, { paths: [this.root] })
+        ?? resolve(this.root, moduleId)
+      return import(path)
     }
     if (!isAbsolute(moduleId)) {
       moduleId = resolve(this.root, moduleId)
