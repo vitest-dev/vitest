0c7eaac16ef2aa71121d7f9c3924725e26fc389f
Joaquín Sánchez <userquin@gmail.com>
2026-01-19 17:26:27 +0100

    feat(ui): add project filter/sort support (#8689)

--- packages/ui/client/composables/explorer/search.ts
+++ packages/ui/client/composables/explorer/search.ts
@@ -1,21 +1,32 @@
 import type { Ref } from 'vue'
+import type { ProjectSortUIType } from '~/composables/explorer/types'
 import { debouncedWatch } from '@vueuse/core'
 import { computed, ref, watch } from 'vue'
 import { explorerTree } from '~/composables/explorer'
 import {
+  ALL_PROJECTS,
+  currentProject,
+  currentProjectName,
+  disableClearProjects,
+  enableProjects,
   filter,
   filteredFiles,
   initialized,
   isFiltered,
   isFilteredByStatus,
   openedTreeItems,
+  projectSort,
   search,
   testsTotal,
   treeFilter,
   uiEntries,
 } from './state'
 
-export function useSearch(searchBox: Ref<HTMLDivElement | undefined>) {
+export function useSearch(
+  searchBox: Ref<HTMLInputElement | undefined>,
+  selectProject: Ref<HTMLSelectElement | undefined>,
+  sortProject: Ref<HTMLSelectElement | undefined>,
+) {
   const disableFilter = computed(() => {
     if (isFilteredByStatus.value) {
       return false
@@ -23,8 +34,11 @@ export function useSearch(searchBox: Ref<HTMLDivElement | undefined>) {
 
     return !filter.onlyTests
   })
+
   const disableClearSearch = computed(() => search.value === '')
   const debouncedSearch = ref(search.value)
+  const disableProjectSort = computed(() => currentProject.value !== ALL_PROJECTS)
+  const disableClearProjectSort = computed(() => disableProjectSort.value || projectSort.value === 'default')
 
   debouncedWatch(() => search.value, (value) => {
     debouncedSearch.value = value?.trim() ?? ''
@@ -47,9 +61,25 @@ export function useSearch(searchBox: Ref<HTMLDivElement | undefined>) {
     }
   }
 
+  function clearProject(focus: boolean) {
+    currentProject.value = ALL_PROJECTS
+    if (focus) {
+      selectProject.value?.focus()
+    }
+  }
+
+  function clearProjectSort(focus: boolean) {
+    projectSort.value = 'default'
+    if (focus) {
+      sortProject.value?.focus()
+    }
+  }
+
   function clearAll() {
     clearFilter(false)
     clearSearch(true)
+    clearProject(false)
+    clearProjectSort(false)
   }
 
   function updateFilterStorage(
@@ -58,6 +88,8 @@ export function useSearch(searchBox: Ref<HTMLDivElement | undefined>) {
     successValue: boolean,
     skippedValue: boolean,
     onlyTestsValue: boolean,
+    projectValue: string,
+    projectSortValue: ProjectSortUIType,
   ) {
     if (!initialized.value) {
       return
@@ -68,6 +100,8 @@ export function useSearch(searchBox: Ref<HTMLDivElement | undefined>) {
     treeFilter.value.success = successValue
     treeFilter.value.skipped = skippedValue
     treeFilter.value.onlyTests = onlyTestsValue
+    treeFilter.value.project = projectValue
+    treeFilter.value.projectSort = projectSortValue === 'default' ? undefined : projectSortValue
   }
 
   watch(
@@ -77,9 +111,19 @@ export function useSearch(searchBox: Ref<HTMLDivElement | undefined>) {
       filter.success,
       filter.skipped,
       filter.onlyTests,
+      currentProject.value,
+      projectSort.value,
     ] as const,
-    ([search, failed, success, skipped, onlyTests]) => {
-      updateFilterStorage(search, failed, success, skipped, onlyTests)
+    ([search, failed, success, skipped, onlyTests, project, projectSort]) => {
+      updateFilterStorage(
+        search,
+        failed,
+        success,
+        skipped,
+        onlyTests,
+        project,
+        projectSort,
+      )
       explorerTree.filterNodes()
     },
     { flush: 'post' },
@@ -105,5 +149,14 @@ export function useSearch(searchBox: Ref<HTMLDivElement | undefined>) {
     filteredFiles,
     testsTotal,
     uiEntries,
+    enableProjects,
+    disableClearProjects,
+    currentProject,
+    currentProjectName,
+    clearProject,
+    projectSort,
+    disableProjectSort,
+    clearProjectSort,
+    disableClearProjectSort,
   }
 }
--- packages/ui/client/composables/explorer/state.ts
+++ packages/ui/client/composables/explorer/state.ts
@@ -1,7 +1,8 @@
 import type { File } from '@vitest/runner'
-import type { FileTreeNode, Filter, FilteredTests, TreeFilterState, UITaskTreeNode } from './types'
+import type { FileTreeNode, Filter, FilteredTests, ProjectSortUIType, TreeFilterState, UITaskTreeNode } from './types'
 import { useLocalStorage } from '@vueuse/core'
 import { computed, reactive, ref, shallowRef } from 'vue'
+import { availableProjects } from '~/composables/client'
 import { explorerTree } from './index'
 
 export const uiFiles = shallowRef<FileTreeNode[]>([])
@@ -11,6 +12,7 @@ export const openedTreeItems = useLocalStorage<string[]>(
   [],
   { shallow: true },
 )
+export const ALL_PROJECTS = '__vitest_ui_all_projects__'
 export const openedTreeItemsSet = computed(() => new Set(openedTreeItems.value))
 export const treeFilter = useLocalStorage<TreeFilterState>(
   'vitest-ui_task-tree-filter',
@@ -21,8 +23,17 @@ export const treeFilter = useLocalStorage<TreeFilterState>(
     skipped: false,
     onlyTests: false,
     search: '',
+    project: ALL_PROJECTS,
+    projectSort: undefined,
   },
 )
+export const projectSort = ref<ProjectSortUIType>(treeFilter.value.projectSort || 'default')
+export const currentProject = shallowRef(treeFilter.value?.project || ALL_PROJECTS)
+export const enableProjects = computed(() => availableProjects.value.length > 1)
+export const disableClearProjects = computed(() => currentProject.value === ALL_PROJECTS)
+export const currentProjectName = computed(() => {
+  return !enableProjects.value || currentProject.value === ALL_PROJECTS ? undefined : currentProject.value
+})
 export const search = ref<string>(treeFilter.value.search)
 const htmlEntities: Record<string, string> = {
   '&': '&amp;',
@@ -75,6 +86,7 @@ export const shouldShowExpandAll = computed(() => {
 export const testsTotal = computed<FilteredTests>(() => {
   const filtered = isFiltered.value
   const filteredByStatus = isFilteredByStatus.value
+  const project = currentProjectName.value
   const onlyTests = filter.onlyTests
   const failed = explorerTree.summary.filesFailed
   const success = explorerTree.summary.filesSuccess
@@ -82,7 +94,7 @@ export const testsTotal = computed<FilteredTests>(() => {
   const running = explorerTree.summary.filesRunning
   const files = filteredFiles.value
   return explorerTree.collectTestsTotal(
-    filtered || filteredByStatus,
+    filtered || filteredByStatus || !!project,
     onlyTests,
     files,
     {
