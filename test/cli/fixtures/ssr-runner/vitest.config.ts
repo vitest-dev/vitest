7d8cbdf4d6b71b09554e0efa61053157896ec3e7
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-01-23 13:52:43 +0900

    test: tweak

--- test/cli/fixtures/ssr-runner/vitest.config.ts
+++ test/cli/fixtures/ssr-runner/vitest.config.ts
@@ -10,11 +10,10 @@ export default defineConfig({
       // vite should be externalized and reference-equal to the directly imported one
       async configureServer(server) {
         const ssr = server.environments.ssr
-        if (isRunnableDevEnvironment(ssr)) {
-          const m = await ssr.runner.import<{ default: typeof import("vite") }>('./test-runner.js')
-          assert(m.default.createServer === createServer)
-          ;(globalThis as any).__testSsrRunner = m.default.version
-        }
+        assert(isRunnableDevEnvironment(ssr))
+        const m = await ssr.runner.import<{ default: typeof import("vite") }>('./test-runner.js')
+        assert(m.default.createServer === createServer)
+        ;(globalThis as any).__testSsrRunner = m.default.version
       },
     },
   ],
