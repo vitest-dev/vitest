dff600d70f3874db238519a8fb41d7e5e1c6166f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-01-29 19:07:15 +0900

    perf: use devalue

--- packages/utils/src/serialization.ts
+++ packages/utils/src/serialization.ts
@@ -1,6 +1,32 @@
-import * as flatted from 'flatted'
+// import * as flatted from 'flatted'
 
-export { parse, toJSON } from 'flatted'
+// export { parse, toJSON } from 'flatted'
+
+// /**
+//  * Replacer function for serialization methods such as JSON.stringify() or
+//  * flatted.stringify(). Handles Error objects by extracting all properties.
+//  */
+// function stringifyReplace(_key: string, value: unknown): unknown {
+//   if (value instanceof Error) {
+//     const cloned = cloneByOwnProperties(value)
+//     return {
+//       name: value.name,
+//       message: value.message,
+//       stack: value.stack,
+//       ...cloned,
+//     }
+//   }
+//   return value
+// }
+
+// /**
+//  * Serialize data with circular reference handling and proper Error serialization.
+//  */
+// export function stringify(value: unknown): string {
+//   return flatted.stringify(value, stringifyReplace)
+// }
+
+import * as devalue from 'devalue'
 
 function cloneByOwnProperties(value: object): Record<string, unknown> {
   // Clones the value's properties into a new Object. The simpler approach of
@@ -14,26 +40,32 @@ function cloneByOwnProperties(value: object): Record<string, unknown> {
   )
 }
 
-/**
- * Replacer function for serialization methods such as JSON.stringify() or
- * flatted.stringify(). Handles Error objects by extracting all properties.
- */
-function stringifyReplace(_key: string, value: unknown): unknown {
-  if (value instanceof Error) {
-    const cloned = cloneByOwnProperties(value)
-    return {
-      name: value.name,
-      message: value.message,
-      stack: value.stack,
-      ...cloned,
-    }
+function serializeError(error: Error) {
+  return {
+    name: error.name,
+    message: error.message,
+    stack: error.stack,
+    ...cloneByOwnProperties(error),
   }
-  return value
 }
 
-/**
- * Serialize data with circular reference handling and proper Error serialization.
- */
+const customTypes = {
+  stringify: {
+    Error: (v: unknown) => v instanceof Error ? serializeError(v) : undefined,
+  },
+  parse: {
+    Error: (v: unknown) => v,
+  },
+}
+
+export function parse(text: string): unknown {
+  return devalue.parse(text, customTypes.parse)
+}
+
 export function stringify(value: unknown): string {
-  return flatted.stringify(value, stringifyReplace)
+  return devalue.stringify(value, customTypes.stringify)
+}
+
+export function toJSON(value: unknown): unknown {
+  return parse(stringify(value))
 }
