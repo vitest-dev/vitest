754b63423c34beeb7dcc65f10b3180e737a60245
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-02-05 18:18:46 +0900

    test: debug webkit

--- test/browser/fixtures/assertion-helper/basic.test.ts
+++ test/browser/fixtures/assertion-helper/basic.test.ts
@@ -36,3 +36,35 @@ test('soft', () => {
 test('soft async', async () => {
   await myEqualSoftAsync('left', 'right')
 })
+
+// debug
+
+const manual = helper((a: any, b: any) => {
+  expect(a).toEqual(b)
+})
+
+const manualAsync = helper(async (a: any, b: any) => {
+  await new Promise(r => setTimeout(r, 1))
+  expect(a).toEqual(b)
+})
+
+test("manual", () => {
+  manual('manual', 'no')
+})
+
+test("manual async", async () => {
+  await manualAsync('manual async', 'no')
+})
+
+function helper<F extends (...args: any) => any>(fn: F): F {
+  function __MANUAL_HELPER__(...args: any[]): any {
+    const result = fn(...args);
+    if (result && typeof result === "object" && typeof result.then === "function") {
+      return (async function __MANUAL_HELPER_ASYNC__() {
+        return await result;
+      })();
+    }
+    return result;
+  }
+  return __MANUAL_HELPER__ as F;
+}
--- test/browser/specs/assertion-helper.test.ts
+++ test/browser/specs/assertion-helper.test.ts
@@ -8,7 +8,7 @@ test('vi.helper hides internal stack traces', async () => {
 
   expect(stderr).toMatchInlineSnapshot(`
     "
-    ⎯⎯⎯⎯⎯⎯⎯ Failed Tests 4 ⎯⎯⎯⎯⎯⎯⎯
+    ⎯⎯⎯⎯⎯⎯⎯ Failed Tests 6 ⎯⎯⎯⎯⎯⎯⎯
 
      FAIL  |chromium| basic.test.ts > sync
     AssertionError: expected 'left' to deeply equal 'right'
@@ -20,14 +20,14 @@ test('vi.helper hides internal stack traces', async () => {
     Received: "left"
 
      ❯ basic.test.ts:25:2
-         23|
+         23| 
          24| test('sync', () => {
          25|   myEqual('left', 'right')
            |  ^
          26| })
-         27|
+         27| 
 
-    ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯[1/4]⎯
+    ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯[1/6]⎯
 
      FAIL  |chromium| basic.test.ts > async
     AssertionError: expected 'left' to deeply equal 'right'
@@ -39,14 +39,14 @@ test('vi.helper hides internal stack traces', async () => {
     Received: "left"
 
      ❯ basic.test.ts:29:2
-         27|
+         27| 
          28| test('async', async () => {
          29|   await myEqualAsync('left', 'right')
            |  ^
          30| })
-         31|
+         31| 
 
-    ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯[2/4]⎯
+    ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯[2/6]⎯
 
      FAIL  |chromium| basic.test.ts > soft
     AssertionError: expected 'left' to deeply equal 'right'
@@ -58,14 +58,14 @@ test('vi.helper hides internal stack traces', async () => {
     Received: "left"
 
      ❯ basic.test.ts:33:2
-         31|
+         31| 
          32| test('soft', () => {
          33|   myEqualSoft('left', 'right')
            |  ^
          34| })
-         35|
+         35| 
 
-    ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯[3/4]⎯
+    ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯[3/6]⎯
 
      FAIL  |chromium| basic.test.ts > soft async
     AssertionError: expected 'left' to deeply equal 'right'
@@ -77,14 +77,56 @@ test('vi.helper hides internal stack traces', async () => {
     Received: "left"
 
      ❯ basic.test.ts:37:2
-         35|
+         35| 
          36| test('soft async', async () => {
          37|   await myEqualSoftAsync('left', 'right')
            |  ^
          38| })
-         39|
+         39| 
 
-    ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯[4/4]⎯
+    ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯[4/6]⎯
+
+     FAIL  |chromium| basic.test.ts > manual
+    AssertionError: expected 'manual' to deeply equal 'no'
+
+    Failure screenshot:
+      - fixtures/assertion-helper/__screenshots__/basic.test.ts/manual-1.png
+
+    Expected: "no"
+    Received: "manual"
+
+     ❯ basic.test.ts:43:12
+         41| 
+         42| const manual = helper((a: any, b: any) => {
+         43|   expect(a).toEqual(b)
+           |            ^
+         44| })
+         45| 
+     ❯ __MANUAL_HELPER__ basic.test.ts:61:19
+     ❯ basic.test.ts:52:2
+
+    ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯[5/6]⎯
+
+     FAIL  |chromium| basic.test.ts > manual async
+    AssertionError: expected 'manual async' to deeply equal 'no'
+
+    Failure screenshot:
+      - fixtures/assertion-helper/__screenshots__/basic.test.ts/manual-async-1.png
+
+    Expected: "no"
+    Received: "manual async"
+
+     ❯ basic.test.ts:48:12
+         46| const manualAsync = helper(async (a: any, b: any) => {
+         47|   await new Promise(r => setTimeout(r, 1))
+         48|   expect(a).toEqual(b)
+           |            ^
+         49| })
+         50| 
+     ❯ __MANUAL_HELPER_ASYNC__ basic.test.ts:64:15
+     ❯ basic.test.ts:56:2
+
+    ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯[6/6]⎯
 
     "
   `)
@@ -93,6 +135,12 @@ test('vi.helper hides internal stack traces', async () => {
       "basic.test.ts": {
         "async": [
           "expected 'left' to deeply equal 'right'",
+        ],
+        "manual": [
+          "expected 'manual' to deeply equal 'no'",
+        ],
+        "manual async": [
+          "expected 'manual async' to deeply equal 'no'",
         ],
         "soft": [
           "expected 'left' to deeply equal 'right'",
