38821f35f8f63f839c7dbef781dc8428501af58c
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-02-06 09:19:11 +0900

    test: wip

--- test/browser/fixtures/assertion-helper/basic.test.ts
+++ test/browser/fixtures/assertion-helper/basic.test.ts
@@ -1,6 +1,6 @@
 import { expect, test, vi } from 'vitest'
 
-// copy of
+// based on
 // test/cli/fixtures/stacktraces-helper
 
 const myEqual = vi.helper((a: any, b: any) => {
@@ -22,60 +22,60 @@ const myEqualSoftAsync = vi.helper(async (a: any, b: any) => {
 })
 
 test('sync', () => {
-  myEqual('left', 'right')
+  myEqual('sync', 'x')
 })
 
 test('async', async () => {
-  await myEqualAsync('left', 'right')
+  await myEqualAsync('async', 'x')
 })
 
 test('soft', () => {
-  myEqualSoft('left', 'right')
+  myEqualSoft('soft', 'x')
 })
 
 test('soft async', async () => {
-  await myEqualSoftAsync('left', 'right')
+  await myEqualSoftAsync('soft async', 'x')
 })
 
 // debug
 
-const manual = helper((a: any, b: any) => {
-  expect(a).toEqual(b)
-})
+// const manual = helper((a: any, b: any) => {
+//   expect(a).toEqual(b)
+// })
 
-const manualAsync = helper(async (a: any, b: any) => {
-  await new Promise(r => setTimeout(r, 1))
-  expect(a).toEqual(b)
-})
+// const manualAsync = helper(async (a: any, b: any) => {
+//   await new Promise(r => setTimeout(r, 1))
+//   expect(a).toEqual(b)
+// })
 
-test("manual", () => {
-  manual('manual', 'no')
-})
+// test("manual", () => {
+//   manual('manual', 'no')
+// })
 
-test("manual async", async () => {
-  await manualAsync('manual async', 'no')
-})
+// test("manual async", async () => {
+//   await manualAsync('manual async', 'no')
+// })
 
-function helper<F extends (...args: any) => any>(fn: F): F {
-  function __MANUAL_HELPER__(...args: any[]): any {
-    const result = fn(...args);
-    if (result && typeof result === "object" && typeof result.then === "function") {
-      return (async function __MANUAL_HELPER_ASYNC__() {
-        return await result;
-      })();
-    }
-    return result;
-  }
-  return __MANUAL_HELPER__ as F;
-}
+// function helper<F extends (...args: any) => any>(fn: F): F {
+//   function __MANUAL_HELPER__(...args: any[]): any {
+//     const result = fn(...args);
+//     if (result && typeof result === "object" && typeof result.then === "function") {
+//       return (async function __MANUAL_HELPER_ASYNC__() {
+//         return await result;
+//       })();
+//     }
+//     return result;
+//   }
+//   return __MANUAL_HELPER__ as F;
+// }
 
-test('debug', async () => {
-  const debugAsync = helper(async () => {
-    await new Promise(r => setTimeout(r, 1))
-    console.error(new Error("foo").stack)
-  })
-  await debugAsync()
-})
+// test('debug', async () => {
+//   const debugAsync = helper(async () => {
+//     await new Promise(r => setTimeout(r, 1))
+//     console.error("++++++++++++++++++\n" + new Error("foo").stack + "\n++++++++++++++++++")
+//   })
+//   await debugAsync()
+// })
 
 // TODO:
 // - non builtin assertion
--- test/browser/specs/assertion-helper.test.ts
+++ test/browser/specs/assertion-helper.test.ts
@@ -6,157 +6,21 @@ test('vi.helper hides internal stack traces', async () => {
     root: './fixtures/assertion-helper',
   })
 
-  expect(stderr).toMatchInlineSnapshot(`
-    "stderr | basic.test.ts > debug
-    Error: foo
-        at http://127.0.0.1:63315/home/hiroshi/code/others/vitest/test/browser/fixtures/assertion-helper/basic.test.ts?import&browserv=1770284597581:56:19
-        at async __MANUAL_HELPER_ASYNC__ (http://127.0.0.1:63315/home/hiroshi/code/others/vitest/test/browser/fixtures/assertion-helper/basic.test.ts?import&browserv=1770284597581:46:16)
-        at async http://127.0.0.1:63315/home/hiroshi/code/others/vitest/test/browser/fixtures/assertion-helper/basic.test.ts?import&browserv=1770284597581:58:3
-        at async http://127.0.0.1:63315/@fs/home/hiroshi/code/others/vitest/packages/runner/dist/index.js:1225:20
-
-    ⎯⎯⎯⎯⎯⎯⎯ Failed Tests 6 ⎯⎯⎯⎯⎯⎯⎯
-
-     FAIL  |chromium| basic.test.ts > sync
-    AssertionError: expected 'left' to deeply equal 'right'
-
-    Failure screenshot:
-      - fixtures/assertion-helper/__screenshots__/basic.test.ts/sync-1.png
-
-    Expected: "right"
-    Received: "left"
-
-     ❯ basic.test.ts:25:2
-         23| 
-         24| test('sync', () => {
-         25|   myEqual('left', 'right')
-           |  ^
-         26| })
-         27| 
-
-    ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯[1/6]⎯
-
-     FAIL  |chromium| basic.test.ts > async
-    AssertionError: expected 'left' to deeply equal 'right'
-
-    Failure screenshot:
-      - fixtures/assertion-helper/__screenshots__/basic.test.ts/async-1.png
-
-    Expected: "right"
-    Received: "left"
-
-     ❯ basic.test.ts:29:2
-         27| 
-         28| test('async', async () => {
-         29|   await myEqualAsync('left', 'right')
-           |  ^
-         30| })
-         31| 
-
-    ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯[2/6]⎯
-
-     FAIL  |chromium| basic.test.ts > soft
-    AssertionError: expected 'left' to deeply equal 'right'
-
-    Failure screenshot:
-      - fixtures/assertion-helper/__screenshots__/basic.test.ts/soft-1.png
-
-    Expected: "right"
-    Received: "left"
-
-     ❯ basic.test.ts:33:2
-         31| 
-         32| test('soft', () => {
-         33|   myEqualSoft('left', 'right')
-           |  ^
-         34| })
-         35| 
-
-    ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯[3/6]⎯
-
-     FAIL  |chromium| basic.test.ts > soft async
-    AssertionError: expected 'left' to deeply equal 'right'
-
-    Failure screenshot:
-      - fixtures/assertion-helper/__screenshots__/basic.test.ts/soft-async-1.png
-
-    Expected: "right"
-    Received: "left"
-
-     ❯ basic.test.ts:37:2
-         35| 
-         36| test('soft async', async () => {
-         37|   await myEqualSoftAsync('left', 'right')
-           |  ^
-         38| })
-         39| 
-
-    ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯[4/6]⎯
-
-     FAIL  |chromium| basic.test.ts > manual
-    AssertionError: expected 'manual' to deeply equal 'no'
-
-    Failure screenshot:
-      - fixtures/assertion-helper/__screenshots__/basic.test.ts/manual-1.png
-
-    Expected: "no"
-    Received: "manual"
-
-     ❯ basic.test.ts:43:12
-         41| 
-         42| const manual = helper((a: any, b: any) => {
-         43|   expect(a).toEqual(b)
-           |            ^
-         44| })
-         45| 
-     ❯ __MANUAL_HELPER__ basic.test.ts:61:19
-     ❯ basic.test.ts:52:2
-
-    ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯[5/6]⎯
-
-     FAIL  |chromium| basic.test.ts > manual async
-    AssertionError: expected 'manual async' to deeply equal 'no'
-
-    Failure screenshot:
-      - fixtures/assertion-helper/__screenshots__/basic.test.ts/manual-async-1.png
-
-    Expected: "no"
-    Received: "manual async"
-
-     ❯ basic.test.ts:48:12
-         46| const manualAsync = helper(async (a: any, b: any) => {
-         47|   await new Promise(r => setTimeout(r, 1))
-         48|   expect(a).toEqual(b)
-           |            ^
-         49| })
-         50| 
-     ❯ __MANUAL_HELPER_ASYNC__ basic.test.ts:64:15
-     ❯ basic.test.ts:56:2
-
-    ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯[6/6]⎯
-
-    "
-  `)
+  expect(stderr).not.toBe('')
   expect(errorTree()).toMatchInlineSnapshot(`
     {
       "basic.test.ts": {
         "async": [
-          "expected 'left' to deeply equal 'right'",
-        ],
-        "debug": "passed",
-        "manual": [
-          "expected 'manual' to deeply equal 'no'",
-        ],
-        "manual async": [
-          "expected 'manual async' to deeply equal 'no'",
+          "expected 'async' to deeply equal 'x'",
         ],
         "soft": [
-          "expected 'left' to deeply equal 'right'",
+          "expected 'soft' to deeply equal 'x'",
         ],
         "soft async": [
-          "expected 'left' to deeply equal 'right'",
+          "expected 'soft async' to deeply equal 'x'",
         ],
         "sync": [
-          "expected 'left' to deeply equal 'right'",
+          "expected 'sync' to deeply equal 'x'",
         ],
       },
     }
