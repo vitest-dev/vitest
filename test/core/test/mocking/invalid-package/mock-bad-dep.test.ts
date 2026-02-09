5ac4afd7631371d5e0adc882d7c48cd754cbfe15
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-02-09 19:53:01 +0900

    chore: lint

--- test/core/test/mocking/invalid-package/mock-bad-dep.test.ts
+++ test/core/test/mocking/invalid-package/mock-bad-dep.test.ts
@@ -1,6 +1,5 @@
 // @ts-expect-error no type
 import * as dep from '@vitest/test-dep-invalid'
-
 import { expect, test, vi } from 'vitest'
 
 vi.mock('@vitest/test-dep-invalid', () => ({ hi: 'yo' }))
