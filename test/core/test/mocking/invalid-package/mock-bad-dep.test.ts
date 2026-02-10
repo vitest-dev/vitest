742586107845cddbb831f11cbf85a74ffdc0f3cb
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-02-10 13:43:55 +0900

    chore: remove stale

--- test/core/test/mocking/invalid-package/mock-bad-dep.test.ts
+++
@@ -1,9 +0,0 @@
-// @ts-expect-error no type
-import * as dep from 'test-dep-invalid'
-import { expect, test, vi } from 'vitest'
-
-vi.mock('test-dep-invalid', () => ({ mocked: 'ok' }))
-
-test('repro', () => {
-  expect(dep).toMatchObject({ mocked: 'ok' })
-})
--- test/core/test/mocking/invalid-package/wrapper.ts
+++
@@ -1,4 +0,0 @@
-// @ts-expect-error no type
-import * as dep from 'test-dep-invalid'
-
-export const hello = () => dep
