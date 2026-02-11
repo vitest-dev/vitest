794b3d4369fbe4fc4111efe0003de7de2fb3b218
22mb <249426134+22mb@users.noreply.github.com>
2026-02-11 09:40:45 +0900

    test: add non-browser test for mocking with spaces in path
    Add a core test case where `vi.mock()` caller is in a directory with spaces.
    Also update test titles to reflect the actual issue.

--- test/browser/fixtures/mocking/space dir/mocked-spaced-dir.test.ts
+++ test/browser/fixtures/mocking/space dir/mocked-spaced-dir.test.ts
@@ -3,6 +3,6 @@ import { testConst } from './module.js'
 
 vi.mock('./module.ts', () => ({ testConst: 20 }))
 
-test('module with space in directory path is mocked', () => {
+test('can mock modules when vi.mock caller is inside a directory with spaces', () => {
   expect(testConst).toBe(20)
 })
