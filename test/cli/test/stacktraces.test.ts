8a8391ed86297613633d1679518693b1bf67a178
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-02-17 12:43:46 +0900

    test: trailing space

--- test/cli/test/stacktraces.test.ts
+++ test/cli/test/stacktraces.test.ts
@@ -262,12 +262,12 @@ it('resolves/rejects', async () => {
      FAIL  repro.test.ts > rejects: resolves when rejection expected
     AssertionError: promise resolved "3" instead of rejecting
 
-    - Expected:
+    - Expected: 
     Error {
       "message": "rejected promise",
     }
 
-    + Received:
+    + Received: 
     3
 
      ❯ repro.test.ts:13:40
