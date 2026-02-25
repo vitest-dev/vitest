8c517d3ff9ef866b0cdab9ef9eedfec565ff7aaa
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-02-25 14:29:56 +0900

    test: wrong coverage with oxc decorators

--- test/coverage-test/test/decorators.test.ts
+++ test/coverage-test/test/decorators.test.ts
@@ -1,8 +1,9 @@
-import { expect } from 'vitest'
+import { expect, vi } from 'vitest'
+import { rolldownVersion } from 'vitest/node'
 import { DecoratorsTester } from '../fixtures/src/decorators'
 import { coverageTest, normalizeURL, readCoverageMap, runVitest, test } from '../utils'
 
-test('decorators generated metadata is ignored', async () => {
+test('decorators generated metadata is ignored', async ({ onTestFinished }) => {
   await runVitest({
     include: [normalizeURL(import.meta.url)],
     config: 'fixtures/configs/vitest.config.decorators.ts',
@@ -14,14 +15,41 @@ test('decorators generated metadata is ignored', async () => {
   const lineCoverage = fileCoverage.getLineCoverage()
   const branchCoverage = fileCoverage.getBranchCoverageByLine()
 
-  expect(lineCoverage['4']).toBeUndefined()
-  expect(branchCoverage['4']).toBeUndefined()
+  expect.soft(lineCoverage['4']).toBeUndefined()
+  expect.soft(branchCoverage['4']).toBeUndefined()
 
   // Covered branch should be marked correctly
-  expect(lineCoverage['7']).toBe(1)
+  expect.soft(lineCoverage['7']).toBe(1)
 
   // Uncovered branch should be marked correctly
-  expect(lineCoverage['12']).toBe(0)
+  expect.soft(lineCoverage['12']).toBe(0)
+
+  if (rolldownVersion) {
+    vi.stubEnv('TEST_OXC_DECORATOR', 'true')
+    onTestFinished(() => {
+      vi.unstubAllEnvs()
+    })
+
+    await runVitest({
+      include: [normalizeURL(import.meta.url)],
+      config: 'fixtures/configs/vitest.config.decorators.ts',
+      coverage: { reporter: ['json', 'html'] },
+    })
+
+    const coverageMap = await readCoverageMap()
+    const fileCoverage = coverageMap.fileCoverageFor('<process-cwd>/fixtures/src/decorators.ts')
+    const lineCoverage = fileCoverage.getLineCoverage()
+    const branchCoverage = fileCoverage.getBranchCoverageByLine()
+
+    expect.soft(lineCoverage['4']).toBeUndefined()
+    expect.soft(branchCoverage['4']).toBeUndefined()
+
+    // Covered branch should be marked correctly
+    expect.soft(lineCoverage['7']).toBe(1)
+
+    // Uncovered branch should be marked correctly
+    expect.soft(lineCoverage['12']).toBe(0)
+  }
 })
 
 coverageTest('cover decorators', () => {
