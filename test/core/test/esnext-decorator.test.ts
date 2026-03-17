92ffc0ebeef5f93f222a0c5d47e11dd4400b0a07
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-03-17 17:24:40 +0900

    test: adjust unstable decorator use

--- test/core/test/esnext-decorator.test.ts
+++ test/core/test/esnext-decorator.test.ts
@@ -1,19 +1,17 @@
 import { expect, it } from 'vitest'
-import { rolldownVersion } from 'vitest/node'
 
 it('decorators work', () => {
-  // babel decorator seems to have a bug in class static and decorator ordering
-  // https://github.com/babel/babel/issues/17875
-  expect(Sut.mocked).toBe(!rolldownVersion)
-  expect((Sut as any).mockedEasy).toBe(true)
+  expect(Sut.mocked).toBe(true)
   expect(new Sut()).toBeInstanceOf(Sut)
 })
 function exampleDecorator(ClassExample: any, context: ClassDecoratorContext): any {
   if (context.kind !== 'class') {
     throw new Error('not a class to decorate')
   }
-  ClassExample.mocked = true
-  ClassExample.mockedEasy = true
+  // https://github.com/babel/babel/issues/17875#issuecomment-4072950690
+  context.addInitializer(() => {
+    ClassExample.mocked = true
+  })
   return ClassExample
 }
 
