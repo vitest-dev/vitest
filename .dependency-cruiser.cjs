174d9c8a02d402b7b64eeea371d7d1d2adc3f4b6
池下 克彦 <kik@nvstl.com>
2026-03-28 17:08:59 +0900

    chore: drop deprecated core depcruise rule

--- .dependency-cruiser.cjs
+++ .dependency-cruiser.cjs
@@ -139,41 +139,6 @@ module.exports = {
       to: {},
     },
     {
-      name: 'no-deprecated-core-in-dist',
-      comment:
-        'A module depends on a node core module that has been deprecated. Find an alternative - these are '
-        + 'bound to exist - node doesn\'t deprecate lightly.',
-      severity: 'warn',
-      from: { path: distPaths },
-      to: {
-        dependencyTypes: [
-          'core',
-        ],
-        path: [
-          '^v8/tools/codemap$',
-          '^v8/tools/consarray$',
-          '^v8/tools/csvparser$',
-          '^v8/tools/logreader$',
-          '^v8/tools/profile_view$',
-          '^v8/tools/profile$',
-          '^v8/tools/SourceMap$',
-          '^v8/tools/splaytree$',
-          '^v8/tools/tickprocessor-driver$',
-          '^v8/tools/tickprocessor$',
-          '^node-inspect/lib/_inspect$',
-          '^node-inspect/lib/internal/inspect_client$',
-          '^node-inspect/lib/internal/inspect_repl$',
-          '^async_hooks$',
-          '^punycode$',
-          '^domain$',
-          '^constants$',
-          '^sys$',
-          '^_linklist$',
-          '^_stream_wrap$',
-        ],
-      },
-    },
-    {
       name: 'not-to-deprecated-in-dist',
       comment:
         'This module uses a (version of an) npm module that has been deprecated. Either upgrade to a later '
