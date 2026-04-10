ab5c12abca5ae69d9ed43ec49e4be45e69e5c30a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-04-10 16:35:21 +0900

    test: other providers in packages/ui

--- packages/ui/vitest.config.ts
+++ packages/ui/vitest.config.ts
@@ -1,23 +1,42 @@
 import { playwright } from '@vitest/browser-playwright'
+import { preview } from '@vitest/browser-preview'
+import { webdriverio } from '@vitest/browser-webdriverio'
 import { mergeConfig } from 'vite'
 import { defineConfig } from 'vitest/config'
 import viteConfig from './vite.config'
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    optimizeDeps: {
-      include: ['vue-router', 'splitpanes', 'd3-graph-controller', 'vue-virtual-scroller', 'rrweb-snapshot'],
+// only playwright works so far
+const providerName = (process.env.PROVIDER || 'playwright') as
+  | 'playwright'
+  | 'webdriverio'
+  | 'preview'
+
+const testConfig = defineConfig({
+  optimizeDeps: {
+    include: [
+      'vue-router',
+      'splitpanes',
+      'd3-graph-controller',
+      'vue-virtual-scroller',
+      'rrweb-snapshot',
+    ],
+  },
+  test: {
+    browser: {
+      enabled: true,
+      traceView: true,
+      headless: providerName !== 'preview',
+      provider:
+        providerName === 'preview'
+          ? preview()
+          : providerName === 'webdriverio'
+            ? webdriverio()
+            : playwright({
+                actionTimeout: 5000,
+              }),
+      instances: [providerName === 'webdriverio' ? { browser: 'chrome' } : { browser: 'chromium' }],
     },
-    test: {
-      browser: {
-        enabled: true,
-        traceView: true,
-        provider: playwright({
-          actionTimeout: 5000,
-        }),
-        instances: [{ browser: 'chromium' }],
-      },
-    },
-  }),
-)
+  },
+})
+
+export default mergeConfig(viteConfig, testConfig)
