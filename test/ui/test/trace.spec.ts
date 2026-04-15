f93229ee24d49b55e01463b45f1ac02c3776930b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-04-15 17:23:30 +0900

    test: e2e html reporter

--- test/ui/test/trace.spec.ts
+++ test/ui/test/trace.spec.ts
@@ -1,6 +1,9 @@
+import type { PreviewServer } from 'vite'
 import type { Vitest } from 'vitest/node'
 import assert from 'node:assert'
+import { Writable } from 'node:stream'
 import { expect, test } from '@playwright/test'
+import { preview } from 'vite'
 import { startVitest } from 'vitest/node'
 
 // TODO: reuse same test cases for HTML reporter + preview
@@ -9,6 +12,9 @@ test.describe('ui', () => {
   let baseURL: string
 
   test.beforeAll(async () => {
+    // silence Vitest logs
+    const stdout = new Writable({ write: (_, __, callback) => callback() })
+    const stderr = new Writable({ write: (_, __, callback) => callback() })
     vitest = await startVitest(
       'test',
       undefined,
@@ -17,24 +23,9 @@ test.describe('ui', () => {
         watch: true,
         ui: true,
         open: false,
-        reporters: [
-          {
-            // reporter to surface minimal vitest side info
-            onTestRunStart() {
-              // eslint-disable-next-line no-console
-              console.log('[fixtures-trace:onTestRunStart]')
-            },
-            onTestRunEnd(testModules, unhandledErrors, reason) {
-              // eslint-disable-next-line no-console
-              console.log('[fixtures-trace:onTestRunEnd]', {
-                testModules: testModules.length,
-                unhandledErrors: unhandledErrors.length,
-                reason,
-              })
-            },
-          },
-        ],
       },
+      {},
+      { stdout, stderr },
     )
     const address = vitest.vite.httpServer?.address()
     assert(address && typeof address === 'object', 'Invalid server address')
@@ -47,6 +38,45 @@ test.describe('ui', () => {
 
   test('basic', async ({ page }) => {
     await page.goto(baseURL)
-    await expect(page.getByTestId('tests-entry')).toContainText('11 Pass 2 Fail 13 Total')
+    await expect.soft(page.getByTestId('tests-entry')).toContainText('11 Pass 2 Fail 13 Total')
+  })
+})
+
+test.describe('html reporter', () => {
+  let previewServer: PreviewServer
+  let baseURL: string
+
+  test.beforeAll(async () => {
+    // silence Vitest logs
+    const stdout = new Writable({ write: (_, __, callback) => callback() })
+    const stderr = new Writable({ write: (_, __, callback) => callback() })
+    await startVitest(
+      'test',
+      undefined,
+      {
+        root: './fixtures-trace',
+        run: true,
+        ui: false,
+        reporters: 'html',
+      },
+      {},
+      { stdout, stderr },
+    )
+    previewServer = await preview({
+      root: './fixtures-trace',
+      build: { outDir: 'html' },
+    })
+    const address = previewServer.httpServer?.address()
+    assert(address && typeof address === 'object', 'Invalid server address')
+    baseURL = `http://localhost:${address.port}/`
+  })
+
+  test.afterAll(async () => {
+    await previewServer.close()
+  })
+
+  test('basic', async ({ page }) => {
+    await page.goto(baseURL)
+    await expect.soft(page.getByTestId('tests-entry')).toContainText('11 Pass 2 Fail 13 Total')
   })
 })
