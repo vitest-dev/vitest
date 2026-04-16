58fd527a2bc8abb880561a0c23ce5e62bcfd8797
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-04-16 19:19:25 +0900

    chore: comment

--- test/browser/fixtures/trace/vitest.config.ts
+++ test/browser/fixtures/trace/vitest.config.ts
@@ -3,6 +3,8 @@ import { instances, provider } from '../../settings'
 
 // TEST_BROWSER=chromium pnpm -C test/browser test-fixtures --root fixtures/trace
 // TEST_BROWSER=chromium pnpm -C test/browser test-fixtures --root fixtures/trace --ui --browser.headless
+// TEST_BROWSER=chromium pnpm -C test/browser test-fixtures --root fixtures/trace --reporter=html --browser.headless --run
+// pnpm dlx serve test/browser/fixtures/trace/html
 export default defineConfig({
   test: {
     browser: {
