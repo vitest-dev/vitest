744e27631ecf5d8f202de3c791973419795ebca8
Raul Macarie <raul@macarie.me>
2026-04-30 12:06:06 +0200

    chore: reorganize tests

--- test/core/test/mocking/vi-when.test.ts
+++ test/core/test/mocking/vi-when.test.ts
@@ -6,7 +6,7 @@ vi.mock('../../src/mockedA')
 interface FnData {
   args: [a: string, b: number]
   value: number
-  entries: [FnData['args'], FnData['value']][]
+  entries: { args: FnData['args']; value: FnData['value'] }[]
 }
 
 type Fn = (...args: FnData['args']) => FnData['value']
@@ -16,265 +16,355 @@ describe('vi.when()', () => {
     vi.restoreAllMocks()
   })
 
-  test('matches arguments', () => {
-    const spy = vi.fn<Fn>()
+  describe('basic usage', () => {
+    test('returns provided value when arguments match', () => {
+      const spy = vi.fn<Fn>()
 
-    const entries: FnData['entries'] = [
-      [['a', 0], 97],
-      [['b', 1], 99],
-    ]
+      const entries: FnData['entries'] = [
+        { args: ['a', 0], value: 97 },
+        { args: ['b', 1], value: 99 },
+      ]
 
-    vi.when(spy)
-      .calledWith(...entries[0][0])
-      .thenReturn(entries[0][1])
-      .calledWith(...entries[1][0])
-      .thenReturn(entries[1][1])
+      const w = vi.when(spy)
+        .calledWith(...entries[0].args)
+        .thenReturn(entries[0].value)
+        .calledWith(...entries[1].args)
+        .thenReturn(entries[1].value)
 
-    expect(spy(...entries[0][0])).toBe(entries[0][1])
+      expect(w.isExhausted()).toBe(false)
 
-    expect(spy).toHaveBeenLastCalledWith(...entries[0][0])
-    expect(spy).toHaveLastReturnedWith(entries[0][1])
+      expect(spy(...entries[0].args)).toBe(entries[0].value)
 
-    expect(spy(...entries[1][0])).toBe(entries[1][1])
+      expect(spy).toHaveBeenLastCalledWith(...entries[0].args)
+      expect(spy).toHaveLastReturnedWith(entries[0].value)
 
-    expect(spy).toHaveBeenLastCalledWith(...entries[1][0])
-    expect(spy).toHaveLastReturnedWith(entries[1][1])
+      expect(w.isExhausted()).toBe(false)
 
-    expect(spy).toHaveBeenCalledTimes(2)
-  })
+      expect(spy(...entries[1].args)).toBe(entries[1].value)
 
-  test('falls back to default implementation when not matching', () => {
-    const spy = vi.fn<Fn>((a, b) => b * a.charCodeAt(0))
+      expect(spy).toHaveBeenLastCalledWith(...entries[1].args)
+      expect(spy).toHaveLastReturnedWith(entries[1].value)
 
-    const args: FnData['args'] = ['a', 0]
-    const value: FnData['value'] = 97
+      expect(spy).toHaveBeenCalledTimes(2)
 
-    vi.when(spy)
-      .calledWith(...args)
-      .thenReturn(value)
+      expect(w.isExhausted()).toBe(true)
+    })
 
-    expect(spy('b', 1)).toBe(98)
+    test('falls back to default implementation when arguments don\'t match', () => {
+      const spy = vi.fn<Fn>((a, b) => b * a.charCodeAt(0))
 
-    expect(spy).not.toHaveBeenLastCalledWith(...args)
-    expect(spy).not.toHaveLastReturnedWith(value)
+      const args: FnData['args'] = ['a', 0]
+      const value: FnData['value'] = 97
 
-    expect(spy).toHaveBeenCalledOnce()
-  })
-
-  test('supports once matchers', () => {
-    const spy = vi.fn<Fn>((a, b) => b * a.charCodeAt(0))
-
-    const args: FnData['args'] = ['a', 0]
-    const values: FnData['value'][] = [
-      97,
-      98,
-    ]
-
-    vi.when(spy)
-      .calledWith(...args)
-      .thenReturn(values[0])
-      .calledWith(...args)
-      .thenReturnOnce(values[1])
-
-    expect(spy(...args)).toBe(values[1])
-
-    expect(spy).toHaveBeenLastCalledWith(...args)
-    expect(spy).toHaveLastReturnedWith(values[1])
-
-    expect(spy(...args)).toBe(values[0])
-
-    expect(spy).toHaveBeenLastCalledWith(...args)
-    expect(spy).toHaveLastReturnedWith(values[0])
-
-    expect(spy).toHaveBeenCalledTimes(2)
-  })
-
-  test('resolves matchers right to left', () => {
-    const spy = vi.fn<Fn>()
-
-    const args: FnData['args'] = ['a', 0]
-    const values: FnData['value'][] = [
-      97,
-      98,
-      99,
-    ]
-
-    vi.when(spy)
-      .calledWith(...args)
-      .thenReturn(values[0])
-      .calledWith(...args)
-      .thenReturnOnce(values[1])
-      .calledWith(...args)
-      .thenReturn(values[2])
-
-    expect(spy(...args)).toBe(values[2])
-
-    expect(spy).toHaveBeenLastCalledWith(...args)
-    expect(spy).toHaveLastReturnedWith(values[2])
-
-    expect(spy(...args)).toBe(values[2])
-
-    expect(spy).toHaveBeenLastCalledWith(...args)
-    expect(spy).toHaveLastReturnedWith(values[2])
-
-    expect(spy(...args)).toBe(values[2])
-
-    expect(spy).toHaveBeenLastCalledWith(...args)
-    expect(spy).toHaveLastReturnedWith(values[2])
-
-    expect(spy).toHaveBeenCalledTimes(3)
-  })
-
-  test('throws when using toThrow', () => {
-    const spy = vi.fn<Fn>()
-
-    const args: FnData['args'] = ['a', 0]
-    const error = new TypeError('Expected second argument > 0')
-
-    vi.when(spy)
-      .calledWith(...args)
-      .thenThrow(error)
-
-    expect(() => spy(...args)).toThrow(error)
-
-    expect(spy).toHaveBeenLastCalledWith(...args)
-
-    expect(spy).toHaveBeenCalledOnce()
-  })
-
-  test('resolves a promise when using toResolve', async () => {
-    const spy = vi.fn<Fn>()
-
-    const args: FnData['args'] = ['a', 0]
-    const value: FnData['value'] = 97
-
-    vi.when(spy)
-      .calledWith(...args)
-      .thenResolve(value)
-
-    await expect(spy(...args)).resolves.toBe(value)
-
-    expect(spy).toHaveBeenLastCalledWith(...args)
-    expect(spy).toHaveLastResolvedWith(value)
-
-    expect(spy).toHaveBeenCalledOnce()
-  })
-
-  test('rejects a promise when using toReject', async () => {
-    const spy = vi.fn<Fn>()
-
-    const args: FnData['args'] = ['a', 0]
-    const error = new TypeError('Expected second argument > 0')
-
-    vi.when(spy)
-      .calledWith(...args)
-      .thenReject(error)
-
-    await expect(spy(...args)).rejects.toThrow(error)
-
-    expect(spy).toHaveBeenLastCalledWith(...args)
-
-    expect(spy).toHaveBeenCalledOnce()
-  })
-
-  test('works with asymmetric matchers', () => {
-    const spy = vi.fn<Fn>(() => Number.NaN)
-
-    const args: FnData['args'] = [expect.stringContaining('--'), expect.any(Number)]
-    const value: FnData['value'] = 0
-
-    vi.when(spy)
-      .calledWith(...args)
-      .thenReturn(value)
-
-    expect(spy('a--z', Number.NEGATIVE_INFINITY)).toBe(value)
-    expect(spy('z--a', Number.POSITIVE_INFINITY)).toBe(value)
-
-    expect(spy('a__z', Number.NEGATIVE_INFINITY)).toBe(Number.NaN)
-    expect(spy('z__a', Number.NEGATIVE_INFINITY)).toBe(Number.NaN)
-  })
-
-  test('groups asymmetric matchers correctly', () => {
-    const spy = vi.fn<Fn>(() => Number.NaN)
-
-    const value: FnData['value'] = 0
-    const once: FnData['value'] = 1
-
-    vi.when(spy)
-      .calledWith(expect.stringContaining('--'), expect.any(Number))
-      .thenReturn(value)
-      .calledWith(expect.stringContaining('--'), expect.any(Number))
-      .thenReturnOnce(once)
-
-    expect(spy('a--z', 0)).toBe(once)
-    expect(spy('a--z', 0)).toBe(value)
-  })
-
-  test('works with vi.mock()', () => {
-    expect(a.mockedA()).toBe(undefined)
-
-    vi.when(a.mockedA).calledWith().thenReturnOnce('B')
-
-    expect(a.mockedA()).toBe('B')
-
-    expect(a.mockedA()).toBe(undefined)
-  })
-
-  test('works with vi.spyOn()', () => {
-    const message = 'ResizeObserver loop completed with undelivered notifications.'
-    const value = 'not undefined'
-
-    vi.when(vi.spyOn(console, 'error'))
-      .calledWith(
-        expect.objectContaining({ message }),
-      )
-      .thenReturn(value as never)
-
-    expect(console.error({ message })).toBe(value)
-    expect(console.error({ message: message.slice(0, 14) })).toBe(undefined)
-  })
-
-  test('works without having to re-declare arguments in a chain', () => {
-    const spy = vi.fn<Fn>()
-
-    const args: FnData['args'] = ['a', 0]
-    const values: FnData['value'][] = [
-      97,
-      98,
-    ]
-
-    vi.when(spy)
-      .calledWith(...args)
-      .thenReturn(values[0])
-      .thenReturnOnce(values[1])
-
-    expect(spy(...args)).toBe(values[1])
-
-    expect(spy).toHaveBeenLastCalledWith(...args)
-    expect(spy).toHaveLastReturnedWith(values[1])
-
-    expect(spy(...args)).toBe(values[0])
-
-    expect(spy).toHaveBeenLastCalledWith(...args)
-    expect(spy).toHaveLastReturnedWith(values[0])
-
-    expect(spy).toHaveBeenCalledTimes(2)
-  })
-
-  test.runIf(Symbol.dispose)('disposes of its mock', () => {
-    const spy = vi.fn<Fn>((a, b) => b * a.charCodeAt(0))
-
-    const args: FnData['args'] = ['a', 0]
-    const value: FnData['value'] = 97
-
-    {
-      using _ = vi.when(spy)
+      const w = vi.when(spy)
         .calledWith(...args)
         .thenReturn(value)
 
-      expect(spy(...args)).toBe(value)
-    }
+      expect(w.isExhausted()).toBe(false)
 
-    expect(spy(...args)).toBe(0)
+      expect(spy('b', 1)).toBe(98)
+
+      expect(spy).not.toHaveBeenLastCalledWith(...args)
+      expect(spy).not.toHaveLastReturnedWith(value)
+
+      expect(spy).toHaveBeenCalledOnce()
+
+      expect(w.isExhausted()).toBe(false)
+    })
+
+    test('returns provided value when arguments match using asymmetric matchers', () => {
+      const spy = vi.fn<Fn>(() => Number.NaN)
+
+      const args: FnData['args'] = [expect.stringContaining('--'), expect.any(Number)]
+      const value: FnData['value'] = 0
+
+      const w = vi.when(spy)
+        .calledWith(...args)
+        .thenReturn(value)
+
+      expect(w.isExhausted()).toBe(false)
+
+      expect(spy('a--z', Number.NEGATIVE_INFINITY)).toBe(value)
+      expect(spy('z--a', Number.POSITIVE_INFINITY)).toBe(value)
+
+      expect(w.isExhausted()).toBe(true)
+
+      expect(spy('a__z', Number.NEGATIVE_INFINITY)).toBe(Number.NaN)
+      expect(spy('z__a', Number.NEGATIVE_INFINITY)).toBe(Number.NaN)
+    })
+
+    test('throws when using `toThrow`', () => {
+      const spy = vi.fn<Fn>()
+
+      const args: FnData['args'] = ['a', 0]
+      const error = new TypeError('Expected second argument > 0')
+
+      const w = vi.when(spy)
+        .calledWith(...args)
+        .thenThrow(error)
+
+      expect(w.isExhausted()).toBe(false)
+
+      expect(() => spy(...args)).toThrow(error)
+
+      expect(spy).toHaveBeenLastCalledWith(...args)
+
+      expect(spy).toHaveBeenCalledOnce()
+
+      expect(w.isExhausted()).toBe(true)
+    })
+
+    test('resolves a promise when using `toResolve`', async () => {
+      const spy = vi.fn<Fn>()
+
+      const args: FnData['args'] = ['a', 0]
+      const value: FnData['value'] = 97
+
+      const w = vi.when(spy)
+        .calledWith(...args)
+        .thenResolve(value)
+
+      expect(w.isExhausted()).toBe(false)
+
+      await expect(spy(...args)).resolves.toBe(value)
+
+      expect(spy).toHaveBeenLastCalledWith(...args)
+      expect(spy).toHaveLastResolvedWith(value)
+
+      expect(spy).toHaveBeenCalledOnce()
+
+      expect(w.isExhausted()).toBe(true)
+    })
+
+    test('rejects a promise when using `toReject`', async () => {
+      const spy = vi.fn<Fn>()
+
+      const args: FnData['args'] = ['a', 0]
+      const error = new TypeError('Expected second argument > 0')
+
+      const w = vi.when(spy)
+        .calledWith(...args)
+        .thenReject(error)
+
+      expect(w.isExhausted()).toBe(false)
+
+      await expect(spy(...args)).rejects.toThrow(error)
+
+      expect(spy).toHaveBeenLastCalledWith(...args)
+
+      expect(spy).toHaveBeenCalledOnce()
+
+      expect(w.isExhausted()).toBe(true)
+    })
+
+    test.runIf(Symbol.dispose)('disposes of its mock', () => {
+      const spy = vi.fn<Fn>((a, b) => b * a.charCodeAt(0))
+
+      const args: FnData['args'] = ['a', 0]
+      const value: FnData['value'] = 97
+
+      {
+        using w = vi.when(spy)
+          .calledWith(...args)
+          .thenReturn(value)
+
+        expect(w.isExhausted()).toBe(false)
+
+        expect(spy(...args)).toBe(value)
+        expect(w.isExhausted()).toBe(true)
+      }
+
+      expect(spy(...args)).toBe(0)
+    })
+  })
+
+  describe('exhausting behaviors', () => {
+    test('returns the provided value as described by the `times` option', () => {
+      const spy = vi.fn<Fn>()
+
+      const args: FnData['args'] = ['a', 0]
+      const values: FnData['value'][] = [
+        97,
+        98,
+      ]
+
+      const w = vi.when(spy)
+        .calledWith(...args)
+        .thenReturn(values[0])
+        .calledWith(...args)
+        .thenReturn(values[1], { times: 2 })
+
+      expect(w.isExhausted()).toBe(false)
+
+      expect(spy(...args)).toBe(values[1])
+      expect(w.isExhausted()).toBe(false)
+
+      expect(spy(...args)).toBe(values[1])
+      expect(w.isExhausted()).toBe(false)
+
+      expect(spy(...args)).toBe(values[0])
+      expect(w.isExhausted()).toBe(true)
+
+      expect(spy).toHaveBeenCalledTimes(3)
+    })
+
+    test('multiple behaviors can be chained on a single `calledWith` call', () => {
+      const spy = vi.fn<Fn>()
+
+      const args: FnData['args'] = ['a', 0]
+      const values: FnData['value'][] = [
+        97,
+        98,
+      ]
+
+      const w = vi.when(spy)
+        .calledWith(...args)
+        .thenReturn(values[0])
+        .thenReturn(values[1], { times: 2 })
+
+      expect(w.isExhausted()).toBe(false)
+
+      expect(spy(...args)).toBe(values[1])
+      expect(w.isExhausted()).toBe(false)
+
+      expect(spy(...args)).toBe(values[1])
+      expect(w.isExhausted()).toBe(false)
+
+      expect(spy(...args)).toBe(values[0])
+      expect(w.isExhausted()).toBe(true)
+
+      expect(spy).toHaveBeenCalledTimes(3)
+    })
+
+    test('groups behaviors based on asymmetric matchers correctly', () => {
+      const spy = vi.fn<Fn>(() => Number.NaN)
+
+      const value: FnData['value'] = 0
+      const once: FnData['value'] = 1
+
+      const w = vi.when(spy)
+        .calledWith(expect.stringContaining('--'), expect.any(Number))
+        .thenReturn(value)
+        .calledWith(expect.stringContaining('--'), expect.any(Number))
+        .thenReturnOnce(once)
+
+      expect(w.isExhausted()).toBe(false)
+
+      expect(spy('a--z', 0)).toBe(once)
+      expect(w.isExhausted()).toBe(false)
+
+      expect(spy('a--z', 1)).toBe(value)
+      expect(w.isExhausted()).toBe(true)
+    })
+
+    test('`*Once` behaviors are sugar syntax for `times: 1`', () => {
+      const spy = vi.fn<Fn>()
+
+      const args: FnData['args'] = ['a', 0]
+      const values: FnData['value'][] = [
+        97,
+        98,
+      ]
+
+      const w = vi.when(spy)
+        .calledWith(...args)
+        .thenReturn(values[0])
+        .thenReturnOnce(values[1])
+
+      expect(w.isExhausted()).toBe(false)
+
+      expect(spy(...args)).toBe(values[1])
+
+      expect(spy).toHaveBeenLastCalledWith(...args)
+      expect(spy).toHaveLastReturnedWith(values[1])
+
+      expect(w.isExhausted()).toBe(false)
+
+      expect(spy(...args)).toBe(values[0])
+
+      expect(spy).toHaveBeenLastCalledWith(...args)
+      expect(spy).toHaveLastReturnedWith(values[0])
+
+      expect(w.isExhausted()).toBe(true)
+
+      expect(spy).toHaveBeenCalledTimes(2)
+    })
+
+    test('resolves behaviors in a LIFO-style manner', () => {
+      const spy = vi.fn<Fn>()
+
+      const args: FnData['args'] = ['a', 0]
+      const values: FnData['value'][] = [
+        97,
+        98,
+        99,
+      ]
+
+      const w = vi.when(spy)
+        .calledWith(...args)
+        .thenReturn(values[0])
+        .thenReturnOnce(values[1])
+        .thenReturn(values[2])
+
+      expect(spy(...args)).toBe(values[2])
+
+      expect(spy).toHaveBeenLastCalledWith(...args)
+      expect(spy).toHaveLastReturnedWith(values[2])
+
+      expect(spy(...args)).toBe(values[2])
+
+      expect(spy).toHaveBeenLastCalledWith(...args)
+      expect(spy).toHaveLastReturnedWith(values[2])
+
+      expect(spy(...args)).toBe(values[2])
+
+      expect(spy).toHaveBeenLastCalledWith(...args)
+      expect(spy).toHaveLastReturnedWith(values[2])
+
+      expect(spy).toHaveBeenCalledTimes(3)
+
+      expect(w.isExhausted()).toBe(false) // still `false` as the first two behaviors cannot be reached
+    })
+  })
+
+  describe('module mocks and spies', () => {
+    test('works with vi.mock()', () => {
+      expect(a.mockedA()).toBe(undefined)
+
+      const w = vi.when(a.mockedA).calledWith().thenReturnOnce('B')
+
+      expect(w.isExhausted()).toBe(false)
+
+      expect(a.mockedA()).toBe('B')
+      expect(w.isExhausted()).toBe(true)
+
+      expect(a.mockedA()).toBe(undefined)
+    })
+
+    test('works with vi.spyOn()', () => {
+      const message = 'ResizeObserver loop completed with undelivered notifications.'
+      const value = 'not undefined'
+
+      const w = vi.when(vi.spyOn(console, 'error'))
+        .calledWith(expect.objectContaining({ message }))
+        .thenReturn(value as never)
+
+      expect(w.isExhausted()).toBe(false)
+
+      expect(console.error({ message })).toBe(value)
+      expect(w.isExhausted()).toBe(true)
+
+      expect(console.error({ message: message.slice(0, 14) })).toBe(undefined)
+    })
+  })
+
+  describe('edge cases', () => {
+    test('`isExhausted` returns false when no behaviors are registered', () => {
+      const spy = vi.fn<Fn>()
+
+      const w = vi.when(spy)
+
+      expect(w.isExhausted()).toBe(false)
+    })
   })
 })
