ca16126a9c9af95e02c10750b8ae06f20e64a9ec
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-05-01 15:15:49 +0900

    test: move `test/snapshots` to `test/e2e`

---
+++ test/e2e/README.md
@@ -0,0 +1,11 @@
+# e2e
+
+## snapshots
+
+```sh
+# run integration
+pnpm test --project=snapshots
+
+# run individual fixtures manually
+pnpm test --root snapshots/fixtures/domain
+```
--- test/e2e/vitest.config.ts
+++ test/e2e/vitest.config.ts
@@ -1,30 +1,52 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   test: {
-    include: ['test/**/**.{test,spec}.ts'],
-    includeTaskLocation: true,
     reporters: ['verbose'],
-    testTimeout: 60_000,
-    isolate: false,
-    fileParallelism: false,
-    // TODO: should enabled when support for older node is dropped?
-    // experimental: {
-    //   viteModuleRunner: false,
-    //   nodeLoader: false,
-    // },
-    chaiConfig: {
-      truncateThreshold: 999,
-    },
-    typecheck: {
-      enabled: true,
-      include: ['./test/reporters/configuration-options.test-d.ts'],
-    },
     onConsoleLog(log) {
       if (log.includes('watcher is ready')) {
         return false
       }
     },
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: 'main',
+          include: ['test/**/**.{test,spec}.ts'],
+          includeTaskLocation: true,
+          testTimeout: 60_000,
+          isolate: false,
+          fileParallelism: false,
+          // TODO: should enabled when support for older node is dropped?
+          // experimental: {
+          //   viteModuleRunner: false,
+          //   nodeLoader: false,
+          // },
+          chaiConfig: {
+            truncateThreshold: 999,
+          },
+          typecheck: {
+            enabled: true,
+            include: ['./test/reporters/configuration-options.test-d.ts'],
+          },
+          sequence: {
+            groupOrder: 0,
+          },
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: 'snapshots',
+          root: path.join(import.meta.dirname, 'snapshots'),
+          sequence: {
+            groupOrder: 1,
+          },
+        },
+      },
+    ],
   },
   server: {
     watch: {
