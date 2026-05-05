87b7d2365a52ba50a9a83363f1f68efa96f17e36
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2026-05-05 10:53:30 +0100

    test: remove unused fixtures

--- test/config/fixtures/mode/example.benchmark.ts
+++
@@ -1,8 +0,0 @@
-import { test } from 'vitest'
-
-test('simple', async ({ bench }) => {
-  await bench('simple', () => {
-    let _ = 0
-    _ += 1
-  }).run({ iterations: 1, time: 1, warmupIterations: 0, warmupTime: 0 })
-})
