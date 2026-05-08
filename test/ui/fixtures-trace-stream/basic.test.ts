5e29b8f5a75cd2882e7d5dc9c238396a8db042df
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-05-08 12:13:33 +0900

    test: tweak

--- test/ui/fixtures-trace-stream/basic.test.ts
+++ test/ui/fixtures-trace-stream/basic.test.ts
@@ -4,10 +4,16 @@ import { page } from 'vitest/browser'
 test('simple', async () => {
   document.body.innerHTML = '<button>A</button>'
   await page.getByRole('button', { name: 'A' }).click()
+
   await new Promise(r => setTimeout(r, 2000))
   document.body.innerHTML += '<button>B</button>'
   await page.getByRole('button', { name: 'B' }).click()
+
   await new Promise(r => setTimeout(r, 2000))
   document.body.innerHTML += '<button>C</button>'
   await page.getByRole('button', { name: 'C' }).click()
+
+  await new Promise(r => setTimeout(r, 2000))
+  document.body.innerHTML += '<button>D</button>'
+  await page.getByRole('button', { name: 'D' }).click()
 })
