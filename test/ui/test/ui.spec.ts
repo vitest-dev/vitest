5a9f90714e33c3a703af54798b3101af29e1b135
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-05-11 19:00:19 +0900

    test: update

--- test/ui/test/ui.spec.ts
+++ test/ui/test/ui.spec.ts
@@ -72,9 +72,7 @@ test.describe('ui', () => {
     await page.goto(pageUrl)
 
     // dashboard
-    await expect(page.getByTestId('pass-entry')).toContainText('17 Pass')
-    await expect(page.getByTestId('fail-entry')).toContainText('2 Fail')
-    await expect(page.getByTestId('total-entry')).toContainText('19 Total')
+    await assertTestCounts(page, { pass: 17, fail: 3 })
 
     // unhandled errors
     await expect(page.getByTestId('unhandled-errors')).toContainText(
@@ -383,6 +381,11 @@ test.describe('ui', () => {
 // TODO: consolidate in https://github.com/vitest-dev/vitest/pull/10237
 function getExplorerItem(page: Page, name: string) {
   return page.getByTestId('explorer-item').and(page.getByLabel(name, { exact: true }))
+}
+
+async function assertTestCounts(page: Page, options: { pass: number; fail: number }) {
+  await expect.soft(page.getByTestId('tests-entry'))
+    .toContainText(`${options.pass} Pass ${options.fail} Fail ${options.pass + options.fail} Total`)
 }
 
 test.describe('standalone', () => {
