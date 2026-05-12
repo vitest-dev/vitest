a4367d1e19eb0af5e2c27606dd2249da6d158c01
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-05-12 15:19:49 +0900

    deslop

--- test/ui/test/helper.ts
+++ test/ui/test/helper.ts
@@ -1,24 +1,18 @@
 import type { Page } from '@playwright/test'
-import type { PreviewServer } from 'vite'
-import type { Vitest } from 'vitest/node'
+import type { InlineConfig, PreviewServer } from 'vite'
+import type { CliOptions, Vitest } from 'vitest/node'
 import assert from 'node:assert'
 import { Writable } from 'node:stream'
 import { expect } from '@playwright/test'
 import { preview } from 'vite'
 import { startVitest } from 'vitest/node'
 
-type StartVitestFilters = Parameters<typeof startVitest>[1]
-type StartVitestOptions = NonNullable<Parameters<typeof startVitest>[2]>
-type PreviewOptions = Parameters<typeof preview>[0]
-
 export async function startVitestUi(
-  options: StartVitestOptions,
-  filters: StartVitestFilters = [],
+  cliOptions: CliOptions,
 ): Promise<{ vitest: Vitest; url: string }> {
   const stdout = new Writable({ write: (_, __, callback) => callback() })
   const stderr = new Writable({ write: (_, __, callback) => callback() })
-  const vitest = await startVitest('test', filters, options, {}, { stdout, stderr })
-  assert(vitest, 'Failed to start Vitest')
+  const vitest = await startVitest('test', undefined, cliOptions, {}, { stdout, stderr })
 
   const address = vitest.vite.httpServer?.address()
   assert(address && typeof address === 'object', 'Invalid server address')
@@ -30,13 +24,12 @@ export async function startVitestUi(
 }
 
 export async function startHtmlReportPreview(
-  vitestOptions: StartVitestOptions,
-  previewOptions: PreviewOptions,
-  filters: StartVitestFilters = [],
+  cliOptions: CliOptions,
+  previewOptions: InlineConfig,
 ): Promise<{ previewServer: PreviewServer; url: string }> {
   const stdout = new Writable({ write: (_, __, callback) => callback() })
   const stderr = new Writable({ write: (_, __, callback) => callback() })
-  await startVitest('test', filters, vitestOptions, {}, { stdout, stderr })
+  await startVitest('test', undefined, cliOptions, {}, { stdout, stderr })
 
   const previewServer = await preview(previewOptions)
   const address = previewServer.httpServer?.address()
@@ -52,7 +45,10 @@ export function getExplorerItem(page: Page, name: string) {
   return page.getByTestId('explorer-item').and(page.getByLabel(name, { exact: true }))
 }
 
-export async function assertTestCounts(page: Page, options: { pass: number; fail: number }) {
-  await expect.soft(page.getByTestId('tests-entry'))
-    .toContainText(`${options.pass} Pass ${options.fail} Fail ${options.pass + options.fail} Total`)
+export async function assertTestCounts(page: Page, { pass, fail }: { pass: number; fail: number }) {
+  await expect
+    .soft(page.getByTestId('tests-entry'))
+    .toContainText(
+      `${pass} Pass ${fail} Fail ${pass + fail} Total`,
+    )
 }
