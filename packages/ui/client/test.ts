fec3dfcdecc82aa1b443c56966e5554f99996cff
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-05-13 13:57:24 +0900

    tweak types

--- packages/ui/client/test.ts
+++ packages/ui/client/test.ts
@@ -1,13 +1,17 @@
+import type { ComponentRenderOptions, RenderResult } from 'vitest-browser-vue'
 import { vTooltip } from 'floating-vue'
 import { vi } from 'vitest'
 import {
-  render as renderOriginal,
+  render as _render,
 } from 'vitest-browser-vue'
 
 export { page } from 'vitest/browser'
 
-export const render: typeof renderOriginal = vi.defineHelper((component, options) => {
-  return renderOriginal(component, {
+export const render = vi.defineHelper(<C>(
+  component: C,
+  options?: ComponentRenderOptions<C, any>,
+): PromiseLike<RenderResult<any>> => {
+  return _render(component, {
     ...options,
     global: {
       directives: {
