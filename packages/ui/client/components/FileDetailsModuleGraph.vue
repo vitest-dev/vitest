cbbc74a84b4a92792c2dca0999c0b44008ff0a5c
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-05-19 12:13:28 +0900

    refactor: useAsyncState

--- packages/ui/client/components/FileDetailsModuleGraph.vue
+++ packages/ui/client/components/FileDetailsModuleGraph.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import type { ModuleGraphData, RunnerTestFile } from 'vitest'
-import { computed, onMounted, ref } from 'vue'
+import type { RunnerTestFile } from 'vitest'
+import { useAsyncState } from '@vueuse/core'
+import { computed, ref } from 'vue'
 import { browserState, client } from '~/composables/client'
 import { getModuleGraph } from '~/composables/module-graph'
 import ViewModuleGraph from './views/ViewModuleGraph.vue'
@@ -10,19 +11,17 @@ const props = defineProps<{
   projectName: string
 }>()
 
-const graphData = ref<ModuleGraphData>()
-const loading = ref(true)
 const hideNodeModules = ref(true)
 const NODE_MODULES_RE = /[/\\]node_modules[/\\]/
 
-onMounted(async () => {
-  graphData.value = await client.rpc.getModuleGraph(
+const { state: graphData, isLoading } = useAsyncState(
+  () => client.rpc.getModuleGraph(
     props.projectName,
     props.file.filepath,
     !!browserState,
-  )
-  loading.value = false
-})
+  ),
+  undefined,
+)
 
 const graph = computed(() => {
   if (!graphData.value) {
@@ -42,7 +41,7 @@ const graph = computed(() => {
 
 <template>
   <div flex-1 overflow-hidden>
-    <div v-if="loading" h-full flex items-center justify-center op-70>
+    <div v-if="isLoading" h-full flex items-center justify-center op-70>
       Loading module graph...
     </div>
     <ViewModuleGraph
