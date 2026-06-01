200e8bed377e2b3c357942412d9bb815dd989c3b
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2026-06-01 11:36:28 +0200

    chore: use non semver tag

--- scripts/publish-ci.ts
+++ scripts/publish-ci.ts
@@ -39,5 +39,5 @@ if (releaseTag) {
   await $`pnpm -r publish --access public --no-git-checks --tag ${releaseTag}`
 }
 else {
-  await $`pnpm -r publish --access public --no-git-checks --tag v3 --filter="!vite-node" --filter="!@vitest/ws-client"`
+  await $`pnpm -r publish --access public --no-git-checks --tag V3 --filter="!vite-node" --filter="!@vitest/ws-client"`
 }
