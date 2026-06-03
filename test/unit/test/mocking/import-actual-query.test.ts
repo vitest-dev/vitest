e8d306a10fc6250485d7de9611cccc21d20738b5
Hiroshi Ogawa <4232207+hi-ogawa@users.noreply.github.com>
2026-06-03 18:27:30 +0900

    test: deal with type

--- test/unit/test/mocking/import-actual-query.test.ts
+++ test/unit/test/mocking/import-actual-query.test.ts
@@ -1,8 +1,6 @@
 import { expect, test, vi } from 'vitest'
-// @ts-expect-error no type
 import * as target from './import-actual-query-target?raw'
 
-// @ts-expect-error no type
 vi.mock(import('./import-actual-query-target?raw'), async (importOriginal) => {
   const original = await importOriginal<any>()
   return {
