fa5b7650cc03003374dff704216d3b6cb35739f2
David Ortner <david@ortner.se>
2026-06-09 01:06:22 +0200

    fix(happy-dom): clear timeouts during teardown

--- packages/vitest/src/integrations/env/happy-dom.ts
+++ packages/vitest/src/integrations/env/happy-dom.ts
@@ -1,103 +1,105 @@
-import type { Environment } from "../../types/environment";
-import { populateGlobal } from "./utils";
+import type { Environment } from '../../types/environment'
+import { populateGlobal } from './utils'
 
 async function teardownWindow(win: {
   happyDOM: {
-    abort?: () => Promise<void>;
-    close?: () => Promise<void>;
-    cancelAsync: () => void;
-  };
-  close?: () => void;
+    abort?: () => Promise<void>
+    close?: () => Promise<void>
+    cancelAsync: () => void
+  }
+  close?: () => void
 }) {
   if (win.close && win.happyDOM.close) {
-    await win.happyDOM.close();
-  } else if (win.close && win.happyDOM.abort) {
-    await win.happyDOM.abort();
-    win.close();
-  } else {
-    win.happyDOM.cancelAsync();
+    await win.happyDOM.close()
+  }
+  else if (win.close && win.happyDOM.abort) {
+    await win.happyDOM.abort()
+    win.close()
+  }
+  else {
+    win.happyDOM.cancelAsync()
   }
 }
 
 export default <Environment>{
-  name: "happy-dom",
-  viteEnvironment: "client",
+  name: 'happy-dom',
+  viteEnvironment: 'client',
   async setupVM({ happyDOM = {} }) {
-    const { Window } = await import("happy-dom");
+    const { Window } = await import('happy-dom')
     let win = new Window({
       ...happyDOM,
       console: console && globalThis.console ? globalThis.console : undefined,
-      url: happyDOM.url || "http://localhost:3000",
+      url: happyDOM.url || 'http://localhost:3000',
       settings: {
         ...happyDOM.settings,
         disableErrorCapturing: true,
       },
-    }) as any;
+    }) as any
 
     // TODO: browser doesn't expose Buffer, but a lot of dependencies use it
-    win.Buffer = Buffer;
+    win.Buffer = Buffer
 
     // inject structuredClone if it exists
-    if (typeof structuredClone !== "undefined" && !win.structuredClone) {
-      win.structuredClone = structuredClone;
+    if (typeof structuredClone !== 'undefined' && !win.structuredClone) {
+      win.structuredClone = structuredClone
     }
 
     return {
       getVmContext() {
-        return win;
+        return win
       },
       async teardown() {
-        await teardownWindow(win);
-        win = undefined;
+        await teardownWindow(win)
+        win = undefined
       },
-    };
+    }
   },
   async setup(global, { happyDOM = {} }) {
     // happy-dom v3 introduced a breaking change to Window, but
     // provides GlobalWindow as a way to use previous behaviour
-    const { Window, GlobalWindow } = await import("happy-dom");
+    const { Window, GlobalWindow } = await import('happy-dom')
     const win = new (GlobalWindow || Window)({
       ...happyDOM,
       console: console && global.console ? global.console : undefined,
-      url: happyDOM.url || "http://localhost:3000",
+      url: happyDOM.url || 'http://localhost:3000',
       settings: {
         ...happyDOM.settings,
         disableErrorCapturing: true,
       },
-    });
+    })
 
     const { keys, originals } = populateGlobal(global, win, {
       bindFunctions: true,
       // jsdom doesn't support fetch API, but happy-dom does
       additionalKeys: [
-        "Request",
-        "Response",
-        "MessagePort",
-        "fetch",
-        "Headers",
-        "AbortController",
-        "AbortSignal",
-        "URL",
-        "URLSearchParams",
-        "FormData",
+        'Request',
+        'Response',
+        'MessagePort',
+        'fetch',
+        'Headers',
+        'AbortController',
+        'AbortSignal',
+        'URL',
+        'URLSearchParams',
+        'FormData',
 
         // in order for happy-dom to be able to teardown timers, we need to use the implementation from happy-dom
-        "setTimeout",
-        "clearTimeout",
-        "setInterval",
-        "clearInterval",
-        "requestAnimationFrame",
-        "cancelAnimationFrame",
-        "queueMicrotask",
+        'setTimeout',
+        'clearTimeout',
+        'setInterval',
+        'clearInterval',
+        'requestAnimationFrame',
+        'cancelAnimationFrame',
+        'queueMicrotask',
       ],
-    });
+    })
 
     return {
       async teardown(global) {
-        await teardownWindow(win);
-        keys.forEach((key) => delete global[key]);
-        originals.forEach((v, k) => (global[k] = v));
+        await teardownWindow(win)
+        keys.forEach(key => delete global[key])
+        originals.forEach((v, k) => (global[k] = v))
       },
-    };
+    }
   },
-};
+}
