1cbb2334e6aaf849c3b1ecfb79b17c200d8cfd29
Hiroshi Ogawa <4232207+hi-ogawa@users.noreply.github.com>
2026-06-09 15:48:59 +0900

    nit

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,20 +1,25 @@
-#!/usr/bin/env tsx
-
 import { versionBump } from 'bumpp'
 import { glob } from 'tinyglobby'
 
-const packages = await glob(['package.json', './packages/*/package.json'], {
-  expandDirectories: false,
-})
+async function main() {
+  const packages = await glob(['package.json', './packages/*/package.json'], {
+    expandDirectories: false,
+  })
 
-console.log('Bumping versions in packages:', packages.join(', '), '\n')
+  console.log('Bumping versions in packages:', packages.join(', '), '\n')
 
-const release = process.env.RELEASE_VERSION || process.env.RELEASE_TYPE
+  const release = process.env.RELEASE_VERSION || process.env.RELEASE_TYPE
 
-await versionBump({
-  files: packages,
-  release,
-  tag: false,
-  push: false,
-  confirm: !release,
+  await versionBump({
+    files: packages,
+    release,
+    tag: false,
+    push: false,
+    confirm: !release,
+  })
+}
+
+main().catch((error) => {
+  console.error('Error during version bump:', error)
+  process.exit(1)
 })
