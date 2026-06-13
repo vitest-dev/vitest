6f794b11db1a811dbc3843a369371c171473dacd
abeer0 <47961062+iiio2@users.noreply.github.com>
2026-06-13 22:09:19 +0600

    chore: update links (#10591)

--- packages/snapshot/src/port/mockSerializer.ts
+++ packages/snapshot/src/port/mockSerializer.ts
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * https://github.com/facebook/jest/blob/4eb4f6a59b6eae0e05b8e51dd8cd3fdca1c7aff1/packages/jest-snapshot/src/mockSerializer.ts#L4
+ * https://github.com/jestjs/jest/blob/4eb4f6a59b6eae0e05b8e51dd8cd3fdca1c7aff1/packages/jest-snapshot/src/mockSerializer.ts#L4
  */
 
 import type { NewPlugin } from '@vitest/pretty-format'
