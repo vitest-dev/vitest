35645b7133a3e5f047c34a8d04b9417d178b0587
Hiroshi Ogawa <4232207+hi-ogawa@users.noreply.github.com>
2026-08-13 19:10:28 +0900

    nit

--- packages/ui/client/utils/project.ts
+++ packages/ui/client/utils/project.ts
@@ -1,18 +1,16 @@
 import type { SerializedRootConfig } from 'vitest'
 import { getBadgeNameColor, getBadgeTextColor } from './task'
 
-// TODO: config shouldn't be partial in valid app lifetime. rework later.
-type ProjectConfigSource = Partial<SerializedRootConfig>
-
 export function getProjectConfigByName(
-  config: ProjectConfigSource,
+  // TODO: config shouldn't be partial in valid app lifetime. rework later.
+  config: Partial<SerializedRootConfig>,
   projectName: string | undefined,
 ) {
   return config.projects?.find(project => project.name === projectName)
 }
 
 export function getProjectBadgeStyle(
-  config: ProjectConfigSource,
+  config: Partial<SerializedRootConfig>,
   projectName: string | undefined,
 ) {
   const backgroundColor = getProjectConfigByName(config, projectName)?.color
