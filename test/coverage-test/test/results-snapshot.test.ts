c6174a6cf2847b23075ee35ba5151c2184a70594
Ari Perkkiö <ari.perkkio@gmail.com>
2026-08-20 16:59:13 +0300

    fix(coverage): v8 to ignore Vite SSR's generated import bindings (#11023)

--- test/coverage-test/test/results-snapshot.test.ts
+++ test/coverage-test/test/results-snapshot.test.ts
@@ -1,5 +1,5 @@
 import { expect } from 'vitest'
-import { readCoverageMap, runVitest, test } from '../utils'
+import { isBrowser, readCoverageMap, runVitest, test } from '../utils'
 
 test('coverage results matches snapshot', async () => {
   await runVitest({
@@ -64,6 +64,44 @@ test('coverage results matches snapshot', async () => {
         "35": 0,
         "46": 0,
         "9": 0,
+      },
+    }
+  `)
+})
+
+test.for(['node', 'jsdom'] as const)('vite SSR generated code ignored in { environment: %s }', async (environment, { skip }) => {
+  skip(isBrowser(), 'SSR not relevant to browser environment')
+
+  await runVitest({
+    include: ['fixtures/test/vite-ssr-imports.test.ts'],
+    environment,
+    coverage: { reporter: 'json',
+    },
+  })
+
+  const coverageMap = await readCoverageMap()
+  const fileCoverages = coverageMap.files().map(file => coverageMap.fileCoverageFor(file))
+
+  expect(fileCoverages).toMatchInlineSnapshot(`
+    {
+      "<process-cwd>/fixtures/src/node-built-ins.ts": {
+        "branches": "0/0 (100%)",
+        "functions": "1/1 (100%)",
+        "lines": "1/1 (100%)",
+        "statements": "1/1 (100%)",
+      },
+    }
+  `)
+
+  const lineCoverages = coverageMap.files().reduce((all, file) => ({
+    [file]: coverageMap.fileCoverageFor(file).getLineCoverage(),
+    ...all,
+  }), {})
+
+  expect(lineCoverages).toMatchInlineSnapshot(`
+    {
+      "<process-cwd>/fixtures/src/node-built-ins.ts": {
+        "4": 1,
       },
     }
   `)
