8f69e5c569a64a17f030c63573ed5aa2b044e06d
Hiroshi Ogawa <4232207+hi-ogawa@users.noreply.github.com>
2026-08-25 18:02:17 +0900

    nit

--- test/ui/test/playwright-trace.spec.ts
+++ test/ui/test/playwright-trace.spec.ts
@@ -4,33 +4,6 @@ import type { Vitest } from 'vitest/node'
 import { expect, test } from '@playwright/test'
 import { assertTestCounts, openExplorerItem, startHtmlReportPreview, startVitestUi } from './helper'
 
-async function mockTraceViewer(page: Page) {
-  await page.context().route('https://trace.playwright.dev/', async (route) => {
-    await route.fulfill({
-      contentType: 'text/html',
-      body: `<script>
-        addEventListener('message', (event) => {
-          if (event.data?.method === 'load' && event.data.params?.trace instanceof Blob && event.data.params.trace.size > 0)
-            document.title = 'Loaded Playwright Trace'
-        })
-      </script>`,
-    })
-  })
-}
-
-async function testPlaywrightTrace(page: Page, baseURL: string) {
-  await mockTraceViewer(page)
-  await page.goto(baseURL)
-  await assertTestCounts(page, { pass: 1, fail: 0 })
-  await openExplorerItem(page, 'playwright trace')
-
-  const popupPromise = page.waitForEvent('popup')
-  await page.getByRole('button', { name: 'Open trace' }).click()
-  const popup = await popupPromise
-  await expect(popup).toHaveURL('https://trace.playwright.dev/')
-  await expect(popup).toHaveTitle('Loaded Playwright Trace')
-}
-
 test.describe('ui', () => {
   let vitest: Vitest | undefined
   let baseURL: string
@@ -92,3 +65,30 @@ test.describe('html reporter', () => {
     await testPlaywrightTrace(page, baseURL)
   })
 })
+
+async function testPlaywrightTrace(page: Page, baseURL: string) {
+  await mockTraceViewer(page)
+  await page.goto(baseURL)
+  await assertTestCounts(page, { pass: 1, fail: 0 })
+  await openExplorerItem(page, 'playwright trace')
+
+  const popupPromise = page.waitForEvent('popup')
+  await page.getByRole('button', { name: 'Open trace' }).click()
+  const popup = await popupPromise
+  await expect(popup).toHaveURL('https://trace.playwright.dev/')
+  await expect(popup).toHaveTitle('Loaded Playwright Trace')
+}
+
+async function mockTraceViewer(page: Page) {
+  await page.context().route('https://trace.playwright.dev/', async (route) => {
+    await route.fulfill({
+      contentType: 'text/html',
+      body: `<script>
+        addEventListener('message', (event) => {
+          if (event.data?.method === 'load' && event.data.params?.trace instanceof Blob && event.data.params.trace.size > 0)
+            document.title = 'Loaded Playwright Trace'
+        })
+      </script>`,
+    })
+  })
+}
