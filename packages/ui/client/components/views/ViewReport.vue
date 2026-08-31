85f561277b466745da0a73ccde488dcb6fc45ee9
Hiroshi Ogawa <4232207+hi-ogawa@users.noreply.github.com>
2026-08-31 15:30:51 +0900

    comment

--- packages/ui/client/components/views/ViewReport.vue
+++ packages/ui/client/components/views/ViewReport.vue
@@ -32,7 +32,7 @@ function collectFailed(task: RunnerTask, level: number): LeveledTask[] {
 const failed = computed(() => {
   const suite = props.suite
   const failedFlatMap = suite.tasks.flatMap(t => collectFailed(t, 0))
-  // append suite level errors as same indent level as children errors
+  // prepend suite level errors as same indent level as children errors
   if (suite.result?.errors?.length) {
     const taskError: LeveledTask = {
       ...suite,
