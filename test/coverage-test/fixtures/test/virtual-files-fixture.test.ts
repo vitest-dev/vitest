e6ca35c92e1d4767ab200cad7cc0ee471f807ac4
Ari Perkkiö <ari.perkkio@gmail.com>
2026-09-02 15:50:11 +0300

    test: show me windows ci crash

--- test/coverage-test/fixtures/test/virtual-files-fixture.test.ts
+++ test/coverage-test/fixtures/test/virtual-files-fixture.test.ts
@@ -2,10 +2,11 @@ import { expect, test } from 'vitest'
 import { getVirtualFileImports} from '../src/virtual-files'
 
 test("verify virtual files work", () => {
-  const {virtualFile1, virtualFile2, virtualMath} = getVirtualFileImports()
+  const {virtualFile1, virtualFile2, virtualFile3, virtualMath} = getVirtualFileImports()
 
   expect(virtualFile1).toBe('This file should be excluded from coverage report #1')
   expect(virtualFile2).toBe('This file should be excluded from coverage report #2')
+  expect(virtualFile3).toBe('This file should be excluded from coverage report #3')
 
   expect(virtualMath).toHaveProperty('sum')
   expect(virtualMath.sum(50, 65)).toBe(115)
