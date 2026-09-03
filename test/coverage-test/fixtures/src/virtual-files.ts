c4473e48ee046938a9ab6784fe2735257c2c0f72
Ari Perkkiö <ari.perkkio@gmail.com>
2026-09-03 13:31:02 +0300

    fix(coverage): prevent crash on `/@fs/` prepended virtual files (#11119)

--- test/coverage-test/fixtures/src/virtual-files.ts
+++ test/coverage-test/fixtures/src/virtual-files.ts
@@ -5,8 +5,11 @@ import virtualFile1 from 'virtual:vitest-custom-virtual-file-1'
 import virtualFile2 from '\0vitest-custom-virtual-file-2'
 
 // @ts-expect-error -- untyped virtual file provided by custom plugin
+import virtualFile3 from 'virtual:vitest-custom-virtual-file-3'
+
+// @ts-expect-error -- untyped virtual file provided by custom plugin
 import * as virtualMath from 'vitest-custom-virtual:math'
 
 export function getVirtualFileImports() {
-  return { virtualFile1, virtualFile2, virtualMath }
+  return { virtualFile1, virtualFile2, virtualFile3, virtualMath }
 }
